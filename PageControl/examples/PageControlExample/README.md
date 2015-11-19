9f19d144028c90e308c37322b7c11479636af01e
Jeff Verkoeyen <featherless@google.com>
2015-11-19 13:37:30 -0500

    PageControl pull request
    Reviewers: ajsecord, featherless
    
    Reviewed By: featherless
    
    Subscribers: ajsecord
    
    Differential Revision: http://mux-oss-phabricator.appspot.com/D8

---
+++ PageControl/examples/PageControlExample/README.md
@@ -0,0 +1,16 @@
+# PageControl example app
+
+This app demonstrates how to to create a MDCPageControl.
+
+## Where is the code?
+
+The relevant code may be found in the 'App' directory.
+
+## Code structure
+
+This example shows how one might use a paging scroll view with a material page control
+that reacts to the scroll view's content offset.
+
+### ViewController
+
+Creates a horizontal paging scroll view and a MDCPageControl.
---
+++ PageControl/examples/PageControlExample/ViewController.m
@@ -0,0 +1,97 @@
+/*
+ Copyright 2015-present Google Inc. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "ViewController.h"
+
+#import "MDCPageControl.h"
+
+#define RGBCOLOR(r, g, b) [UIColor colorWithRed:(r) / 255.0f green:(g) / 255.0f blue:(b) / 255.0f alpha:1]
+#define HEXCOLOR(hex) RGBCOLOR((((hex) >> 16) & 0xFF), (((hex) >> 8) & 0xFF), ((hex)&0xFF))
+
+@interface ViewController () <UIScrollViewDelegate>
+@end
+
+@implementation ViewController {
+  UIScrollView *_scrollView;
+  MDCPageControl *_pageControl;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  CGFloat boundsWidth = CGRectGetWidth(self.view.bounds);
+  CGFloat boundsHeight = CGRectGetHeight(self.view.bounds);
+
+  NSArray *pageColors = @[ HEXCOLOR(0x81D4FA), HEXCOLOR(0x80CBC4), HEXCOLOR(0xFFCC80) ];
+
+  // Scroll view configuration
+  _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+  _scrollView.delegate = self;
+  _scrollView.pagingEnabled = YES;
+  _scrollView.contentSize = CGSizeMake(boundsWidth * pageColors.count, boundsHeight);
+  _scrollView.showsHorizontalScrollIndicator = NO;
+  [self.view addSubview:_scrollView];
+
+  // Add pages to scrollView.
+  for (NSInteger i = 0; i < pageColors.count; i++) {
+    CGRect pageFrame = CGRectOffset(self.view.bounds, i * boundsWidth, 0);
+    UILabel *page = [[UILabel alloc] initWithFrame:pageFrame];
+    page.text = [NSString stringWithFormat:@"Page %zd", i + 1];
+    page.font = [UIFont systemFontOfSize:50];
+    page.textColor = [UIColor colorWithWhite:0 alpha:0.8];
+    page.textAlignment = NSTextAlignmentCenter;
+    page.backgroundColor = pageColors[i];
+    [_scrollView addSubview:page];
+  }
+
+  // Page control configuration.
+  _pageControl = [[MDCPageControl alloc] init];
+
+  // We want the page control to span the bottom of the screen.
+  CGFloat bottomPadding = 20.f;
+  CGSize pageControlSize = [_pageControl sizeThatFits:self.view.bounds.size];
+  _pageControl.frame = CGRectMake(0, boundsHeight - pageControlSize.height - bottomPadding,
+                                  boundsWidth, pageControlSize.height);
+  _pageControl.numberOfPages = pageColors.count;
+  [_pageControl addTarget:self
+                   action:@selector(didChangePage:)
+         forControlEvents:UIControlEventValueChanged];
+  [self.view addSubview:_pageControl];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+  [_pageControl scrollViewDidScroll:scrollView];
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+  [_pageControl scrollViewDidEndDecelerating:scrollView];
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+  [_pageControl scrollViewDidEndScrollingAnimation:scrollView];
+}
+
+#pragma mark - User events
+
+- (void)didChangePage:(MDCPageControl *)sender {
+  CGPoint offset = _scrollView.contentOffset;
+  offset.x = sender.currentPage * _scrollView.bounds.size.width;
+  [_scrollView setContentOffset:offset animated:YES];
+}
+
+@end
