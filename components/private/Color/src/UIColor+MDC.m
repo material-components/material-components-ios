1c34c26780d74c9c6596c95525c32a5e06f1051b
Adrian Secord <ajsecord@google.com>
2016-01-27 06:54:32 -0800

    Added further conversions that Junius found.

--- components/private/Color/src/UIColor+MDC.m
+++ components/private/Color/src/UIColor+MDC.m
@@ -22,27 +22,27 @@
                                     toColor:(UIColor *)toColor
                                     percent:(CGFloat)percent {
   // Clamp percent to [0.0, 1.0]
-  percent = MAX(0.0, percent);
-  percent = MIN(1.0, percent);
+  percent = MAX(0, percent);
+  percent = MIN(1, percent);
 
   CGFloat r1, g1, b1, a1;
-  r1 = g1 = b1 = a1 = 1.0;
+  r1 = g1 = b1 = a1 = 1;
   if (![fromColor getRed:&r1 green:&g1 blue:&b1 alpha:&a1]) {
     [fromColor getWhite:&r1 alpha:&a1];
     g1 = b1 = r1;
   };
 
   CGFloat r2, g2, b2, a2;
-  r2 = g2 = b2 = a2 = 1.0;
+  r2 = g2 = b2 = a2 = 1;
   if (![toColor getRed:&r2 green:&g2 blue:&b2 alpha:&a2]) {
     [toColor getWhite:&r2 alpha:&a2];
     g2 = b2 = r2;
   }
 
-  CGFloat rfinal = r1 * (1.0 - percent) + r2 * percent;
-  CGFloat gfinal = g1 * (1.0 - percent) + g2 * percent;
-  CGFloat bfinal = b1 * (1.0 - percent) + b2 * percent;
-  CGFloat afinal = a1 * (1.0 - percent) + a2 * percent;
+  CGFloat rfinal = r1 * (1 - percent) + r2 * percent;
+  CGFloat gfinal = g1 * (1 - percent) + g2 * percent;
+  CGFloat bfinal = b1 * (1 - percent) + b2 * percent;
+  CGFloat afinal = a1 * (1 - percent) + a2 * percent;
 
   return [UIColor colorWithRed:rfinal green:gfinal blue:bfinal alpha:afinal];
 }
