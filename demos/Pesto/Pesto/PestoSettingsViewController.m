55cca1036d73dd2b7342f8200efa6e0d5fe21a18
Travis Kaufman <traviskaufman@google.com>
2016-02-01 14:55:34 -0500

    Implement Settings VC for basic settings
    Summary:
    This commit adds the basic Settings UI for all of the basic settings (e.g.
    everything besides account settings). It uses a UITableViewController with
    customized headers and cells in lieu of a collection view, for now. It also
    uses preformatted dummy data which can be factored out later.
    
    Test Plan:
    1. Open the app
    2. Tap the side menu
    3. Tap "Settings"
    4. Observe a Settings menu appears that has the same basic settings as the demo
    web application.
    5. Observe that there is not yet an account cell within settings.
    
    Reviewers: junius, #material_components_ios_owners
    
    Reviewed By: junius, #material_components_ios_owners
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D95

--- demos/Pesto/Pesto/PestoSettingsViewController.m
+++ demos/Pesto/Pesto/PestoSettingsViewController.m
@@ -3,59 +3,45 @@
 #import "MaterialSwitch.h"
 #import "MaterialTypography.h"
 
-static CGFloat kPestoSettingsViewControllerCellViewInset = 20.f;
-static CGFloat kPestoSettingsViewControllerCellViewHeight = 48.f;
+static CGFloat kPestoSettingsTableViewOffsetTop = 10.f;
 
-@interface PestoSettingsViewControllerCellView : UIView
+static NSString *const kPestoSettingsTableViewCellReuseIdentifier = @"PestoSettingsTableViewCell";
+static NSString *const kPestoSettingsTableViewHeaderViewReuseIdentifier =
+    @"PestoSettingsTableViewHeaderView";
+
+static CGFloat kPestoSettingsTableViewHeaderSeparatorWidth = 1.f;
+
+@interface PestoSettingsTableViewCell : UITableViewCell
 
 @property(nonatomic) NSString *labelText;
 @property(nonatomic) BOOL on;
 
 @end
 
-@interface PestoSettingsViewControllerCellView ()
+@interface PestoSettingsTableViewCell ()
 
-@property(nonatomic) UILabel *labelView;
 @property(nonatomic) MDCSwitch *switchView;
 
 @end
 
-@implementation PestoSettingsViewControllerCellView
+@implementation PestoSettingsTableViewCell
 
-- (id)initWithCoder:(NSCoder *)aDecoder {
-  self = [super initWithCoder:aDecoder];
-  return self;
-}
-
-- (id)initWithFrame:(CGRect)frame {
-  self = [super initWithFrame:frame];
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+  self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
   if (self) {
-    CGRect descRect = CGRectMake(0, 0, self.bounds.size.width * 0.75f, self.bounds.size.height);
-    _labelView = [[UILabel alloc] initWithFrame:descRect];
-    _labelView.font = [MDCTypography body1Font];
-    _labelView.alpha = [MDCTypography body1FontOpacity];
-    _labelView.autoresizingMask = UIViewAutoresizingFlexibleWidth |
-                                  UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleRightMargin;
+    self.textLabel.font = [MDCTypography body1Font];
+    self.textLabel.alpha = [MDCTypography body1FontOpacity];
 
     _switchView = [[MDCSwitch alloc] initWithFrame:CGRectZero];
-    CGFloat switchX = self.bounds.size.width - _switchView.frame.size.width;
-    CGFloat switchY = (self.bounds.size.height - _switchView.frame.size.height) / 2.f;
-    _switchView.frame = CGRectOffset(_switchView.frame, switchX, switchY);
     _switchView.onTintColor = [UIColor colorWithRed:0.09f green:0.54f blue:0.44f alpha:1.f];
-    _switchView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
-                                   UIViewAutoresizingFlexibleTopMargin |
-                                   UIViewAutoresizingFlexibleBottomMargin;
-
-    [self addSubview:_labelView];
-    [self addSubview:_switchView];
+    self.accessoryView = _switchView;
   }
   return self;
 }
 
 - (void)setLabelText:(NSString *)labelText {
   _labelText = labelText;
-  _labelView.text = _labelText;
-  [self setNeedsLayout];
+  self.textLabel.text = _labelText;
 }
 
 - (void)setOn:(BOOL)on {
@@ -65,24 +51,152 @@ static CGFloat kPestoSettingsViewControllerCellViewHeight = 48.f;
 
 @end
 
+@interface PestoSettingsTableViewHeaderView : UITableViewHeaderFooterView
+
+@property(nonatomic) UIColor *separatorColor;
+
+@end
+
+@interface PestoSettingsTableViewHeaderView ()
+
+@property(nonatomic) CALayer *separator;
+
+@end
+
+@implementation PestoSettingsTableViewHeaderView
+
+- (id)initWithReuseIdentifier:(NSString *)reuseIdentifier {
+  self = [super initWithReuseIdentifier:reuseIdentifier];
+  if (self) {
+    self.textLabel.font = [MDCTypography headlineFont];
+    self.textLabel.alpha = [MDCTypography headlineFontOpacity];
+    self.textLabel.textColor = [UIColor colorWithRed:0.09f green:0.54f blue:0.44f alpha:1.f];
+
+    self.backgroundView = [[UIView alloc] initWithFrame:self.bounds];
+    self.backgroundView.backgroundColor = [UIColor whiteColor];
+
+    _separator = [CALayer layer];
+    [self.contentView.layer addSublayer:_separator];
+  }
+  return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  CGFloat borderBottomYPos = CGRectGetMaxY(self.contentView.bounds) -
+                             kPestoSettingsTableViewHeaderSeparatorWidth;
+  _separator.frame = CGRectMake(0,
+                                borderBottomYPos,
+                                CGRectGetWidth(self.contentView.bounds),
+                                kPestoSettingsTableViewHeaderSeparatorWidth);
+  self.backgroundView.frame = self.bounds;
+}
+
+- (void)setSeparatorColor:(UIColor *)separatorColor {
+  _separatorColor = separatorColor;
+  _separator.backgroundColor = _separatorColor.CGColor;
+}
+
+@end
+
+@interface PestoSettingsViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property(nonatomic) NSArray *dummySettingHeaders;
+@property(nonatomic) NSArray *dummySettingTitles;
+@property(nonatomic) NSArray *dummySettingVals;
+@property(nonatomic) UITableView *settingsTableView;
+
+@end
+
 @implementation PestoSettingsViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  CGRect testViewFrame = CGRectMake(0,
-                                    10.f,
-                                    self.view.bounds.size.width,
-                                    kPestoSettingsViewControllerCellViewHeight);
-  CGRect insetFrame = CGRectInset(testViewFrame, kPestoSettingsViewControllerCellViewInset, 0);
-  PestoSettingsViewControllerCellView *testCellView = [[PestoSettingsViewControllerCellView alloc]
-      initWithFrame:insetFrame];
-  testCellView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-  testCellView.labelText = @"Settings view needs more work?";
-  testCellView.on = YES;
 
   self.view.backgroundColor = [UIColor whiteColor];
 
-  [self.view addSubview:testCellView];
+  _dummySettingHeaders = @[ @"Account", @"Notification" ];
+  _dummySettingTitles = @[ @[ @"Public Profile", @"Subscribe to Daily Digest" ],
+                           @[ @"Get email notifications", @"Get text notifications" ] ];
+  _dummySettingVals = @[ @[ @YES, @NO ], @[ @NO, @YES ] ];
+
+  CGRect settingsTableViewFrame =
+      CGRectMake(0,
+                 kPestoSettingsTableViewOffsetTop,
+                 self.view.bounds.size.width,
+                 self.view.bounds.size.height - kPestoSettingsTableViewOffsetTop);
+  _settingsTableView = [[UITableView alloc] initWithFrame:settingsTableViewFrame
+                                                    style:UITableViewStylePlain];
+  _settingsTableView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+  _settingsTableView.allowsSelection = NO;
+  _settingsTableView.backgroundColor = self.view.backgroundColor;
+  _settingsTableView.dataSource = self;
+  _settingsTableView.delegate = self;
+  _settingsTableView.separatorColor = [[self class] tableViewSeparatorColor];
+  // Ensure empty rows are not shown.
+  _settingsTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+
+  [_settingsTableView registerClass:[PestoSettingsTableViewCell class]
+             forCellReuseIdentifier:kPestoSettingsTableViewCellReuseIdentifier];
+  [_settingsTableView registerClass:[PestoSettingsTableViewHeaderView class]
+      forHeaderFooterViewReuseIdentifier:kPestoSettingsTableViewHeaderViewReuseIdentifier];
+
+  [_settingsTableView reloadData];
+
+  [self.view addSubview:_settingsTableView];
+}
+
++ (UIColor *)tableViewSeparatorColor {
+  return [UIColor colorWithWhite:0.f alpha:0.1f];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return [_dummySettingHeaders count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return [(NSArray *)_dummySettingTitles[section] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSString *settingLabel = _dummySettingTitles[indexPath.section][indexPath.row];
+  PestoSettingsTableViewCell *cell =
+      [tableView dequeueReusableCellWithIdentifier:kPestoSettingsTableViewCellReuseIdentifier
+                                      forIndexPath:indexPath];
+  cell.labelText = settingLabel;
+  cell.on = [_dummySettingVals[indexPath.section][indexPath.row] boolValue];
+
+  return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+  NSString *reuseId = kPestoSettingsTableViewHeaderViewReuseIdentifier;
+  PestoSettingsTableViewHeaderView *header =
+      [tableView dequeueReusableHeaderFooterViewWithIdentifier:reuseId];
+  header.textLabel.text = _dummySettingHeaders[section];
+  header.separatorColor = [[self class] tableViewSeparatorColor];
+  return header;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+  return 50.f;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(nonnull UITableViewCell *)cell
+forRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+  if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+    [cell setSeparatorInset:UIEdgeInsetsZero];
+  }
+
+  if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+    [cell setLayoutMargins:UIEdgeInsetsZero];
+    cell.preservesSuperviewLayoutMargins = NO;
+  }
 }
 
 @end
