2c0c935bb46602a129f86863d822350a4218053f
Adrian Secord <ajsecord@google.com>
2016-02-16 17:15:00 -0500

    Adding FigIrisAutoTrimmerMotionSampleExport to the exceptions. Sheesh.

--- catalog/MDCCatalog/Runtime.swift
+++ catalog/MDCCatalog/Runtime.swift
@@ -42,8 +42,10 @@ func getAllClasses() -> [AnyClass] {
 func classesRespondingToSelector(selector: Selector) -> [AnyClass] {
   return getAllClasses().filter {
     let className = NSStringFromClass($0)
+    NSLog("%@", className);
     if className == "SwiftObject" || className == "Object" || className.hasPrefix("Swift.") ||
-          className.hasPrefix("_") || className.hasPrefix("JS") || className == "NSLeafProxy" {
+          className.hasPrefix("_") || className.hasPrefix("JS") || className == "NSLeafProxy" ||
+          className == "FigIrisAutoTrimmerMotionSampleExport" {
       return false
     }
     return $0.respondsToSelector(selector)
