b251235c9dfb15aebdcacdb65225ec63a3d35245
Jeff Verkoeyen <featherless@google.com>
2016-03-24 16:43:39 -0400

    [Icons] Add missing header search paths in pod specs.
    Reviewers: #mdc_ios_owners, randallli
    
    Reviewed By: #mdc_ios_owners, randallli
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D338

--- scripts/generated/icons.rb
+++ scripts/generated/icons.rb
@@ -3,11 +3,15 @@
 def registerIcons(s)
 
   s.subspec "Icons" do |iss|
+    iss.public_header_files = "components/private/Icons/src/*.h"
     iss.source_files = "components/private/Icons/src/*.{h,m}", "components/private/Icons/src/private/*.{h,m}"
+    iss.header_mappings_dir = "components/private/Icons/src/*"
 
 
     iss.subspec "ic_arrow_back" do |ss|
+      ss.public_header_files = "components/private/Icons/icons/ic_arrow_back/src/*.h"
       ss.source_files = "components/private/Icons/icons/ic_arrow_back/src/*.{h,m}"
+      ss.header_mappings_dir = "components/private/Icons/icons/ic_arrow_back/src/*"
       ss.resource_bundles = {
         "MaterialIcon_ic_arrow_back" => [
           "components/private/Icons/icons/ic_arrow_back/src/MaterialIcon+ic_arrow_back.bundle/**/*.png",
