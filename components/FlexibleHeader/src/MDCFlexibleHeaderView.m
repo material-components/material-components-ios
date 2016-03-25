a3c8f6a801407f6f8a12fe52071b7e51582b9847
Jeff Verkoeyen <featherless@google.com>
2016-03-25 14:43:30 -0400

    [FlexibleHeader] Revert tracking scroll view delegate assertion.
    Summary: This is causing false positives. Will investigate and re-enable once we've identified the cause of the false positives.
    
    Reviewers: #mdc_ios_owners, randallli
    
    Reviewed By: #mdc_ios_owners, randallli
    
    Subscribers: randallli
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D351

--- components/FlexibleHeader/src/MDCFlexibleHeaderView.m
+++ components/FlexibleHeader/src/MDCFlexibleHeaderView.m
@@ -761,6 +761,7 @@ static const CGFloat kMinimumVisibleProportion = 0.25;
                                                   action:@selector(fhv_scrollViewDidPan:)];
   [trackingScrollView.panGestureRecognizer addTarget:self action:@selector(fhv_scrollViewDidPan:)];
 
+#if 0  // TODO(featherless): https://github.com/google/material-components-ios/issues/214
   // Verify existence of a delegate.
   NSAssert(!trackingScrollView || trackingScrollView.delegate,
            @"The provided tracking scroll view %@ has no delegate. Without a delegate, %@ will not"
@@ -768,6 +769,7 @@ static const CGFloat kMinimumVisibleProportion = 0.25;
            @" This assertion will only fire in debug builds.",
            NSStringFromClass([trackingScrollView class]),
            NSStringFromClass([self class]));
+#endif
 #endif
 
   // If this header is shared by many scroll views then we leave the insets when switching the
