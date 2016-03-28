9eb4834a846b8b66e798e2cc4241605fb13d3e22
Will Larche <larche@google.com>
2016-03-28 17:48:03 -0400

    Pesto detail presentation and dismissal.
    Summary: Changed the view hierarchy order to allow the menu button to spin independantly of the modal presentation or dismissal.
    
    Reviewers: #mdc_ios_owners, ajsecord
    
    Reviewed By: #mdc_ios_owners, ajsecord
    
    Subscribers: ajsecord
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D384

--- demos/Pesto/Pesto/PestoFlexibleHeaderContainerViewController.m
+++ demos/Pesto/Pesto/PestoFlexibleHeaderContainerViewController.m
@@ -28,10 +28,13 @@ static NSString *const kPestoDetailViewControllerBackMenu = @"mdc_sprite_menu__a
 static NSString *const kPestoDetailViewControllerMenuBack = @"mdc_sprite_arrow_back__menu";
 
 @interface PestoFlexibleHeaderContainerViewController () <PestoCollectionViewControllerDelegate,
-                                                          PestoSideViewDelegate>
+                                                          PestoSideViewDelegate,
+                                                          UIViewControllerAnimatedTransitioning,
+                                                          UIViewControllerTransitioningDelegate>
 
 @property(nonatomic) PestoCollectionViewController *collectionViewController;
 @property(nonatomic) PestoSideView *sideView;
+@property(nonatomic) MDCSpritedAnimationView *animatedMenuArrow;
 @property(nonatomic) UIImageView *zoomableView;
 @property(nonatomic) UIView *zoomableCardView;
 
@@ -62,17 +65,6 @@ static NSString *const kPestoDetailViewControllerMenuBack = @"mdc_sprite_arrow_b
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  UIImage *spriteImage = [UIImage imageNamed:kPestoDetailViewControllerBackMenu];
-  MDCSpritedAnimationView *animationView = [[MDCSpritedAnimationView alloc]
-      initWithSpriteSheetImage:spriteImage];
-  animationView.frame = CGRectMake(20.f, 20.f, 24.f, 24.f);
-  animationView.tintColor = [UIColor whiteColor];
-  [self.view addSubview:animationView];
-
-  UIButton *button = [[UIButton alloc] initWithFrame:animationView.frame];
-  [self.view addSubview:button];
-  [button addTarget:self action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
-
   self.sideView = [[PestoSideView alloc] initWithFrame:self.view.bounds];
   self.sideView.hidden = YES;
   self.sideView.autoresizingMask =
@@ -80,14 +72,25 @@ static NSString *const kPestoDetailViewControllerMenuBack = @"mdc_sprite_arrow_b
   self.sideView.delegate = self;
   [self.view addSubview:self.sideView];
 
+  UIImage *spriteImage = [UIImage imageNamed:kPestoDetailViewControllerBackMenu];
+  self.animatedMenuArrow = [[MDCSpritedAnimationView alloc]
+      initWithSpriteSheetImage:spriteImage];
+  self.animatedMenuArrow.frame = CGRectMake(20.f, 20.f, 24.f, 24.f);
+  self.animatedMenuArrow.tintColor = [UIColor whiteColor];
+  [self.view addSubview:self.animatedMenuArrow];
+
+  UIButton *button = [[UIButton alloc] initWithFrame:self.animatedMenuArrow.frame];
+  [self.view addSubview:button];
+  [button addTarget:self action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
+
   self.zoomableCardView = [[UIView alloc] initWithFrame:CGRectZero];
   self.zoomableCardView.backgroundColor = [UIColor whiteColor];
-  [self.view addSubview:self.zoomableCardView];
+  [self.view insertSubview:self.zoomableCardView belowSubview:self.animatedMenuArrow];
 
   self.zoomableView = [[UIImageView alloc] initWithFrame:CGRectZero];
   self.zoomableView.backgroundColor = [UIColor lightGrayColor];
   self.zoomableView.contentMode = UIViewContentModeScaleAspectFill;
-  [self.view addSubview:self.zoomableView];
+  [self.view insertSubview:self.zoomableView belowSubview:self.animatedMenuArrow];
 }
 
 - (BOOL)prefersStatusBarHidden {
@@ -120,7 +123,13 @@ static NSString *const kPestoDetailViewControllerMenuBack = @"mdc_sprite_arrow_b
                  cell.frame.size.width,
                  cell.frame.size.height);
   dispatch_async(dispatch_get_main_queue(), ^{
-    [self.zoomableView setImage:cell.image];
+    self.zoomableView.image = cell.image;
+
+    [self.animatedMenuArrow startAnimatingWithCompletion:^{
+      UIImage *spriteImageArrowToMenu = [UIImage imageNamed:kPestoBackArrowToMenu];
+      self.animatedMenuArrow.spriteSheetImage = spriteImageArrowToMenu;
+    }];
+
     [UIView animateWithDuration:kPestoAnimationDuration
         delay:0.0
         options:UIViewAnimationOptionCurveEaseOut
@@ -138,15 +147,73 @@ static NSString *const kPestoDetailViewControllerMenuBack = @"mdc_sprite_arrow_b
           detailVC.title = cell.title;
           detailVC.iconImageName = cell.iconImageName;
           detailVC.descText = cell.descText;
+
+          detailVC.modalPresentationStyle = UIModalPresentationCustom;
+          detailVC.transitioningDelegate = self;
+
           [self presentViewController:detailVC
                              animated:NO
                            completion:^() {
                              self.zoomableView.frame = CGRectZero;
                              self.zoomableCardView.frame = CGRectZero;
+                             self.animatedMenuArrow.spriteSheetImage =
+                                 [UIImage imageNamed:kPestoMenuToBackArrow];
                              completionBlock();
                            }];
         }];
   });
+}
+
+#pragma mark - UIViewControllerTransitioningDelegate
+- (nullable id<UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:
+                                                          (UIViewController *)presented
+                                                                           presentingController:(UIViewController *)presenting
+                                                                               sourceController:(UIViewController *)source {
+  return nil;
+}
+
+- (nullable id<UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:
+        (UIViewController *)dismissed {
+  return self;
+}
+
+#pragma mark - UIViewControllerAnimatedTransitioning
+- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
+  UIViewController *const fromController = [transitionContext
+      viewControllerForKey:UITransitionContextFromViewControllerKey];
+
+  UIViewController *const toController = [transitionContext
+      viewControllerForKey:UITransitionContextToViewControllerKey];
+
+  if ([fromController isKindOfClass:[PestoDetailViewController class]] &&
+      [toController isKindOfClass:self.class]) {
+    // This is our custom dismissal that keeps the arrow button at the top of the controller
+    UIImage *spriteImageArrowToMenu = [UIImage imageNamed:kPestoMenuToBackArrow];
+    [self.view insertSubview:fromController.view belowSubview:self.animatedMenuArrow];
+    ((PestoDetailViewController *)fromController).animationView.hidden = YES;
+
+    [self.animatedMenuArrow startAnimatingWithCompletion:^{
+      self.animatedMenuArrow.spriteSheetImage = spriteImageArrowToMenu;
+    }];
+
+    CGRect detailFrame = fromController.view.frame;
+    detailFrame.origin.y = self.view.frame.size.height;
+
+    [UIView animateWithDuration:[self transitionDuration:transitionContext]
+        delay:0.f
+        options:UIViewAnimationOptionCurveEaseIn
+        animations:^{
+          fromController.view.frame = detailFrame;
+        }
+        completion:^(BOOL finished) {
+          [fromController.view removeFromSuperview];
+          [transitionContext completeTransition:YES];
+        }];
+  }
+}
+
+- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
+  return 0.2f;
 }
 
 #pragma mark - PestoSideViewDelegate
