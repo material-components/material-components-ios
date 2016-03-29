89812644e2de070dd9c574fcb94bd69d0c62bd13
Yiran Mao <yiranm@google.com>
2016-03-29 12:04:47 -0400

    GJSLint and formatted over mdccoderenderer.js file

--- jekyll-site-src/js/mdccoderender.js
+++ jekyll-site-src/js/mdccoderender.js
@@ -4,7 +4,7 @@
  * snippets into material design styling.
  */
 
-MDCCodeRender = (function(){
+(function() {
   'use strict';
   // A Boolean value to control line numbers generation. Device width smaller
   // than 600px will be considered as mobile device.
@@ -48,13 +48,13 @@ MDCCodeRender = (function(){
   function renderSimpleCodeRenderer(source, lineno) {
     function shellFilter() {
       var lines = cm.display.wrapper.querySelectorAll('.CodeMirror-line>span');
-      for(var i = 0; i < lines.length; i++) {
+      for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
-        if(line.querySelector('.cm-def') &&
+        if (line.querySelector('.cm-def') &&
           line.innerText.indexOf('$') !== -1) {
             var childrenOfLine = line.childNodes;
-            for(var j = 0; j < childrenOfLine.length; j++) {
-              if(childrenOfLine[j].classList &&
+            for (var j = 0; j < childrenOfLine.length; j++) {
+              if (childrenOfLine[j].classList &&
                  childrenOfLine[j].classList.contains('cm-def') !== -1) {
                 break;
               }
@@ -80,14 +80,14 @@ MDCCodeRender = (function(){
     });
 
     // If the language is Shell, this piece of logic process user path properly
-    if(language === 'Shell') {
+    if (language === 'Shell') {
       shellFilter();
     }
     return {
       language: language,
       cm: cm
     };
-	}
+  }
 
 
   /**
@@ -96,7 +96,7 @@ MDCCodeRender = (function(){
    * @param {string} id An string to identify the renderer.
    * @return {Object} An Object of codeMirror bject and language.
    */
-	function renderComplexCodeRenderer(renderer, id) {
+  function renderComplexCodeRenderer(renderer, id) {
     // Allowed Language for complex material code render.
     var _complexRendererAllowedLang = ['Objective-C', 'Swift'];
     // A Utility class to set/get page selected language.
@@ -104,7 +104,7 @@ MDCCodeRender = (function(){
       set: function(value) {
         value = _complexRendererAllowedLang.indexOf(value) !== -1 ?
                 value : _complexRendererAllowedLang[0];
-        if(typeof(window.sessionStorage) !== "undefined") {
+        if (typeof(window.sessionStorage) !== 'undefined') {
           window.sessionStorage.setItem('selectedLanguage', value);
         }
         else {
@@ -113,7 +113,7 @@ MDCCodeRender = (function(){
       },
       get: function() {
         var value;
-        if(typeof(window.sessionStorage) !== "undefined") {
+        if (typeof(window.sessionStorage) !== 'undefined') {
           value = window.sessionStorage.getItem('selectedLanguage');
         }
         else {
@@ -121,10 +121,10 @@ MDCCodeRender = (function(){
         }
         return value || _complexRendererAllowedLang[0];
       }
-    }
+    };
     /**
      * Generate Single radio button element from frontend template
-     * @param {string} groupname An string to identify the group the radio button
+     * @param {string} groupname An string to identify the group of radio button
      * belongs to.
      * @param {string} label The displayed name of the radio button.
      * @return {Element} An unattached DOM Node for a radio button.
@@ -141,12 +141,12 @@ MDCCodeRender = (function(){
 
       radioLabel.addEventListener('click', function(e) {
         var lang = this.querySelector('.radio-input').value;
-        if(selectedLanguage.get() == lang) {
+        if (selectedLanguage.get() == lang) {
           return false;
         }
         selectedLanguage.set(this.querySelector('.radio-input').value);
-        var evt = document.createEvent("HTMLEvents");
-        evt.initEvent("selectLangChange", false, true);
+        var evt = document.createEvent('HTMLEvents');
+        evt.initEvent('selectLangChange', false, true);
         document.dispatchEvent(evt);
       });
       return radioLabel;
@@ -156,12 +156,12 @@ MDCCodeRender = (function(){
     var availableLanguage = [];
     // Before generate:
     // 1. Take care of invalid code snippet case.
-    for(var i = 0; sources && i < sources.length; i++) {
+    for (var i = 0; sources && i < sources.length; i++) {
       var source = sources[i];
-      var kramdownLang = source.classList.length == 0? '' :
+      var kramdownLang = source.classList.length == 0 ? '' :
         source.classList[0].replace('language-', '');
       var language = kramdownToCodeMirrorMap[kramdownLang].language;
-      if(_complexRendererAllowedLang.indexOf[language] === -1) {
+      if (_complexRendererAllowedLang.indexOf[language] === -1) {
         source.parentNode.removeChild(source);
       }
       else {
@@ -169,56 +169,56 @@ MDCCodeRender = (function(){
       }
     }
     // 2. Take care of non code snippet case after invalid snippets are deleted.
-    if(!sources || sources.length === 0) {
+    if (!sources || sources.length === 0) {
       renderer.parentNode.removeChild(renderer);
       return;
     }
     // Generate Complex Code Renderer:
     // 1. Variables set up
-		var maxHeight = 0;
-		var radioForm = document.createElement('form');
-		radioForm.classList.add('language');
-		var cmMap = {};
+    var maxHeight = 0;
+    var radioForm = document.createElement('form');
+    radioForm.classList.add('language');
+    var cmMap = {};
     var radioName = 'MaterialCodeRenderer' + id;
     var useLineNumbers = !_mobileSized;
     if (useLineNumbers) {
       renderer.classList.add('line-numbers');
     }
     // 2. Convert code snippet to code mirror
-		for(var i = 0; i < sources.length; i++) {
-			var source = sources[i];
+    for (var i = 0; i < sources.length; i++) {
+      var source = sources[i];
       var simpleRender = renderSimpleCodeRenderer(source, useLineNumbers);
       var radioEl = new MaterialRadioButton(radioName, simpleRender.language);
-		  var clientHeight = simpleRender.cm.getScrollInfo().clientHeight;
-		  maxHeight = maxHeight < clientHeight? clientHeight : maxHeight;
-		  radioForm.appendChild(radioEl);
-		  cmMap[simpleRender.language] = simpleRender.cm;
-		}
+      var clientHeight = simpleRender.cm.getScrollInfo().clientHeight;
+      maxHeight = maxHeight < clientHeight ? clientHeight : maxHeight;
+      radioForm.appendChild(radioEl);
+      cmMap[simpleRender.language] = simpleRender.cm;
+    }
     // 3. Add radioForm into DOM
     renderer.insertBefore(radioForm, renderer.firstChild);
     // 4. Listen to selectLangChange event and change code snippet in display.
-		radioForm.addEventListener('selectLangChange', function() {
-			var targetLanguage = selectedLanguage.get();
+    radioForm.addEventListener('selectLangChange', function() {
+      var targetLanguage = selectedLanguage.get();
       targetLanguage = availableLanguage.indexOf(targetLanguage) == -1 ?
                        availableLanguage[0] : targetLanguage;
       radioForm.querySelector('.radio-input[value="' +
                               targetLanguage + '"]').checked = true;
-			renderer.querySelector('.CodeMirror.active').classList.remove('active');
-			cmMap[targetLanguage].display.wrapper.classList.add('active');
+      renderer.querySelector('.CodeMirror.active').classList.remove('active');
+      cmMap[targetLanguage].display.wrapper.classList.add('active');
       return false;
-		});
+    });
     // 5. Set the code renderer container height to the highest code mirror.
-		renderer.style.height = maxHeight + radioForm.offsetHeight + 'px';
+    renderer.style.height = maxHeight + radioForm.offsetHeight + 'px';
 
     // After Generation:
-		// Initialize with targetLanguage
+    // Initialize with targetLanguage
     var targetLanguage = selectedLanguage.get();
     targetLanguage = availableLanguage.indexOf(targetLanguage) == -1 ?
                      availableLanguage[0] : targetLanguage;
-		radioForm.querySelector('.radio-input[value="' +
+    radioForm.querySelector('.radio-input[value="' +
                              targetLanguage + '"]').checked = true;
-		cmMap[targetLanguage].display.wrapper.classList.add('active');
-	}
+    cmMap[targetLanguage].display.wrapper.classList.add('active');
+  }
 
 
   window.addEventListener('load', function() {
@@ -226,25 +226,25 @@ MDCCodeRender = (function(){
       _mobileSized = true;
     }
     // First: renders material code wrapper
-  	var complexrenders = document.querySelectorAll('.material-code-render');
-  	for(var i = 0; i < complexrenders.length; i++) {
+    var complexrenders = document.querySelectorAll('.material-code-render');
+    for (var i = 0; i < complexrenders.length; i++) {
       // RendererIndex assigns a unique id to each code renderer. The id will
       // be used by radioForm for each code renderer to form radio button group.
       renderComplexCodeRenderer(complexrenders[i], i);
-  	}
+    }
     // Second: renders all other code snippet
     var simplerenders = document.querySelectorAll('pre code');
-    for(var j = 0; j < simplerenders.length; j++) {
+    for (var j = 0; j < simplerenders.length; j++) {
       renderSimpleCodeRenderer(simplerenders[j]);
     }
     // Listen to selectLangChange event at document level and forward that event
     // to exsiting material code renders
     document.addEventListener('selectLangChange', function(e) {
-      console.log("value changes");
-      for(var i = 0; i < complexrenders.length; i++) {
+      console.log('value changes');
+      for (var i = 0; i < complexrenders.length; i++) {
         var evt = new e.constructor(e.type, e);
         complexrenders[i].querySelector('.language').dispatchEvent(evt);
       }
-    })
+    });
   });
 })();
