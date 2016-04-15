cdc18fb8ccda075e1f8c56fd638c3a66df6f4a93
Junius Gunaratne <junius@google.com>
2016-04-15 16:05:19 -0400

    [Catalog] Update Header Stack View demo visuals, move layout code into supplemental
    Summary: Screenshot http://codereview.cc/M23
    
    Reviewers: ajsecord, featherless, #mdc_ios_owners
    
    Reviewed By: featherless, #mdc_ios_owners
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D624

---
+++ components/HeaderStackView/examples/HeaderStackViewTypicalUse.m
@@ -0,0 +1,43 @@
+/*
+ Copyright 2016-present Google Inc. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+#import "HeaderStackViewTypicalUseSupplemental.h"
+#import "MaterialHeaderStackView.h"
+
+@interface HeaderStackViewTypicalUse ()
+
+@end
+
+@implementation HeaderStackViewTypicalUse
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [self setupExampleViews];
+
+  self.stackView = [[MDCHeaderStackView alloc] init];
+  self.stackView.topBar = self.topView;
+  self.stackView.bottomBar = self.navBar;
+
+  CGRect frame = self.view.bounds;
+  self.stackView.frame = frame;
+  [self.stackView sizeToFit];
+
+  [self.view addSubview:self.stackView];
+}
+
+@end
---
+++ components/HeaderStackView/examples/supplemental/HeaderStackViewTypicalUseSupplemental.h
@@ -0,0 +1,42 @@
+/*Copyright 2016-present Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/* IMPORTANT:
+ This file contains supplemental code used to populate the examples with dummy data and/or
+ instructions. It is not necessary to import this file to implement any Material Design Components.
+ */
+
+#import <UIKit/UIKit.h>
+
+@class ExampleInstructionsViewHeaderStackViewTypicalUse;
+@class HeaderStackViewTypicalUse;
+@class MDCHeaderStackView;
+@class MDCNavigationBar;
+
+@interface HeaderStackViewTypicalUse : UIViewController
+
+@property(nonatomic) BOOL toggled;
+@property(nonatomic) ExampleInstructionsViewHeaderStackViewTypicalUse *_Nullable exampleView;
+@property(nonatomic) MDCHeaderStackView *_Nullable stackView;
+@property(nonatomic) MDCNavigationBar *_Nullable navBar;
+@property(nonatomic) UIBarButtonItem *_Nullable moreButton;
+@property(nonatomic) UIView *_Nullable topView;
+
+@end
+
+@interface HeaderStackViewTypicalUse (Supplemental)
+
+- (void)setupExampleViews;
+
+@end
