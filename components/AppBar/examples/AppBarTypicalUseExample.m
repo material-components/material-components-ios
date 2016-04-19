0dc8870271222083a2a4dbbf55dd249a5b5cd4f5
randallli <randallli@google.com>
2016-04-19 17:45:16 -0400

    [AppBar] Typical Use Example moving logic from init into viewDidLoad
    Reviewers: ajsecord, featherless, #mdc_ios_owners
    
    Reviewed By: ajsecord, featherless, #mdc_ios_owners
    
    Subscribers: featherless
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D606

--- components/AppBar/examples/AppBarTypicalUseExample.m
+++ components/AppBar/examples/AppBarTypicalUseExample.m
@@ -32,16 +32,8 @@
   if (self) {
     // Step 2: Initialize the App Bar and add the headerViewController as a child.
     _appBar = [[MDCAppBar alloc] init];
-    _appBar.navigationBar.tintColor = [UIColor whiteColor];
     [self addChildViewController:_appBar.headerViewController];
-
     self.title = @"App Bar";
-
-    UIColor *color = [UIColor colorWithRed:(CGFloat)0x03 / (CGFloat)255
-                                     green:(CGFloat)0xA9 / (CGFloat)255
-                                      blue:(CGFloat)0xF4 / (CGFloat)255
-                                     alpha:1];
-    _appBar.headerViewController.headerView.backgroundColor = color;
   }
   return self;
 }
@@ -59,6 +51,14 @@
 
   // Step 3: Register the App Bar views.
   [self.appBar addSubviewsToParent];
+
+  // Optional: Change the App Bar's background color and tint color.
+  UIColor *color = [UIColor colorWithRed:(CGFloat)0x03 / (CGFloat)255
+                                   green:(CGFloat)0xA9 / (CGFloat)255
+                                    blue:(CGFloat)0xF4 / (CGFloat)255
+                                   alpha:1];
+  _appBar.headerViewController.headerView.backgroundColor = color;
+  _appBar.navigationBar.tintColor = [UIColor whiteColor];
 
   self.tableView.layoutMargins = UIEdgeInsetsZero;
   self.tableView.separatorInset = UIEdgeInsetsZero;
