d612f1fe94ddfeb92637b4c4795bd2c2b29a179a
Adrian Secord <ajsecord@google.com>
2016-04-21 14:35:48 -0400

    Added touch tracking for demos/videos. Triple-tap to enable.
    Fixes #413.
    
    Summary:
    Added MDCCatalogWindow.
    Added touch logic.
    
    Reviewers: #mdc_ios_owners, featherless
    
    Reviewed By: #mdc_ios_owners, featherless
    
    Projects: #material_components_ios
    
    Differential Revision: http://codereview.cc/D710

--- catalog/MDCCatalog/AppDelegate.swift
+++ catalog/MDCCatalog/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    self.window = MDCCatalogWindow(frame: UIScreen.mainScreen().bounds)
 
     let tree = CBCCreateNavigationTree()
 
---
+++ catalog/MDCCatalog/MDCCatalogWindow.swift
@@ -0,0 +1,100 @@
+/*
+Copyright 2016-present Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import UIKit
+
+/**
+ A custom UIWindow that displays the user's touches for recording video or demos.
+
+ Triple tapping anywhere will toggle the visible touches.
+ */
+class MDCCatalogWindow: UIWindow {
+  var enabled = false
+
+  private var views = [NSNumber: UIView]()
+
+  override func sendEvent(event: UIEvent) {
+    let touches = event.allTouches()!
+    for touch in touches {
+      switch touch.phase {
+      case .Began:
+        if (enabled) {
+          beginDisplayingTouch(touch)
+        }
+        continue
+      case .Moved:
+        updateTouch(touch)
+        continue
+      case .Stationary:
+        continue
+      case .Ended:
+        if (touch.tapCount == 3) {
+          enabled = !enabled
+        }
+        fallthrough
+      case .Cancelled:
+        endDisplayingTouch(touch)
+        continue
+      }
+    }
+
+    super.sendEvent(event)
+  }
+
+  private func beginDisplayingTouch(touch: UITouch) {
+    let view = MDCTouchView()
+    view.center = touch.locationInView(self)
+    views[touch.hash] = view
+    self.addSubview(view)
+  }
+
+  private func updateTouch(touch: UITouch) {
+    views[touch.hash]?.center = touch.locationInView(self)
+  }
+
+  private func endDisplayingTouch(touch: UITouch) {
+    views[touch.hash]?.removeFromSuperview()
+    views[touch.hash] = nil
+  }
+}
+
+/** A circular view that represents a user's touch. */
+class MDCTouchView: UIView {
+  private let touchCircleSize: CGFloat = 80
+  private let touchCircleAlpha: CGFloat = 0.25
+  private let touchCircleColor = UIColor.redColor()
+  private let touchCircleBorderColor = UIColor.blackColor()
+  private let touchCircleBorderWidth: CGFloat = 1
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonMDCTouchViewInit()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonMDCTouchViewInit()
+  }
+
+  private func commonMDCTouchViewInit() {
+    self.backgroundColor = touchCircleColor
+    self.alpha = touchCircleAlpha
+    self.frame.size = CGSizeMake(touchCircleSize, touchCircleSize)
+    self.layer.cornerRadius = touchCircleSize / 2
+    self.layer.borderColor = touchCircleBorderColor.CGColor
+    self.layer.borderWidth = touchCircleBorderWidth
+  }
+}
