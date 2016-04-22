material-components/material-components-ios

commit a037ae0184ade3b7d91595e7180a7fe573808590

[Catalog] Add +injectedInkViewForView extension to MDCInkTouchController and add ink to Catalog's root controller.
Summary:
This method makes it possible to easily add ink to an arbitrary view without having to subclass the view or store an associated property. We use this to add ink to the Catalog's root collection view.

Part of https://github.com/google/material-components-ios/issues/368.

Reviewers: ajsecord, #mdc_ios_owners

Reviewed By: ajsecord, #mdc_ios_owners

Projects: #material_components_ios

Differential Revision: http://codereview.cc/D727

catalog/MDCCatalog/MDCInkTouchController+Injection.h
@@ -1,0 +1,36 @@
+/*
+ Copyright 2016-present Google Inc. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MaterialInk.h"
+
+@interface MDCInkTouchController (ManyInkViews)
+
+/**
+ Enumerates the given view's subviews for an instance of MDCInkView and returns it if found, or
+ creates and adds a new instance of MDCInkView if not.
+
+ This method is a convenience method for adding ink to an arbitrary view without needing to subclass
+ the target view. Use this method in situations where you expect there to be many distinct ink views
+ in existence for a single ink touch controller. Example scenarios include:
+
+ - Adding ink to individual collection view/table view cells
+
+ This method can be used in your MDCInkTouchController delegate's
+ -inkTouchController:inkViewAtTouchLocation; implementation.
+ */
++ (nonnull MDCInkView *)injectedInkViewForView:(nonnull UIView *)view;
+
+@end

catalog/MDCCatalog.xcodeproj/project.pbxproj
@@ -15,6 +15,7 @@
 		664524B91C6BA62A001ADBF8 /* MDCNodeListViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 664524B81C6BA62A001ADBF8 /* MDCNodeListViewController.swift */; };
 		664524BE1C6BA62A001ADBF8 /* Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 664524BD1C6BA62A001ADBF8 /* Assets.xcassets */; };
 		664524C11C6BA62A001ADBF8 /* LaunchScreen.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = 664524BF1C6BA62A001ADBF8 /* LaunchScreen.storyboard */; };
+		66519B071CCA980600E5423E /* MDCInkTouchController+Injection.m in Sources */ = {isa = PBXBuildFile; fileRef = 66519B061CCA980600E5423E /* MDCInkTouchController+Injection.m */; };
 		666CA70D1CAEBCA9001B1884 /* CBCNodeViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = 666CA70A1CAEBCA9001B1884 /* CBCNodeViewController.m */; };
 		6681FDFD1CC586660013A0C7 /* MDCCatalogTileView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6681FDFC1CC586660013A0C7 /* MDCCatalogTileView.swift */; };
 		DE1944861CBD9E40009E0321 /* MDCCatalogTileData.m in Sources */ = {isa = PBXBuildFile; fileRef = DE1944681CBD9E40009E0321 /* MDCCatalogTileData.m */; };
@@ -89,6 +90,8 @@
 		664524BD1C6BA62A001ADBF8 /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
 		664524C01C6BA62A001ADBF8 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/LaunchScreen.storyboard; sourceTree = "<group>"; };
 		664524C21C6BA62A001ADBF8 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+		66519B051CCA97E100E5423E /* MDCInkTouchController+Injection.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = "MDCInkTouchController+Injection.h"; sourceTree = "<group>"; };
+		66519B061CCA980600E5423E /* MDCInkTouchController+Injection.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = "MDCInkTouchController+Injection.m"; sourceTree = "<group>"; };
 		665A34D91C6BD01900962055 /* MDCCatalog-Bridging-Header.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = "MDCCatalog-Bridging-Header.h"; sourceTree = "<group>"; };
 		666CA7081CAEBCA9001B1884 /* CBCCatalogExample.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = CBCCatalogExample.h; sourceTree = "<group>"; };
 		666CA7091CAEBCA9001B1884 /* CBCNodeViewController.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = CBCNodeViewController.h; sourceTree = "<group>"; };
@@ -195,6 +198,8 @@
 				6681FDFC1CC586660013A0C7 /* MDCCatalogTileView.swift */,
 				0B4A5E6C1CC9307A00D2AC5D /* MDCCatalogWindow.swift */,
 				664524B81C6BA62A001ADBF8 /* MDCNodeListViewController.swift */,
+				66519B051CCA97E100E5423E /* MDCInkTouchController+Injection.h */,
+				66519B061CCA980600E5423E /* MDCInkTouchController+Injection.m */,
 				665A34DE1C6BDAE700962055 /* Resources */,
 			);
 			path = MDCCatalog;
@@ -459,6 +464,7 @@
 				DE19448C1CBD9E40009E0321 /* MDCCatalogTileDataInk.m in Sources */,
 				0B4A5E6D1CC9307A00D2AC5D /* MDCCatalogWindow.swift in Sources */,
 				664524B91C6BA62A001ADBF8 /* MDCNodeListViewController.swift in Sources */,
+				66519B071CCA980600E5423E /* MDCInkTouchController+Injection.m in Sources */,
 				DE19448D1CBD9E40009E0321 /* MDCCatalogTileDataMisc.m in Sources */,
 				DE19448B1CBD9E40009E0321 /* MDCCatalogTileDataHeaderStackView.m in Sources */,
 				DE1944881CBD9E40009E0321 /* MDCCatalogTileDataButtonBar.m in Sources */,

catalog/MDCCatalog/MDCCatalog-Bridging-Header.h
@@ -16,3 +16,4 @@
 
 #import "CatalogByConvention.h"
 #import "MDCCatalogTiles.h"
+#import "MDCInkTouchController+Injection.h"

catalog/MDCCatalog/MDCCatalogComponentsController.swift
@@ -17,13 +17,15 @@ limitations under the License.
 import UIKit
 import MaterialComponents
 
-class MDCCatalogComponentsController: UICollectionViewController {
+class MDCCatalogComponentsController: UICollectionViewController, MDCInkTouchControllerDelegate {
 
   let spacing = CGFloat(1)
   let inset = CGFloat(16)
   let node: CBCNode
   var headerViewController: MDCFlexibleHeaderViewController
   let imageNames = NSMutableArray()
+
+  var inkController: MDCInkTouchController?
 
   init(collectionViewLayout ignoredLayout: UICollectionViewLayout, node: CBCNode) {
     self.node = node
@@ -60,6 +62,11 @@ class MDCCatalogComponentsController: UICollectionViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    inkController = MDCInkTouchController(view: self.collectionView!)!
+    inkController!.addInkView()
+    inkController!.delaysInkSpread = true
+    inkController!.delegate = self
 
     let containerView = UIView(frame: self.headerViewController.headerView.bounds)
     containerView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
@@ -120,10 +127,30 @@ class MDCCatalogComponentsController: UICollectionViewController {
     return node.children.count
   }
 
+  func inkViewForView(view: UIView) -> MDCInkView {
+    let foundInkView = MDCInkTouchController.injectedInkViewForView(view)
+    foundInkView.inkStyle = .Unbounded
+    foundInkView.inkColor = UIColor(red: 0.012, green: 0.663, blue: 0.957, alpha: 0.2)
+    return foundInkView
+  }
+
+  // MARK: MDCInkTouchControllerDelegate
+
+  func inkTouchController(inkTouchController: MDCInkTouchController, shouldProcessInkTouchesAtTouchLocation location: CGPoint) -> Bool {
+    return self.collectionView!.indexPathForItemAtPoint(location) != nil
+  }
+
+  func inkTouchController(inkTouchController: MDCInkTouchController, inkViewAtTouchLocation location: CGPoint) -> MDCInkView {
+    if let indexPath = self.collectionView!.indexPathForItemAtPoint(location) {
+      let cell = self.collectionView!.cellForItemAtIndexPath(indexPath)
+      return self.inkViewForView(cell!)
+    }
+    return MDCInkView()
+  }
+
   // MARK: UICollectionViewDelegate
 
-  override func collectionView(collectionView: UICollectionView,
-    cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+  override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MDCCatalogCollectionViewCell",
       forIndexPath: indexPath)
     cell.backgroundColor = UIColor.whiteColor()
@@ -133,12 +160,13 @@ class MDCCatalogComponentsController: UICollectionViewController {
       catalogCell.populateView(componentName)
     }
 
+    // Ensure that ink animations aren't recycled.
+    MDCInkTouchController.injectedInkViewForView(view).cancelAllAnimationsAnimated(false)
+
     return cell
   }
 
-  func collectionView(collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
     let pad = CGFloat(1)
     var cellWidth = (self.view.frame.size.width - 3 * pad) / 2
     if (self.view.frame.size.width > self.view.frame.size.height) {

catalog/MDCCatalog/MDCInkTouchController+Injection.m
@@ -1,0 +1,37 @@
+/*
+ Copyright 2016-present Google Inc. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MDCInkTouchController+Injection.h"
+
+@implementation MDCInkTouchController (ManyInkViews)
+
++ (MDCInkView *)injectedInkViewForView:(UIView *)view {
+  MDCInkView *foundInkView = nil;
+  for (MDCInkView *subview in view.subviews) {
+    if ([subview isKindOfClass:[MDCInkView class]]) {
+      foundInkView = subview;
+      break;
+    }
+  }
+  if (!foundInkView) {
+    foundInkView = [[MDCInkView alloc] initWithFrame:view.bounds];
+    foundInkView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [view addSubview:foundInkView];
+  }
+  return foundInkView;
+}
+
+@end

commit 132750ec68e73e38a72ac096a210070d541616bb

[Catalog] Import proper header.
Summary: Resolves build breakage introduced in a037ae0184ade3b7d91595e7180a7fe573808590..

Reviewers: ajsecord, #mdc_ios_owners

Reviewed By: ajsecord, #mdc_ios_owners

Subscribers: ajsecord

Projects: #material_components_ios

Differential Revision: http://codereview.cc/D733

catalog/MDCCatalog/MDCInkTouchController+Injection.h
@@ -14,7 +14,7 @@
  limitations under the License.
  */
 
-#import "MaterialInk.h"
+#import <MaterialComponents/MaterialInk.h>
 
 @interface MDCInkTouchController (ManyInkViews)
 