dd761f7f81c4797e0e751305b4f6a6fc42ad7790
Justin Shephard <shepj@google.com>
2016-06-29 13:21:40 -0400

    MDC_#388 - Added NSLayoutConstraints to _animationView and label in SpritedAnimationViewTypicalUseViewController.m
    Summary:
    Files Changed:
    SpritedAnimationViewTypicalUseViewController.m
    
    MDC_#388 - Added NSLayoutConstraints to _animationView and label in SpritedAnimationViewTypicalUseViewController.m
    
    Files Changed:
    SpritedAnimationViewTypicalUseViewController.m
    
    Merge branch 'MDC_#388' of https://github.com/google/material-components-ios into MDC_#388
    
    # Conflicts:
    #	components/SpritedAnimationView/examples/SpritedAnimationViewTypicalUseViewController.m
    
    Reviewers: O1 Material components iOS, ajsecord
    
    Reviewed By: O1 Material components iOS, ajsecord
    
    Tags: #material_components_ios
    
    Differential Revision: http://codereview.cc/D1037

--- components/SpritedAnimationView/examples/SpritedAnimationViewTypicalUseViewController.m
+++ components/SpritedAnimationView/examples/SpritedAnimationViewTypicalUseViewController.m
@@ -51,16 +51,94 @@ static NSString *const kSpriteGrid = @"mdc_sprite_grid__list";
   _animationView = [[MDCSpritedAnimationView alloc] initWithSpriteSheetImage:spriteImage];
   _animationView.frame = CGRectMake(0, 0, 30, 30);
   _animationView.center = self.view.center;
+  _animationView.translatesAutoresizingMaskIntoConstraints = NO;
   UIColor *blueColor = [UIColor colorWithRed:0.012 green:0.663 blue:0.957 alpha:1];
   _animationView.tintColor = blueColor;
   [self.view addSubview:_animationView];
 
+  // AnimationView Layout Constraints
+  NSLayoutConstraint *animationViewWidthConstraint =
+      [NSLayoutConstraint constraintWithItem:_animationView
+                                   attribute:NSLayoutAttributeWidth
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:nil
+                                   attribute:nil
+                                  multiplier:1.0
+                                    constant:30];
+  animationViewWidthConstraint.active = true;
+  NSLayoutConstraint *animationViewHeightConstraint =
+      [NSLayoutConstraint constraintWithItem:_animationView
+                                   attribute:NSLayoutAttributeHeight
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:nil
+                                   attribute:nil
+                                  multiplier:1.0
+                                    constant:30];
+  animationViewHeightConstraint.active = true;
+  NSLayoutConstraint *animationViewXConstraint =
+      [NSLayoutConstraint constraintWithItem:self.view
+                                   attribute:NSLayoutAttributeCenterX
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:_animationView
+                                   attribute:NSLayoutAttributeCenterX
+                                  multiplier:1.0
+                                    constant:0.0];
+  animationViewXConstraint.active = true;
+  NSLayoutConstraint *animationViewYConstraint =
+      [NSLayoutConstraint constraintWithItem:self.view
+                                   attribute:NSLayoutAttributeCenterY
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:_animationView
+                                   attribute:NSLayoutAttributeCenterY
+                                  multiplier:1.0
+                                    constant:0.0];
+  animationViewYConstraint.active = true;
+
   // Add label with tap instructions.
   UILabel *label = [[UILabel alloc] initWithFrame:CGRectOffset(self.view.bounds, 0, 30)];
+  label.translatesAutoresizingMaskIntoConstraints = NO;
   label.text = @"Tap anywhere to animate icon.";
   label.textColor = [UIColor colorWithWhite:0 alpha:0.8];
   label.textAlignment = NSTextAlignmentCenter;
   [self.view addSubview:label];
+
+  // Label Layout Constraints
+  NSLayoutConstraint *labelWidthConstraint =
+      [NSLayoutConstraint constraintWithItem:label
+                                   attribute:NSLayoutAttributeWidth
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:self.view
+                                   attribute:NSLayoutAttributeWidth
+                                  multiplier:1.0
+                                    constant:0.0];
+  labelWidthConstraint.active = true;
+  NSLayoutConstraint *labelHeightConstraint =
+      [NSLayoutConstraint constraintWithItem:label
+                                   attribute:NSLayoutAttributeHeight
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:self.view
+                                   attribute:NSLayoutAttributeHeight
+                                  multiplier:1.0
+                                    constant:0.0];
+  labelHeightConstraint.active = true;
+  NSLayoutConstraint *labelXConstraint =
+      [NSLayoutConstraint constraintWithItem:label
+                                   attribute:NSLayoutAttributeCenterX
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:self.view
+                                   attribute:NSLayoutAttributeCenterX
+                                  multiplier:1.0
+                                    constant:0.0];
+  labelXConstraint.active = true;
+  NSLayoutConstraint *labelYConstraint =
+      [NSLayoutConstraint constraintWithItem:label
+                                   attribute:NSLayoutAttributeCenterY
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:self.view
+                                   attribute:NSLayoutAttributeCenterY
+                                  multiplier:1.0
+                                    constant:30.0];
+  labelYConstraint.active = true;
 
   // Add tap gesture to view.
   UITapGestureRecognizer *tapGesture =
