a107d2d3cefccadaf82fd7df59e27f7d06607041
Jeff Verkoeyen <featherless@google.com>
2016-06-29 11:27:13 -0400

    [Catalog] Use CatalogByConvention pod dependency.
    Summary: Removed direct dependency on catalog by convention source.
    
    Reviewers: ajsecord, O1 Material components iOS
    
    Reviewed By: ajsecord, O1 Material components iOS
    
    Subscribers: ajsecord
    
    Tags: #material_components_ios
    
    Differential Revision: http://codereview.cc/D1055

--- catalog/MDCCatalog/MDCCatalogComponentsController.swift
+++ catalog/MDCCatalog/MDCCatalogComponentsController.swift
@@ -14,8 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import UIKit
+import CatalogByConvention
 import MaterialComponents
+import UIKit
 
 class MDCCatalogComponentsController: UICollectionViewController, MDCInkTouchControllerDelegate {
 
