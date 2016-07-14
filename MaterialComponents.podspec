3742dd4849c64e334a46e6b027ddfa9c75e6034c
Ian Gordon <iangordon@google.com>
2016-07-14 13:51:43 -0400

    [Dialogs] Initial Import
    Summary:
    Initial import of our modal presentation controller and transition controller.
    
    The interfaces have been previously reviewed in our API Review process.
    
    This will land on the branch feature-dialog.
    
    Test Plan: Example added to catalog.
    
    Reviewers: randallli, cjcox, ajsecord, O1 Material components iOS
    
    Reviewed By: ajsecord, O1 Material components iOS
    
    Subscribers: samnm, randallli
    
    Tags: #material_components_ios
    
    Differential Revision: http://codereview.cc/D1066

--- MaterialComponents.podspec
+++ MaterialComponents.podspec
@@ -126,6 +126,16 @@ Pod::Spec.new do |s|
     ss.dependency "MaterialComponents/Typography"
   end
 
+  s.subspec "Dialogs" do |ss|
+    ss.public_header_files = "components/#{ss.base_name}/src/*.h"
+    ss.source_files = "components/#{ss.base_name}/src/*.{h,m}", "components/#{ss.base_name}/src/private/*.{h,m}"
+    ss.header_mappings_dir = "components/#{ss.base_name}/src"
+
+    ss.dependency "MaterialComponents/Buttons"
+    ss.dependency "MaterialComponents/ShadowElevations"
+    ss.dependency "MaterialComponents/ShadowLayer"
+  end
+
   s.subspec "FlexibleHeader" do |ss|
     ss.public_header_files = "components/#{ss.base_name}/src/*.h"
     ss.source_files = "components/#{ss.base_name}/src/*.{h,m}", "components/#{ss.base_name}/src/private/*.{h,m}"
---
+++ components/Dialogs/examples/supplemental/DialogsTypicalUseSupplemental.m
@@ -0,0 +1,71 @@
+/* IMPORTANT:
+ This file contains supplemental code used to populate the examples with dummy data and/or
+ instructions. It is not necessary to import this file to implement any Material Design Components.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "DialogsTypicalUseSupplemental.h"
+#import "MaterialButtons.h"
+#import "MaterialDialogs.h"
+#import "MaterialTypography.h"
+
+#pragma mark - DialogsTypicalUseViewController
+
+@implementation DialogsTypicalUseViewController (CatalogByConvention)
+
++ (NSArray *)catalogBreadcrumbs {
+  return @[ @"Dialogs", @"Dialogs" ];
+}
+
++ (NSString *)catalogDescription {
+  return @"Dialogs includes a presentation controller that displays your modal interfaces in a"
+          "material spec defined context.";
+}
+
++ (BOOL)catalogIsPrimaryDemo {
+  return YES;
+}
+
++ (NSString *)catalogStoryboardName {
+  return @"DialogsTypicalUseViewController";
+}
+
+@end
+
+@interface ProgrammaticViewController ()
+
+@property(nonatomic, strong) MDCRaisedButton *dismissButton;
+
+@end
+
+@implementation ProgrammaticViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  self.view.backgroundColor = [UIColor whiteColor];
+
+  _dismissButton = [[MDCRaisedButton alloc] init];
+  _dismissButton.autoresizingMask =
+      UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin |
+      UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+  [_dismissButton setTitle:@"Dismiss" forState:UIControlStateNormal];
+  [_dismissButton sizeToFit];
+  [_dismissButton addTarget:self
+                     action:@selector(dismiss:)
+           forControlEvents:UIControlEventTouchUpInside];
+
+  [self.view addSubview:_dismissButton];
+  _dismissButton.center = self.view.center;
+}
+
+- (CGSize)preferredContentSize {
+  return CGSizeMake(200.0, 140.0);
+}
+
+- (IBAction)dismiss:(id)sender {
+  [self.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end
