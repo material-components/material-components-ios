9b18a45eaa8b03044b4a86ad18cb31d757da8f62
Brian Moore <brianjmoore@users.noreply.github.com>
2016-10-04 12:29:16 -0400

    Initial implementation of MDCTabBar and private MDCItemBar (#711)
    * Add initial headers for MDC*Bar
    
    * Initial chunk of work
    
    * It works…ish
    
    * Update demo images and update style with appearance
    
    * Update Podfile.lock
    
    * Merge
    
    * Merge
    
    * Revert bottom bar for now
    
    * Update description
    
    * Formatting and cleanup
    
    * Manual revert of mistaken changes
    
    * Manual revert of mistaken changes
    
    * Manual revert of mistaken changes
    
    * Update with consistent terminology
    
    * Update constant names
    
    * Update localizable strings
    
    * More naming updates
    
    * Rename TabBar to Tabs, move ItemBar into Tabs/private
    
    * Rename string keys
    
    * Rename string files from ItemBar to Tabs
    
    * Rename string keys header
    
    * Update podspec and move bundle
    
    * Reset Podfile.lock in preparation for re-updating
    
    * Clean update to Podfile.lock, adding MaterialTabs
    
    * Update import paths
    
    * Move CatalogByConvention method to end of file
    
    * Add copyright notice on all h/m files
    
    * Add safe fallbacks for switches
    
    * Use conventional common init method name
    
    * Podfile.lock checksum update

---
+++ components/Tabs/src/private/MDCItemBarAlignment.h
@@ -0,0 +1,35 @@
+/*
+ Copyright 2016-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+/** Alignment styles for items in a tab bar. */
+typedef NS_ENUM(NSInteger, MDCItemBarAlignment) {
+  /** Items are aligned on the leading edge and sized to fit their content. */
+  MDCItemBarAlignmentLeading,
+
+  /** Items are justified to equal size across the width of the screen. */
+  MDCItemBarAlignmentJustified,
+
+  /**
+   * Items are sized to fit their content and center-aligned as a group. If they do not fit in view,
+   * they will be leading-aligned instead.
+   */
+  MDCItemBarAlignmentCenter,
+
+  /** Items are center-aligned on the selected item. */
+  MDCItemBarAlignmentCenterSelected,
+};
---
+++ components/Tabs/src/private/MDCItemBarStyle.h
@@ -0,0 +1,76 @@
+/*
+ Copyright 2016-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+#import "MaterialInk.h"
+
+/** Describes the visual style of individual items in an item bar. */
+@interface MDCItemBarStyle : NSObject <NSCopying>
+
+/** The default height of the bar. */
+@property(nonatomic) CGFloat defaultHeight;
+
+/** Determines if the selection indicator bar should be shown. */
+@property(nonatomic) BOOL shouldDisplaySelectionIndicator;
+
+/** Color used for the selection indicator bar which indicates the selected item. */
+@property(nonatomic, strong, nullable) UIColor *selectionIndicatorColor;
+
+/** The maximum width for individual items within the bar. If zero, items have no maximum width. */
+@property(nonatomic) CGFloat maximumItemWidth;
+
+#pragma mark - Item Style
+
+/** Indicates if the title should be displayed. */
+@property(nonatomic) BOOL shouldDisplayTitle;
+
+/** Indicates if the image should be displayed. */
+@property(nonatomic) BOOL shouldDisplayImage;
+
+/** Indicates if a badge may be shown. */
+@property(nonatomic) BOOL shouldDisplayBadge;
+
+/** Indicates if the cell's components should grow slightly when selected. (Bottom navigation) */
+@property(nonatomic) BOOL shouldGrowOnSelection;
+
+/** Color of title text when not selected. Default is opaque white. */
+@property(nonatomic, strong, nonnull) UIColor *titleColor;
+
+/** Color of title text when selected. Default is opaque white. */
+@property(nonatomic, strong, nonnull) UIColor *selectedTitleColor;
+
+/** Font used for item titles. */
+@property(nonatomic, nonnull) UIFont *titleFont;
+
+/** Style of ink animations on item interaction. */
+@property(nonatomic) MDCInkStyle inkStyle;
+
+/** Color of ink splashes. Default is 25% white. */
+@property(nonatomic, strong, nonnull) UIColor *inkColor;
+
+/** Padding in points between the title and image components, according to the MD spec. */
+@property(nonatomic) CGFloat titleImagePadding;
+
+/**
+ Indicates if all tab titles should be uppercased for display. If NO, item titles will be
+ displayed verbatim.
+
+ Default is YES and is recommended whenever possible.
+ */
+@property(nonatomic) BOOL displaysUppercaseTitles;
+
+@end
