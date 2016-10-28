6551f029bd1e0f306ef18c4a3050c460cfcb5a57
Randall Li <randall.li@gmail.com>
2016-10-28 14:01:42 -0400

    Make Shrine demo use module map (#811)
    Made necessary changes to Shrine project to use module maps instead of bridging header for Material Components.
    
    Note: importing the entire framework in this PR. I will work on splitting out just the used components in a future PR.

--- demos/Shrine/Shrine/ShrineCollectionViewCell.swift
+++ demos/Shrine/Shrine/ShrineCollectionViewCell.swift
@@ -15,6 +15,7 @@
  */
 
 import UIKit
+import MaterialComponents
 
 class ShrineCollectionViewCell: UICollectionViewCell {
 
--- demos/Shrine/Shrine/ShrineCollectionViewController.swift
+++ demos/Shrine/Shrine/ShrineCollectionViewController.swift
@@ -15,6 +15,7 @@
  */
 
 import UIKit
+import MaterialComponents
 
 class ShrineCollectionViewController: UICollectionViewController {
 
