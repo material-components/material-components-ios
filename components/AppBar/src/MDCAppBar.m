7f16c68ff4b5fa3f0c756823f719d08055fce838
Justin Shephard <shepj@google.com>
2016-11-09 18:46:51 -0500

    MDC_#702 - Readability

--- components/AppBar/src/MDCAppBar.m
+++ components/AppBar/src/MDCAppBar.m
@@ -40,14 +40,18 @@ static const CGFloat kStatusBarHeight = 20;
 - (void)applyAccessibilityTitleColorOnAppBar:(nonnull MDCAppBar *)appBar {
 
   // Determine what is the appropriate background color
-  UIColor *backgroundColor = appBar.headerViewController.headerView.backgroundColor ?: appBar.navigationBar.backgroundColor;
+  UIColor *backgroundColor = appBar.headerViewController.headerView.backgroundColor ?:
+      appBar.navigationBar.backgroundColor;
   if (!backgroundColor) {
     return;
   }
 
   // Update title label color based on navigationBar backgroundColor
-  NSMutableDictionary *textAttr = [NSMutableDictionary dictionaryWithDictionary:[appBar.navigationBar titleTextAttributes]];
-  UIColor *textColor = [MDFTextAccessibility textColorOnBackgroundColor:backgroundColor targetTextAlpha:1.0 font:[textAttr objectForKey:NSFontAttributeName]];
+  NSMutableDictionary *textAttr =
+      [NSMutableDictionary dictionaryWithDictionary:[appBar.navigationBar titleTextAttributes]];
+  UIColor *textColor = [MDFTextAccessibility textColorOnBackgroundColor:backgroundColor
+                                                        targetTextAlpha:1.0
+                                                  font:[textAttr objectForKey:NSFontAttributeName]];
   [textAttr setObject:textColor forKey:NSForegroundColorAttributeName];
   [appBar.navigationBar setTitleTextAttributes:textAttr];
 
