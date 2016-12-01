d267fcdab6d5c7e3a18bcf936a19b903f7f0db86
randallli <randallli@google.com>
2016-12-01 16:19:47 -0500

    [PageControl] Fixed swift import to be more specific

--- components/PageControl/examples/PageControlTypicalUseExample.swift
+++ components/PageControl/examples/PageControlTypicalUseExample.swift
@@ -16,7 +16,7 @@ limitations under the License.
 
 import Foundation
 
-import MaterialComponents
+import MaterialComponents.MaterialPageControl
 
 class PageControlSwiftExampleViewController: UIViewController, UIScrollViewDelegate {
 
