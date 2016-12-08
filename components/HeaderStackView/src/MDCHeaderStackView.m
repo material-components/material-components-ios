2ebebab53566bcaf3e17442ee3b6deb5b468c2ac
Justin Shephard <shepj@google.com>
2016-12-08 14:45:26 -0500

    [HeaderStackView] - Added IB_Designable, NSCoding support, swift example (#983)

--- components/HeaderStackView/src/MDCHeaderStackView.m
+++ components/HeaderStackView/src/MDCHeaderStackView.m
@@ -16,7 +16,34 @@
 
 #import "MDCHeaderStackView.h"
 
+static NSString *const MDCHeaderStackViewTopBarKey = @"MDCHeaderStackViewTopBarKey";
+static NSString *const MDCHeaderStackViewBottomBarKey = @"MDCHeaderStackViewBottomBarKey";
+
 @implementation MDCHeaderStackView
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    if ([aDecoder containsValueForKey:MDCHeaderStackViewTopBarKey]) {
+      _topBar = [aDecoder decodeObjectForKey:MDCHeaderStackViewTopBarKey];
+    }
+
+    if ([aDecoder containsValueForKey:MDCHeaderStackViewBottomBarKey]) {
+      _bottomBar = [aDecoder decodeObjectForKey:MDCHeaderStackViewBottomBarKey];
+    }
+  }
+  return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+  [super encodeWithCoder:aCoder];
+  if (self.topBar) {
+    [aCoder encodeObject:self.topBar forKey:MDCHeaderStackViewTopBarKey];
+  }
+  if (self.bottomBar) {
+    [aCoder encodeObject:self.bottomBar forKey:MDCHeaderStackViewBottomBarKey];
+  }
+}
 
 - (CGSize)sizeThatFits:(CGSize)size {
   if (_bottomBar) {
