837aa16953a9536c470845ae47b40a45f4dd4dcc
Sam Morrison <sam@sam.vg>
2016-12-14 15:24:57 -0500

    RTL support for MDCThumbTrack (#1073)
    * Temporarily rename the Slider card for easier dev
    
    * Add RTL support for MDCThumbTrack
    
    * Revert "Temporarily rename the Slider card for easier dev"
    
    This reverts commit 3c87a42624169582448fa43f6f9ce35202326994.
    
    * Add comments

--- components/private/ThumbTrack/src/MDCThumbTrack.m
+++ components/private/ThumbTrack/src/MDCThumbTrack.m
@@ -19,6 +19,7 @@
 #import "MDCNumericValueLabel.h"
 #import "MDCThumbView.h"
 #import "MaterialInk.h"
+#import "MaterialRTL.h"
 #import "UIColor+MDC.h"
 
 static const CGFloat kAnimationDuration = 0.25f;
@@ -864,6 +865,10 @@ static inline CGFloat DistanceFromPointToPoint(CGPoint point1, CGPoint point2) {
 - (CGFloat)valueForThumbPosition:(CGPoint)position {
   CGFloat relValue = (position.x - _thumbRadius) / self.thumbPanRange;
   relValue = MAX(0, MIN(relValue, 1));
+  // For RTL we invert the value
+  if (self.mdc_effectiveUserInterfaceLayoutDirection == UIUserInterfaceLayoutDirectionRightToLeft) {
+    relValue = 1 - relValue;
+  }
   return (1 - relValue) * _minimumValue + relValue * _maximumValue;
 }
 
@@ -903,7 +908,12 @@ static inline CGFloat DistanceFromPointToPoint(CGPoint point1, CGPoint point2) {
   if (CGFloatEqual(_minimumValue, _maximumValue)) {
     return _minimumValue;
   }
-  return (value - _minimumValue) / Fabs(_minimumValue - _maximumValue);
+  CGFloat relValue = (value - _minimumValue) / Fabs(_minimumValue - _maximumValue);
+  // For RTL we invert the value
+  if (self.mdc_effectiveUserInterfaceLayoutDirection == UIUserInterfaceLayoutDirectionRightToLeft) {
+    relValue = 1 - relValue;
+  }
+  return relValue;
 }
 
 - (CGFloat)closestValueToTargetValue:(CGFloat)targetValue {
