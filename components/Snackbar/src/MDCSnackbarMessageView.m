1effd8ad0cb879048f00972b366bcdf481e3c2b6
Louis Romero <lpromero@google.com>
2017-01-25 00:18:12 +0800

    Update file path for private file. (#1168)
    Prior to this change, one needed to add `components/Snackbar/src/private` to the list of `include_dirs`.
    
    Closes #1167.

--- components/Snackbar/src/MDCSnackbarMessageView.m
+++ components/Snackbar/src/MDCSnackbarMessageView.m
@@ -19,11 +19,11 @@
 #import "MDCSnackbarMessage.h"
 #import "MDCSnackbarMessageView.h"
 #import "MaterialButtons.h"
-#import "MDCSnackbarOverlayView.h"
 #import "MaterialAnimationTiming.h"
 #import "MaterialButtons.h"
 #import "MaterialTypography.h"
 #import "private/MDCSnackbarMessageViewInternal.h"
+#import "private/MDCSnackbarOverlayView.h"
 
 NSString *const MDCSnackbarMessageTitleAutomationIdentifier =
     @"MDCSnackbarMessageTitleAutomationIdentifier";
