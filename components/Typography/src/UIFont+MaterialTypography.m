64945cd16255502d60887875fa2fb0d89ae1c4d9
Ian Gordon <iangordon@google.com>
2017-01-26 19:14:02 -0500

    [Typography] Initial MDC Font Styles

--- components/Typography/src/UIFont+MaterialTypography.m
+++ components/Typography/src/UIFont+MaterialTypography.m
@@ -1,12 +1,9 @@
 /*
- Copyright 2015-present the Material Components for iOS authors. All Rights Reserved.
-
+ Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
  http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,16 +11,20 @@
  limitations under the License.
  */
 
-/**
- The Typography component provides methods for getting sized fonts and opacities following Material
- style guidelines.
-
- This header is the umbrella header for the component and should be imported by consumers of the
- Typography component. Please do not directly import other headers. This will allow the componet to
- expand or contract the header file space without consumer modifications.
- */
-
-#import "MDCTypography.h"
-#import "MDCFontTextStyle.h"
 #import "UIFont+MaterialTypography.h"
+
 #import "UIFontDescriptor+MaterialTypography.h"
+
+@implementation UIFont (MaterialTypography)
+
++ (UIFont *)mdc_preferredFontForMaterialTextStyle:(MDCFontTextStyle)style {
+  UIFontDescriptor *fontDescriptor =
+      [UIFontDescriptor mdc_preferredFontDescriptorForMaterialTextStyle:style];
+
+  // Size is included in the fontDescriptor, so we pass in 0.0 in the parameter
+  UIFont *font = [UIFont fontWithDescriptor:fontDescriptor size:0.0];
+
+  return font;
+}
+
+@end
---
+++ components/Typography/src/private/MDCFontTraits.h
@@ -0,0 +1,34 @@
+/*
+ Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+#import "MDCFontTextStyle.h"
+
+/*
+ This class is based off Apple recommendation in WWDC 2016 - Typography session
+ */
+
+@interface MDCFontTraits : NSObject
+
+@property (nonatomic, readonly) CGFloat pointSize;
+@property (nonatomic, readonly) CGFloat weight;
+@property (nonatomic, readonly) CGFloat leading;
+@property (nonatomic, readonly) CGFloat tracking;
+
+- (instancetype)initWithPointSize:(CGFloat)pointSize weight:(CGFloat)weight leading:(CGFloat)leading tracking:(CGFloat)tracking;
+
++ (MDCFontTraits *)traitsForTextStyle:(MDCFontTextStyle)style sizeCategory:(NSString *)sizeCategory;
+
+
+@end
