09a10ffd2aea4977f669e6f8d1066aebcf146d46
Will Larche <willlarche@users.noreply.github.com>
2017-03-07 14:42:01 -0500

    [TextFields] API Review: Convenience init for better Swifting. (#1238)
    * [TextFields] Convenience init for better Swifting.
    
    * changed the initWithInput: to initWithTextInput:
    
    * [TextFields] Swift name for initWithTextInput added to behavior

--- components/TextFields/src/MDCTextInputBehavior.h
+++ components/TextFields/src/MDCTextInputBehavior.h
@@ -143,6 +143,14 @@ typedef NS_ENUM(NSUInteger, MDCTextInputPresentationStyle) {
     MDCTextInputPresentationStyle presentationStyle NS_SWIFT_NAME(presentation);
 
 /**
+ Convenience init. Never fails.
+
+ @param input An MDCTextInput this behavior will manage.
+ */
+- (nonnull instancetype)initWithTextInput:(nullable UIView <MDCTextInput> *)input
+    NS_SWIFT_NAME(init(input:));
+
+/**
  Sets the state of the controller by setting the values of properties errorText and
  errorAccessibilityValue.
 
