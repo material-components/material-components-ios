4cbdfb9e3190225f3c13a6826b0bd9fd99c02d26
Louis Romero <lpromero@google.com>
2017-03-21 15:54:13 +0100

    [CollectionCells] Add accessibility hint in edit mode (#1258)
    * [CollectionCells] Add accessibility hint in edit mode
    
    When editing, selectable/deselectable cells now give an accessibility
    hint: "Double tap to select.". This helps users to know what they can do
    while in edit mode.
    
    Related: https://github.com/material-components/material-components-ios/issues/1257
    
    * Add tests

---
+++ components/CollectionCells/tests/unit/CollectionCellsIssue1257Tests.m
@@ -0,0 +1,69 @@
+/*
+ Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing software
+ distributed under the License is distributed on an "AS IS" BASIS
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import "MDCCollectionViewCell.h"
+#import "MDCCollectionViewLayoutAttributes.h"
+
+// Expose internal helper method.
+@interface MDCCollectionViewCell (Private)
++ (NSString *)localizedStringWithKey:(NSString *)key;
+@end
+
+// Tests confirming that the accessibility hint for (de)selectable cells is correctly set up.
+//
+// Based on issue https://github.com/material-components/material-components-ios/issues/1257
+@interface CollectionCellsIssue1257Tests : XCTestCase
+@property(nonatomic, strong) MDCCollectionViewCell *cell;
+@property(nonatomic, strong) MDCCollectionViewLayoutAttributes *layoutAttributes;
+@end
+
+@implementation CollectionCellsIssue1257Tests
+
+- (void)setUp {
+  [super setUp];
+  self.cell = [[MDCCollectionViewCell alloc] init];
+  self.layoutAttributes = [[MDCCollectionViewLayoutAttributes alloc] init];
+}
+
+- (void)testSelectableSelected {
+  self.layoutAttributes.shouldShowSelectorStateMask = YES;
+  [self.cell applyLayoutAttributes:self.layoutAttributes];
+  self.cell.selected = YES;
+
+  XCTAssertEqual(self.cell.accessibilityHint,
+                 [MDCCollectionViewCell localizedStringWithKey:kSelectedCellAccessibilityHintKey]);
+}
+
+- (void)testSelectableDeselected {
+  self.layoutAttributes.shouldShowSelectorStateMask = YES;
+  [self.cell applyLayoutAttributes:self.layoutAttributes];
+  self.cell.selected = NO;
+
+  XCTAssertEqual(
+      self.cell.accessibilityHint,
+      [MDCCollectionViewCell localizedStringWithKey:kDeselectedCellAccessibilityHintKey]);
+}
+
+- (void)testUnselectable {
+  self.layoutAttributes.shouldShowSelectorStateMask = NO;
+  [self.cell applyLayoutAttributes:self.layoutAttributes];
+
+  XCTAssertEqual(self.cell.accessibilityHint, nil);
+}
+
+@end
