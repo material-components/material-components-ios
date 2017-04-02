18576b5c0a7b36621e3cb3889d990ec20dffc182
Will Larche <willlarche@gmail.com>
2017-04-01 20:05:37 -0400

    [TextFields] Objc example.

--- components/TextFields/examples/TextFieldExample.m
+++ components/TextFields/examples/TextFieldExample.m
@@ -18,6 +18,7 @@
 #import "TextFieldExample.h"
 
 #import "MaterialTextFields.h"
+#import "MaterialAppBar.h"
 
 @interface TextFieldExample ()
 
@@ -34,8 +35,11 @@
 
   self.title = @"Material Text Fields";
 
+  [self setupScrollView];
+
   MDCTextField *textFieldDefaultCharMax = [[MDCTextField alloc] init];
   [self.scrollView addSubview:textFieldDefaultCharMax];
+  textFieldDefaultCharMax.translatesAutoresizingMaskIntoConstraints = NO;
 
   textFieldDefaultCharMax.placeholder = @"Enter up to 50 characters";
   textFieldDefaultCharMax.delegate = self;
@@ -43,18 +47,41 @@
   // Second the controller is created to manage the text field
   MDCTextInputController *textFieldControllerDefaultCharMax = [[MDCTextInputController alloc] initWithTextInput: textFieldDefaultCharMax];
   textFieldControllerDefaultCharMax.characterCountMax = 50;
+
+  MDCTextField *textFieldFloating = [[MDCTextField alloc] init];
+  [self.scrollView addSubview:textFieldFloating];
+  textFieldFloating.translatesAutoresizingMaskIntoConstraints = NO;
+
+  textFieldFloating.placeholder = @"Full Name";
+  textFieldFloating.delegate = self;
+  textFieldFloating.clearButtonMode = UITextFieldViewModeUnlessEditing;
+
+  MDCTextInputController *textFieldControllerFloating = [[MDCTextInputController alloc] initWithTextInput:textFieldFloating];
+
+  textFieldControllerFloating.presentationStyle = MDCTextInputPresentationStyleFloatingPlaceholder;
+
+}
+
+- (void)setupScrollView {
+  self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectZero];
+  [self.view addSubview:self.scrollView];
+  self.scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+
+  [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[scrollView]|" options:0 metrics:nil views:@{@"scrollView": self.scrollView}]];
+  [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[scrollView]|" options:0 metrics:nil views:@{@"scrollView": self.scrollView}]];
+
+  CGFloat marginOffset = 16;
+  UIEdgeInsets margins = UIEdgeInsetsMake(0, marginOffset, 0, marginOffset);
+  self.scrollView.layoutMargins = margins;
+
 }
 
 + (NSArray *)catalogBreadcrumbs {
-  return @[ @"Text Field", @"Typical Use" ];
-}
-
-+ (BOOL)catalogIsPrimaryDemo {
-  return YES;
+  return @[ @"Text Field", @"Typical Use (Objective C)" ];
 }
 
 + (NSString *)catalogDescription {
-  return @"Text fields allow users to input text into your app. They are a direct connection to your users' thoughts and intentions via on-screen, or physical, keyboard. The Material Design Text Fields take the familiar element to a new level by adding useful animations, character counts, helper text and error states.";
+  return @"The Material Design Text Fields take the familiar element to a new level by adding useful animations, character counts, helper text and error states.";
 }
 
 @end
