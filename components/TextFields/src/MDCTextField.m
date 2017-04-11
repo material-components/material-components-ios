62eaa8321372ea7369ecfeb6fbb962fce7011ee6
Will Larche <larche@google.com>
2017-04-10 20:48:23 -0400

    [TextFields] Renaming because PR.

--- components/TextFields/src/MDCTextField.m
+++ components/TextFields/src/MDCTextField.m
@@ -31,7 +31,7 @@ static const CGFloat MDCTextInputTextRectRightPaddingCorrection = -4.f;
 static const CGFloat MDCTextInputEditingRectRightPaddingCorrection = -2.f;
 static const CGFloat MDCTextInputEditingRectClearPaddingCorrection = -8.f;
 
-const CGFloat MDCClearButtonImageSquareSize = 24.f;
+const CGFloat MDCClearButtonImageSquareWidthHeight = 24.f;
 static const CGFloat MDCClearButtonImageSystemSquareSize = 14.0f;
 
 static inline CGFloat MDCRound(CGFloat value) {
