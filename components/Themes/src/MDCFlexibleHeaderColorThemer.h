a977a7e5237df181849ab1c2bd31f63a225b1c0d
Junius Gunaratne <junius@google.com>
2017-05-16 10:37:52 -0400

    Adding component themers

---
+++ components/Themes/src/MDCFlexibleHeaderColorThemer.h
@@ -0,0 +1,48 @@
+/*
+ Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+#import "MaterialFlexibleHeader.h"
+#import "MDCColorScheme.h"
+
+/**
+ Used to apply a color scheme to theme MDCFlexibleHeaderView.
+ */
+@interface MDCFlexibleHeaderThemer : NSObject
+
+/**
+ Applies a color scheme to theme a MDCFlexibleHeaderView.
+ 
+ @param colorScheme The color scheme to apply to MDCFlexibleHeaderView.
+ @param flexibleHeaderView A MDCFlexibleHeaderView instance to apply a color scheme. Use a
+     UIAppearance proxy to apply a color scheme to all instances of MDCFlexibleHeaderView.
+ */
++ (void)applyColorScheme:(MDCColorScheme *)colorScheme
+    toFlexibleHeaderView:(MDCFlexibleHeaderView *)flexibleHeaderView;
+
+/**
+ Applies a color scheme to theme a MDCFlexibleHeaderViewController.
+ 
+ @param colorScheme The color scheme to apply to MDCFlexibleHeaderView.
+ @param flexibleHeaderController A MDCFlexibleHeaderViewController instance to apply a color scheme.
+     Use a UIAppearance proxy to apply a color scheme to all instances of
+     MDCFlexibleHeaderViewController.
+ */
++ (void)applyColorScheme:(MDCColorScheme *)colorScheme
+    toMDCFlexibleHeaderController:(MDCFlexibleHeaderViewController *)flexibleHeaderController;
+
+@end
