81719e104eef5ecfbffef6ff1b271d4e85e76fd1
Junius Gunaratne <junius@google.com>
2017-06-06 09:48:20 -0400

    Override navigation bar color theme for imagery example.

--- components/AppBar/examples/AppBarImageryExample.m
+++ components/AppBar/examples/AppBarImageryExample.m
@@ -45,9 +45,10 @@
 
   // We want navigation bar + status bar tint color to be white, so we set tint color here and
   // implement -preferredStatusBarStyle.
-  self.appBar.headerViewController.headerView.tintColor = [UIColor whiteColor];
+  self.appBar.headerViewController.headerView.tintColor = [UIColor clearColor];
   self.appBar.navigationBar.titleTextAttributes =
       @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+  self.appBar.navigationBar.backgroundColor = [UIColor clearColor];
 
   // Allow the header to show more of the image.
   self.appBar.headerViewController.headerView.maximumHeight = 200;
