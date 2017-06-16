224f66b206b48d8db7d75e5f46f22a2e563b4224
Justin Shephard <shepj@google.com>
2017-06-15 20:01:37 -0400

    [ActivityIndicator] - Add ticket to TODO

--- components/ActivityIndicator/src/MDCActivityIndicator.m
+++ components/ActivityIndicator/src/MDCActivityIndicator.m
@@ -407,7 +407,8 @@ static const CGFloat kSingleCycleRotation =
   if (_cycleColors.count > 0) {
     [self setStrokeColor:_cycleColors[_currentColorCount]];
   }
-  // TODO: REMOVE HACK BELOW THAT PROTECTS AGAINST EMPTY cycleColors ARRAY
+  // TODO(https://github.com/material-components/material-components-ios/issues/1508): REMOVE HACK
+  // BELOW THAT PROTECTS AGAINST EMPTY cycleColors ARRAY
   else {
     [self setStrokeColor:[[UIColor alloc] initWithRed:0.129f green:0.588f blue:0.953f alpha:1]];
   }
