40a926d912d9774cc6a156418398d9e54a25843a
Will Larche <larche@google.com>
2017-06-19 00:30:51 -0400

    [TextFields] ErrorText tests.

--- components/TextFields/tests/unit/TextFieldControllerTests.swift
+++ components/TextFields/tests/unit/TextFieldControllerTests.swift
@@ -77,12 +77,14 @@ class TextFieldControllerTests: XCTestCase {
     textField.leadingUnderlineLabel.textColor = .green
     XCTAssertEqual(.green, textField.leadingUnderlineLabel.textColor)
     XCTAssertEqual(altLeading, textField.leadingUnderlineLabel.text)
+    XCTAssertNil(controller.errorText)
 
     // Setting error text should change the color and content of the leading underline label
     let error = "Error Test"
     controller.setErrorText(error, errorAccessibilityValue: nil)
     XCTAssertNotEqual(altLeading, textField.leadingUnderlineLabel.text)
     XCTAssertEqual(error, textField.leadingUnderlineLabel.text)
+    XCTAssertEqual(error, controller.errorText)
 
     // Setting an error should change the leading label's text color.
     XCTAssertNotEqual(.green, textField.leadingUnderlineLabel.textColor)
