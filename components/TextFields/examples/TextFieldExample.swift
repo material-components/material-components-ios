1e14d4109178956486863dec607996100c91795e
Martin Petrov <martin@martinpetrov.com>
2017-07-10 19:46:22 -0400

    Explicitly annotating some known ObjC methods. (#1617)
    This improves Swift4 compatibility, see:
    https://help.apple.com/xcode/mac/current/#/deve838b19a1

--- components/TextFields/examples/TextFieldExample.swift
+++ components/TextFields/examples/TextFieldExample.swift
@@ -209,11 +209,11 @@ final class TextFieldSwiftExample: UIViewController {
 
   // MARK: - Actions
 
-  func tapDidTouch(sender: Any) {
+  @objc func tapDidTouch(sender: Any) {
     self.view.endEditing(true)
   }
 
-  func buttonDidTouch(sender: Any) {
+  @objc func buttonDidTouch(sender: Any) {
     let alert = UIAlertController(title: "Floating Enabled",
                                   message: nil,
                                   preferredStyle: .actionSheet)
@@ -296,7 +296,7 @@ extension TextFieldSwiftExample {
       object: nil)
   }
 
-  func keyboardWillShow(notif: Notification) {
+  @objc func keyboardWillShow(notif: Notification) {
     guard let frame = notif.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? CGRect else {
       return
     }
@@ -306,7 +306,7 @@ extension TextFieldSwiftExample {
                                            right: 0.0)
   }
 
-  func keyboardWillHide(notif: Notification) {
+  @objc func keyboardWillHide(notif: Notification) {
     scrollView.contentInset = UIEdgeInsets()
   }
 }
