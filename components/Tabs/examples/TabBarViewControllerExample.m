2b7c865e444c5a963bdb48429df785eb620ed5b2
Adrian Secord <ajsecord@google.com>
2017-08-10 15:05:11 -0400

    Added missing license stanzas.

--- components/Tabs/examples/TabBarViewControllerExample.m
+++ components/Tabs/examples/TabBarViewControllerExample.m
@@ -1,6 +1,5 @@
 /*
- Copyright 2017-present the Material Components for iOS authors. All Rights
- Reserved.
+ Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
