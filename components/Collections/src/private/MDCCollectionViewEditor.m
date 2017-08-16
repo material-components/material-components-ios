cbe93e6fcb97f9dc6dc99c76d7c397fead53d4b0
Gauthier Ambard <gambard@chromium.org>
2017-08-16 13:29:06 +0200

    [Collections] Limit the drag needed to dismiss an item
    When using the swipe-to-dimiss gesture on large screens, the distance on which the user has to move the item can be big.
    This commit limits the distance by using the width of the item instead of the width of the collection.
    
    Close #1780

--- components/Collections/src/private/MDCCollectionViewEditor.m
+++ components/Collections/src/private/MDCCollectionViewEditor.m
@@ -767,7 +767,11 @@ typedef NS_ENUM(NSInteger, MDCAutoscrollPanningDirection) {
 
 // The distance an item must be panned before it is dismissed. Currently half of the bounds width.
 - (CGFloat)distanceThresholdForDismissal {
-  return _collectionView.bounds.size.width / 2;
+  if (_cellSnapshot) {
+    return _cellSnapshot.bounds.size.width / 2;
+  } else {
+    return _collectionView.bounds.size.width / 2;
+  }
 }
 
 - (CGFloat)dismissalAlphaForTranslationX:(CGFloat)translationX {
