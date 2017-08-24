2d4a3ea302d36fc2f79998456e0fcf009ba020d1
Will Larche <larche@google.com>
2017-08-24 15:41:01 -0400

    [TextFields] Adding text insets mode to kitchen sync example.

--- components/TextFields/examples/TextFieldKitchenSinkExample.swift
+++ components/TextFields/examples/TextFieldKitchenSinkExample.swift
@@ -87,6 +87,7 @@ final class TextFieldKitchenSinkSwiftExample: UIViewController {
   let unstyledTextField = MDCTextField()
   let unstyledMultilineTextField = MDCMultilineTextField()
 
+  lazy var textInsetsModeButton: MDCButton = self.setupButton()
   lazy var characterModeButton: MDCButton = self.setupButton()
   lazy var clearModeButton: MDCButton = self.setupButton()
   lazy var underlineButton: MDCButton = self.setupButton()
