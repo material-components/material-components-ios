195e9e2ff53c0c6fcb7d95f0b4571ce22827376f
Cody Weaver <codyleeweaver@gmail.com>
2017-09-01 09:39:10 -0700

    Fix issue where past branches were still there

--- components/AnimationTiming/examples/AnimationTimingExample.swift
+++ components/AnimationTiming/examples/AnimationTimingExample.swift
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  
  Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
@@ -21,20 +20,6 @@ import UIKit
 import MaterialComponents.MaterialAnimationTiming
 
 struct Constants {
-=======
-//
-//  ViewController.swift
-//  Test
-//
-//  Created by Cody Weaver on 8/23/17.
-//  Copyright © 2017 BlueBerry. All rights reserved.
-//
-
-import UIKit
-import MaterialComponents
-
-struct k {
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
    struct AnimationTime {
       static let interval: Double = 1.0
       static let delay: Double = 0.5
@@ -45,10 +30,6 @@ struct k {
       static let textOffset: CGFloat = 16.0
       static let circleSize: CGSize = CGSize(width: 48.0, height: 48.0)
    }
-<<<<<<< HEAD
-=======
-   
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
 }
 
 class AnimationTimingExample: UIViewController {
@@ -58,29 +39,13 @@ class AnimationTimingExample: UIViewController {
    fileprivate let materialEaseInOutView: UIView = UIView()
    fileprivate let materialEaseOutView: UIView = UIView()
    fileprivate let materialEaseInView: UIView = UIView()
-<<<<<<< HEAD
-   let overflowView: UIView = UIView()
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-=======
 
-   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
       
       view.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
       title = "Animation Timing"
       setupExampleViews()
-   }
-   
-<<<<<<< HEAD
-   override func viewWillLayoutSubviews() {
-      super.viewWillLayoutSubviews()
-      let size = view.frame.size
-      overflowView.frame = CGRect(x: 0, y: size.height, width: size.width, height: 200)
    }
    
    
@@ -89,19 +54,13 @@ class AnimationTimingExample: UIViewController {
       let timeInterval: TimeInterval = 2 * (Constants.AnimationTime.interval + Constants.AnimationTime.delay)
       var _: Timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(self.playAnimations), userInfo: nil, repeats: true)
       playAnimations()
-=======
-   override func viewDidAppear(_ animated: Bool) {
-      super.viewDidAppear(animated)
-      let timeInterval: TimeInterval = 2 * (k.AnimationTime.interval + k.AnimationTime.delay)
-      var _: Timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(self.playAnimations), userInfo: nil, repeats: true)
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
+
    }
    
    func playAnimations() {
       let linearCurve: CAMediaTimingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
       applyAnimation(toView: linearView, withTimingFunction: linearCurve)
       
-<<<<<<< HEAD
       if let materialEaseInOut = CAMediaTimingFunction.mdc_function(withType: .easeInOut) {
          applyAnimation(toView: materialEaseInOutView, withTimingFunction: materialEaseInOut)
       } else {
@@ -119,32 +78,16 @@ class AnimationTimingExample: UIViewController {
       } else {
          materialEaseInView.removeFromSuperview()
       }
-=======
-      let materialEaseInOut: CAMediaTimingFunction = CAMediaTimingFunction.mdc_function(withType: .easeInOut)
-      applyAnimation(toView: materialEaseInOutView, withTimingFunction: materialEaseInOut)
-      
-      let materialEaseOut: CAMediaTimingFunction = CAMediaTimingFunction.mdc_function(withType: .easeOut)
-      applyAnimation(toView: materialEaseOutView, withTimingFunction: materialEaseOut)
-      
-      let materialEaseIn: CAMediaTimingFunction = CAMediaTimingFunction.mdc_function(withType: .easeIn)
-      applyAnimation(toView: materialEaseInView, withTimingFunction: materialEaseIn)
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
+
    }
    
    func applyAnimation(toView view: UIView, withTimingFunction timingFunction : CAMediaTimingFunction) {
       let animWidth: CGFloat = self.view.frame.size.width - view.frame.size.width - 32.0
       let transform: CGAffineTransform = CGAffineTransform.init(translationX: animWidth, y: 0)
-<<<<<<< HEAD
       UIView.mdc_animate(with: timingFunction, duration: Constants.AnimationTime.interval, delay: Constants.AnimationTime.delay, options: [], animations: {
          view.transform = transform
       }, completion: { Bool in
          UIView.mdc_animate(with: timingFunction, duration: Constants.AnimationTime.interval, delay: Constants.AnimationTime.delay, options: [], animations: {
-=======
-      UIView.mdc_animate(with: timingFunction, duration: k.AnimationTime.interval, delay: k.AnimationTime.delay, options: [], animations: {
-         view.transform = transform
-      }, completion: { Bool in
-         UIView.mdc_animate(with: timingFunction, duration: k.AnimationTime.interval, delay: k.AnimationTime.delay, options: [], animations: {
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
             view.transform = CGAffineTransform.identity
          }, completion: nil)
       })
@@ -176,7 +119,6 @@ extension AnimationTimingExample {
       
       let lineSpace: CGFloat = (view.frame.size.height - 50.0) / 4.0
       let linearLabel: UILabel = curveLabel("Linear")
-<<<<<<< HEAD
       linearLabel.frame = CGRect(x: Constants.Sizes.leftGutter, y: Constants.Sizes.topMargin, width: linearLabel.frame.size.width, height: linearLabel.frame.size.height)
       scrollView.addSubview(linearLabel)
       
@@ -214,45 +156,7 @@ extension AnimationTimingExample {
       materialEaseInView.frame = materialEaseInViewFrame
       materialEaseInView.backgroundColor = defaultColors[3]
       materialEaseInView.layer.cornerRadius = Constants.Sizes.circleSize.width / 2.0
-=======
-      linearLabel.frame = CGRect(x: k.Sizes.leftGutter, y: k.Sizes.topMargin, width: linearLabel.frame.size.width, height: linearLabel.frame.size.height)
-      scrollView.addSubview(linearLabel)
-      
-      let linearViewFrame: CGRect = CGRect(x: k.Sizes.leftGutter, y: k.Sizes.leftGutter + k.Sizes.topMargin, width: k.Sizes.circleSize.width, height: k.Sizes.circleSize.height)
-      linearView.frame = linearViewFrame
-      linearView.backgroundColor = defaultColors[0]
-      linearView.layer.cornerRadius = k.Sizes.circleSize.width / 2.0
-      scrollView.addSubview(linearView)
-      
-      let materialEaseInOutLabel: UILabel = curveLabel("MDCAnimationTimingFunctionEaseInOut")
-      materialEaseInOutLabel.frame = CGRect(x: k.Sizes.leftGutter, y: lineSpace, width: materialEaseInOutLabel.frame.size.width, height: materialEaseInOutLabel.frame.size.height)
-      scrollView.addSubview(materialEaseInOutLabel)
-      
-      let materialEaseInOutViewFrame: CGRect = CGRect(x: k.Sizes.leftGutter, y: lineSpace + k.Sizes.textOffset, width: k.Sizes.circleSize.width, height: k.Sizes.circleSize.height)
-      materialEaseInOutView.frame = materialEaseInOutViewFrame
-      materialEaseInOutView.backgroundColor = defaultColors[1]
-      materialEaseInOutView.layer.cornerRadius = k.Sizes.circleSize.width / 2.0
-      scrollView.addSubview(materialEaseInOutView)
-      
-      let materialEaseOutLabel: UILabel = curveLabel("MDCAnimationTimingFunctionEaseOut")
-      materialEaseOutLabel.frame = CGRect(x: k.Sizes.leftGutter, y: lineSpace * 2.0, width: materialEaseOutLabel.frame.size.width, height: materialEaseOutLabel.frame.size.height)
-      scrollView.addSubview(materialEaseOutLabel)
-      
-      let materialEaseOutViewFrame: CGRect = CGRect(x: k.Sizes.leftGutter, y: lineSpace * 2.0 + k.Sizes.textOffset, width: k.Sizes.circleSize.width, height: k.Sizes.circleSize.height)
-      materialEaseOutView.frame = materialEaseOutViewFrame
-      materialEaseOutView.backgroundColor = defaultColors[2]
-      materialEaseOutView.layer.cornerRadius = k.Sizes.circleSize.width / 2.0
-      scrollView.addSubview(materialEaseOutView)
-      
-      let materialEaseInLabel: UILabel = curveLabel("MDCAnimationTimingFunctionEaseIn")
-      materialEaseInLabel.frame = CGRect(x: k.Sizes.leftGutter, y: lineSpace * 3.0, width: materialEaseInLabel.frame.size.width, height: materialEaseInLabel.frame.size.height)
-      scrollView.addSubview(materialEaseInLabel)
-      
-      let materialEaseInViewFrame: CGRect = CGRect(x: k.Sizes.leftGutter, y: lineSpace * 3.0 + k.Sizes.textOffset, width: k.Sizes.circleSize.width, height: k.Sizes.circleSize.height)
-      materialEaseInView.frame = materialEaseInViewFrame
-      materialEaseInView.backgroundColor = defaultColors[3]
-      materialEaseInView.layer.cornerRadius = k.Sizes.circleSize.width / 2.0
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
+
       scrollView.addSubview(materialEaseInView)
    }
    
@@ -260,14 +164,8 @@ extension AnimationTimingExample {
       return ["Animation Timing", "Animation Timing (Swift)"]
    }
    
-<<<<<<< HEAD
    class func catalogIsPrimaryDemo() -> Bool {
       return false
    }
    
 }
-=======
-}
-
-
->>>>>>> 15b64e46b5687bb9ee09dde4355bc250b7078c99
