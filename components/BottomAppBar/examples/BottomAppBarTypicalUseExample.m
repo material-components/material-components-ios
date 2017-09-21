25c264dea352cc6938a6d7aaf01079497670aefa
Junius Gunaratne <junius@google.com>
2017-09-21 09:40:56 -0400

    Update description in example

--- components/BottomAppBar/examples/BottomAppBarTypicalUseExample.m
+++ components/BottomAppBar/examples/BottomAppBarTypicalUseExample.m
@@ -88,7 +88,8 @@
 }
 
 + (NSString *)catalogDescription {
-  return @"The bottom app bar is a bar docked at the bottom of the screen.";
+  return @"The bottom app bar is a bar docked at the bottom of the screen that has a floating "
+         @"action button and can provide navigation.";
 }
 
 + (BOOL)catalogIsPrimaryDemo {
