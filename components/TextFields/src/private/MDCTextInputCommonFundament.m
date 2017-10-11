2122853523d1561bf7189a120963e24f4b48c2d2
Will Larche <larche@google.com>
2017-10-11 18:07:36 -0400

    [TextFields] Removes ambiguous layout.

--- components/TextFields/src/private/MDCTextInputCommonFundament.m
+++ components/TextFields/src/private/MDCTextInputCommonFundament.m
@@ -896,6 +896,9 @@ static inline UIColor *MDCTextInputUnderlineColor() {
 
   self.leadingUnderlineLeading.constant = textInsets.left;
   self.trailingUnderlineTrailing.constant = -1 * textInsets.right;
+
+  [self.leadingUnderlineLabel setContentHuggingPriority:UILayoutPriorityDefaultLow - 1
+                                                forAxis:UILayoutConstraintAxisHorizontal];
 }
 
 #pragma mark - Text Input Events
