9cb8ec36dfeca9ff94385b2c995b0198f0ecee9b
Will Larche <larche@google.com>
2017-10-13 10:16:31 -0400

    [TextFields] Some alphabetizing. (#2166)

--- components/TextFields/src/MDCTextInputControllerLegacyFullWidth.m
+++ components/TextFields/src/MDCTextInputControllerLegacyFullWidth.m
@@ -448,22 +448,6 @@ static UIFont *_trailingUnderlineLabelFontDefault;
   }
 }
 
-- (UIRectCorner)roundedCorners {
-  return 0;
-}
-
-- (void)setRoundedCorners:(__unused UIRectCorner)roundedCorners {
-  // Not implemented. There are no corners to round.
-}
-
-+ (UIRectCorner)roundedCornersDefault {
-  return 0;
-}
-
-+ (void)setRoundedCornersDefault:(__unused UIRectCorner)roundedCornersDefault {
-  // Not implemented. There are no corners to round.
-}
-
 - (void)setDisabledColor:(__unused UIColor *)disabledColor {
   [self updateUnderline];
 }
@@ -663,6 +647,22 @@ static UIFont *_trailingUnderlineLabelFontDefault;
 
 - (void)setPreviousPlaceholderColor:(UIColor *)previousPlaceholderColor {
   _previousPlaceholderColor = previousPlaceholderColor;
+}
+
+- (UIRectCorner)roundedCorners {
+  return 0;
+}
+
+- (void)setRoundedCorners:(__unused UIRectCorner)roundedCorners {
+  // Not implemented. There are no corners to round.
+}
+
++ (UIRectCorner)roundedCornersDefault {
+  return 0;
+}
+
++ (void)setRoundedCornersDefault:(__unused UIRectCorner)roundedCornersDefault {
+  // Not implemented. There are no corners to round.
 }
 
 - (void)setTextInput:(UIView<MDCTextInput> *)textInput {
