9bc0125a91f24a4c788fbd9808bb67b9ab7a1141
Sam Morrison <samnm@google.com>
2017-10-17 11:49:39 -0400

    Expand snackbar to cover safeAreaInsets

--- components/Snackbar/src/MDCSnackbarMessageView.m
+++ components/Snackbar/src/MDCSnackbarMessageView.m
@@ -211,7 +211,7 @@ static const CGFloat kButtonInkRadius = 64.0f;
 
 - (void)setSnackbarMessageViewBackgroundColor:(UIColor *)snackbarMessageViewBackgroundColor {
   _snackbarMessageViewBackgroundColor = snackbarMessageViewBackgroundColor;
-  _containerView.backgroundColor = snackbarMessageViewBackgroundColor;
+  self.backgroundColor = snackbarMessageViewBackgroundColor;
 }
 
 - (void)setSnackbarShadowColor:(UIColor *)snackbarMessageViewShadowColor {
@@ -240,12 +240,14 @@ static const CGFloat kButtonInkRadius = 64.0f;
     _message = message;
     _dismissalHandler = [handler copy];
 
-    self.backgroundColor = [UIColor clearColor];
+    self.backgroundColor = _snackbarMessageViewBackgroundColor;
     self.layer.cornerRadius = kCornerRadius;
     self.layer.shadowColor = _snackbarMessageViewShadowColor.CGColor;
     self.layer.shadowOpacity = kShadowAlpha;
     self.layer.shadowOffset = kShadowOffset;
     self.layer.shadowRadius = kShadowSpread;
+
+    _anchoredToScreenEdge = YES;
 
     // Borders are drawn inside of the bounds of a layer. Because our border is translucent, we need
     // to have a view with transparent background and border only (@c self). Inside will be a
@@ -254,7 +256,7 @@ static const CGFloat kButtonInkRadius = 64.0f;
     [self addSubview:_containerView];
 
     [_containerView setTranslatesAutoresizingMaskIntoConstraints:NO];
-    _containerView.backgroundColor = _snackbarMessageViewBackgroundColor;
+    _containerView.backgroundColor = [UIColor clearColor];
     _containerView.layer.cornerRadius = kCornerRadius;
     _containerView.layer.masksToBounds = YES;
 
@@ -436,6 +438,17 @@ static const CGFloat kButtonInkRadius = 64.0f;
 
 #pragma mark - Constraints and layout
 
+- (void)setAnchoredToScreenEdge:(BOOL)anchoredToScreenEdge {
+  _anchoredToScreenEdge = anchoredToScreenEdge;
+  [self invalidateIntrinsicContentSize];
+
+  if (self.viewConstraints) {
+    [self removeConstraints:self.viewConstraints];
+    self.viewConstraints = nil;
+    [self updateConstraints];
+  }
+}
+
 - (void)updateConstraints {
   [super updateConstraints];
 
@@ -466,6 +479,7 @@ static const CGFloat kButtonInkRadius = 64.0f;
     @"kTitleImagePadding" : @(kTitleImagePadding),
     @"kTopMargin" : @(kContentMargin.top),
     @"kTitleButtonPadding" : @(kTitleButtonPadding),
+    @"kContentSafeBottomInset" : @(kBorderWidth +  [self contentSafeBottomInset]),
   };
   NSDictionary *views = @{
     @"container" : self.containerView,
@@ -486,7 +500,7 @@ static const CGFloat kButtonInkRadius = 64.0f;
                                                                              views:views]];
 
   // Pin the top and bottom edges of the container view to the snackbar.
-  formatString = @"V:|-(==kBorderMargin)-[container]-(==kBorderMargin)-|";
+  formatString = @"V:|-(==kBorderMargin)-[container]-(==kContentSafeBottomInset)-|";
   [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:formatString
                                                                            options:0
                                                                            metrics:metrics
@@ -725,7 +739,23 @@ static const CGFloat kButtonInkRadius = 64.0f;
   // Make sure that the height of the image and text is larger than the minimum height;
   height = MAX(kMinimumHeight, height);
 
+  height = MAX(kMinimumHeight, height) + [self contentSafeBottomInset];
   return CGSizeMake(UIViewNoIntrinsicMetric, height);
+}
+
+- (CGFloat)contentSafeBottomInset {
+  // If a bottom offset has been set to raise the HUD, e.g. above a tab bar, we should ignore
+  // any safeAreaInsets, since it is no longer 'anchored' to the bottom of the screen. This is set
+  // by the GOOHUDOverlayView whenever the bottomOffset is non-zero.
+  if (!self.anchoredToScreenEdge) {
+    return 0;
+  }
+#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
+  if (@available(iOS 11.0, *)) {
+    return self.window.safeAreaInsets.bottom;
+  }
+#endif
+  return 0;
 }
 
 #pragma mark - Event Handlers
