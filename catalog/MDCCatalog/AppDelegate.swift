80079b704f700331886f2dc4584dd7ecd3ff08f3
Will Larche <larche@google.com>
2017-11-12 19:21:55 -0500

    [Catalog] New primary color in scheme.

--- catalog/MDCCatalog/AppDelegate.swift
+++ catalog/MDCCatalog/AppDelegate.swift
@@ -58,7 +58,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     self.window?.rootViewController = navigationController
     self.window?.makeKeyAndVisible()
 
-    colorScheme = MDCBasicColorScheme(primaryColor: UIColor.init(white: 0.2, alpha: 1),
+    colorScheme = MDCBasicColorScheme(primaryColor: UIColor(red: 33 / 255.0,
+                                                            green: 33 / 255.0,
+                                                            blue: 33 / 255.0,
+                                                            alpha: 1),
                                       primaryLightColor: .init(white: 0.7, alpha: 1),
                                       primaryDarkColor: .init(white: 0, alpha: 1))
 
