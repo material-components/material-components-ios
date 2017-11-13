b10fc630bd6a6e25f2ea2d128b2ab4aa65abd8c4
Adrian Secord <ajsecord@google.com>
2017-11-13 11:34:52 -0500

    Change block comments on MDCLibraryInfo.h

--- components/LibraryInfo/src/MDCLibraryInfo.h
+++ components/LibraryInfo/src/MDCLibraryInfo.h
@@ -14,7 +14,9 @@
  limitations under the License.
  */
 
-// Information about the Material Components library.
+/**
+ Information about the Material Components library.
+ */
 @interface MDCLibraryInfo: NSObject
 
 /**
