146c57c8dc1f43317a5931b10b77c47706c2de7b
Robert Moore <rsmoore@google.com>
2017-11-15 22:13:22 -0500

    Adding example themer

---
+++ components/Buttons/src/ColorThemer/ExampleFloatingButtonThemer.m
@@ -0,0 +1,50 @@
+//
+//  ExampleFloatingButtonThemer.m
+//  MaterialComponents
+//
+//  Created by Rob Moore on 11/15/17.
+//
+
+#import "ExampleFloatingButtonThemer.h"
+#import "MDCButtonColorThemer.h"
+
+@implementation ExampleFloatingButtonThemer
+
++ (void)applyToButton:(nonnull MDCFloatingButton *)button
+      withColorScheme:(nullable id<MDCColorScheme>)colorScheme {
+  if (colorScheme) {
+    [MDCButtonColorThemer applyColorScheme:colorScheme toButton:button];
+  }
+  UIEdgeInsets compactInsets = UIEdgeInsetsZero;
+  UIEdgeInsets expandedLeadingInsets = UIEdgeInsetsMake(4, 6, 4, 8);
+  UIEdgeInsets expandedTrailingInsets = UIEdgeInsetsMake(4, 8, 4, 6);
+
+  UIEdgeInsets hitAreaCompactDefault = UIEdgeInsetsMake(-8, -8, -8, -8);
+  UIEdgeInsets hitAreaCompactMini = UIEdgeInsetsMake(-16, -16, -16, -16);
+
+  CGSize sizeCompactDefault = CGSizeMake(50, 50);
+  CGSize sizeExtendedMinimum = CGSizeMake(32, 72);
+
+  [button setContentEdgeInsets:compactInsets forShape:MDCFloatingButtonShapeDefault];
+  [button setContentEdgeInsets:compactInsets forShape:MDCFloatingButtonShapeMini];
+  [button setContentEdgeInsets:compactInsets forShape:MDCFloatingButtonShapeLargeIcon];
+  [button setContentEdgeInsets:expandedLeadingInsets
+                      forShape:MDCFloatingButtonShapeExtendedLeadingIcon];
+  [button setContentEdgeInsets:expandedTrailingInsets
+                      forShape:MDCFloatingButtonShapeExtendedTrailingIcon];
+
+  [button setHitAreaInsets:hitAreaCompactDefault forShape:MDCFloatingButtonShapeDefault];
+  [button setHitAreaInsets:hitAreaCompactDefault forShape:MDCFloatingButtonShapeLargeIcon];
+  [button setHitAreaInsets:hitAreaCompactMini forShape:MDCFloatingButtonShapeMini];
+
+  [button setMinimumSize:sizeCompactDefault forShape:MDCFloatingButtonShapeDefault];
+  [button setMaximumSize:sizeCompactDefault forShape:MDCFloatingButtonShapeDefault];
+  [button setMinimumSize:sizeCompactDefault forShape:MDCFloatingButtonShapeLargeIcon];
+  [button setMaximumSize:sizeCompactDefault forShape:MDCFloatingButtonShapeLargeIcon];
+  [button setMinimumSize:sizeExtendedMinimum forShape:MDCFloatingButtonShapeExtendedLeadingIcon];
+  [button setMinimumSize:sizeExtendedMinimum forShape:MDCFloatingButtonShapeExtendedTrailingIcon];
+
+  [button setImageTitlePadding:4];
+}
+
+@end
