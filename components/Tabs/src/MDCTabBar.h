5ff5976f630cb5de7c7468d8ebf95825f60229cf
Brian Moore <brianjmoore@google.com>
2017-11-20 10:04:29 -0500

    [Tabs] Add bar padding property

--- components/Tabs/src/MDCTabBar.h
+++ components/Tabs/src/MDCTabBar.h
@@ -98,6 +98,12 @@ IB_DESIGNABLE
     UI_APPEARANCE_SELECTOR;
 
 /**
+ Padding on the left and right of the tab bar's content for scrollable tabs.
+ Default depends on the horizontal size class.
+ */
+@property(nonatomic) IBInspectable CGFloat horizontalPadding UI_APPEARANCE_SELECTOR;
+
+/**
  Padding on the left and right of each item's content (title or images) for non-justified alignment.
  Default depends on the horizontal size class.
  */
