de77befeae52d11f94dfe9041180fb57f90795ca
Robert Moore <rsmoore@google.com>
2017-11-29 14:48:43 -0500

    [Buttons] Add extended FAB layout code (#2528)
    Creating (internal for now) the required properties and layout code to support
    the extended FAB. Unit tests to confirm the behavior and some minor
    adjustments to existing values like default contentEdgeInsets values.
    
    Implements #2495

--- components/Buttons/src/MDCFloatingButton.m
+++ components/Buttons/src/MDCFloatingButton.m
@@ -19,9 +19,45 @@
 #import "MaterialShadowElevations.h"
 #import "private/MDCButton+Subclassing.h"
 
+#import <MDFInternationalization/MDFInternationalization.h>
+
 static const CGFloat MDCFloatingButtonDefaultDimension = 56.0f;
 static const CGFloat MDCFloatingButtonMiniDimension = 40.0f;
+static const CGFloat MDCFloatingButtonDefaultImageTitleSpacing = 8;
+static const UIEdgeInsets internalLayoutSpacingInsets = (UIEdgeInsets){0, 16, 0, 24};
+
 static NSString *const MDCFloatingButtonShapeKey = @"MDCFloatingButtonShapeKey";
+static NSString *const MDCFloatingButtonModeKey = @"MDCFloatingButtonModeKey";
+static NSString *const MDCFloatingButtonImageLocationKey = @"MDCFloatingButtonImageLocationKey";
+static NSString *const MDCFloatingButtonImageTitleSpacingKey = @"MDCFloatingButtonImageTitleSpacingKey";
+
+// TODO(rsmoore): All properties within here will be made public in a future PR.
+@interface MDCFloatingButton ()
+
+/**
+ The mode of the floating button can either be .normal (a circle) or .expanded (a pill-shaped
+ rounded rectangle).
+
+ The default value is @c .normal .
+ */
+@property(nonatomic, assign) MDCFloatingButtonMode mode UI_APPEARANCE_SELECTOR;
+
+/**
+ The location of the image relative to the title when the floating button is in @c expanded mode.
+
+ The default value is @c .leading .
+ */
+@property(nonatomic, assign) MDCFloatingButtonImageLocation imageLocation UI_APPEARANCE_SELECTOR;
+
+/**
+ The horizontal spacing in points between the @c imageView and @c titleLabel when the button is in
+ @c .expanded mode. If set to a negative value, the image and title may overlap.
+
+ The default value is 8.
+ */
+@property(nonatomic, assign) CGFloat imageTitleSpacing UI_APPEARANCE_SELECTOR;
+
+@end
 
 @implementation MDCFloatingButton {
   MDCFloatingButtonShape _shape;
@@ -62,6 +98,7 @@ static NSString *const MDCFloatingButtonShapeKey = @"MDCFloatingButtonShapeKey";
     // Set contentEdgeInsets again to ensure the defaults are for the correct shape.
     self.contentEdgeInsets = [self defaultContentEdgeInsets];
     self.hitAreaInsets = [self defaultHitAreaInsets];
+    [self commonMDCFloatingButtonInit];
   }
   return self;
 }
@@ -71,10 +108,21 @@ static NSString *const MDCFloatingButtonShapeKey = @"MDCFloatingButtonShapeKey";
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
   self = [super initWithCoder:aDecoder];
   if (self) {
+    [self commonMDCFloatingButtonInit];
     _shape = [aDecoder decodeIntegerForKey:MDCFloatingButtonShapeKey];
     // Required to migrate any previously-archived FloatingButtons from .largeIcon shape value
     if (@(_shape).integerValue >= 2) {
       _shape = MDCFloatingButtonShapeDefault;
+    }
+    if ([aDecoder containsValueForKey:MDCFloatingButtonModeKey]) {
+      _mode = [aDecoder decodeIntegerForKey:MDCFloatingButtonModeKey];
+    }
+    if ([aDecoder containsValueForKey:MDCFloatingButtonImageLocationKey]) {
+      _imageLocation = [aDecoder decodeIntegerForKey:MDCFloatingButtonImageLocationKey];
+    }
+    if ([aDecoder containsValueForKey:MDCFloatingButtonImageTitleSpacingKey]) {
+      _imageTitleSpacing =
+          (CGFloat)[aDecoder decodeDoubleForKey:MDCFloatingButtonImageTitleSpacingKey];
     }
   }
   return self;
@@ -83,43 +131,152 @@ static NSString *const MDCFloatingButtonShapeKey = @"MDCFloatingButtonShapeKey";
 - (void)encodeWithCoder:(NSCoder *)aCoder {
   [super encodeWithCoder:aCoder];
   [aCoder encodeInteger:_shape forKey:MDCFloatingButtonShapeKey];
+  [aCoder encodeInteger:self.mode forKey:MDCFloatingButtonModeKey];
+  [aCoder encodeInteger:self.imageLocation forKey:MDCFloatingButtonImageLocationKey];
+  [aCoder encodeDouble:self.imageTitleSpacing forKey:MDCFloatingButtonImageTitleSpacingKey];
+}
+
+- (void)commonMDCFloatingButtonInit {
+  _mode = MDCFloatingButtonModeNormal;
+  _imageLocation = MDCFloatingButtonImageLocationLeading;
+  _imageTitleSpacing = MDCFloatingButtonDefaultImageTitleSpacing;
 }
 
 #pragma mark - UIView
 
 - (CGSize)sizeThatFits:(__unused CGSize)size {
+  return [self intrinsicContentSize];
+}
+
+- (CGSize)intrinsicContentSizeForModeNormal {
   switch (_shape) {
     case MDCFloatingButtonShapeDefault:
-      return CGSizeMake([[self class] defaultDimension], [[self class] defaultDimension]);
+      return CGSizeMake(56, 56);
     case MDCFloatingButtonShapeMini:
-      return CGSizeMake([[self class] miniDimension], [[self class] miniDimension]);
+      return CGSizeMake(40, 40);
   }
 }
 
+- (CGSize)intrinsicContentSizeForModeExpanded {
+  const CGSize intrinsicTitleSize =
+      [self.titleLabel sizeThatFits:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
+  const CGSize intrinsicImageSize =
+      [self.imageView sizeThatFits:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
+  CGFloat intrinsicWidth = intrinsicTitleSize.width + intrinsicImageSize.width +
+      self.imageTitleSpacing + internalLayoutSpacingInsets.left +
+      internalLayoutSpacingInsets.right + self.contentEdgeInsets.left +
+      self.contentEdgeInsets.right;
+  CGFloat intrinsicHeight = MAX(intrinsicTitleSize.height, intrinsicImageSize.height) +
+      self.contentEdgeInsets.top + self.contentEdgeInsets.bottom + internalLayoutSpacingInsets.top +
+      internalLayoutSpacingInsets.bottom;
+  return CGSizeMake(intrinsicWidth, intrinsicHeight);
+}
+
 - (CGSize)intrinsicContentSize {
-  switch (_shape) {
-    case MDCFloatingButtonShapeDefault:
-      return CGSizeMake([[self class] defaultDimension], [[self class] defaultDimension]);
-    case MDCFloatingButtonShapeMini:
-      return CGSizeMake([[self class] miniDimension], [[self class] miniDimension]);
+  CGSize contentSize = CGSizeZero;
+  if (self.mode == MDCFloatingButtonModeNormal) {
+    contentSize = [self intrinsicContentSizeForModeNormal];
+  } else if (self.mode == MDCFloatingButtonModeExpanded) {
+    contentSize = [self intrinsicContentSizeForModeExpanded];
   }
+
+  if (self.minimumSize.height > 0) {
+    contentSize.height = MAX(self.minimumSize.height, contentSize.height);
+  }
+  if (self.maximumSize.height > 0) {
+    contentSize.height = MIN(self.maximumSize.height, contentSize.height);
+  }
+  if (self.minimumSize.width > 0) {
+    contentSize.width = MAX(self.minimumSize.width, contentSize.width);
+  }
+  if (self.maximumSize.width > 0) {
+    contentSize.width = MIN(self.maximumSize.width, contentSize.width);
+  }
+  return contentSize;
 }
 
 - (void)layoutSubviews {
   // We have to set cornerRadius before laying out subviews so that the boundingPath is correct.
   self.layer.cornerRadius = CGRectGetHeight(self.bounds) / 2;
   [super layoutSubviews];
+
+   if (self.mode == MDCFloatingButtonModeNormal) {
+    return;
+  }
+
+  // Position the imageView and titleView
+  //
+  // +------------------------------------+
+  // |    |  |  |  CEI TOP            |   |
+  // |CEI +--+  |       +-----+       |CEI|
+  // | LT ||SP|-- A --|Title|-- A --|RGT|
+  // |    +--+  |       +-----+       |   |
+  // |    |  |  |  CEI BOT            |   |
+  // +------------------------------------+
+  //
+  // (A) The same spacing on either side of the label.
+  // (SP) The spacing between the image and title
+  // (CEI) Content Edge Insets
+  //
+  // The diagram above assumes an LTR user interface orientation
+  // and a .leadingIcon imageLocation for this button.
+
+  BOOL isLeadingIcon = self.imageLocation == MDCFloatingButtonImageLocationLeading;
+  UIEdgeInsets adjustedLayoutInsets =
+      isLeadingIcon ? internalLayoutSpacingInsets
+                    : MDFInsetsFlippedHorizontally(internalLayoutSpacingInsets);
+
+  const CGRect insetBounds = UIEdgeInsetsInsetRect(UIEdgeInsetsInsetRect(self.bounds,
+                                                                         adjustedLayoutInsets),
+                                                   self.contentEdgeInsets);
+
+  const CGFloat imageViewWidth = CGRectGetWidth(self.imageView.bounds);
+  const CGFloat boundsCenterY = CGRectGetMidY(insetBounds);
+  CGFloat titleWidthAvailable = CGRectGetWidth(insetBounds);
+  titleWidthAvailable -= imageViewWidth;
+  titleWidthAvailable -= self.imageTitleSpacing;
+
+  const CGFloat availableHeight = CGRectGetHeight(insetBounds);
+  CGSize titleIntrinsicSize
+      = [self.titleLabel sizeThatFits:CGSizeMake(titleWidthAvailable, availableHeight)];
+
+  const CGSize titleSize = CGSizeMake(MAX(0, MIN(titleIntrinsicSize.width, titleWidthAvailable)),
+                                      MAX(0, MIN(titleIntrinsicSize.height, availableHeight)));
+
+  CGPoint titleCenter;
+  CGPoint imageCenter;
+  BOOL isLTR =
+      self.mdf_effectiveUserInterfaceLayoutDirection == UIUserInterfaceLayoutDirectionLeftToRight;
+
+  // If we are LTR with a leading image, the image goes on the left.
+  // If we are RTL with a trailing image, the image goes on the left.
+  if ((isLTR && isLeadingIcon) || (!isLTR && !isLeadingIcon)) {
+    const CGFloat imageCenterX = CGRectGetMinX(insetBounds) + (imageViewWidth / 2);
+    const CGFloat titleCenterX = CGRectGetMaxX(insetBounds) - (titleWidthAvailable / 2);
+    titleCenter = CGPointMake(titleCenterX, boundsCenterY);
+    imageCenter = CGPointMake(imageCenterX, boundsCenterY);
+  }
+  // If we are LTR with a trailing image, the image goes on the right.
+  // If we are RTL with a leading image, the image goes on the right.
+  else {
+    const CGFloat imageCenterX = CGRectGetMaxX(insetBounds) - (imageViewWidth / 2);
+    const CGFloat titleCenterX = CGRectGetMinX(insetBounds) + (titleWidthAvailable / 2);
+    imageCenter = CGPointMake(imageCenterX, boundsCenterY);
+    titleCenter = CGPointMake(titleCenterX, boundsCenterY);
+  }
+
+  self.imageView.center = imageCenter;
+  self.imageView.frame = UIEdgeInsetsInsetRect(self.imageView.frame, self.imageEdgeInsets);
+  self.titleLabel.center = titleCenter;
+  CGRect newBounds = CGRectStandardize(self.titleLabel.bounds);
+  self.titleLabel.bounds = (CGRect){newBounds.origin, titleSize};
+  self.titleLabel.frame = UIEdgeInsetsInsetRect(self.titleLabel.frame, self.titleEdgeInsets);
 }
 
 #pragma mark - Subclassing
 
 - (UIEdgeInsets)defaultContentEdgeInsets {
-  switch (_shape) {
-    case MDCFloatingButtonShapeDefault:
-      return UIEdgeInsetsZero;
-    case MDCFloatingButtonShapeMini:
-      return UIEdgeInsetsMake(8, 8, 8, 8);
-  }
+  return UIEdgeInsetsZero;
 }
 
 - (UIEdgeInsets)defaultHitAreaInsets {
