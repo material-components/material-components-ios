c9d1b7163db9f09c62f2acc596e54897a8b1282b
Junius Gunaratne <junius@google.com>
2017-12-01 19:03:35 -0500

    [Ink] Enable updated ink on MDCButton, add support for shadowPaths (#2530)
    * Enable updated ink on MDCButton, add support for shadowPaths
    
    * Only change frame inset when legacy ink is used
    
    * Update formatting
    
    * Create mask layer once
    
    * Update animation and opacity based on designer feedback
    
    * Remove commented animations
    
    * Move constants

--- components/Buttons/src/MDCButton.m
+++ components/Buttons/src/MDCButton.m
@@ -301,6 +301,7 @@ static NSAttributedString *uppercaseAttributedString(NSAttributedString *string)
 
   // Set up ink layer.
   _inkView = [[MDCInkView alloc] initWithFrame:self.bounds];
+  _inkView.usesLegacyInkRipple = NO;
   [self insertSubview:_inkView belowSubview:self.imageView];
 
   // UIButton has a drag enter/exit boundary that is outside of the frame of the button itself.
@@ -362,7 +363,7 @@ static NSAttributedString *uppercaseAttributedString(NSAttributedString *string)
   }
 
   // Center unbounded ink view frame taking into account possible insets using contentRectForBounds.
-  if (_inkView.inkStyle == MDCInkStyleUnbounded) {
+  if (_inkView.inkStyle == MDCInkStyleUnbounded && _inkView.usesLegacyInkRipple) {
     CGRect contentRect = [self contentRectForBounds:self.bounds];
     CGPoint contentCenterPoint =
         CGPointMake(CGRectGetMidX(contentRect), CGRectGetMidY(contentRect));
