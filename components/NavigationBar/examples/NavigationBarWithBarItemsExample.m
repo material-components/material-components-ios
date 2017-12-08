3078727d30f0f22448ad33694d867db5a7c8b820
Yarden Eitan <yar@google.com>
2017-12-08 15:17:38 -0500

    [NavigationBar] Fix navbar placement in catalog for iPhoneX (#2581)

--- components/NavigationBar/examples/NavigationBarWithBarItemsExample.m
+++ components/NavigationBar/examples/NavigationBarWithBarItemsExample.m
@@ -50,20 +50,29 @@
 
   self.navBar.translatesAutoresizingMaskIntoConstraints = NO;
 
-  NSDictionary *viewBindings = @{ @"navBar" : self.navBar };
-  NSMutableArray<__kindof NSLayoutConstraint *> *arrayOfConstraints = [NSMutableArray array];
-  [arrayOfConstraints
-      addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[navBar]|"
-                                                                  options:0
-                                                                  metrics:nil
-                                                                    views:viewBindings]];
-  [arrayOfConstraints
-      addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[navBar]"
-                                                                  options:0
-                                                                  metrics:nil
-                                                                    views:viewBindings]];
+#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
+  if (@available(iOS 11.0, *)) {
+    [self.view.safeAreaLayoutGuide.topAnchor constraintEqualToAnchor:self.navBar.topAnchor].active = YES;
+  } else {
+#endif
+    [NSLayoutConstraint constraintWithItem:self.topLayoutGuide
+                                 attribute:NSLayoutAttributeBottom
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.navBar
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1.0
+                                  constant:0].active = YES;
+#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
+  }
+#endif
 
-  [self.view addConstraints:arrayOfConstraints];
+  NSDictionary *viewsBindings = @{@"navBar": self.navBar};
+
+  [NSLayoutConstraint
+   activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[navBar]|"
+                                                               options:0
+                                                               metrics:nil
+                                                                 views:viewsBindings]];
 
   [self setupExampleViews];
 }
