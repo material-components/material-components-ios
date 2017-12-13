83bb4b20f7315cdcce94c83e34bc172b442768bb
Will Larche <larche@google.com>
2017-12-13 18:21:33 -0500

    [TextFields] Seems to be working good.

--- components/TextFields/examples/TextFieldStatesViewController.m
+++ components/TextFields/examples/TextFieldStatesViewController.m
@@ -35,6 +35,8 @@
 @property (nonatomic) id <MDCTextInputController> erroredDisabledController;
 @property (nonatomic) MDCTextField *erroredDisabled;
 
+@property (nonatomic) UIScrollView *scrollView;
+
 @end
 
 @implementation TextFieldStatesViewController
@@ -43,23 +45,25 @@
   [super viewDidLoad];
 
   self.title = @"Text Field States";
-  self.view.backgroundColor = UIColor.whiteColor;
+  [self setupScrollView];
 
   self.enabled = [[MDCTextField alloc] initWithFrame:CGRectZero];
   self.enabled.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.enabled];
+  [self.scrollView addSubview:self.enabled];
 
   self.disabled = [[MDCTextField alloc] initWithFrame:CGRectZero];
   self.disabled.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.disabled];
+  [self.scrollView addSubview:self.disabled];
+  self.disabled.enabled = NO;
 
   self.errored = [[MDCTextField alloc] initWithFrame:CGRectZero];
   self.errored.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.errored];
+  [self.scrollView addSubview:self.errored];
 
   self.erroredDisabled = [[MDCTextField alloc] initWithFrame:CGRectZero];
   self.erroredDisabled.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.erroredDisabled];
+  [self.scrollView addSubview:self.erroredDisabled];
+  self.erroredDisabled.enabled = NO;
 
   [self setupControllers];
 
@@ -69,6 +73,94 @@
                                       target:self
                                       action:@selector(styleButtonDidTouch:)];
   self.navigationItem.rightBarButtonItem = styleButton;
+
+  [NSLayoutConstraint constraintWithItem:self.enabled
+                               attribute:NSLayoutAttributeLeading
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.view
+                               attribute:NSLayoutAttributeLeadingMargin
+                              multiplier:1
+                                constant:0]
+  .active = YES;
+  [NSLayoutConstraint constraintWithItem:self.enabled
+                               attribute:NSLayoutAttributeTrailing
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.view
+                               attribute:NSLayoutAttributeTrailingMargin
+                              multiplier:1
+                                constant:0]
+  .active = YES;
+  [NSLayoutConstraint constraintWithItem:self.enabled
+                               attribute:NSLayoutAttributeTrailing
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.scrollView
+                               attribute:NSLayoutAttributeTrailingMargin
+                              multiplier:1
+                                constant:0]
+  .active = YES;
+
+  NSDictionary *views = @{@"enabled": self.enabled,
+                          @"disabled": self.disabled,
+                          @"errored": self.errored,
+                          @"erroredDisabled": self.erroredDisabled};
+  [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[enabled]-[disabled]-[errored]-[erroredDisabled]" options:NSLayoutFormatAlignAllLeading | NSLayoutFormatAlignAllTrailing metrics:nil views:views]];
+
+#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
+  if (@available(iOS 11.0, *)) {
+    [NSLayoutConstraint constraintWithItem:self.enabled
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.scrollView.contentLayoutGuide
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:20]
+    .active = YES;
+    [NSLayoutConstraint constraintWithItem:self.erroredDisabled
+                                 attribute:NSLayoutAttributeBottom
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.scrollView.contentLayoutGuide
+                                 attribute:NSLayoutAttributeBottomMargin
+                                multiplier:1
+                                  constant:-20]
+    .active = YES;
+
+  } else {
+    [NSLayoutConstraint constraintWithItem:self.enabled
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.scrollView
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:20]
+    .active = YES;
+    [NSLayoutConstraint constraintWithItem:self.erroredDisabled
+                                 attribute:NSLayoutAttributeBottom
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.scrollView
+                                 attribute:NSLayoutAttributeBottomMargin
+                                multiplier:1
+                                  constant:-20]
+    .active = YES;
+  }
+#else
+  [NSLayoutConstraint constraintWithItem:self.enabled
+                               attribute:NSLayoutAttributeTop
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.scrollView
+                               attribute:NSLayoutAttributeTop
+                              multiplier:1
+                                constant:20]
+  .active = YES;
+  [NSLayoutConstraint constraintWithItem:self.erroredDisabled
+                               attribute:NSLayoutAttributeBottom
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.scrollView
+                               attribute:NSLayoutAttributeBottomMargin
+                              multiplier:1
+                                constant:-20]
+  .active = YES;
+#endif
+
 }
 
 - (void)setupControllers {
@@ -84,13 +176,58 @@
   if ([self.selectedClass conformsToProtocol:@protocol(MDCTextInputController)]) {
     self.enabledController =
         [(id <MDCTextInputController>)[self.selectedClass alloc] initWithTextInput:self.enabled];
+    self.enabledController.placeholderText = @"Enabled";
+
     self.disabledController =
         [(id <MDCTextInputController>)[self.selectedClass alloc] initWithTextInput:self.disabled];
+    self.disabledController.placeholderText = @"Disabled";
+
     self.erroredController =
         [(id <MDCTextInputController>)[self.selectedClass alloc] initWithTextInput:self.errored];
+    self.erroredController.placeholderText = @"Errored";
+    [self.erroredController setErrorText:@"Errored" errorAccessibilityValue:nil];
+
     self.erroredDisabledController =
         [(id <MDCTextInputController>)[self.selectedClass alloc] initWithTextInput:self.erroredDisabled];
+    self.erroredDisabledController.placeholderText = @"Errored Disabled";
+    [self.erroredDisabledController setErrorText:@"Errored Disabled" errorAccessibilityValue:nil];
   }
+}
+
+- (void)setupScrollView {
+  self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectZero];
+  self.scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:self.scrollView];
+  self.scrollView.backgroundColor = UIColor.whiteColor;
+
+  [NSLayoutConstraint
+   activateConstraints:[NSLayoutConstraint
+                        constraintsWithVisualFormat:@"V:|[scrollView]|"
+                        options:0
+                        metrics:nil
+                        views:@{
+                                @"scrollView" : self.scrollView
+                                }]];
+  [NSLayoutConstraint
+   activateConstraints:[NSLayoutConstraint
+                        constraintsWithVisualFormat:@"H:|[scrollView]|"
+                        options:0
+                        metrics:nil
+                        views:@{
+                                @"scrollView" : self.scrollView
+                                }]];
+
+  CGFloat marginOffset = 16;
+  UIEdgeInsets margins = UIEdgeInsetsMake(0, marginOffset, 0, marginOffset);
+  self.scrollView.layoutMargins = margins;
+
+  UITapGestureRecognizer *tapRecognizer =
+  [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDidTouch)];
+  [self.view addGestureRecognizer:tapRecognizer];
+}
+
+- (void)tapDidTouch {
+  [self.view endEditing:YES];
 }
 
 - (void)styleButtonDidTouch:(UIBarButtonItem *)sender {
