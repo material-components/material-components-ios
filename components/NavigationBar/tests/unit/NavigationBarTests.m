64381bf47f007f81ef864282c240c26ff480f0b2
Robert Moore <rsmoore@google.com>
2017-12-14 19:58:54 -0500

    [NavigationBar] Add Accessibility method tests (#2721)
    Extending test coverage to include accessibility tests

--- components/NavigationBar/tests/unit/NavigationBarTests.m
+++ components/NavigationBar/tests/unit/NavigationBarTests.m
@@ -15,6 +15,8 @@
  */
 
 #import <XCTest/XCTest.h>
+
+#import "MaterialButtonBar.h"
 #import "MaterialNavigationBar.h"
 
 static const CGFloat kEpsilonAccuracy = 0.001f;
@@ -134,6 +136,132 @@ static const CGFloat kEpsilonAccuracy = 0.001f;
   XCTAssertEqual(navBar.leadingItemsSupplementBackButton,
                 unarchivedBar.leadingItemsSupplementBackButton);
   XCTAssertEqual(navBar.titleAlignment, unarchivedBar.titleAlignment);
+}
+
+
+#pragma mark - Accessibility
+
+- (void)testNavigationBarIsNotAccessibilityElement {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+
+  // Then
+  XCTAssertFalse(navBar.isAccessibilityElement);
+}
+
+- (void)testAccessibilityItemsCountWithNoTitle {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+
+  // Then
+  const NSInteger elementsCount = 3; // Leading bar, titleLabel, trailing bar
+  XCTAssertEqual(elementsCount, navBar.accessibilityElementCount);
+}
+
+- (void)testAccessibilityItemsCountWithTitleView {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+
+  // When
+  navBar.titleView = [[UIView alloc] init];
+
+  // Then
+  const NSInteger elementsCount = 3; // Leading bar, titleView, trailing bar
+  XCTAssertEqual(elementsCount, navBar.accessibilityElementCount);
+}
+
+- (void)testAccessibilityItemAtIndexDefault {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+
+  // Then
+  XCTAssertTrue([[navBar accessibilityElementAtIndex:0] isKindOfClass:[MDCButtonBar class]]);
+  XCTAssertEqual(navBar.titleLabel, [navBar accessibilityElementAtIndex:1]);
+  XCTAssertTrue([[navBar accessibilityElementAtIndex:2] isKindOfClass:[MDCButtonBar class]]);
+}
+
+- (void)testIndexOfAccessibilityElementDefault {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+
+  // Then
+  XCTAssertEqual(1, [navBar indexOfAccessibilityElement:navBar.titleLabel]);
+  XCTAssertEqual(NSNotFound, [navBar indexOfAccessibilityElement:navBar.leftBarButtonItem]);
+}
+
+- (void)testIndexOfAccessibilityElementWithTitleView {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+
+  // When
+  navBar.titleView = [[UIView alloc] init];
+
+  // Then
+  XCTAssertEqual(1, [navBar indexOfAccessibilityElement:navBar.titleView]);
+  XCTAssertEqual(NSNotFound, [navBar indexOfAccessibilityElement:navBar.titleLabel]);
+}
+
+- (void)testAccessibilityElementsWithNoTitle {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+  UIBarButtonItem *leadingItem = [[UIBarButtonItem alloc] init];
+  leadingItem.title = @"Leading";
+  UIBarButtonItem *trailingItem = [[UIBarButtonItem alloc] init];
+  trailingItem.title = @"Trailing";
+
+  // When
+  navBar.leadingBarButtonItem = leadingItem;
+  navBar.trailingBarButtonItems = @[leadingItem, trailingItem];
+
+  // Then
+  NSArray *elements = navBar.accessibilityElements;
+  XCTAssertNotNil(elements);
+  XCTAssertEqual(3U, elements.count);
+  id firstItem = elements[0];
+  XCTAssertTrue([firstItem isKindOfClass:[MDCButtonBar class]]);
+  if ([firstItem isKindOfClass:[MDCButtonBar class]]) {
+    MDCButtonBar *leadingButtonBar = (MDCButtonBar *)firstItem;
+    XCTAssertEqual(1U, leadingButtonBar.subviews.count);
+  }
+  XCTAssertEqualObjects(navBar.titleLabel, elements[1]);
+  id secondItem = elements[2];
+  XCTAssertTrue([secondItem isKindOfClass:[MDCButtonBar class]]);
+  if ([secondItem isKindOfClass:[MDCButtonBar class]]) {
+    MDCButtonBar *trailingButtonBar = (MDCButtonBar *)secondItem;
+    XCTAssertEqual(2U, trailingButtonBar.subviews.count);
+  }
+}
+
+- (void)testAccessibilityElementsWithTitleView {
+  // Given
+  MDCNavigationBar *navBar = [[MDCNavigationBar alloc] init];
+  UIBarButtonItem *leadingItem = [[UIBarButtonItem alloc] init];
+  leadingItem.title = @"Leading";
+  UIBarButtonItem *trailingItem = [[UIBarButtonItem alloc] init];
+  trailingItem.title = @"Trailing";
+
+  // When
+  navBar.titleView = [[UIView alloc] init];
+  navBar.leadingBarButtonItem = leadingItem;
+  navBar.trailingBarButtonItems = @[leadingItem, trailingItem];
+
+  // Then
+  NSArray *elements = navBar.accessibilityElements;
+  XCTAssertNotNil(elements);
+  XCTAssertEqual(3U, elements.count);
+  id firstItem = elements[0];
+  XCTAssertTrue([firstItem isKindOfClass:[MDCButtonBar class]]);
+  if ([firstItem isKindOfClass:[MDCButtonBar class]]) {
+    MDCButtonBar *leadingButtonBar = (MDCButtonBar *)firstItem;
+    XCTAssertEqual(1U, leadingButtonBar.subviews.count);
+  }
+  XCTAssertEqualObjects(navBar.titleView, elements[1]);
+  id secondItem = elements[2];
+  XCTAssertTrue([secondItem isKindOfClass:[MDCButtonBar class]]);
+  if ([secondItem isKindOfClass:[MDCButtonBar class]]) {
+    MDCButtonBar *trailingButtonBar = (MDCButtonBar *)secondItem;
+    XCTAssertEqual(2U, trailingButtonBar.subviews.count);
+  }
 }
 
 @end
