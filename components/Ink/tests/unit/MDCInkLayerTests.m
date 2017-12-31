9ee4f3f65432ae273d3a0482b622a54735a32d98
Robert Moore <rsmoore@google.com>
2017-12-30 22:00:56 -0500

    [Ink] Add NSCoding support to MDCInkView/Layer
    Both MDCInkLayer (and MDCLegacyInkLayer) and MDCInkView should support
    NSCoding methods since their superclasses conform to an NSCoding (or
    NSSecureCoding) protocol.
    
    Closes #2772

---
+++ components/Ink/tests/unit/MDCInkLayerTests.m
@@ -0,0 +1,95 @@
+/*
+ Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import "MDCInkLayer.h"
+
+#pragma mark - Fake classes
+
+@interface FakeMDCInkLayerAnimationDelegate : NSObject <MDCInkLayerDelegate, NSCoding>
+@property(nonatomic, strong) MDCInkLayer *inkLayer;
+@end
+
+@implementation FakeMDCInkLayerAnimationDelegate
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+  self = [super init];
+  if (self) {
+    _inkLayer = [aDecoder decodeObjectForKey:@"inkLayer"];
+  }
+  return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+  [aCoder encodeObject:self.inkLayer forKey:@"inkLayer"];
+}
+
+@end
+
+#pragma mark - Tests
+
+@interface MDCInkLayerTests : XCTestCase
+
+@end
+
+@implementation MDCInkLayerTests
+
+- (void)testInit {
+  // Given
+  MDCInkLayer *inkLayer = [[MDCInkLayer alloc] init];
+
+  // Then
+  XCTAssertNil(inkLayer.delegate);
+  XCTAssertFalse(inkLayer.isStartAnimationActive);
+  XCTAssertEqualWithAccuracy(inkLayer.endAnimationDelay, 0, 0.0001);
+  XCTAssertEqualWithAccuracy(inkLayer.initialRadius, 0, 0.0001);
+  XCTAssertEqualWithAccuracy(inkLayer.finalRadius, 0, 0.0001);
+  XCTAssertEqualWithAccuracy(inkLayer.maxRippleRadius, 0, 0.0001);
+  XCTAssertEqualObjects(inkLayer.inkColor, [UIColor colorWithWhite:0 alpha:(CGFloat)0.08]);
+}
+
+- (void)testEncoding {
+  // Given
+  FakeMDCInkLayerAnimationDelegate *delegate = [[FakeMDCInkLayerAnimationDelegate alloc] init];
+  MDCInkLayer *inkLayer = [[MDCInkLayer alloc] init];
+  delegate.inkLayer = inkLayer;
+  inkLayer.delegate = delegate;
+  inkLayer.endAnimationDelay = 1;
+  inkLayer.initialRadius = 2;
+  inkLayer.finalRadius = 3;
+  inkLayer.maxRippleRadius = 4;
+  inkLayer.inkColor = UIColor.magentaColor;
+
+  // When
+  NSData *archive = [NSKeyedArchiver archivedDataWithRootObject:delegate];
+  FakeMDCInkLayerAnimationDelegate *unarchivedDelegate =
+      [NSKeyedUnarchiver unarchiveObjectWithData:archive];
+  MDCInkLayer *unarchivedInkLayer = unarchivedDelegate.inkLayer;
+
+  // Then
+  XCTAssertEqual(unarchivedInkLayer.delegate, unarchivedDelegate);
+  XCTAssertEqualWithAccuracy(unarchivedInkLayer.endAnimationDelay,
+                             inkLayer.endAnimationDelay,
+                             0.0001);
+  XCTAssertEqualWithAccuracy(unarchivedInkLayer.initialRadius, inkLayer.initialRadius, 0.0001);
+  XCTAssertEqualWithAccuracy(unarchivedInkLayer.finalRadius, inkLayer.finalRadius, 0.0001);
+  XCTAssertEqualWithAccuracy(unarchivedInkLayer.maxRippleRadius, inkLayer.maxRippleRadius, 0.0001);
+  XCTAssertEqualObjects(unarchivedInkLayer.inkColor, inkLayer.inkColor);
+  XCTAssertEqual(unarchivedInkLayer.sublayers.count, inkLayer.sublayers.count);
+}
+
+@end
