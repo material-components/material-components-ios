a62dc842c52b7f7d0b4813e4a370121217a94270
Yarden Eitan <yar@google.com>
2018-01-16 16:21:41 -0500

    added tests and documentation

--- components/Cards/src/MDCCard.h
+++ components/Cards/src/MDCCard.h
@@ -1,38 +1,96 @@
-//
-//  MDCCards.h
-//  MaterialComponents
-//
-//  Created by Yarden Eitan on 1/8/18.
-//
+/*
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 #import "MaterialShadowLayer.h"
 #import "MaterialInk.h"
 
 typedef NS_ENUM(NSInteger, MDCCardsState) {
+  /** The default state for the card. */
   MDCCardsStateDefault,
+
+  /** The visual state when the card is pressed (i.e. dragging). */
   MDCCardsStatePressed,
+
+  /** The visual state when you select the card (i.e. editing). */
   MDCCardsStateSelect,
+
+  /** The visual state when the card is already selected (i.e. editing). */
   MDCCardsStateSelected,
+
+  /** The visual state when the card is already unselected (i.e. editing) */
   MDCCardStateUnselected
 };
 
 @interface MDCCard : UIControl
 
+/**
+ Initializing the MDCCard using the frame and if it is the
+ underlying view for MDCCollectionViewCardCell
+
+ @param frame CGRect for the view's frame.
+ @param isUsingCell YES if this is the underlying view for MDCCollectionViewCardCell,
+                    otherwise, NO.
+ */
 - (nonnull instancetype)initWithFrame:(CGRect)frame withIsUsingCollectionViewCell:(BOOL)isUsingCell;
 
+/**
+ Setter for the cornerRadius of the MDCCard.
+
+ @param cornerRadius CGFloat of the radius to set the corner
+ */
 - (void)setCornerRadius:(CGFloat)cornerRadius;
+
+/**
+ Getter for the cornerRadius of the MDCCard.
+ */
 - (CGFloat)cornerRadius;
 
+/**
+ Setter for the shadow elevation surrounding the MDCCard.
+
+ @param elevation CGFloat of the elevation for the shadow of the MDDCCard.
+ */
 - (void)setShadowElevation:(CGFloat)elevation;
+
+/**
+ Getter for the shadow elevation surrounding the MDCCard.
+ */
 - (CGFloat)shadowElevation;
 
+/**
+ Sets the style for the MDCCard based on the defined state. Please see the MDCCardState definition
+ above to see all the possible states.
+
+ @param state MDCCardState this defines the state in which the card should visually be set to
+ @param location CGPoint some states may need the touch location to begin/end the ink from
+ @param completion MDCInkCompletionBlock the completion block for the ink animation
+ */
 - (void)styleForState:(MDCCardsState)state
          withLocation:(CGPoint)location
        withCompletion:(nullable MDCInkCompletionBlock)completion;
 
-//@property(nonatomic, strong, nullable) UIView *opacityMask;
+/**
+  The inkView for the card that is initiated on tap
+ */
 @property(nonatomic, strong, nullable) MDCInkView *inkView;
+
+/**
+ The image view that is seen when the card is in the selected state
+ */
 @property(nonatomic, strong, nonnull) UIImageView *selectedImageView;
 
 @end
--- components/Cards/src/MDCCollectionViewCardCell.h
+++ components/Cards/src/MDCCollectionViewCardCell.h
@@ -1,34 +1,92 @@
-//
-//  MDCCollectionViewCardCell.h
-//  MaterialComponents
-//
-//  Created by Yarden Eitan on 1/8/18.
-//
+/*
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 #import "MDCCard.h"
 
 typedef NS_ENUM(NSInteger, MDCCardCellSelectionState) {
+  /** The visual state when you are now selecting the cell. */
   MDCCardCellSelectionStateSelect,
+
+  /** The visual state when you have already selected the cell. */
   MDCCardCellSelectionStateSelected,
+
+  /** The visual state when you are now unselecting the cell. */
   MDCCardCellSelectionStateUnselect,
+
+  /** The visual state when you have already unselected the cell. */
   MDCCardCellSelectionStateUnselected
 };
 
 @interface MDCCollectionViewCardCell : UICollectionViewCell
 
+/**
+ The underlying card view for the cell
+ */
 @property(nonatomic, strong, nullable) MDCCard *cardView;
+
+/**
+ editMode is toggled to true once any of the selection states are invoked
+ to insure that the default touch recognizer isn't invoked like a regular tap.
+ */
 @property(nonatomic, assign) BOOL editMode;
 
+/**
+ Setter for the background color of the view
+
+ @param backgroundColor UIColor for the background color
+ */
 - (void)setBackgroundColor:(nullable UIColor *)backgroundColor;
+
+/**
+ Getter for the background color of the view
+ */
 - (nullable UIColor *)backgroundColor;
 
+
+/**
+ Setter for the cornerRadius of the cell and the underlying card.
+
+ @param cornerRadius CGFloat of the radius to set the corner
+ */
 - (void)setCornerRadius:(CGFloat)cornerRadius;
+
+/**
+ Getter for the cornerRadius of the cell and the underlying card.
+ */
 - (CGFloat)cornerRadius;
 
+/**
+ Setter for the shadow elevation surrounding the cell.
+
+ @param elevation CGFloat of the elevation for the shadow of the MDDCCard.
+ */
 - (void)setShadowElevation:(CGFloat)elevation;
+
+/**
+ Getter for the shadow elevation surrounding the cell.
+ */
 - (CGFloat)shadowElevation;
 
+/**
+ selectionState sets the selection state of the cell when in editing/selection mode.
+ This method also puts the cell in editMode which then makes it follow the selection visuals.
+
+ @param state MDCCardCellSelectionState is the visual state of the cell in regards to selection.
+ */
 - (void)selectionState:(MDCCardCellSelectionState)state;
 
 @end
