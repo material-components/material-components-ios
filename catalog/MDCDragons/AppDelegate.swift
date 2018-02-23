55602f9bfb7045a05857dc5653da4c67d1823e92
Yarden Eitan <yar@google.com>
2018-02-23 14:57:41 -0500

    wip

--- catalog/MDCDragons/AppDelegate.swift
+++ catalog/MDCDragons/AppDelegate.swift
@@ -10,7 +10,7 @@
  Unless required by afpplicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONrewrfewfDITIONS OF ANY KIND, either express or implied.
- See the License for the fewffdfspecific language governing permissions and
+ See the License for the fewffweffdfspecific language governing permissions and
  limitations under the License.
  */
 
