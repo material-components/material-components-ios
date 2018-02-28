dd7262c5624743e14f2b1c9d877ea18b8d4e7286
Robert Moore <rsmoore@google.com>
2018-02-28 14:02:57 -0500

    [ButtonBar] Remove UIAppearance call in themer
    ButtonBar was explicitly calling UIAppearance for itself when contained
    in an MDCNavigationBar to clear the backgroundColor property. This is
    "magic" behavior and should not be included in the themer. Instead,
    clients should explicitly customize the ButtonBar color behavior when
    contained inside other views.
    
    Closes #2789

---
+++ catalog/MDCCatalog/MDCCatalogThemer.m
@@ -0,0 +1,90 @@
+/*
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#import <Foundation/Foundation.h>
+
+#import "MDCCatalogThemer.h"
+#import "MDCActivityIndicatorColorThemer.h"
+#import "MDCAlertColorThemer.h"
+#import "MDCButtonBarColorThemer.h"
+#import "MDCButtonColorThemer.h"
+#import "MDCFeatureHighlightColorThemer.h"
+#import "MDCFlexibleHeaderColorThemer.h"
+#import "MDCHeaderStackViewColorThemer.h"
+#import "MDCNavigationBarColorThemer.h"
+#import "MDCPageControlColorThemer.h"
+#import "MDCProgressViewColorThemer.h"
+#import "MDCSliderColorThemer.h"
+#import "MDCTabBarColorThemer.h"
+#import "MDCTextFieldColorThemer.h"
+#import "MaterialTextFields.h"
+
+@implementation MDCCatalogThemer
+
++ (void)applyColorScheme:(NSObject<MDCColorScheme> *)colorScheme {
+  // Apply color scheme to material design components using component themers.
+  [MDCActivityIndicatorColorThemer applyColorScheme:colorScheme
+                                toActivityIndicator:MDCActivityIndicator.appearance];
+  [MDCAlertColorThemer applyColorScheme:colorScheme];
+  [MDCButtonBarColorThemer applyColorScheme:colorScheme toButtonBar:MDCButtonBar.appearance];
+  // When a button bar is contained in a navigation bar do not color the button bar. This is useful
+  // for when an app bar with imagery in the flexible header has a button bar. If the button bar has
+  // a color then the imagery is obstucted by the solid color of the button bar.
+#if defined(__IPHONE_9_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_9_0)
+  NSOperatingSystemVersion iOS9Version = {9, 0, 0};
+  if ([NSProcessInfo.processInfo isOperatingSystemAtLeastVersion:iOS9Version]) {
+    MDCButtonBar *appearanceProxy =
+        [MDCButtonBar appearanceWhenContainedInInstancesOfClasses:@[MDCNavigationBar.class]];
+    appearanceProxy.backgroundColor = nil;
+  } else {
+#endif
+    [MDCButtonBar appearanceWhenContainedIn:MDCNavigationBar.class, nil].backgroundColor = nil;
+#if defined(__IPHONE_9_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_9_0)
+  }
+#endif
+
+  [MDCButtonColorThemer applyColorScheme:colorScheme toButton:MDCButton.appearance];
+  MDCBasicColorScheme *clearScheme =
+      [[MDCBasicColorScheme alloc] initWithPrimaryColor:UIColor.clearColor];
+  [MDCButtonColorThemer applyColorScheme:clearScheme toButton:MDCFlatButton.appearance];
+  [MDCFeatureHighlightColorThemer applyColorScheme:colorScheme
+                            toFeatureHighlightView:MDCFeatureHighlightView.appearance];
+  [MDCFlexibleHeaderColorThemer applyColorScheme:colorScheme
+                            toFlexibleHeaderView:MDCFlexibleHeaderView.appearance];
+  [MDCHeaderStackViewColorThemer applyColorScheme:colorScheme
+                                toHeaderStackView:MDCHeaderStackView.appearance];
+  [MDCNavigationBarColorThemer applyColorScheme:colorScheme
+                                toNavigationBar:MDCNavigationBar.appearance];
+  [MDCPageControlColorThemer applyColorScheme:colorScheme toPageControl:MDCPageControl.appearance];
+  [MDCProgressViewColorThemer applyColorScheme:colorScheme
+                                toProgressView:MDCProgressView.appearance];
+  [MDCSliderColorThemer applyColorScheme:colorScheme toSlider:MDCSlider.appearance];
+  [MDCTabBarColorThemer applyColorScheme:colorScheme toTabBar:MDCTabBar.appearance];
+  [MDCTextFieldColorThemer applyColorScheme:colorScheme
+           toAllTextInputControllersOfClass:MDCTextInputControllerUnderline.class];
+  [MDCTextFieldColorThemer applyColorScheme:colorScheme
+           toAllTextInputControllersOfClass:MDCTextInputControllerLegacyDefault.class];
+  [MDCTextFieldColorThemer applyColorScheme:colorScheme
+           toAllTextInputControllersOfClass:MDCTextInputControllerFilled.class];
+  [MDCTextFieldColorThemer applyColorScheme:colorScheme
+           toAllTextInputControllersOfClass:MDCTextInputControllerOutlined.class];
+  [MDCTextFieldColorThemer applyColorScheme:colorScheme
+           toAllTextInputControllersOfClass:MDCTextInputControllerOutlinedTextArea.class];
+
+  // Apply color scheme to UIKit components.
+  UISlider.appearance.tintColor = colorScheme.primaryColor;
+  UISwitch.appearance.onTintColor = colorScheme.primaryColor;
+}
+@end
