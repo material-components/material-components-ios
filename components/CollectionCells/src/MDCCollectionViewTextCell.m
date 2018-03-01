af4b188fcf74d5b9ed70fa37263f876dd8b710fa
Ian Gordon <iangordon@google.com>
2018-03-01 13:17:04 -0500

    [Collections] Image sizing fix

--- components/CollectionCells/src/MDCCollectionViewTextCell.m
+++ components/CollectionCells/src/MDCCollectionViewTextCell.m
@@ -186,8 +186,9 @@ static inline CGRect AlignRectToUpperPixel(CGRect rect) {
 
   // Image layout.
   CGRect imageFrame = CGRectZero;
-  imageFrame.size = CGSizeMake(kImageSize, kImageSize);
-  imageFrame.origin.x = kCellImagePaddingLeading;
+  imageFrame.size.width = MIN(_imageView.image.size.width, kImageSize);
+  imageFrame.size.height = MIN(_imageView.image.size.height, kImageSize);
+  imageFrame.origin.x = kCellImagePaddingLeading + (kImageSize - imageFrame.size.width) / 2;
   imageFrame.origin.y =
       (CGRectGetHeight(self.contentView.frame) / 2) - (imageFrame.size.height / 2);
 
