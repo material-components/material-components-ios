b2cb6e66478e909f025011ebc0298bcb197a83b0
mohammadcazi@google.com <mohammadcazi@google.com>
2018-03-13 14:57:41 -0400

    test files header.

--- components/Snackbar/src/private/MDCSnackbarOverlayView.m
+++ components/Snackbar/src/private/MDCSnackbarOverlayView.m
@@ -489,7 +489,7 @@ static const CGFloat kMaximumHeight = 80.0f;
                         options:0
                      animations:^{
                        // Trigger snackbar animation.
-                       [self.containingView layoutIfNeeded];
+                       [_containingView layoutIfNeeded];
                      }
                      completion:nil];
     [snackbarView animateContentOpacityFrom:fromContentOpacity
