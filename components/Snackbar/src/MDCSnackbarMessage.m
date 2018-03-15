d758a8319a217ac0e09e082850d83c51135d5b4e
randallli <randallli@google.com>
2018-03-15 16:00:31 -0400

    Revert "Revert "[Snackbar] custom font and dynamic type support, UI_APPEARANCE support for color theming  (#3090)""
    This reverts commit 05f0dfa8cc9e269b57544a7330696c914fd0ecec.

--- components/Snackbar/src/MDCSnackbarMessage.m
+++ components/Snackbar/src/MDCSnackbarMessage.m
@@ -66,8 +66,6 @@ static BOOL _usesLegacySnackbar = YES;
   copy.duration = self.duration;
   copy.category = self.category;
   copy.accessibilityLabel = self.accessibilityLabel;
-  copy.buttonTextColor = self.buttonTextColor;
-  copy.highlightedButtonTextColor = self.highlightedButtonTextColor;
 
   // Unfortunately there's not really a concept of 'copying' a block (in the same way you would copy
   // a string, for example). A block's pointer is immutable once it is created and copied to the
