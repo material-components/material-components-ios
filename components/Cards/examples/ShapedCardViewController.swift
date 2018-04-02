4852236b3704428eccaab1a4bf8e70601b79cb13
yar <yarneo@gmail.com>
2018-04-02 15:17:24 +0300

    update

--- components/Cards/examples/ShapedCardViewController.swift
+++ components/Cards/examples/ShapedCardViewController.swift
@@ -212,33 +212,3 @@ extension ShapedCardViewController {
     return true
   }
 }
-
-extension UIView {
-  func mask(withRect rect: CGRect, inverse: Bool = false) {
-    let path = UIBezierPath(rect: rect)
-    let maskLayer = CAShapeLayer()
-
-    if inverse {
-      path.append(UIBezierPath(rect: self.bounds))
-      maskLayer.fillRule = kCAFillRuleEvenOdd
-    }
-
-    maskLayer.path = path.cgPath
-
-    self.layer.mask = maskLayer
-  }
-
-  func mask(withPath path: UIBezierPath, inverse: Bool = false) {
-    let path = path
-    let maskLayer = CAShapeLayer()
-
-    if inverse {
-      path.append(UIBezierPath(rect: self.bounds))
-      maskLayer.fillRule = kCAFillRuleEvenOdd
-    }
-
-    maskLayer.path = path.cgPath
-
-    self.layer.mask = maskLayer
-  }
-}
