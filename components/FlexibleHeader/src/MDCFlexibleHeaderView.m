e511948afaec80ac7b617b27a1170038ce57622f
Robert Moore <rsmoore@google.com>
2018-04-04 13:34:03 -0400

    Revert "Revert "[FlexibleHeader] Fix bug where shadow layer's opacity wouldn't be set without a tracking scroll view. (#3201)" (#3225)"
    This reverts commit bcecad50f8a385ac641fd6b10f5968bec49c4dd9.

--- components/FlexibleHeader/src/MDCFlexibleHeaderView.m
+++ components/FlexibleHeader/src/MDCFlexibleHeaderView.m
@@ -911,6 +911,7 @@ static NSString *const MDCFlexibleHeaderDelegateKey = @"MDCFlexibleHeaderDelegat
 
 - (void)fhv_updateLayout {
   if (!_trackingScrollView) {
+    [self fhv_commitAccumulatorToFrame];
     return;
   }
 
