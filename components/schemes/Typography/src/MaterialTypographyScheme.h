6620da4d965890af8ea3f9e8fce0e518b9ed33b0
featherless <jverkoey@gmail.com>
2018-04-05 22:19:41 -0400

    Partial roll-forward of "[Typography] Migrate from FontScheme to TypographyScheming (#3219)" (#3258)
    This change only adds the new Typography Scheme APIs and target with no component modifications.
    
    This partially reverts commit 83b9ea1.
    
    Pivotal story: https://www.pivotaltracker.com/story/show/156392054

--- components/schemes/Typography/src/MaterialTypographyScheme.h
+++ components/schemes/Typography/src/MaterialTypographyScheme.h
@@ -1,5 +1,5 @@
 /*
- Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -14,5 +14,5 @@
  limitations under the License.
  */
 
-#import "MDCFontSchemeMigration.h"
-#import "MDCColorSchemeMigration.h"
+#import "MDCFontScheme.h"
+#import "MDCTypographyScheme.h"
