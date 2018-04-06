92078157171bb976ea6624197b3a06c9bfebbd59
featherless <jverkoey@gmail.com>
2018-04-06 11:03:04 -0400

    [NavigationBar] Add an API for customizing the title label's text color. (#3266)
    Pivotal story: https://www.pivotaltracker.com/story/show/155525164
    
    ![simulator screen shot - iphone x - 2018-04-06 at 09 22 24](https://user-images.githubusercontent.com/45670/38423500-18ce7586-397c-11e8-9cdf-72ce1e2de7c8.png)

--- components/NavigationBar/src/MDCNavigationBar.h
+++ components/NavigationBar/src/MDCNavigationBar.h
@@ -104,23 +104,14 @@ IB_DESIGNABLE
  Both Default and null_resettable value is MDCTypography's titleFont.
  Note that the font attribute of titleTextAttributes will take precedence over this property.
  */
-@property (nonatomic, strong, null_resettable) UIFont *titleFont;
+@property(nonatomic, strong, null_resettable) UIFont *titleFont;
 
 /**
- Display attributes for the titleView's title text.
+ The title label's text color.
 
- Font attribute will take precedence over titleFont property.
- Setting this property will render an NSAttributedString with the assigned attributes across the
- entire text.
- Note: this property will be deprecated in future, please use title font instead.
+ Default is nil (text draws black).
  */
-#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
-@property(nonatomic, copy, nullable)
-    NSDictionary<NSAttributedStringKey, id> *titleTextAttributes UI_APPEARANCE_SELECTOR;
-#else
-@property(nonatomic, copy, nullable)
-    NSDictionary<NSString *, id> *titleTextAttributes UI_APPEARANCE_SELECTOR;
-#endif
+@property(nonatomic, strong, nullable) UIColor *titleTextColor;
 
 /** The back button to be displayed, if any. */
 @property(nonatomic, strong, nullable) UIBarButtonItem *backItem;
@@ -202,6 +193,25 @@ IB_DESIGNABLE
 
 /* Equivalent to leadingItemsSupplementBackButton. */
 @property(nonatomic) BOOL leftItemsSupplementBackButton;
+
+#pragma mark - To be deprecated
+
+/**
+ Display attributes for the titleView's title text.
+
+ Font attribute will take precedence over titleFont property.
+ Setting this property will render an NSAttributedString with the assigned attributes across the
+ entire text.
+
+ Note: this property will be deprecated in future, please use titleFont and titleTextColor instead.
+ */
+#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
+@property(nonatomic, copy, nullable)
+    NSDictionary<NSAttributedStringKey, id> *titleTextAttributes UI_APPEARANCE_SELECTOR;
+#else
+@property(nonatomic, copy, nullable)
+    NSDictionary<NSString *, id> *titleTextAttributes UI_APPEARANCE_SELECTOR;
+#endif
 
 #pragma mark - Deprecated
 
