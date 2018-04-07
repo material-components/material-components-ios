796b906f37a5486a7d0d83a4dbd75138cb0dfac9
Jeff Verkoeyen <featherless@google.com>
2018-04-06 22:11:10 -0400

    Update dangerfile.js.

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,5 @@
 const {danger, warn} = require('danger')
 
-// No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
