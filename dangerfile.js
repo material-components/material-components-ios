2ae8a31953f66f1fb645346a3b195fde0f7dc6ce
Jeff Verkoeyen <featherless@google.com>
2018-04-06 21:55:03 -0400

    Don't check for changelogs.

--- dangerfile.js
+++ dangerfile.js
@@ -4,12 +4,3 @@ const {danger, warn} = require('danger')
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
-
-// Check for a CHANGELOG entry
-const hasChangelog = danger.git.modified_files.some(f => f === 'CHANGELOG.md')
-const description = danger.github.pr.body + danger.github.pr.title
-const isTrivial = description.includes('#trivial')
-
-if (!hasChangelog && !isTrivial) {
-  warn('Please add a changelog entry for your changes.')
-}
