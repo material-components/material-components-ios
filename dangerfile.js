a7e946123bf5971e070e7b45b15af05a745273c1
Jeff Verkoeyen <featherless@google.com>
2018-04-06 22:19:16 -0400

    Add pivotal failure.

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,10 @@
 const {danger, warn} = require('danger')
 
 if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
+  warn('Please provide a detailed description of the change.');
+}
+
+if (!danger.github.pr.body.includes("pivotaltracker.com")
+    || danger.github.pr.body.includes("No pivotal story required.")) {
+  error('Please either provide a link to a pivotal story or include the text "No pivotal story required." in your PR description.');
 }
