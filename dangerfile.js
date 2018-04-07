8cc3162bf3b591f2ff9b61df01547d1b987c34bd
Jeff Verkoeyen <featherless@google.com>
2018-04-06 22:42:16 -0400

    MOre work.

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,8 @@
 const {danger, warn, fail} = require('danger')
 
-const modified = danger.git.modified_files
+const affectedFiles = danger.git.modified_files
+  .concat(danger.git.created_files)
+  .concat(danger.git.deleted_files)
 
 if (danger.github.pr.body.length < 10) {
   warn('Please provide a detailed description of the change.');
@@ -11,4 +13,8 @@ if (!danger.github.pr.body.includes("pivotaltracker.com")
   fail('Please either provide a link to a pivotal story or include the text "No pivotal story required." in your PR description.');
 }
 
-message(modified.join(", "))
+const modifiedComponentFiles = affectedFiles.filter(p => p.includes("components/"))
+
+if (modifiedComponentFiles.length > 0) {
+  message(modifiedComponentFiles.join(", "))
+}
