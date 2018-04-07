464c0d707f3968085e533664cd1fb2b63e51edeb
Jeff Verkoeyen <featherless@google.com>
2018-04-06 22:30:56 -0400

    Show modified files.

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,7 @@
 const {danger, warn, fail} = require('danger')
 
+const modified = danger.git.modified_files
+
 if (danger.github.pr.body.length < 10) {
   warn('Please provide a detailed description of the change.');
 }
@@ -8,3 +10,5 @@ if (!danger.github.pr.body.includes("pivotaltracker.com")
     || danger.github.pr.body.includes("No pivotal story required.")) {
   fail('Please either provide a link to a pivotal story or include the text "No pivotal story required." in your PR description.');
 }
+
+message(modified)
