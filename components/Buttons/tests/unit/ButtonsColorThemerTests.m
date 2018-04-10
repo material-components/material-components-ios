ac56c9024ad186f0868658c2086ac45d906e9907
Yarden Eitan <yar@google.com>
2018-04-10 14:08:00 +0300

    Update ButtonsColorThemerTests.m

--- components/Buttons/tests/unit/ButtonsColorThemerTests.m
+++ components/Buttons/tests/unit/ButtonsColorThemerTests.m
@@ -53,8 +53,8 @@
             CGColorEqualToColor([button titleColorForState:UIControlStateDisabled].CGColor,
                                 [colorScheme.onSurfaceColor colorWithAlphaComponent:0.26f].CGColor));
   XCTAssertEqual([button backgroundColorForState:UIControlStateNormal], colorScheme.primaryColor);
-  XCTAssertEqual([button backgroundColorForState:UIControlStateHighlighted], colorScheme.onPrimaryColor);
-  XCTAssertEqual([button backgroundColorForState:UIControlStateSelected], colorScheme.onPrimaryColor);
+  XCTAssertEqual([button backgroundColorForState:UIControlStateHighlighted], colorScheme.primaryColor);
+  XCTAssertEqual([button backgroundColorForState:UIControlStateSelected], colorScheme.primaryColor);
   XCTAssert(
       CGColorEqualToColor([button backgroundColorForState:UIControlStateDisabled].CGColor,
                           [colorScheme.onSurfaceColor colorWithAlphaComponent:0.12f].CGColor));
