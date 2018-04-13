2869609c2eb48826fa2c4de773746779c53224c9
Yarden Eitan <yar@google.com>
2018-04-13 16:02:11 +0300

    [Buttons] Implement a color themer for an MDCRaisedButton (#3335)
    Addition of a unit test, and updated example to use the new themer.
    
    Pivotal: https://www.pivotaltracker.com/story/show/156640797
    
    ![simulator screen shot - iphone 8 - 2018-04-12 at 11 47 17](https://user-images.githubusercontent.com/4066863/38667891-5d161162-3e4b-11e8-90ac-efa4c0a88bcb.png)

--- components/Buttons/src/ColorThemer/MDCButtonColorThemer.h
+++ components/Buttons/src/ColorThemer/MDCButtonColorThemer.h
@@ -26,6 +26,7 @@
  Applies a color scheme to theme to an MDCButton.
 
  @param colorScheme The color scheme to apply to @c button.
+ @param button An MDCButton instance to apply a color scheme.
  */
 + (void)applySemanticColorScheme:(nonnull id<MDCColorScheming>)colorScheme
                         toButton:(nonnull MDCButton *)button;
@@ -40,7 +41,16 @@
                     toFlatButton:(nonnull MDCFlatButton *)flatButton;
 
 /**
- Applies a color scheme to theme an MDCButton. Use a UIAppearance proxy to apply a color scheme to
+ Applies a color scheme to theme to an MDCRaisedButton.
+
+ @param colorScheme The color scheme to apply to @c raisedButton.
+ @param raisedButton An MDCRaisedButton instance to apply a color scheme.
+ */
++ (void)applySemanticColorScheme:(nonnull id<MDCColorScheming>)colorScheme
+                  toRaisedButton:(nonnull MDCRaisedButton *)raisedButton;
+
+/**
+ Applies a color scheme to theme a MDCButton. Use a UIAppearance proxy to apply a color scheme to
  all instances of MDCButton.
 
  This method will soon be deprecated. Consider using applySemanticColorScheme:colorScheme.
