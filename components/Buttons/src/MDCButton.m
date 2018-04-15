5a91f3e4b08a8f78566cbbe7afad3eb032359b61
yar <yarneo@gmail.com>
2018-04-15 12:45:01 +0300

    update

--- components/Buttons/src/MDCButton.m
+++ components/Buttons/src/MDCButton.m
@@ -328,8 +328,6 @@ static NSAttributedString *uppercaseAttributedString(NSAttributedString *string)
   // Set up ink layer.
   _inkView = [[MDCInkView alloc] initWithFrame:self.bounds];
   _inkView.usesLegacyInkRipple = NO;
-//  [self.layer insertSublayer:_inkView.layer below:self.imageView.layer];
-//  [self insertSubview:_inkView belowSubview:self.imageView];
   [self addSubview:_inkView];
   // UIButton has a drag enter/exit boundary that is outside of the frame of the button itself.
   // Because this is not exposed externally, we can't use -touchesMoved: to calculate when to
@@ -938,9 +936,8 @@ static NSAttributedString *uppercaseAttributedString(NSAttributedString *string)
 
   self.layer.shapeGenerator = shapeGenerator;
   self.layer.shadowMaskEnabled = NO;
-//  self.layer
   [self updateBackgroundColor];
-//  [self updateImageViewLayerZPosition];
+  [self updateImageViewLayerZPosition];
   [self updateInkForShape];
 }
 
