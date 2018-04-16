material-components/material-components-ios

commit 8af9e9fc8180fbcbc72250f963d2074772aedfcd

[ButtonBar] Allow customizing the inkColor of the buttons (#3250)
Make inkColor for buttons in MDCButtonBar customizable. Also expose an API on MDCNavigationBar to allow using the custom inkColor in AppBar.

closes #1660

components/ButtonBar/src/private/MDCAppBarButtonBarBuilder.m
@@ -88,7 +88,8 @@ static const UIEdgeInsets kImageOnlyButtonInset = {0, 12.0f, 0, 12.0f};
   MDCButtonBarButton *button = [[MDCButtonBarButton alloc] init];
   [button setBackgroundColor:[UIColor clearColor] forState:UIControlStateNormal];
   button.disabledAlpha = kDisabledButtonAlpha;
-
+  button.inkColor = buttonBar.inkColor ? buttonBar.inkColor : button.inkColor;
+  
   button.exclusiveTouch = YES;
 
   [MDCAppBarButtonBarBuilder configureButton:button fromButtonItem:buttonItem];

components/AppBar/examples/AppBarTypicalUseExample.m
@@ -40,6 +40,8 @@
     _appBar.headerViewController.headerView.shiftBehavior = MDCFlexibleHeaderShiftBehaviorEnabled;
     [_appBar.headerViewController.headerView hideViewWhenShifted:_appBar.headerStackView];
 
+    _appBar.navigationBar.inkColor = [UIColor colorWithWhite:0.9f alpha:0.1f];
+
     MDCSemanticColorScheme *colorScheme = [[MDCSemanticColorScheme alloc] init];
     [MDCAppBarColorThemer applySemanticColorScheme:colorScheme toAppBar:_appBar];
 

components/ButtonBar/src/MDCButtonBar.h
@@ -106,6 +106,13 @@ IB_DESIGNABLE
 @property(nonatomic) MDCButtonBarLayoutPosition layoutPosition;
 
 /**
+ The inkColor that is used for all buttons in the button bar.
+
+ If set to nil, button bar buttons use default ink color.
+ */
+@property(nonatomic, strong, nullable) UIColor *inkColor;
+
+/**
  Returns a height adhering to the Material spec for Bars and a width that is able to accommodate
  every item present in the `items` property. The provided size is ignored.
  */

components/ButtonBar/src/MDCButtonBar.m
@@ -222,6 +222,15 @@ static NSString *const MDCButtonBarButtonLayoutPositionKey = @"MDCButtonBarButto
   }
 }
 
+- (void)updateButtonsWithInkColor:(UIColor *)inkColor {
+  for (UIView *viewObj in _buttonViews) {
+    if ([viewObj isKindOfClass:[MDCButton class]]) {
+      MDCButton *buttonView = (MDCButton *)viewObj;
+      buttonView.inkColor = inkColor;
+    }
+  }
+}
+
 - (NSArray<UIView *> *)viewsForItems:(NSArray<UIBarButtonItem *> *)barButtonItems {
   if (![barButtonItems count]) {
     return nil;
@@ -421,6 +430,14 @@ static NSString *const MDCButtonBarButtonLayoutPositionKey = @"MDCButtonBarButto
   _buttonTitleBaseline = buttonTitleBaseline;
 
   [self setNeedsLayout];
+}
+
+- (void)setInkColor:(UIColor *)inkColor {
+  if (_inkColor == inkColor) {
+    return;
+  }
+  _inkColor = inkColor;
+  [self updateButtonsWithInkColor:_inkColor];
 }
 
 - (void)reloadButtonViews {

components/NavigationBar/src/MDCNavigationBar.h
@@ -113,6 +113,13 @@ IB_DESIGNABLE
  */
 @property(nonatomic, strong, nullable) UIColor *titleTextColor;
 
+/**
+ The inkColor that is used for all buttons in trailing and leading button bars.
+
+ If set to nil, button bar buttons use default ink color.
+ */
+@property(nonatomic, strong, nullable) UIColor *inkColor;
+
 /** The back button to be displayed, if any. */
 @property(nonatomic, strong, nullable) UIBarButtonItem *backItem;
 

components/NavigationBar/src/MDCNavigationBar.m
@@ -773,6 +773,15 @@ static NSString *const MDCNavigationBarTitleAlignmentKey = @"MDCNavigationBarTit
   [self setNeedsLayout];
 }
 
+- (void)setInkColor:(UIColor *)inkColor {
+  if (_inkColor == inkColor) {
+    return;
+  }
+  _inkColor = inkColor;
+  _leadingButtonBar.inkColor = inkColor;
+  _trailingButtonBar.inkColor = inkColor;
+}
+
 - (void)setObservedNavigationItem:(UINavigationItem *)navigationItem {
   @synchronized(_observedNavigationItemLock) {
     if (navigationItem == _observedNavigationItem) {

commit 65091d5e36c5ccfa4ff624bd8c0be3605020e6b9

[NavigationBar][ButtonBar] Restore the changed default inkColor
The issue was caused by #3250, where we override MDCFlatButton's appearance.inkColor with MDCButton's default inkColor.

https://github.com/material-components/material-components-ios/blob/5fe8cf2752dcafad7b07d6f8935910f1597ed963/components/Buttons/src/MDCFlatButton.m#L32

components/ButtonBar/src/private/MDCAppBarButtonBarBuilder.m
@@ -88,8 +88,10 @@ static const UIEdgeInsets kImageOnlyButtonInset = {0, 12.0f, 0, 12.0f};
   MDCButtonBarButton *button = [[MDCButtonBarButton alloc] init];
   [button setBackgroundColor:[UIColor clearColor] forState:UIControlStateNormal];
   button.disabledAlpha = kDisabledButtonAlpha;
-  button.inkColor = buttonBar.inkColor ? buttonBar.inkColor : button.inkColor;
-  
+  if (buttonBar.inkColor) {
+    button.inkColor = buttonBar.inkColor;
+  }
+
   button.exclusiveTouch = YES;
 
   [MDCAppBarButtonBarBuilder configureButton:button fromButtonItem:buttonItem];