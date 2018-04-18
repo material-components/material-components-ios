dd0c0123c658a88ffaa4e9d8c86191b747c38260
Mohammad Cazi <36271115+mohammadcazig@users.noreply.github.com>
2018-04-18 16:50:10 -0400

    [Chips] Adding Examples/Documentation for all chips types. (#3393)
    * Added example for Action Chips Type.
    Cleaned up/Documented the examples for Filter and Choice chips type.
    Added More documentation to MDCChipCollectionViewCell.
    
    * typo.
    
    PR: https://github.com/material-components/material-components-ios/pull/3393

--- components/Chips/examples/ChipsActionExampleViewController.m
+++ components/Chips/examples/ChipsActionExampleViewController.m
@@ -1,5 +1,5 @@
 /*
- Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -19,24 +19,24 @@
 #import "MaterialChips.h"
 #import "MDCChipViewColorThemer.h"
 
-@implementation ChipsChoiceExampleViewController {
-  MDCSemanticColorScheme *_colorScheme;
+@implementation ChipsActionExampleViewController {
+  UICollectionView *_collectionView;
   MDCChipView *_sizingChip;
+  MDCSemanticColorScheme *_colorScheme;
 }
 
 - (void)loadView {
   [super loadView];
-  self.view.backgroundColor = [UIColor whiteColor];
 
   _colorScheme = [[MDCSemanticColorScheme alloc] init];
-
-  // This is used to calculate the size of each chip based on the chip setup
-  _sizingChip = [[MDCChipView alloc] init];
 
   // Our preferred CollectionView Layout For chips
   MDCChipCollectionViewFlowLayout *layout = [[MDCChipCollectionViewFlowLayout alloc] init];
   layout.minimumInteritemSpacing = 10;
 
+
+  // Action chips should allow single selection, collection view default is based on single
+  // selection. Note that MDCChipCollectionViewCell manages the state of the chip accordingly.
   _collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
 
   // Since there is no scrolling turning off the delaysContentTouches makes the cells respond faster
@@ -46,9 +46,12 @@
   _collectionView.dataSource = self;
   _collectionView.delegate = self;
   _collectionView.backgroundColor = [UIColor whiteColor];
-  _collectionView.contentInset = UIEdgeInsetsMake(20, 20, 20, 20);
+  _collectionView.contentInset = UIEdgeInsetsMake(4, 8, 4, 8);
   [_collectionView registerClass:[MDCChipCollectionViewCell class]
       forCellWithReuseIdentifier:@"Cell"];
+
+  // This is used to calculate the size of each chip based on the chip setup
+  _sizingChip = [[MDCChipView alloc] init];
 
   [self.view addSubview:_collectionView];
 }
@@ -59,7 +62,6 @@
   _collectionView.frame = self.view.bounds;
 }
 
-
 - (NSInteger)collectionView:(UICollectionView *)collectionView
      numberOfItemsInSection:(NSInteger)section {
   return self.titles.count;
@@ -68,20 +70,32 @@
 - (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
                            cellForItemAtIndexPath:(NSIndexPath *)indexPath {
   MDCChipCollectionViewCell *cell =
-      [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+  [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
   MDCChipView *chipView = cell.chipView;
 
   // Customize Chip
   chipView.titleLabel.text = self.titles[indexPath.row];
 
+  // Apply Theming
+  [MDCChipViewColorThemer applySemanticColorScheme:_colorScheme toChipView:cell.chipView];
+
   return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+  // For action chips, we never want the chip to stay in selected state.
+  // Other possible apporaches would be relying on theming or Customizing collectionViewCell
+  // selected state.
+  [collectionView deselectItemAtIndexPath:indexPath animated:NO];
+
+  // Do the action related to the chip
+  [self setTitle:[NSString stringWithFormat:@"Action %d", (int)indexPath.row]];
+
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView
                   layout:(UICollectionViewLayout*)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-
-  // The size of the chip depends on title here.
   _sizingChip.titleLabel.text = self.titles[indexPath.row];
   return [_sizingChip sizeThatFits:collectionView.bounds.size];
 }
@@ -89,14 +103,14 @@
 - (NSArray *)titles {
   if (!_titles) {
     _titles = @[
-      @"The Bronx",
-      @"Brooklyn",
-      @"Manhattan",
-      @"Queens",
-      @"Staten Island",
-    ];
+                @"Change Title to Action 0",
+                @"Change Title to Action 1",
+                @"Change Title to Action 2",
+                @"Change Title to Action 3",
+                ];
   }
   return _titles;
 }
 
 @end
+
