1fa5bdf0b30f6c3741c22585a895ec9803b4b1e9
Randall Li <randallli@google.com>
2018-04-19 14:35:22 -0400

    [Button] Changed text button disabled to do 37% opacity, Ink to use onSurface 16% (#3406)
    Changed typical use example to use MDCTextButtonThemer.
    
    https://www.pivotaltracker.com/story/show/156402729
    
    before:
    ![simulator screen shot - iphone 8 plus - 2018-04-19 at 12 12 57](https://user-images.githubusercontent.com/943565/39004311-096558cc-43cb-11e8-8338-5ab4deb7ed90.png)
    
    after:
    ![simulator screen shot - iphone 8 plus - 2018-04-19 at 14 00 25](https://user-images.githubusercontent.com/943565/39009598-0fc7c3e4-43da-11e8-9215-1184781ca759.png)
    
    Ink screenshots:
    before:
    ![simulator screen shot - iphone 8 plus - 2018-04-19 at 12 13 58](https://user-images.githubusercontent.com/943565/39004389-334189ae-43cb-11e8-86d1-dd4b0e45e731.png)
    
    after:
    ![simulator screen shot - iphone 8 plus - 2018-04-19 at 14 00 27](https://user-images.githubusercontent.com/943565/39009606-14209cb8-43da-11e8-9e0b-d1610d45fed2.png)

--- components/Buttons/tests/unit/ButtonsColorThemerTests.m
+++ components/Buttons/tests/unit/ButtonsColorThemerTests.m
@@ -44,7 +44,7 @@ static const CGFloat kEpsilonAccuracy = 0.001f;
                         UIColor.clearColor);
   XCTAssertEqualObjects([button titleColorForState:UIControlStateNormal], colorScheme.primaryColor);
   XCTAssertEqualObjects([button titleColorForState:UIControlStateDisabled],
-                        [colorScheme.onSurfaceColor colorWithAlphaComponent:0.26f]);
+                        [colorScheme.onSurfaceColor colorWithAlphaComponent:0.37f]);
   NSUInteger maximumStateValue = UIControlStateNormal | UIControlStateSelected |
       UIControlStateHighlighted | UIControlStateDisabled;
   for (NSUInteger state = 0; state <= maximumStateValue; ++state) {
@@ -163,8 +163,10 @@ static const CGFloat kEpsilonAccuracy = 0.001f;
     } else {
       XCTAssert(
           CGColorEqualToColor([button titleColorForState:state].CGColor,
-                              [colorScheme.onSurfaceColor colorWithAlphaComponent:0.26f].CGColor));
-      XCTAssertEqual([button backgroundColorForState:state], UIColor.clearColor);
+                              [colorScheme.onSurfaceColor colorWithAlphaComponent:0.37f].CGColor),
+          @"state:%lu", (unsigned long)state);
+      XCTAssertEqual([button backgroundColorForState:state], UIColor.clearColor, @"state:%lu",
+                     (unsigned long)state);
     }
   }
   XCTAssertEqualWithAccuracy(button.disabledAlpha, 1.f, 0.001f);
