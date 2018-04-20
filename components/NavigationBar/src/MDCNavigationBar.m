817050c8fa0af3c9c2d38febea8584fcc0729efb
Jeff Verkoeyen <featherless@google.com>
2018-04-19 20:18:57 -0400

    [NavigationBar] Add a stateful buttons title color API.
    This API allows a client to customize the title color for buttons for a given control state.
    
    Related to https://www.pivotaltracker.com/story/show/156934328
    
    Screenshot shows a button with a custom normal and highlighted state. The button on the left is highlighted.
    
    ![simulator screen shot - iphone se - 2018-04-19 at 19 51 59](https://user-images.githubusercontent.com/45670/39024065-786db638-440c-11e8-97e8-f6bf3e6c2af7.png)

--- components/NavigationBar/src/MDCNavigationBar.m
+++ components/NavigationBar/src/MDCNavigationBar.m
@@ -718,6 +718,15 @@ static NSString *const MDCNavigationBarTitleAlignmentKey = @"MDCNavigationBarTit
   return [_leadingButtonBar buttonsTitleFontForState:state];
 }
 
+- (void)setButtonsTitleColor:(UIColor *)color forState:(UIControlState)state {
+  [_leadingButtonBar setButtonsTitleColor:color forState:state];
+  [_trailingButtonBar setButtonsTitleColor:color forState:state];
+}
+
+- (UIColor *)buttonsTitleColorForState:(UIControlState)state {
+  return [_leadingButtonBar buttonsTitleColorForState:state];
+}
+
 - (void)setLeadingBarButtonItems:(NSArray<UIBarButtonItem *> *)leadingBarButtonItems {
   _leadingBarButtonItems = [leadingBarButtonItems copy];
   _leadingButtonBar.items = [self mdc_buttonItemsForLeadingBar];
