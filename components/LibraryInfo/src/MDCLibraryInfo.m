65c2bfd02f5dc36fd094b3025a324e69d3b2d47b
Ali Rabbani <rabb@google.com>
2018-04-23 15:39:06 -0400

    bump the version

--- components/LibraryInfo/src/MDCLibraryInfo.m
+++ components/LibraryInfo/src/MDCLibraryInfo.m
@@ -21,7 +21,7 @@
 // This string is updated automatically as a part of the release process and should not be edited
 // manually. Do not rename this constant or change the formatting without updating the release
 // scripts.
-static NSString const *MDCLibraryInfoVersionString = @"54.11.0";
+static NSString const *MDCLibraryInfoVersionString = @"54.11.1";
 
 @implementation MDCLibraryInfo
 
