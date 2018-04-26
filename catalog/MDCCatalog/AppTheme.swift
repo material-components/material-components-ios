98abb252a216afc516a36143a5c65b19cb7f4697
Jeff Verkoeyen <featherless@google.com>
2018-04-26 09:27:53 -0400

    Add missing import.

--- catalog/MDCCatalog/AppTheme.swift
+++ catalog/MDCCatalog/AppTheme.swift
@@ -16,6 +16,7 @@
 
 import Foundation
 import MaterialComponents.MaterialColorScheme
+import MaterialComponents.MaterialTypographyScheme
 
 final class AppTheme {
   let colorScheme: MDCColorScheming
