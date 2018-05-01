06a681c032b9a96143fd7a7d9107e9d2c131a838
Yarden Eitan <yar@google.com>
2018-05-01 14:04:07 -0400

    update

---
+++ components/CollectionCells/examples/supplemental/MDCCollectionViewListCell.h
@@ -0,0 +1,56 @@
+/*
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+#import "MDCCollectionViewCell.h"
+
+@interface MDCCollectionViewListCell : MDCCollectionViewCell
+
+/**
+ A text label. Typically this will be the first line of text in the cell.
+ */
+@property(nonatomic, readonly, strong, nullable) UILabel *titleLabel;
+
+/**
+ A detail text label. Typically this will be the second line of text in the cell.
+ */
+@property(nonatomic, readonly, strong, nullable) UILabel *detailsTextLabel;
+
+/**
+ An image view on the leading side of cell. Default leading padding is 16.0f.
+ */
+@property(nonatomic, readonly, strong, nullable) UIImageView *imageView;
+
+/*
+ Indicates whether the view's contents should automatically update their font when the device’s
+ UIContentSizeCategory changes.
+
+ This property is modeled after the adjustsFontForContentSizeCategory property in the
+ UIContentSizeCategoryAdjusting protocol added by Apple in iOS 10.0.
+
+ Default value is NO.
+ */
+@property(nonatomic, readwrite, setter=mdc_setAdjustsFontForContentSizeCategory:)
+BOOL mdc_adjustsFontForContentSizeCategory;
+
+@property(nonatomic, strong, nullable) UIFont *titleFont;
+@property(nonatomic, strong, nullable) UIFont *detailsFont;
+
+- (void)setCellWidth:(CGFloat)width;
+
+- (void)setImage:(nullable UIImage *)image;
+
+@end
