e81453a1a011e54d21ab8ee8b00f738c2c5bca53
Yarden Eitan <yar@google.com>
2018-05-02 20:24:42 -0400

    [Lists] Added initial example for implementing the List component (#3646)
    Pivotal: https://www.pivotaltracker.com/story/show/155597168
    
    This example uses auto layout constraints to show a way to use UICollectionViewCells with a vanilla UICollectionViewController to achieve a List Cell that is self-sizing, supports dynamic type, RTL, and orientation changes.
    
    This example also supports the correct layout with and without an image set in the cell, and also with an unset titleLabel.text and an unset detailsLabel.text.

--- catalog/MDCCatalog/AppDelegate.swift
+++ catalog/MDCCatalog/AppDelegate.swift
@@ -99,7 +99,7 @@ extension UINavigationController {
     // https://github.com/material-components/material-components-ios/issues/367 is resolved.
     viewController.title = title
     let headerView = container.appBar.headerViewController.headerView
-    if let collectionVC = viewController as? MDCCollectionViewController {
+    if let collectionVC = viewController as? UICollectionViewController {
       headerView.trackingScrollView = collectionVC.collectionView
     } else if let scrollView = viewController.view as? UIScrollView {
       headerView.trackingScrollView = scrollView
