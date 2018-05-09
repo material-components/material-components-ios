a8f3d98b2087a3c80478c30fb7e71c84bcaf5d62
Jeff Verkoeyen <featherless@google.com>
2018-05-08 22:00:45 -0400

    Simpler description.

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,5 @@
 const {danger, warn, fail} = require('danger')
 
 if (danger.github.pr.body.length < 10) {
-  warn('Please provide a detailed description of the change.');
+  warn('This PR does not include a description.');
 }
