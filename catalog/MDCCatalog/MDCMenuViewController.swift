2ca48c87ee94c43f9eda6879ea576138a01b8561
Jeff Verkoeyen <featherless@google.com>
2018-05-18 07:51:31 -0400

    Add missing import.

--- catalog/MDCCatalog/MDCMenuViewController.swift
+++ catalog/MDCCatalog/MDCMenuViewController.swift
@@ -19,6 +19,7 @@ import MaterialComponents.MDCIcons
 import MaterialComponents.MaterialIcons_ic_settings
 import MaterialComponents.MaterialIcons_ic_color_lens
 import MaterialComponents.MaterialIcons_ic_help_outline
+import MaterialComponents.MaterialLibraryInfo
 
 class MDCMenuViewController: UITableViewController {
 
