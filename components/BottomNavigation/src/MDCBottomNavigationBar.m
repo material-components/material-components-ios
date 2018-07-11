caa6b9dd9e262efedd322137d69fcb6f175d9545
Andrew Overton <andrewoverton@google.com>
2018-07-11 17:33:12 -0400

    Give MDCBottomNavigationBar a TabBar accessibilityTrait

--- components/BottomNavigation/src/MDCBottomNavigationBar.m
+++ components/BottomNavigation/src/MDCBottomNavigationBar.m
@@ -191,6 +191,9 @@ static NSString *const kMDCBottomNavigationBarOfAnnouncement = @"of";
                                      UIViewAutoresizingFlexibleRightMargin);
   _containerView.clipsToBounds = YES;
   [self addSubview:_containerView];
+#if defined(__IPHONE_10_0) && (__IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_10_0)
+  _containerView.accessibilityTraits = UIAccessibilityTraitTabBar;
+#endif
   [self setElevation:MDCShadowElevationBottomNavigationBar];
   _itemViews = [NSMutableArray array];
   _itemTitleFont = [UIFont mdc_standardFontForMaterialTextStyle:MDCFontTextStyleCaption];
