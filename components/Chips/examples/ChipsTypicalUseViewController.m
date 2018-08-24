654a292c7bc5c97cd78ec34c050c7dc1caf3cd82
Wenyu Zhang <wenyuz@google.com>
2018-08-24 16:27:08 -0400

    [Chips] Make collectionView in Chip Demo respect safe area

--- components/Chips/examples/ChipsTypicalUseViewController.m
+++ components/Chips/examples/ChipsTypicalUseViewController.m
@@ -79,6 +79,11 @@
                                            selector:@selector(contentSizeCategoryDidChange)
                                                name:UIContentSizeCategoryDidChangeNotification
                                              object:nil];
+#if defined(__IPHONE_11_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0)
+    if (@available(iOS 11.0, *)) {
+      self.collectionView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAlways;
+    }
+#endif
   [self updateClearButton];
 }
 
