01021b9e8b0f1d748d8f59bf18263e68f5cae2a9
Andrew Overton <andrewoverton@google.com>
2018-09-04 11:34:56 -0400

    [List] Add Self Sizing Stereo Cell (#4953)
    * Four prototypes: (#4382)
    
    Two have manual layout and two have auto layout.
    Two have configurable supporting views, two don't.
    
    * Manual Layout prototype 3
    
    * Fix project by adding initial List Item Cell Demo
    
    * Add other List Item example
    
    * Make view properties readonly and make cellWidth private
    
    * Make cellWidth public again and make more efficient
    
    * Rename MDCListItemCell to MDCBasicStereoCell
    
    * Rename to MDCSelfSizingStereoCell
    
    * Small nits
    
    * Set cellWidth in preferredLayoutAttributes
    
    * Some Jeff feedback
    
    * Extract layout code
    
    * Delete all the prototypes from forever ago
    
    * Fix copyright messages
    
    * Get rid of __IPHONE_11_0 checks
    
    * PR feedback
    
    * Move layout to its own file
    
    * Add dependencies
    
    * Tweak documentation
    
    * Fix copyright stanzas
    
    * Clang format
    
    * Move numberOfLines logic to cell

--- components/List/examples/MDCSelfSizingStereoCellExample.h
+++ components/List/examples/MDCSelfSizingStereoCellExample.h
@@ -12,5 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCBaseCell.h"
-#import "MDCSelfSizingStereoCell.h"
+#import <UIKit/UIKit.h>
+
+@interface MDCSelfSizingStereoCellExample : UIViewController
+
+@end
