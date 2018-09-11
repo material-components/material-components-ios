1589249ff56ca31313b37eab57cbd97b6932468c
Robert Moore <rsmoore@google.com>
2018-09-11 14:52:07 -0400

    [NavigationDrawer] Fix Swift imports in examples. (#5096)
    The Swift examples were not compiling internally because they were
    missing many imports.

--- components/NavigationDrawer/examples/BottomDrawerWithScrollableContentExample.swift
+++ components/NavigationDrawer/examples/BottomDrawerWithScrollableContentExample.swift
@@ -14,6 +14,7 @@
 
 import UIKit
 import MaterialComponentsAlpha.MaterialNavigationDrawer
+import MaterialComponents.MaterialColorScheme
 
 class BottomDrawerWithScrollableContentExample: UIViewController {
   var colorScheme = MDCSemanticColorScheme()
--- components/NavigationDrawer/examples/supplemental/BottomDrawerSupplemental.swift
+++ components/NavigationDrawer/examples/supplemental/BottomDrawerSupplemental.swift
@@ -14,6 +14,7 @@
 
 import UIKit
 import MaterialComponentsAlpha.MaterialNavigationDrawer
+import MaterialComponents.MaterialColorScheme
 
 class DrawerContentViewController: UIViewController {
   var colorScheme = MDCSemanticColorScheme()
