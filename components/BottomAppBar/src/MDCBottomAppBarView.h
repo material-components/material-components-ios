44b76e89a05cb11a81832a0e861ad6276a2c373e
Wenyu Zhang <wenyuz@google.com>
2018-09-17 17:21:45 -0400

    [BottomAppBar] enable customizing the FAB position

--- components/BottomAppBar/src/MDCBottomAppBarView.h
+++ components/BottomAppBar/src/MDCBottomAppBarView.h
@@ -61,6 +61,11 @@ typedef NS_ENUM(NSInteger, MDCBottomAppBarFloatingButtonPosition) {
 @property(nonatomic, strong, nonnull, readonly) MDCFloatingButton *floatingButton;
 
 /**
+ The offset from the center of the floating button to the top edge of the navigation bar
+ */
+@property(nonatomic, assign) CGFloat floatingButtonCenterToNavigationBarTopOffset;
+
+/**
  Navigation bar items that precede the floating action button. There is no limit to the number of
  buttons that can be added, but button bar width overflow is not handled.
  */
