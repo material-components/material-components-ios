6889f3a1d49cc4b70f879cf7734205d96482b28c
= <codyleeweaver@google.com>
2018-09-21 17:49:17 -0400

    Fix error in test

--- components/ActionSheet/tests/unit/MDCActionSheetTableCellTest.m
+++ components/ActionSheet/tests/unit/MDCActionSheetTableCellTest.m
@@ -113,8 +113,8 @@
 
   // Then
   for (MDCActionSheetItemTableViewCell *cell in cells) {
-    XCTAssertEqualObjects(cell.actionImageView.tintColor, [[UIColor alloc] initWithWhite:0
-                                                                                   alpha:0.14f]);
+    XCTAssertEqualObjects(cell.inkTouchController.defaultInkView.inkColor,
+                          [[UIColor alloc] initWithWhite:0 alpha:0.14f]);
   }
 }
 
