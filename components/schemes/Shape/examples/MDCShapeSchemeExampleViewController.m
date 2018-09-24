b26519a1a64ed7767b69df8b7c169216aca68d16
Yarden Eitan <yar@google.com>
2018-09-24 14:08:48 -0400

    update

--- components/schemes/Shape/examples/MDCShapeSchemeExampleViewController.m
+++ components/schemes/Shape/examples/MDCShapeSchemeExampleViewController.m
@@ -16,15 +16,152 @@
 
 #import "MaterialColorScheme.h"
 #import "MaterialShapeScheme.h"
+#import "MaterialShapeLibrary.h"
 #import "MaterialTypographyScheme.h"
 
 @interface MDCShapeSchemeExampleViewController ()
 @property(nonatomic, strong) MDCSemanticColorScheme *colorScheme;
 @property(nonatomic, strong) MDCShapeScheme *shapeScheme;
 @property(nonatomic, strong) MDCTypographyScheme *typographyScheme;
+@property (weak, nonatomic) IBOutlet MDCShapedView *smallSurfaceShape;
+@property (weak, nonatomic) IBOutlet MDCShapedView *mediumSurfaceShape;
+@property (weak, nonatomic) IBOutlet MDCShapedView *largeSurfaceShape;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *smallSurfaceType;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *mediumSurfaceType;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *largeSurfaceType;
+@property (weak, nonatomic) IBOutlet UISlider *smallSurfaceValue;
+@property (weak, nonatomic) IBOutlet UISlider *mediumSurfaceValue;
+@property (weak, nonatomic) IBOutlet UISlider *largeSurfaceValue;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *includeBaselineOverridesToggle;
+@property (weak, nonatomic) IBOutlet UIView *componentContentView;
 @end
 
 @implementation MDCShapeSchemeExampleViewController
+
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+    [self commonShapeSchemeExampleInit];
+  }
+  return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+  self = [super initWithCoder:coder];
+  if (self) {
+    [self commonShapeSchemeExampleInit];
+  }
+  return self;
+}
+
+- (void)commonShapeSchemeExampleInit {
+  _colorScheme = [[MDCSemanticColorScheme alloc] init];
+  _shapeScheme = [[MDCShapeScheme alloc] init];
+  _typographyScheme = [[MDCTypographyScheme alloc] init];
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  _smallSurfaceShape.shapeGenerator = [[MDCRectangleShapeGenerator alloc] init];
+  _mediumSurfaceShape.shapeGenerator = [[MDCRectangleShapeGenerator alloc] init];
+  _largeSurfaceShape.shapeGenerator = [[MDCRectangleShapeGenerator alloc] init];
+
+  [self applySchemeColors];
+}
+
+- (void)applySchemeColors {
+  _smallSurfaceShape.backgroundColor = _colorScheme.primaryColor;
+  _mediumSurfaceShape.backgroundColor = _colorScheme.primaryColor;
+  _largeSurfaceShape.backgroundColor = _colorScheme.primaryColor;
+
+  [_smallSurfaceType setTintColor:_colorScheme.primaryColor];
+  [_mediumSurfaceType setTintColor:_colorScheme.primaryColor];
+  [_largeSurfaceType setTintColor:_colorScheme.primaryColor];
+  [_includeBaselineOverridesToggle setTintColor:_colorScheme.primaryColor];
+}
+
+- (void)updateShapeSchemeValues {
+  MDCRectangleShapeGenerator *smallRect =
+      (MDCRectangleShapeGenerator *)_smallSurfaceShape.shapeGenerator;
+  smallRect.topLeftCorner = _shapeScheme.smallSurfaceShape.topLeftCorner;
+  smallRect.topRightCorner = _shapeScheme.smallSurfaceShape.topRightCorner;
+  smallRect.bottomLeftCorner = _shapeScheme.smallSurfaceShape.bottomLeftCorner;
+  smallRect.bottomRightCorner = _shapeScheme.smallSurfaceShape.bottomRightCorner;
+  _smallSurfaceShape.shapeGenerator = smallRect;
+
+  MDCRectangleShapeGenerator *mediumRect =
+      (MDCRectangleShapeGenerator *)_mediumSurfaceShape.shapeGenerator;
+  mediumRect.topLeftCorner = _shapeScheme.mediumSurfaceShape.topLeftCorner;
+  mediumRect.topRightCorner = _shapeScheme.mediumSurfaceShape.topRightCorner;
+  mediumRect.bottomLeftCorner = _shapeScheme.mediumSurfaceShape.bottomLeftCorner;
+  mediumRect.bottomRightCorner = _shapeScheme.mediumSurfaceShape.bottomRightCorner;
+  _mediumSurfaceShape.shapeGenerator = mediumRect;
+
+  MDCRectangleShapeGenerator *largeRect =
+      (MDCRectangleShapeGenerator *)_largeSurfaceShape.shapeGenerator;
+  largeRect.topLeftCorner = _shapeScheme.largeSurfaceShape.topLeftCorner;
+  largeRect.topRightCorner = _shapeScheme.largeSurfaceShape.topRightCorner;
+  largeRect.bottomLeftCorner = _shapeScheme.largeSurfaceShape.bottomLeftCorner;
+  largeRect.bottomRightCorner = _shapeScheme.largeSurfaceShape.bottomRightCorner;
+  _largeSurfaceShape.shapeGenerator = largeRect;
+}
+
+- (MDCShapeCategory *)changedCategoryFromType:(UISegmentedControl *)sender
+                                     andValue:(UISlider *)slider {
+  MDCShapeCategory *changedCategory;
+  if ([[sender titleForSegmentAtIndex:sender.selectedSegmentIndex] isEqualToString:@"Cut"]) {
+    changedCategory = [[MDCShapeCategory alloc] initCornersWithFamily:MDCShapeCornerFamilyAngled
+                                                              andSize:slider.value];
+  } else {
+    // Rounded
+    changedCategory = [[MDCShapeCategory alloc] initCornersWithFamily:MDCShapeCornerFamilyRounded
+                                                              andSize:slider.value];
+  }
+  return changedCategory;
+}
+
+- (IBAction)smallSurfaceTypeChanged:(UISegmentedControl *)sender {
+  _shapeScheme.smallSurfaceShape = [self changedCategoryFromType:sender
+                                                        andValue:_smallSurfaceValue];
+  [self updateShapeSchemeValues];
+}
+
+- (IBAction)smallSurfaceValueChanged:(UISlider *)sender {
+  _shapeScheme.smallSurfaceShape = [self changedCategoryFromType:_smallSurfaceType andValue:sender];
+
+  [self updateShapeSchemeValues];
+}
+
+- (IBAction)mediumSurfaceTypeChanged:(UISegmentedControl *)sender {
+  _shapeScheme.mediumSurfaceShape = [self changedCategoryFromType:sender
+                                                         andValue:_mediumSurfaceValue];
+  [self updateShapeSchemeValues];
+}
+
+- (IBAction)mediumSurfaceValueChanged:(UISlider *)sender {
+  _shapeScheme.mediumSurfaceShape = [self changedCategoryFromType:_mediumSurfaceType
+                                                         andValue:sender];
+
+  [self updateShapeSchemeValues];
+}
+
+- (IBAction)largeSurfaceTypeChanged:(UISegmentedControl *)sender {
+  _shapeScheme.largeSurfaceShape = [self changedCategoryFromType:sender
+                                                        andValue:_largeSurfaceValue];
+  [self updateShapeSchemeValues];
+}
+
+- (IBAction)largeSurfaceValueChanged:(UISlider *)sender {
+  _shapeScheme.largeSurfaceShape = [self changedCategoryFromType:_largeSurfaceType andValue:sender];
+
+  [self updateShapeSchemeValues];
+}
+
+- (IBAction)baselineOverrideValueChanged:(UISegmentedControl *)sender {
+}
 
 @end
 
@@ -38,6 +175,7 @@
     @"primaryDemo": @YES,
     @"presentable": @NO,
     @"storyboardName": @"MDCShapeSchemeExampleViewController",
+    @"debug": @YES,
   };
 }
 
