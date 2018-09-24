c47e0136a368604be9facdebee6fe574596092cc
Andrew Overton <andrewoverton@google.com>
2018-09-24 11:16:57 -0400

    Handle presentation differently depending on whether presentedViewController is an MDCBottomDrawerViewController or not

--- components/NavigationDrawer/src/MDCBottomDrawerPresentationController.m
+++ components/NavigationDrawer/src/MDCBottomDrawerPresentationController.m
@@ -37,6 +37,16 @@ static UIColor *DrawerOverlayBackgroundColor(void) {
 
 @implementation MDCBottomDrawerPresentationController
 
+- (UIView *)presentedView {
+  if ([self.presentedViewController isKindOfClass:[MDCBottomDrawerViewController class]]) {
+    return super.presentedView;
+  } else {
+    // Override the presentedView property getter to return our container view controller's
+    // view instead of the default.
+    return self.bottomDrawerContainerViewController.view;
+  }
+}
+
 - (void)presentationTransitionWillBegin {
   MDCBottomDrawerContainerViewController *bottomDrawerContainerViewController =
       [[MDCBottomDrawerContainerViewController alloc]
@@ -67,7 +77,12 @@ static UIColor *DrawerOverlayBackgroundColor(void) {
   self.scrimView.accessibilityTraits |= UIAccessibilityTraitButton;
 
   [self.containerView addSubview:self.scrimView];
-  [self.presentedView addSubview:self.bottomDrawerContainerViewController.view];
+
+  if ([self.presentedViewController isKindOfClass:[MDCBottomDrawerViewController class]]) {
+    [self.presentedView addSubview:self.bottomDrawerContainerViewController.view];
+  } else {
+    [self.containerView addSubview:self.bottomDrawerContainerViewController.view];
+  }
 
   id<UIViewControllerTransitionCoordinator> transitionCoordinator =
       [[self presentingViewController] transitionCoordinator];
