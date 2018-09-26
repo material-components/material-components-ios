a6b20fc30d1546f766a8810c2c9ee43302c99834
Randall Li <randallli@google.com>
2018-09-26 12:19:05 -0400

    [appbar] Corrected import of example imports

--- components/AppBar/examples/AppBarManualTabsExample.swift
+++ components/AppBar/examples/AppBarManualTabsExample.swift
@@ -21,6 +21,7 @@ import MaterialComponents.MaterialAppBar_ColorThemer
 import MaterialComponents.MaterialAppBar_TypographyThemer
 import MaterialComponents.MaterialTabs
 import MaterialComponents.MaterialTypographyScheme
+import MaterialComponents.MaterialFlexibleHeader_CanAlwaysExpandToMaximumHeight
 
 // An example that demonstrates the behavior of an App Bar with Tabs and manually swapped tab view
 // controllers. This example is distinct from a typical tab bar view controller in that it does not
