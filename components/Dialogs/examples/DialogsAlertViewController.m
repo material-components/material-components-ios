ec9ef84e832d15bcb8953ae3c62be2a3628e663a
Randall Li <randallli@google.com>
2018-09-26 09:52:15 -0400

    [Dialog/Alert] Using umbrella header of themer

--- components/Dialogs/examples/DialogsAlertViewController.m
+++ components/Dialogs/examples/DialogsAlertViewController.m
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCAlertControllerThemer.h"
-#import "MDCAlertScheme.h"
+#import "MaterialDialogs+DialogThemer.h"
 #import "MaterialButtons.h"
 #import "MaterialDialogs.h"
 #import "supplemental/DialogsAlertViewControllerSupplemental.h"
