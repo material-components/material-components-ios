d297d2f031f120ecec65c061f14e9193fca7eebe
Randall Li <randallli@google.com>
2018-09-26 10:34:34 -0400

    [Dialog/alert] Added import of collections

--- components/Dialogs/examples/DialogsAlertCustomizationViewController.swift
+++ components/Dialogs/examples/DialogsAlertCustomizationViewController.swift
@@ -15,6 +15,7 @@
 import Foundation
 import MaterialComponents.MaterialButtons
 import MaterialComponents.MaterialDialogs
+import MaterialComponents.MaterialCollections
 
 class CustomAlertViewController: UIViewController {
 
