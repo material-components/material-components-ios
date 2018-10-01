54169d063a212955f9c358faf36612ad3594bd5d
Ali Rabbani <rabb@google.com>
2018-10-01 11:00:52 -0400

    [FlexibleHeader] Clear the shadow opacity when a nil trackingScrollView is set. (#5282)
    Fixes #5281

--- components/FlexibleHeader/src/MDCFlexibleHeaderView.m
+++ components/FlexibleHeader/src/MDCFlexibleHeaderView.m
@@ -867,12 +867,6 @@ static inline MDCFlexibleHeaderShiftBehavior ShiftBehaviorForCurrentAppContext(
 #pragma mark Shift Accumulator
 
 - (void)fhv_accumulatorDidChange {
-  if (!_trackingScrollView) {
-    // Set the shadow opacity directly.
-    self.layer.shadowOpacity = _visibleShadowOpacity;
-    return;
-  }
-
   CGRect frame = self.frame;
 
   CGFloat frameBottomEdge = [self fhv_projectedHeaderBottomEdge];
@@ -1466,7 +1460,7 @@ static BOOL isRunningiOS10_3OrAbove() {
 
     // When the tracking scroll view is cleared we need a shadow update.
     if (!self.trackingScrollView) {
-      [self fhv_accumulatorDidChange];
+      self.layer.shadowOpacity = 0;
     }
   };
   if (wasTrackingScrollView) {
