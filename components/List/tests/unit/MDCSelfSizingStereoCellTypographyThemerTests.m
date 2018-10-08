4afe7a81f1bfb9d035d3e2290ce1d7643db84480
Andrew Overton <andrewoverton@google.com>
2018-10-08 10:40:52 -0400

    [List] Add tests for List themers (#5315)
    Adds testing to the MDCSelfSizingStereoCell's typography and color themers. Closes #5314.

---
+++ components/List/tests/unit/MDCSelfSizingStereoCellTypographyThemerTests.m
@@ -0,0 +1,35 @@
+// Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "MaterialList+TypographyThemer.h"
+#import "MaterialList.h"
+
+#import <XCTest/XCTest.h>
+
+@interface MDCSelfSizingStereoCellTypographyThemerTests : XCTestCase
+
+@end
+
+@implementation MDCSelfSizingStereoCellTypographyThemerTests
+
+- (void)testApplyingTypographyScheme {
+  MDCSelfSizingStereoCell *cell = [[MDCSelfSizingStereoCell alloc] initWithFrame:CGRectZero];
+  MDCTypographyScheme *typographyScheme = [[MDCTypographyScheme alloc] init];
+  [MDCSelfSizingStereoCellTypographyThemer applyTypographyScheme:typographyScheme
+                                          toSelfSizingStereoCell:cell];
+  XCTAssertEqualObjects(cell.titleLabel.font, typographyScheme.subtitle1);
+  XCTAssertEqualObjects(cell.detailLabel.font, typographyScheme.body2);
+}
+
+@end
