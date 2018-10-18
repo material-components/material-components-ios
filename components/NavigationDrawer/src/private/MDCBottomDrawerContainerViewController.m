5b92ce2a140d5b1ab703e610bd0a8b70e558bab8
Yarden Eitan <yar@google.com>
2018-10-18 11:53:25 -0400

    [NavigationDrawer] Present the drawer in full screen when in a compact size class (#5443)
    When the Drawer is presented in a mobile landscape presentation (or a compact size class), it should be presented in full screen based on design guidelines.
    
    There is an issue opened regarding a full screen presentation with 32 bit devices that needs to be addressed: https://github.com/material-components/material-components-ios/issues/5442

--- components/NavigationDrawer/src/private/MDCBottomDrawerContainerViewController.m
+++ components/NavigationDrawer/src/private/MDCBottomDrawerContainerViewController.m
@@ -37,18 +37,6 @@ static UIColor *DrawerShadowColor(void) {
   return [[UIColor blackColor] colorWithAlphaComponent:0.2f];
 }
 
-/**
- The drawer height factor defines how much percentage of the screen space the drawer will take up
- when displayed. The expected range is 0 - 1 (0% - 100%).
-
- Default value is 0.5. If VoiceOver is enabled, the default value becomes 1.0.
- */
-static CGFloat InitialDrawerHeightFactor(void) {
-  BOOL enableAccessibilityMode =
-      UIAccessibilityIsVoiceOverRunning() || UIAccessibilityIsSwitchControlRunning();
-  return enableAccessibilityMode ? 1.0f : 0.5f;
-}
-
 @interface MDCBottomDrawerContainerViewController (LayoutCalculations)
 
 /**
@@ -290,6 +278,25 @@ static CGFloat InitialDrawerHeightFactor(void) {
   return UIAccessibilityIsVoiceOverRunning() || UIAccessibilityIsSwitchControlRunning();
 }
 
+- (BOOL)isMobileLandscape {
+  return self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassCompact;
+}
+
+- (BOOL)shouldPresentFullScreen {
+  return [self isAccessibilityMode] || [self isMobileLandscape];
+}
+
+/**
+ The drawer height factor defines how much percentage of the screen space the drawer will take up
+ when displayed. The expected range is 0 - 1 (0% - 100%).
+
+ Default value is 0.5. If VoiceOver is enabled, or the mobile device is in landscape,
+ the default value becomes 1.0.
+ */
+- (CGFloat)initialDrawerFactor {
+  return [self shouldPresentFullScreen] ? 1.0f : 0.5f;
+}
+
 - (void)addScrollViewObserver {
   if (self.scrollViewObserved) {
     return;
@@ -398,7 +405,7 @@ static CGFloat InitialDrawerHeightFactor(void) {
     }
   } else {
     contentViewFrame.size.height = self.contentViewController.preferredContentSize.height;
-    if ([self isAccessibilityMode]) {
+    if ([self shouldPresentFullScreen]) {
       contentViewFrame.size.height =
           MAX(contentViewFrame.size.height,
               self.presentingViewBounds.size.height - self.topHeaderHeight);
@@ -629,7 +636,7 @@ static CGFloat InitialDrawerHeightFactor(void) {
   CGFloat contentHeaderHeight = self.contentHeaderHeight;
   CGFloat containerHeight = self.presentingViewBounds.size.height;
   CGFloat contentHeight = self.contentViewController.preferredContentSize.height;
-  if ([self isAccessibilityMode]) {
+  if ([self shouldPresentFullScreen]) {
     contentHeight = MAX(contentHeight, containerHeight - self.topHeaderHeight);
   }
   _contentVCPreferredContentSizeHeightCached = contentHeight;
@@ -640,13 +647,13 @@ static CGFloat InitialDrawerHeightFactor(void) {
   CGFloat totalHeight = contentHeight + contentHeaderHeight;
   CGFloat contentHeightThresholdForScrollability =
       MIN(containerHeight - MDCDeviceTopSafeAreaInset(),
-          containerHeight * InitialDrawerHeightFactor() + contentHeaderHeight);
+          containerHeight * [self initialDrawerFactor] + contentHeaderHeight);
   BOOL contentScrollsToReveal = totalHeight >= contentHeightThresholdForScrollability;
 
   if (_contentHeaderTopInset == NSNotFound) {
     // The content header top inset is only set once.
     if (contentScrollsToReveal) {
-      _contentHeaderTopInset = containerHeight * (1.f - InitialDrawerHeightFactor());
+      _contentHeaderTopInset = containerHeight * (1.f - [self initialDrawerFactor]);
       // The minimum inset value should be the size of the safe area inset, as
       // kInitialDrawerHeightFactor discounts the safe area when receiving the height factor.
       if (_contentHeaderTopInset <= self.topHeaderHeight - self.contentHeaderHeight) {
@@ -706,7 +713,8 @@ static CGFloat InitialDrawerHeightFactor(void) {
 }
 
 - (BOOL)contentReachesFullscreen {
-  return [self isAccessibilityMode] ? YES : self.contentHeightSurplus >= self.contentHeaderTopInset;
+  return [self shouldPresentFullScreen] ? YES
+                                        : self.contentHeightSurplus >= self.contentHeaderTopInset;
 }
 
 - (BOOL)contentScrollsToReveal {
