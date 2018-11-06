30c7d9ea527be188d6bf79a0eb9c57b4226c7c80
Jeff Verkoeyen <featherless@google.com>
2018-11-06 16:50:44 -0500

    [AppBar] Annotate APIs that need to be deprecated.

--- components/AppBar/src/ColorThemer/MDCAppBarColorThemer.h
+++ components/AppBar/src/ColorThemer/MDCAppBarColorThemer.h
@@ -44,6 +44,22 @@
 + (void)applySurfaceVariantWithColorScheme:(nonnull id<MDCColorScheming>)colorScheme
                     toAppBarViewController:(nonnull MDCAppBarViewController *)appBarViewController;
 
+@end
+
+@interface MDCAppBarColorThemer (ToBeDeprecated)
+
+/**
+ Applies a color scheme's properties to an MDCAppBar.
+
+ @warning This method will soon be deprecated. Consider using @c +applySemanticColorScheme:toAppBar:
+ instead. Learn more at components/schemes/Color/docs/migration-guide-semantic-color-scheme.md
+
+ @param colorScheme The color scheme to apply to the component instance.
+ @param appBar A component instance to which the color scheme should be applied.
+ */
++ (void)applyColorScheme:(nonnull id<MDCColorScheme>)colorScheme
+                toAppBar:(nonnull MDCAppBar *)appBar;
+
 /**
  Applies a color scheme's properties to an MDCAppBar using the primary mapping.
 
@@ -65,21 +81,5 @@
  */
 + (void)applySurfaceVariantWithColorScheme:(nonnull id<MDCColorScheming>)colorScheme
                                   toAppBar:(nonnull MDCAppBar *)appBar;
-
-@end
-
-@interface MDCAppBarColorThemer (ToBeDeprecated)
-
-/**
- Applies a color scheme's properties to an MDCAppBar.
-
- @warning This method will soon be deprecated. Consider using @c +applySemanticColorScheme:toAppBar:
- instead. Learn more at components/schemes/Color/docs/migration-guide-semantic-color-scheme.md
-
- @param colorScheme The color scheme to apply to the component instance.
- @param appBar A component instance to which the color scheme should be applied.
- */
-+ (void)applyColorScheme:(nonnull id<MDCColorScheme>)colorScheme
-                toAppBar:(nonnull MDCAppBar *)appBar;
 
 @end
