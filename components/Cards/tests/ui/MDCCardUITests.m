6ae7892d08702338081b4033bdebdf00bf3e7563
Rami Abou Ghanem <raboughanem@google.com>
2018-11-21 11:48:13 -0500

    Allow for shadows to be rendered correctly

--- components/Cards/tests/ui/MDCCardUITests.m
+++ components/Cards/tests/ui/MDCCardUITests.m
@@ -24,18 +24,38 @@
 
 - (void)testDefaultCard {
   // Uncomment below to recreate the golden
-    self.recordMode = YES;
+  //  self.recordMode = YES;
 
   UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 70, 70)];
-  backgroundView.backgroundColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1];
+  backgroundView.backgroundColor = UIColor.whiteColor;
   MDCCard *card = [[MDCCard alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
   [backgroundView addSubview:card];
   card.center = backgroundView.center;
-  // purposely fail the test
-//  card.backgroundColor = UIColor.greenColor;
 
-  [CATransaction flush];
-  FBSnapshotVerifyView(backgroundView, nil);
+  FBSnapshotVerifyView([self imageViewSnapshotOfView:backgroundView], nil);
+}
+
+// TODO: To be extracted into test helper method
+- (UIImageView *)imageViewSnapshotOfView:(UIView *)view {
+  UIImage *result = nil;
+
+  if (@available(iOS 10, *)) {
+    UIGraphicsImageRenderer *renderer =
+        [[UIGraphicsImageRenderer alloc] initWithSize:view.frame.size];
+    result = [renderer imageWithActions:^(UIGraphicsImageRendererContext *_Nonnull context) {
+      BOOL success = [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:YES];
+      NSAssert(success, @"View %@ must draw correctly", view);
+    }];
+
+    NSAssert(result != nil, @"View %@ must render image", view);
+  } else {
+    NSAssert(NO, @"This test requires iOS 10.0 or later.");
+  }
+
+  UIImageView *imageView = [[UIImageView alloc] initWithFrame:view.frame];
+  imageView.image = result;
+  return imageView;
+
 }
 
 @end
