511c4467b01952549145054ee5e1af363219dd33
Michael Cheung <michael.cheung@outlook.com>
2018-11-28 11:46:28 -0500

    Adding check so it's only removed if it was added.

--- components/NavigationDrawer/src/MDCBottomDrawerPresentationController.m
+++ components/NavigationDrawer/src/MDCBottomDrawerPresentationController.m
@@ -190,7 +190,9 @@ static CGFloat kTopHandleTopMargin = (CGFloat)5.0;
 
 - (void)dismissalTransitionDidEnd:(BOOL)completed {
   if (completed) {
-    [self.bottomDrawerContainerViewController removeFromParentViewController];
+    if ([self.presentedViewController isKindOfClass:[MDCBottomDrawerViewController class]]) {
+      [self.bottomDrawerContainerViewController removeFromParentViewController];
+    }
     [self.scrimView removeFromSuperview];
     [self.topHandle removeFromSuperview];
   }
