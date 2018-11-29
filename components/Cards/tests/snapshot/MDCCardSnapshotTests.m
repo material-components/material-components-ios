c6a300ff2cc31901d9acbaf527952ad912c15322
rami-a <2364772+rami-a@users.noreply.github.com>
2018-11-29 15:56:06 -0500

    Snapshot Testing Proof of Concept (#5754)
    ### The problem
    We currently do not have UI tests on a component level. Integrating snapshot tests would allow us to have peace of mind with each PR that it isn't going to introduce any changes to the UI unless its intended to.
    
    ### The solution
    * Integrate `ios-snapshot-test-case` pod to handle generating and diffing images of components. 
    * Integrate `git-lfs` to handle storage of the goldens.
    
    This PR creates one test to showcase the ability to do snapshot tests. Upon merging this PR, you must install git-lfs in order to properly have the images pulled down. The 3 steps to do this:
    1. `brew install git-lfs`
    2. `git lfs install`
    3. `git lfs pull`
    
    Additionally, the golden is generated using an iOS 11 simulator at 2x scale so that the kokoro jobs are happy.
    
    ### Related bugs
    Closes #5740

---
+++ components/Cards/tests/snapshot/MDCCardSnapshotTests.m
@@ -0,0 +1,60 @@
+// Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
+
+#import "MaterialCards.h"
+
+@interface MDCCardSnapshotTests : FBSnapshotTestCase
+
+@end
+
+@implementation MDCCardSnapshotTests
+
+- (void)testDefaultCard {
+  // Uncomment below to recreate the golden
+  //  self.recordMode = YES;
+
+  UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 70, 70)];
+  backgroundView.backgroundColor = [UIColor colorWithWhite:0.8 alpha:1];
+  MDCCard *card = [[MDCCard alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+  [backgroundView addSubview:card];
+  card.center = backgroundView.center;
+
+  FBSnapshotVerifyView([self imageViewSnapshotOfView:backgroundView], nil);
+}
+
+// TODO: To be extracted into test helper method
+- (UIImageView *)imageViewSnapshotOfView:(UIView *)view {
+  UIImage *result = nil;
+
+  if (@available(iOS 10, *)) {
+    UIGraphicsImageRenderer *renderer =
+        [[UIGraphicsImageRenderer alloc] initWithSize:view.frame.size];
+    result = [renderer imageWithActions:^(UIGraphicsImageRendererContext *_Nonnull context) {
+      BOOL success = [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:YES];
+      NSAssert(success, @"View %@ must draw correctly", view);
+    }];
+
+    NSAssert(result != nil, @"View %@ must render image", view);
+  } else {
+    NSAssert(NO, @"This test requires iOS 10.0 or later.");
+  }
+
+  UIImageView *imageView = [[UIImageView alloc] initWithFrame:view.frame];
+  imageView.image = result;
+  return imageView;
+}
+
+@end
