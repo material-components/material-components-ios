15fef6c76c24eaeee6109f12003d9fdb4cad383f
Rami Abou Ghanem <raboughanem@google.com>
2018-12-03 13:29:19 -0500

    Remove unneeded import

--- components/Cards/tests/snapshot/MDCCardSnapshotTests.m
+++ components/Cards/tests/snapshot/MDCCardSnapshotTests.m
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
-
 #import "MDCSnapshotTestCase.h"
 #import "MaterialCards.h"
 
