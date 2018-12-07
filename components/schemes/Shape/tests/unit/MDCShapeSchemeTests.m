1d4b3006875b4e2736816bc39c46daa6897d7eb2
Wenyu Zhang <wenyuz@google.com>
2018-12-07 12:07:22 -0500

    [Shape] add NSCopying support to ShapeCategory

--- components/schemes/Shape/tests/unit/MDCShapeSchemeTests.m
+++ components/schemes/Shape/tests/unit/MDCShapeSchemeTests.m
@@ -36,6 +36,7 @@
   cat2.bottomRightCorner = corner;
 
   // Then
+  XCTAssertEqual(cat1.hash, cat2.hash);
   XCTAssertEqualObjects(cat1, cat2);
 }
 
@@ -68,4 +69,20 @@
                                                                 andSize:4]);
 }
 
+- (void)testShapeCategoryCopy {
+  // Given
+  MDCShapeCategory *cat = [[MDCShapeCategory alloc] initCornersWithFamily:MDCShapeCornerFamilyCut
+                                                                  andSize:(CGFloat)2.2];
+
+  // When
+  MDCShapeCategory *copiedCat = [cat copy];
+
+  // Then
+  XCTAssertNotEqual(cat, copiedCat);
+  XCTAssertEqualObjects(cat, copiedCat);
+  XCTAssertEqualObjects(cat.topRightCorner, copiedCat.topRightCorner);
+  XCTAssertEqualObjects(cat.topLeftCorner, copiedCat.topLeftCorner);
+  XCTAssertEqualObjects(cat.bottomRightCorner, copiedCat.bottomRightCorner);
+  XCTAssertEqualObjects(cat.bottomLeftCorner, copiedCat.bottomLeftCorner);
+}
 @end
