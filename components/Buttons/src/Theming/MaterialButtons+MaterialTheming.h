59f51e362d25e435ec2228c22f85286f65dcc26b
Cody Weaver <codyleeweaver@gmail.com>
2018-12-07 12:06:21 -0500

    Remane files

--- components/Buttons/src/Theming/MaterialButtons+MaterialTheming.h
+++ components/Buttons/src/Theming/MaterialButtons+MaterialTheming.h
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCButtonBar+Theming.h"
+#import "MDCButton+MaterialTheming.h"
+#import "MDCFloatingButton+MaterialTheming.h"
