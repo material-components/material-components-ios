f91d1fb4c98b8a1532337bd6c5187b22477ffd02
Yarden Eitan <yarneo@gmail.com>
2018-12-08 12:43:14 -0500

    fix keyboard watcher not taking into regards compatibility mode

--- components/private/KeyboardWatcher/src/MDCKeyboardWatcher.m
+++ components/private/KeyboardWatcher/src/MDCKeyboardWatcher.m
@@ -100,7 +100,7 @@ static MDCKeyboardWatcher *_sKeyboardWatcher;
   // If the extent of the keyboard is at or below the bottom of the screen it is docked.
   // This handles the case of an external keyboard on iOS8+ where the entire frame of the keyboard
   // view is used, but on the top, the input accessory section is show.
-  BOOL dockedKeyboard = CGRectGetMaxY(screenBounds) <= CGRectGetMaxY(keyWindowBounds);
+  BOOL dockedKeyboard = CGRectGetMaxY(keyWindowBounds) <= CGRectGetMaxY(keyboardRect);
 
   // If the bottom of the keyboard isn't at the bottom of the screen, then it is undocked, and we
   // shouldn't try to account for it.
