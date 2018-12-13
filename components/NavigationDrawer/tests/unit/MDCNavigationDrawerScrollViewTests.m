1a661624c2d18fb54ec79510c472479bf00cc58a
Yarden Eitan <yarneo@gmail.com>
2018-12-13 10:43:32 -0500

    test

--- components/NavigationDrawer/tests/unit/MDCNavigationDrawerScrollViewTests.m
+++ components/NavigationDrawer/tests/unit/MDCNavigationDrawerScrollViewTests.m
@@ -573,26 +573,22 @@
   MDCNavigationDrawerFakeHeaderViewController *fakeHeader =
       [[MDCNavigationDrawerFakeHeaderViewController alloc] init];
   self.fakeBottomDrawer.headerViewController = fakeHeader;
-  self.drawerViewController.delegate = fakeHeader;
-  self.fakeBottomDrawer.delegate = self.presentationController;
-  [self.presentationController presentationTransitionWillBegin];
   XCTestExpectation *expectation = [self expectationWithDescription:@"expand complete"];
 
   // When
-  [self.fakeBottomDrawer viewWillAppear:YES];
-  [self.fakeBottomDrawer cacheLayoutCalculations];
-
-  // Then
   [self.drawerViewController expandToFullscreenWithDuration:(CGFloat)0.2
                                                  completion:^(BOOL completed) {
                                                    [expectation fulfill];
                                                  }];
   [self waitForExpectationsWithTimeout:1 handler:nil];
 
-  XCTAssertEqualWithAccuracy(self.fakeBottomDrawer.trackingScrollView.frame.origin.y, 0,
-                             (CGFloat)0.01);
-  XCTAssertEqualWithAccuracy(self.fakeBottomDrawer.scrollView.frame.origin.y, 0, (CGFloat)0.01);
-  XCTAssertEqualWithAccuracy(self.fakeBottomDrawer.contentHeaderTopInset, 20, (CGFloat)0.01);
+  // Then
+  XCTAssertEqualWithAccuracy(self.fakeBottomDrawer.trackingScrollView.frame.origin.y, 0, 0.001);
+  CGFloat expectedHeight =
+      self.fakeBottomDrawer.presentingViewBounds.size.height - fakeHeader.preferredContentSize.height;
+  XCTAssertEqualWithAccuracy(CGRectGetHeight(self.fakeBottomDrawer.contentViewController.view.frame), expectedHeight, 0.001);
+  XCTAssertEqualWithAccuracy(CGRectGetMinY(self.fakeBottomDrawer.scrollView.frame), 0, 0.001);
+  XCTAssertEqualWithAccuracy(self.fakeBottomDrawer.contentHeaderTopInset, 20, 0.01);
 }
 
 @end
