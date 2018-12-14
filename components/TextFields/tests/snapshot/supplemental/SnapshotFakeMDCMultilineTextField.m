aa94f5d7a04a74fb64edb1208dac71f736766eaa
Robert Moore <rsmoore@google.com>
2018-12-14 15:32:03 -0500

    [TextFields] Incomplete snapshot RTL/Arabic support (#6023)
    Adding RTL layout with Arabic text would be helpful for Text Fields
    snapshot tests to ensure we don't break RTL layouts. While this change
    paves the way for a scalable way to introduce RTL test classes, it does
    not correctly trigger layouts for RTL.
    
    Part of #5762
    Source of #6022

--- components/TextFields/tests/snapshot/supplemental/SnapshotFakeMDCMultilineTextField.m
+++ components/TextFields/tests/snapshot/supplemental/SnapshotFakeMDCMultilineTextField.m
@@ -13,10 +13,13 @@
 // limitations under the License.
 
 #import "SnapshotFakeMDCMultilineTextField.h"
+#import <MDFInternationalization/MDFInternationalization.h>
 
 @implementation SnapshotFakeMDCMultilineTextField {
   BOOL _isEditing;
   BOOL _isEditingOverridden;
+  UIUserInterfaceLayoutDirection _effectiveUserInterfaceLayoutDirection;
+  BOOL _isEffectiveUserInterfaceLayoutDirectionOverridden;
 }
 
 - (BOOL)isEditing {
@@ -45,6 +48,21 @@
         postNotificationName:UITextFieldTextDidEndEditingNotification
                       object:self];
   }
+}
+
+- (UIUserInterfaceLayoutDirection)effectiveUserInterfaceLayoutDirection {
+  if (_isEffectiveUserInterfaceLayoutDirectionOverridden) {
+    return _effectiveUserInterfaceLayoutDirection;
+  }
+  if (@available(iOS 10.0, *)) {
+    return [super effectiveUserInterfaceLayoutDirection];
+  }
+  return UIApplication.sharedApplication.userInterfaceLayoutDirection;
+}
+
+- (void)MDCtest_setEffectiveUserInterfaceLayoutDirection:(UIUserInterfaceLayoutDirection)direction {
+  _isEffectiveUserInterfaceLayoutDirectionOverridden = YES;
+  _effectiveUserInterfaceLayoutDirection = direction;
 }
 
 @end
