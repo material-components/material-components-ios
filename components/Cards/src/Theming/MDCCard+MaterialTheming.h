fdaf872c605cab44a4579cb8b7f0ea168c445318
rami-a <2364772+rami-a@users.noreply.github.com>
2018-12-18 09:38:37 -0500

    [Cards] Add theming extension for Cards component (#6033)
    ### Context
    As the team pivots to using theming within extensions we continue this work with Cards.
    
    ### The problem
    We currently do not theme cards using a theming extension
    
    ### The fix
    * Add theming extension for MDCCard and MDCCardCollectionCell
    * Write unit tests for new extension
    * Update existing examples to utilize the extension
    
    ### Bug
    Closes #6030 
    
    ### Screenshots
    Everything should be exactly the same visually.
    
    | Before | After |
    | - | - |
    |![simulator screen shot - iphone 7 - 2018-12-17 at 13 59 52](https://user-images.githubusercontent.com/2364772/50110092-65f39680-0207-11e9-9a32-02161f7b2df4.png)|![simulator screen shot - iphone 7 - 2018-12-17 at 14 01 51](https://user-images.githubusercontent.com/2364772/50110130-7dcb1a80-0207-11e9-83d9-53bc98ad374b.png)|

---
+++ components/Cards/src/Theming/MDCCard+MaterialTheming.h
@@ -0,0 +1,40 @@
+// Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "MaterialCards.h"
+#import "MaterialContainerScheme.h"
+
+/**
+ This category is used to style MDCCard instances to a specific Material style which can be found
+ within the [Material Guidelines](https://material.io/design/components/cards.html).
+ */
+@interface MDCCard (MaterialTheming)
+
+/**
+ Applies the Material Card style to an MDCCard instance.
+
+ @param scheme A container scheme instance containing any desired customizations to the theming
+ system.
+ */
+- (void)applyThemeWithScheme:(nonnull id<MDCContainerScheming>)scheme;
+
+/**
+ Applies the Material Outlined Card style to an MDCCard instance.
+
+ @param scheme A container scheme instance containing any desired customizations to the theming
+ system.
+ */
+- (void)applyOutlinedThemeWithScheme:(nonnull id<MDCContainerScheming>)scheme;
+
+@end
