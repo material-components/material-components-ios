1887837c9e963cead6c12b35259f0d848501c886
Bartholomew Furrow <furrow@gmail.com>
2018-12-29 21:09:17 -0700

    [BottomNavigation] Lays out the bar immediately when bottomNav.titleVisibility is changed.

--- components/BottomNavigation/src/private/MDCBottomNavigationItemView.m
+++ components/BottomNavigation/src/private/MDCBottomNavigationItemView.m
@@ -383,6 +383,7 @@ static NSString *const kMDCBottomNavigationItemViewTabString = @"tab";
 -(void)setTitleVisibility:(MDCBottomNavigationBarTitleVisibility)titleVisibility {
   _titleVisibility = titleVisibility;
   [self updateLabelVisibility];
+  [self centerLayoutAnimated:YES];
 }
 
 - (void)setItemTitleFont:(UIFont *)itemTitleFont {
