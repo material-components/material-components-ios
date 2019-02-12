3f7477d8479cc154fe6c72baea516bcc9c282dde
Robert Moore <rsmoore@google.com>
2019-02-12 08:35:49 -0500

    [BottomNavigation] Fix badge position and font. (#6602)
    The item badge font was too large based on the mocks provided on the
    Material.io guidelines article.
    
    It was also incorrectly positioned when the number of characters increased.
    The position of the badge is determined by its top-leading corner and it
    grows with the label toward its trailing edge. The previous alignment
    accidentally positioned the label's center at an offset from the icon
    frame.
    
    |State|Screenshot|
    |---|---|
    |Before|![bn-catalog-develop](https://user-images.githubusercontent.com/1753199/52609607-d60b4b00-2e4b-11e9-87d9-e78233b23307.png)|
    |After|![bn-catalog-after](https://user-images.githubusercontent.com/1753199/52609614-dacfff00-2e4b-11e9-9d66-31b1d781696d.png)|
    
    
    
    ## Animation comparing this change with Material Guidelines
    ![bn-badge-compare-guidelines](https://user-images.githubusercontent.com/1753199/52609487-7b71ef00-2e4b-11e9-812c-a0475f9781d0.gif)
    
    
    Part of #6520

--- components/BottomNavigation/src/private/MDCBottomNavigationItemBadge.m
+++ components/BottomNavigation/src/private/MDCBottomNavigationItemBadge.m
@@ -17,7 +17,8 @@
 
 #import "MaterialMath.h"
 
-static const CGFloat kBadgeFontSize = 10;
+// This is very close to the material.io guidelines article considering the fonts differ.
+static const CGFloat kBadgeFontSize = 8;
 // These padding values get pretty close to the material.io guidelines article.
 static const CGFloat kBadgeYPadding = 2;
 // For an empty badge, ensure that the size is close to the guidelines article.
