99c49efdff341ed191c5331b44aae839e1b0ac1e
Wenyu Zhang <wenyuz@google.com>
2019-02-19 15:58:17 -0500

    test

--- components/Dialogs/examples/DialogsRoundedCornerExampleViewController.m
+++ components/Dialogs/examples/DialogsRoundedCornerExampleViewController.m
@@ -31,9 +31,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  id<MDCColorScheming> colorScheme =
-      self.containerScheme.colorScheme
-          ?: [[MDCSemanticColorScheme alloc] initWithDefaults:MDCColorSchemeDefaultsMaterial201804];
+  id<MDCColorScheming> colorScheme = self.containerScheme.colorScheme ?: [[MDCSemanticColorScheme alloc] initWithDefaults:MDCColorSchemeDefaultsMaterial201804];
   self.view.backgroundColor = colorScheme.backgroundColor;
 
   self.dismissButton = [[MDCButton alloc] initWithFrame:CGRectZero];
