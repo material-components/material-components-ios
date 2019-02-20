136273636d074159a348b6f639148236c0fcfbca
Yarden Eitan <yar@google.com>
2019-02-20 11:32:19 -0500

    [Cards] [Ripple] Integrate the new ripple and states into the Cards component (#6621)
    **Overview**
    This PR integrates the new Ripple component and its state support into our existing Cards and CardCells components. It does so in an opt in form, where you must have MaterialComponentsBeta installed for one to have Cards use the new behavior. The PR also includes 2 examples showcasing the new behavior with MDCCard and MDCCardCollectionCell.
    
    **Resolves**: #6463
    
    **Acceptance Criteria:**
    * The Cards have an opt in property (as ripple is in beta) to be able to activate the ripple and its states support in Cards. It will fallback to the existing (legacy) implementation by default.
    * An MDC example that showcases the work for Cards.
    * Interaction and animation with the card should follow the guidelines from the updated cards design doc.
    
    **Implementation Explained**
    This is a pioneer PR, trying to converge a beta component into a ready component. To do so we had to have our Card component not be aware of the existence of the beta component Ripple, but we needed the Ripple component to be able to plug in to the Card component.
    
    To achieve this I created a few different things:
    1. A ripple delegate that plugs into MDCCard and MDCCardCollectionCell that consists of methods that should be invoked in the component where the Ripple work needs to be inserted. A check for the existence of the delegate and the corresponding delegate method is checked in the Card component before invoked. This allows us to identify if Ripple should be used or not.
    
    2. A MDCCard+Private and MDCCardCollectionCell+Private categories that expose the RippleView property in the implementation files of the Card component, and also exposing other methods that the Ripple integration needs to invoke but aren't exposed in the original Card header files.
    
    3. An MDCCard+Ripple and MDCCardCollectionCell+Ripple categories under beta, that implement the delegate introduced in 1. and have knowledge and access to the beta component Ripple. This is where the actual implementation of the ripple integration sits.
    
    4. Lastly, we need a way to set card.rippleDelegate = self for the +Ripple categories. However, there are a few issues in achieving this. If we create a BOOL property like `shouldIntegrateRipple`, we would need to be able to switch the state from `YES` to `NO` and back correctly. The state is very complicated when you have states, ripple vs ink, etc. Therefore the best way to do this is not to allow users to trigger a BOOL, but to have a separate initializer for `initWithRipple`. However, CardCollectionCells are reused and the dequeue method calls `initWithFrame` and we can't ask it to invoke `initWithRippleandFrame` instead. Therefore, I added performSelector logic in the init methods of the Card component that checks to see if the Cards+Ripple category is apparent (meaning Beta is integrated in the framework), and if so initializes the Ripple. Happy to find alternatives if better ones are offered.
    
    **GIFs before and after**
    
    Before:
    ![oldtop9card](https://user-images.githubusercontent.com/4066863/53095318-8501f380-34ea-11e9-8b79-328af61e0fc1.gif)
    
    After:
    ![newtop9card](https://user-images.githubusercontent.com/4066863/53095334-8c290180-34ea-11e9-9767-73d74e8fefd8.gif)

---
+++ components/Ripple/examples/CardWithRippleExample.swift
@@ -0,0 +1,88 @@
+// Copyright 2019-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import MaterialComponents.MaterialButtons_ButtonThemer
+import MaterialComponents.MaterialCards_Private
+import MaterialComponentsBeta.MaterialCards_Theming
+import MaterialComponentsBeta.MaterialContainerScheme
+import MaterialComponentsBeta.MaterialButtons_Theming
+
+class CardWithRippleExample: UIViewController {
+  var card = MDCCard()
+  var button = MDCButton()
+
+  var containerScheme: MDCContainerScheming
+
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    card.enableBetaBehavior = true
+    containerScheme = MDCContainerScheme()
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    button.setTitle("Button", for: .normal)
+    card.applyTheme(withScheme: containerScheme)
+    card.backgroundColor = UIColor.blue.withAlphaComponent(0.05)
+    button.applyContainedTheme(withScheme: containerScheme)
+    view.addSubview(card)
+    card.addSubview(button)
+    card.translatesAutoresizingMaskIntoConstraints = false
+    button.translatesAutoresizingMaskIntoConstraints = false
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(30)-[card]-(30)-|",
+                                                       options: [],
+                                                       metrics: nil,
+                                                       views: ["card": card]))
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(120)-[card]-(60)-|",
+                                                       options: [],
+                                                       metrics: nil,
+                                                       views: ["card": card]))
+    card.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[button]-|",
+                                                       options: [],
+                                                       metrics: nil,
+                                                       views: ["button": button]))
+    card.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[button]",
+                                                       options: [],
+                                                       metrics: nil,
+                                                       views: ["button": button]))
+    button.addConstraint(NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal,
+                                            toItem: nil, attribute: .notAnAttribute, multiplier: 1,
+                                            constant: 60))
+  }
+
+  override public var traitCollection: UITraitCollection {
+    if UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isPortrait {
+      return UITraitCollection(traitsFrom:[UITraitCollection(horizontalSizeClass: .compact),
+                                           UITraitCollection(verticalSizeClass: .regular)])
+    }
+    return super.traitCollection
+  }
+}
+
+extension CardWithRippleExample {
+
+  class func catalogMetadata() -> [String: Any] {
+    return [
+      "breadcrumbs": ["Ripple", "Card with Ripple"],
+      "primaryDemo": false,
+      "presentable": true,
+    ]
+  }
+}
