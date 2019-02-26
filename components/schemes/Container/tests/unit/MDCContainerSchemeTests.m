ea555863c303aada9152706908ec54206d77348d
Cody Weaver <codyleeweaver@gmail.com>
2019-02-26 08:26:13 -0500

    Remove imports

--- components/schemes/Container/tests/unit/MDCContainerSchemeTests.m
+++ components/schemes/Container/tests/unit/MDCContainerSchemeTests.m
@@ -14,8 +14,6 @@
 
 #import <XCTest/XCTest.h>
 
-#import <MaterialComponents/MaterialColorScheme.h>
-#import <MaterialComponents/MaterialTypographyScheme.h>
 #import "MaterialContainerScheme.h"
 
 @interface MDCContainerSchemeTests : XCTestCase
