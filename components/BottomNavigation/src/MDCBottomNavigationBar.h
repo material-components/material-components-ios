5ca3f347422050a918125db8f92d391695a0cb99
Robert Moore <rsmoore@google.com>
2019-03-01 12:56:59 -0500

    [BottomNavigationBar] Add autolayout support (#6612)
    Adds support to BottomNavigationBar to more easily work with Autolayout.  
    
    * `intrinsicContentSize` returns the correct size of the bar items as content.
    * `barItemsBottomAnchor` allows anchoring the Bottom Navigation bar to the safe area layout guide.
    
    Updates the `MDCBottomNavigationBarController` to use the new layout anchor and eliminates the need to respond to safe area changes in the view controller.
    
    Fixes #4520

--- components/BottomNavigation/src/MDCBottomNavigationBar.h
+++ components/BottomNavigation/src/MDCBottomNavigationBar.h
@@ -163,6 +163,16 @@ typedef NS_ENUM(NSInteger, MDCBottomNavigationBarAlignment) {
 @property(nonatomic, assign) BOOL sizeThatFitsIncludesSafeArea;
 
 /**
+ NSLayoutAnchor for the bottom of the bar items.
+
+ @note It is recommended that this anchor be constrained to the bottom of the safe area layout guide
+ of the superview. This will allow the Bottom Navigation bar to extend to the bottom of the screen
+ and provide sufficient height for its content above the safe area.
+*/
+@property(nonatomic, readonly, nonnull)
+    NSLayoutYAxisAnchor *barItemsBottomAnchor NS_AVAILABLE_IOS(9_0);
+
+/**
  If @c YES, it will truncate titles that don't fit within the bounds available to the item.
 
  Default is @c YES.
