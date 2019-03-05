a79aa4316a3149cce032ab9c91837aea48de802f
Robert Moore <rsmoore@google.com>
2019-03-05 13:41:45 -0500

    [Snackbar] Make initialization threadsafe.
    Initializing a SnackbarManager should be safe on any thread. Delaying
    UIView initialization can make it thread safe and no longer crash.
    
    QA=Tested in iOS 7/iOS 11.2 simulator in Dragons.
    
    Closes #6759

--- components/Snackbar/src/MDCSnackbarManager.m
+++ components/Snackbar/src/MDCSnackbarManager.m
@@ -126,7 +126,6 @@ static NSString *const kAllMessagesCategory = @"$$___ALL_MESSAGES___$$";
     _manager = manager;
     _pendingMessages = [[NSMutableArray alloc] init];
     _suspensionTokens = [NSMutableDictionary dictionary];
-    _overlayView = [[MDCSnackbarOverlayView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   }
   return self;
 }
@@ -267,6 +266,16 @@ static NSString *const kAllMessagesCategory = @"$$___ALL_MESSAGES___$$";
                 });
               }
             }];
+}
+
+- (MDCSnackbarOverlayView *)overlayView {
+  // Ensure that this method is called on the main thread.
+  NSAssert([NSThread isMainThread], @"Method is not called on main thread.");
+
+  if (!_overlayView) {
+    _overlayView = [[MDCSnackbarOverlayView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  }
+  return _overlayView;
 }
 
 - (void)hideSnackbarViewReally:(MDCSnackbarMessageView *)snackbarView
