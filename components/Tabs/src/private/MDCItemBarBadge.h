8f9bcf61dda2e78e7cb2d8b3f65a6537db07c623
Material Components iOS Team <noreply@google.com>
2019-03-12 17:58:18 +0000

    [Tabs] Add Badge view
    Creates a new dedicated class for badging. Design has not approved badging on Tabs but has decided we can build support if we find it necessary. Given the current state of badging on Tabs, it's worthwhile to improve the appearance.
    
    PiperOrigin-RevId: 238051945

--- components/Tabs/src/private/MDCItemBarBadge.h
+++ components/Tabs/src/private/MDCItemBarBadge.h
@@ -1,4 +1,4 @@
-// Copyright 2017-present the Material Components for iOS authors. All Rights Reserved.
+// Copyright 2019-present the Material Components for iOS authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCItemBarCell.h"
+#import <UIKit/UIKit.h>
 
-@interface MDCItemBarCell ()
-@property(nonatomic, strong) UILabel *titleLabel;
-@property(nonatomic, strong, readonly) UILabel *badgeLabel;
+@interface MDCItemBarBadge : UIView
+
+@property(nonatomic, copy) NSString *badgeValue;
+@property(nonatomic, copy) UIColor *badgeColor;
+@property(nonatomic, strong, readonly) UILabel *badgeValueLabel;
+
 @end
