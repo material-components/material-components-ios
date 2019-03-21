850ff565daaa3a2e2dda01fed43be75a8f2eba0f
Wenyu Zhang <wenyuz@google.com>
2019-03-21 11:34:34 -0400

    [Banner] MDCBannerView header review.

--- components/Banner/examples/supplemental/MDCBannerView.h
+++ components/Banner/examples/supplemental/MDCBannerView.h
@@ -18,15 +18,42 @@
 
 __attribute__((objc_subclassing_restricted)) @interface MDCBannerView : UIView
 
-@property(nonatomic, copy, nonnull) NSString *text;
-
-@property(nonatomic, strong, nullable) UIImage *image;  // default is nil
-
-@property(nonatomic, assign) NSUInteger numberOfButtons;  // default is 1
+/**
+ A view that displays the text on a @c MDCBannerView.
+ @discussion Although this property is read-only, its properties are read/write. These properties can be used to configure the text shown on @c MDCBannerView.
+ For example:
+ @code
+ MDCBannerView *bannerView = [[MDCBannerView alloc] init];
+ bannerView.textLabel.font = [UIFont systemFontOfSize: 12];
+ @endcode
+ */
+@property(nonatomic, readonly, strong, nonnull) UILabel *textLabel;
 
 /**
- The buttons representing the banner's actions.
+ A view that displays the image on a @c MDCBannerView.
+ @discussion Although this property is read-only, its properties are read/write. These properties can be used to configure the image shown on @c MDCBannerView.
+ For example:
+ @code
+ MDCBannerView *bannerView = [[MDCBannerView alloc] init];
+ bannerView.imageView.image = [UIImage imageNamed:@"example-image"];
+ bannerView.imageView.tintColor = UIColor.whiteColor;
+ @endcode
+ If its own property @c image is @c nil, this view won't show on the @c MDCBannerView.
  */
-@property(nonatomic, readonly, copy, nonnull) NSArray<MDCButton *> *buttons;
+@property(nonatomic, readonly, strong, nonnull) UIImageView *imageView;
+
+/**
+ A leading button that displays on a @c MDCBannerView.
+ @discussion This @c leadingButton displays on the leading edge of the view. If it can't occupy on the same line with @c trailingButton, it always shows above it.
+ The default value is nil.
+ */
+@property(nonatomic, readwrite, strong, nullable) MDCButton *leadingButton;
+
+/**
+ A trailing button that displays on a @c MDCBannerView.
+ @discussion This @c trailingButton displays on the trailing edge of the view. If it can't occupy on the same line with @c trailingButton, it always shows below it.
+ The default value is nil.
+ */
+@property(nonatomic, readwrite, strong, nullable) MDCButton *trailingButton;
 
 @end
