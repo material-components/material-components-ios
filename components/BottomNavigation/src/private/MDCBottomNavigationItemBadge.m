462560ebc003fb8bd61643ccb2d1afbe6262762e
Robert Moore <rsmoore@google.com>
2019-04-03 11:22:24 -0400

    [BottomNavigation] Increase badge text contrast (#7013)
    Increasing text contrast for the Bottom Navigation badge. It's currently
    4:1 but WCAG AA requires 4.5:1. Material Red 700 provides 5:1.  Material
    Red #600 only provides 4.4:1 against white.
    
    Part of #6981

--- components/BottomNavigation/src/private/MDCBottomNavigationItemBadge.m
+++ components/BottomNavigation/src/private/MDCBottomNavigationItemBadge.m
@@ -16,6 +16,7 @@
 #import <CoreGraphics/CoreGraphics.h>
 
 #import "MaterialMath.h"
+#import "MaterialPalettes.h"
 
 // This is very close to the material.io guidelines article considering the fonts differ.
 static const CGFloat kBadgeFontSize = 8;
@@ -44,7 +45,7 @@ static const CGFloat kMinDiameter = 9;
 
 - (void)commonMDCBottomNavigationItemBadgeInit {
   if (!_badgeColor) {
-    _badgeColor = [UIColor redColor];
+    _badgeColor = MDCPalette.redPalette.tint700;
   }
   self.layer.backgroundColor = _badgeColor.CGColor;
 
