176b02a1ac05c7f882fcc3e284503f0e99d5ac0a
Andrew Overton <andrewoverton@google.com>
2019-04-10 14:37:48 -0400

    [TextFields] Change wording on MDCTextInputController theming extensions doc comments (#7107)
    * Change wording on MDCTextInputController theming extensions doc comments
    
    * Change "a" to "the"

--- components/TextFields/src/Theming/MDCTextInputControllerOutlined+MaterialTheming.h
+++ components/TextFields/src/Theming/MDCTextInputControllerOutlined+MaterialTheming.h
@@ -23,7 +23,7 @@
 @interface MDCTextInputControllerOutlined (MaterialTheming)
 
 /**
- Applies a container scheme to this instance.
+ Applies the Material theme to this instance.
 
  @param scheme A container scheme instance containing any desired customizations to the theming
  system.
