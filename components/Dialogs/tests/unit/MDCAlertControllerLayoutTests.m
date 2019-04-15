ec37b44f2849ec6f1f5bd73170e245e8fc315a3e
Wenyu Zhang <8836258+wenyuzhang666@users.noreply.github.com>
2019-04-15 19:26:55 -0400

    [Dialogs] Layout MDC Dialogs correctly for tall fonts. (#7126)
    Closes https://github.com/material-components/material-components-ios/issues/7062
    
    Reference: cl/241537224
    
    Layout MDC Dialogs correctly for tall fonts
    
    The MDC Dialog layout code had some hard coded assumptions about the amount of vertical space to allocate for action buttons that were violated when using taller fonts such as Urdu with large Dynamic Type sizes.
    There was also a related bug where vertical vs. horizontal action button layout was calculated in two different places.
    
    Snapshot test before change:
    <img width="569" alt="snapshot_test_goldens_goldens_64_MDCAlertControllerLocalizationSnapshotTests_testPreferredContentSizeWithNotoNastaliqUrdu_11_2@2x_Before_01dd7fb1a36bc24b27e05c1079c6420994c9f904" src="https://user-images.githubusercontent.com/8836258/56166158-dc7c8700-5fa2-11e9-8fa1-ed4dbfdc947b.png">
    
    Snapshot test result after change:
    <img width="569" alt="snapshot_test_goldens_goldens_64_MDCAlertControllerLocalizationSnapshotTests_testPreferredContentSizeWithNotoNastaliqUrdu_11_2@2x_After_78d3fd9172a202641400c0536a9c2e3a33b0455b" src="https://user-images.githubusercontent.com/8836258/56166182-e7371c00-5fa2-11e9-976e-4f9c93efc98f.png">
    
    
    Example screenshot before change:
    ![Simulator Screen Shot - iPhone 7 - 2019-04-15 at 17 42 09](https://user-images.githubusercontent.com/8836258/56167426-1dc26600-5fa6-11e9-9d5f-180f7960f458.png)
    
    
    Example screenshot after change:
    ![Simulator Screen Shot - iPhone 7 - 2019-04-15 at 17 43 58](https://user-images.githubusercontent.com/8836258/56167432-2024c000-5fa6-11e9-8e44-5a88b48425f2.png)

---
+++ components/Dialogs/tests/unit/MDCAlertControllerLayoutTests.m
@@ -0,0 +1,80 @@
+// Copyright 2019-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "MaterialDialogs.h"
+
+#import "MDCAlertControllerView+Private.h"
+
+#import <XCTest/XCTest.h>
+
+static const CGFloat kFourInchPortraitWidth = 320.0;
+static const CGFloat kFourInchLandscapeWidth = 568.0;
+
+@interface MDCAlertControllerLayoutTests : XCTestCase
+
+@end
+
+@implementation MDCAlertControllerLayoutTests {
+  MDCAlertController *_alertController;
+}
+
+- (void)setUp {
+  [super setUp];
+}
+
+// The large point size Urdu text in this dialog should lay out the two buttons vertically on
+// a 4" portrait size screen and horizontally in landscape
+- (void)setupLargePointSizeUrduNetworkRetryDialog {
+  NSString *networkFailureInUrdu = @"براہ کرم اپنا نیٹ ورک کنکشن چیک کریں اور دوبارہ کوشش کریں۔";
+  _alertController = [MDCAlertController alertControllerWithTitle:nil message:networkFailureInUrdu];
+  NSString *urduFontName = @"NotoNastaliqUrdu";
+  UIFont *dialogBodyFont;
+  UIFont *dialogButtonFont;
+  if (@available(iOS 11, *)) {
+    // Noto Nastaliq Urdu was added in iOS 11, and is an extremely tall
+    // font for any given nominal point size.
+    dialogBodyFont = [UIFont fontWithName:urduFontName size:20.0];
+    dialogButtonFont = [UIFont fontWithName:urduFontName size:26.0];
+  } else {
+    dialogBodyFont = [UIFont systemFontOfSize:20.0];
+    dialogButtonFont = [UIFont systemFontOfSize:26.0];
+  }
+  _alertController.messageFont = dialogBodyFont;
+  _alertController.buttonFont = dialogButtonFont;
+
+  MDCAlertAction *retryAction = [MDCAlertAction actionWithTitle:@"دوبارہ کوشش کریں" handler:nil];
+  MDCAlertAction *cancelAction = [MDCAlertAction actionWithTitle:@"منسوخ کریں" handler:nil];
+  [_alertController addAction:retryAction];
+  [_alertController addAction:cancelAction];
+}
+
+- (void)tearDown {
+  _alertController = nil;
+  [super tearDown];
+}
+
+- (void)testButtonLayout {
+  [self setupLargePointSizeUrduNetworkRetryDialog];
+  MDCAlertControllerView *view = (MDCAlertControllerView *)_alertController.view;
+
+  CGSize sizeOnFourInchPortrait = [view calculateActionsSizeThatFitsWidth:kFourInchPortraitWidth];
+  CGSize sizeOnFourInchLandscape = [view calculateActionsSizeThatFitsWidth:kFourInchLandscapeWidth];
+
+  // The vertical layout should be approximately twice as high as the horizontal layout, +/-
+  // padding between buttons.
+  XCTAssertEqualWithAccuracy(sizeOnFourInchLandscape.height * 2, sizeOnFourInchPortrait.height, 5);
+  XCTAssertGreaterThan(sizeOnFourInchLandscape.width, sizeOnFourInchPortrait.width);
+}
+
+@end
