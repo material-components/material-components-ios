e2df88e5c3518af4e97a220f77e302a7bc85d3c7
Cody Weaver <codyleeweaver@gmail.com>
2019-04-19 11:12:55 -0400

    Update test

--- components/AppBar/tests/unit/Theming/MDCAppBarViewController_MaterialThemingTests.m
+++ components/AppBar/tests/unit/Theming/MDCAppBarViewController_MaterialThemingTests.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MaterialAppBar+MaterialTheming.h"
+#import "MaterialAppBar+Theming.h"
 
 #import <XCTest/XCTest.h>
 
