565c18ccdc4d4967e052f77b8c67630cfb0d666b
featherless <jverkoey@gmail.com>
2019-04-22 19:56:00 +0300

    [ButtonBar] Annotate themer APIs with the recommended APIs. (#7217)
    Part of https://github.com/material-components/material-components-ios/issues/7108

--- components/ButtonBar/src/ColorThemer/MDCButtonBarColorThemer.h
+++ components/ButtonBar/src/ColorThemer/MDCButtonBarColorThemer.h
@@ -17,18 +17,12 @@
 
 /**
  The Material Design color system's themer for instances of MDCButtonBar.
+
+ @warning This API will eventually be deprecated. The replacement API is:
+ `MDCButtonBar`'s `-applyPrimaryThemeWithScheme:`
+ Learn more at docs/theming.md#migration-guide-themers-to-theming-extensions
  */
 @interface MDCButtonBarColorThemer : NSObject
-
-/**
- Applies a color scheme's properties to an MDCButtonBar.
-
- @param colorScheme The color scheme to apply to the component instance.
- @param buttonBar A component instance to which the color scheme should be applied.
- */
-+ (void)applySemanticColorScheme:(nonnull id<MDCColorScheming>)colorScheme
-                     toButtonBar:(nonnull MDCButtonBar *)buttonBar;
-
 @end
 
 @interface MDCButtonBarColorThemer (ToBeDeprecated)
@@ -36,12 +30,25 @@
 /**
  Applies a color scheme's properties to an MDCButtonBar.
 
- @warning This method will soon be deprecated. Consider using
- @c +applySemanticColorScheme:toButtonBar: instead. Learn more at
- components/schemes/Color/docs/migration-guide-semantic-color-scheme.md
+ @param colorScheme The color scheme to apply to the component instance.
+ @param buttonBar A component instance to which the color scheme should be applied.
+
+ @warning This API will eventually be deprecated. The replacement API is:
+ `MDCButtonBar`'s `-applyPrimaryThemeWithScheme:`
+ Learn more at docs/theming.md#migration-guide-themers-to-theming-extensions
+ */
++ (void)applySemanticColorScheme:(nonnull id<MDCColorScheming>)colorScheme
+                     toButtonBar:(nonnull MDCButtonBar *)buttonBar;
+
+/**
+ Applies a color scheme's properties to an MDCButtonBar.
 
  @param colorScheme The color scheme to apply to the component instance.
  @param buttonBar A component instance to which the color scheme should be applied.
+
+ @warning This API will eventually be deprecated. The replacement API is:
+ `MDCButtonBar`'s `-applyPrimaryThemeWithScheme:`
+ Learn more at docs/theming.md#migration-guide-themers-to-theming-extensions
  */
 + (void)applyColorScheme:(nonnull id<MDCColorScheme>)colorScheme
              toButtonBar:(nonnull MDCButtonBar *)buttonBar;
