2da55bf4f7d1d2d0cb5238c5890851e90533f3aa
Yarden Eitan <yarneo@gmail.com>
2019-04-26 14:28:19 -0400

    update

--- components/Ripple/tests/snapshot/MDCStatefulRippleViewSnapshotTests.m
+++ components/Ripple/tests/snapshot/MDCStatefulRippleViewSnapshotTests.m
@@ -30,7 +30,7 @@
 
   // Uncomment below to recreate all the goldens (or add the following line to the specific
   // test you wish to recreate the golden for).
-     self.recordMode = YES;
+  //   self.recordMode = YES;
 
   self.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
   self.view.backgroundColor = UIColor.whiteColor;
