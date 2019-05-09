2edc2d1b581bec04fed18fd075a4688ff68c9869
Robert Moore <rsmoore@google.com>
2019-05-08 22:15:23 -0400

    Renaming directories/targets

--- components/private/Snapshot/src/MaterialSnapshot.h
+++ components/private/Snapshot/src/MaterialSnapshot.h
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "Implementation/MDCSnapshotTestCase.h"
-#import "Supplemental/UIImage+MDCSnapshot.h"
-#import "Supplemental/UIView+MDCSnapshot.h"
+#import "SnapshotTestCase/MDCSnapshotTestCase.h"
+#import "SnapshotUtilities/UIImage+MDCSnapshot.h"
+#import "SnapshotUtilities/UIView+MDCSnapshot.h"
