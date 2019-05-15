67a3eacceee7f7e279a407f450e752278296b9e5
Cody Weaver <codyleeweaver@gmail.com>
2019-05-14 18:20:47 -0700

    remove record

--- components/Buttons/tests/snapshot/ButtonsThemingSnapshotTests.m
+++ components/Buttons/tests/snapshot/ButtonsThemingSnapshotTests.m
@@ -34,7 +34,7 @@
 
   // Uncomment below to recreate all the goldens (or add the following line to the specific
   // test you wish to recreate the golden for).
-    self.recordMode = YES;
+  //  self.recordMode = YES;
 
   self.button = [[MDCButton alloc] init];
   [self.button setTitle:@"Material" forState:UIControlStateNormal];
