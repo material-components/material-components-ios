b00a00158a719e9861ba2189f63cb9f763a9a3b2
Wenyu Zhang <wenyuz@google.com>
2019-05-16 16:24:45 -0400

    CRC changes.

--- components/Chips/src/MDCChipView.m
+++ components/Chips/src/MDCChipView.m
@@ -135,7 +135,7 @@ static inline CGSize CGSizeShrinkWithInsets(CGSize size, UIEdgeInsets edgeInsets
 - (void)commonMDCChipViewInit {
   _minimumSize = kMDCChipMinimumSizeDefault;
   self.isAccessibilityElement = YES;
-  self.mdc_legacyFontScaling = YES;
+  _mdc_legacyFontScaling = YES;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame {
