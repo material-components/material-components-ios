ed731af080a60df09cfd9c0a5016594add0ee09d
Wenyu Zhang <8836258+wenyuzhang666@users.noreply.github.com>
2019-05-21 22:48:01 -0400

    [Chips] Apply Dynamic Type flag to typography themer. (#7414)
    Part of #7387

--- components/Chips/tests/unit/ChipViewTypographyThemerTests.m
+++ components/Chips/tests/unit/ChipViewTypographyThemerTests.m
@@ -16,6 +16,23 @@
 
 #import "MaterialChips+TypographyThemer.h"
 #import "MaterialChips.h"
+#import "MaterialTypography.h"
+
+/** Fake MDCChipView for unit testing. */
+@interface MDCChipsTypographyThemerTestsFakeChipView : MDCChipView
+
+/** Used to set the value of @c traitCollection. */
+@property(nonatomic, strong) UITraitCollection *traitCollectionOverride;
+
+@end
+
+@implementation MDCChipsTypographyThemerTestsFakeChipView
+
+- (UITraitCollection *)traitCollection {
+  return self.traitCollectionOverride ?: [super traitCollection];
+}
+
+@end
 
 @interface ChipViewTypographyThemerTests : XCTestCase
 
@@ -23,13 +40,35 @@
 
 @implementation ChipViewTypographyThemerTests
 
-- (void)testFontThemer {
+- (void)testFontThemerWithNoDynamicTypeSupport {
   MDCChipView *chip = [[MDCChipView alloc] init];
   MDCTypographyScheme *typographyScheme = [[MDCTypographyScheme alloc] init];
+  typographyScheme.mdc_adjustsFontForContentSizeCategory = NO;
 
   [MDCChipViewTypographyThemer applyTypographyScheme:typographyScheme toChipView:chip];
 
   XCTAssertEqualObjects(chip.titleLabel.font, typographyScheme.body2);
+}
+
+- (void)testFontThemerWithDynamicTypeSupport {
+  if (@available(iOS 10.0, *)) {
+    // Given
+    MDCChipsTypographyThemerTestsFakeChipView *chip =
+        [[MDCChipsTypographyThemerTestsFakeChipView alloc] init];
+    MDCTypographyScheme *typographyScheme = [[MDCTypographyScheme alloc] init];
+    typographyScheme.mdc_adjustsFontForContentSizeCategory = YES;
+    UIContentSizeCategory size = UIContentSizeCategoryExtraExtraLarge;
+    UITraitCollection *traitCollection =
+        [UITraitCollection traitCollectionWithPreferredContentSizeCategory:size];
+    chip.traitCollectionOverride = traitCollection;
+
+    // When
+    [MDCChipViewTypographyThemer applyTypographyScheme:typographyScheme toChipView:chip];
+
+    // Then
+    XCTAssertNotNil(chip.titleFont.mdc_scalingCurve);
+    XCTAssertGreaterThan(chip.titleFont.pointSize, typographyScheme.body2.pointSize);
+  }
 }
 
 @end
