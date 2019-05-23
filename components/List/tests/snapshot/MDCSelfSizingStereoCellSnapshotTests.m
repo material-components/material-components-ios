a21108979e53405ce1907f379aed17872869961d
Wenyu Zhang <8836258+wenyuzhang666@users.noreply.github.com>
2019-05-23 15:49:27 -0400

    [List] Snapshot tests for upcoming Scalable Font behavior. (#7458)
    Adding tests for the upcoming Scalable Fonts Dynamic Type work. This PR focuses on the use case where `mdc_adjustsFontForContentSizeCategory = YES;` and when Scalable Fonts are attached. 
    
    A separate PR will handle the "fall-back" case (when a non-Scalable Font is used) and potentially a third PR for the current (legacy) behavior.
    
    Pre-work of https://github.com/material-components/material-components-ios/issues/7385.

--- components/List/tests/snapshot/MDCSelfSizingStereoCellSnapshotTests.m
+++ components/List/tests/snapshot/MDCSelfSizingStereoCellSnapshotTests.m
@@ -15,12 +15,42 @@
 #import "MaterialSnapshot.h"
 
 #import "MaterialList.h"
+#import "MaterialTypographyScheme.h"
+
+@interface MDCSelfSizingStereoCellSnapshotTestsContentSizeCategoryOverrideWindow : UIWindow
+
+/** Used to override the value of @c preferredContentSizeCategory. */
+@property(nonatomic, copy) UIContentSizeCategory contentSizeCategoryOverride;
+
+@end
+
+@implementation MDCSelfSizingStereoCellSnapshotTestsContentSizeCategoryOverrideWindow
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    self.contentSizeCategoryOverride = UIContentSizeCategoryLarge;
+  }
+  return self;
+}
+
+- (UITraitCollection *)traitCollection {
+  if (@available(iOS 10.0, *)) {
+    UITraitCollection *traitCollection = [UITraitCollection
+        traitCollectionWithPreferredContentSizeCategory:self.contentSizeCategoryOverride];
+    return traitCollection;
+  }
+  return [super traitCollection];
+}
+
+@end
 
 /** Snapshot tests for MDCBaseCell. */
 @interface MDCSelfSizingStereoCellSnapshotTests : MDCSnapshotTestCase
 
 /** The view being tested. */
 @property(nonatomic, strong) MDCSelfSizingStereoCell *cell;
+@property(nonatomic, strong) MDCTypographyScheme *typographyScheme;
 
 @end
 
@@ -38,13 +68,45 @@
 
 - (void)tearDown {
   self.cell = nil;
+  self.typographyScheme = nil;
 
   [super tearDown];
+}
+
+- (UIWindow *)generateWindowWithView:(UIView *)view
+                 contentSizeCategory:(UIContentSizeCategory)sizeCategory
+                              insets:(UIEdgeInsets)insets {
+  MDCSelfSizingStereoCellSnapshotTestsContentSizeCategoryOverrideWindow *backgroundWindow =
+      [[MDCSelfSizingStereoCellSnapshotTestsContentSizeCategoryOverrideWindow alloc]
+          initWithFrame:CGRectMake(0, 0, CGRectGetWidth(view.bounds) + insets.left + insets.right,
+                                   CGRectGetHeight(view.bounds) + insets.top + insets.bottom)];
+  backgroundWindow.contentSizeCategoryOverride = sizeCategory;
+  backgroundWindow.backgroundColor = [UIColor colorWithWhite:(CGFloat)0.8 alpha:1];
+  [backgroundWindow addSubview:view];
+  backgroundWindow.hidden = NO;
+
+  CGRect frame = view.frame;
+  frame.origin = CGPointMake(insets.left, insets.top);
+  view.frame = frame;
+
+  return backgroundWindow;
 }
 
 - (void)generateSnapshotAndVerifyForView:(UIView *)view {
   UIView *snapshotView = [view mdc_addToBackgroundView];
   [self snapshotVerifyView:snapshotView];
+}
+
+- (void)generateSnapshotWithContentSizeCategoryAndNotificationPost:
+            (UIContentSizeCategory)sizeCategory
+                                                  andVerifyForView:(UIView *)view {
+  UIWindow *snapshotWindow = [self generateWindowWithView:view
+                                      contentSizeCategory:sizeCategory
+                                                   insets:UIEdgeInsetsMake(10, 10, 10, 10)];
+  [NSNotificationCenter.defaultCenter
+      postNotificationName:UIContentSizeCategoryDidChangeNotification
+                    object:nil];
+  [self snapshotVerifyView:snapshotWindow];
 }
 
 #pragma mark - Tests
@@ -76,6 +138,44 @@
 
   // Then
   [self generateSnapshotAndVerifyForView:self.cell];
+}
+
+- (void)testCellWithDynamicTypeForContentSizeCategoryExtraSmallEnabledForTitleAndDetail {
+  if (@available(iOS 10.0, *)) {
+    // Given
+    self.typographyScheme =
+        [[MDCTypographyScheme alloc] initWithDefaults:MDCTypographySchemeDefaultsMaterial201902];
+
+    // When
+    self.cell.titleLabel.text = @"Title";
+    self.cell.titleLabel.font = self.typographyScheme.subtitle1;
+    self.cell.detailLabel.text = @"Detail";
+    self.cell.detailLabel.font = self.typographyScheme.button;
+    self.cell.mdc_adjustsFontForContentSizeCategory = YES;
+
+    // Then
+    [self generateSnapshotWithContentSizeCategoryAndNotificationPost:UIContentSizeCategoryExtraSmall
+                                                    andVerifyForView:self.cell];
+  }
+}
+
+- (void)testCellWithDynamicTypeForContentSizeCategoryExtraLargeEnabledForTitleAndDetail {
+  if (@available(iOS 10.0, *)) {
+    // Given
+    self.typographyScheme =
+        [[MDCTypographyScheme alloc] initWithDefaults:MDCTypographySchemeDefaultsMaterial201902];
+
+    // When
+    self.cell.titleLabel.text = @"Title";
+    self.cell.titleLabel.font = self.typographyScheme.subtitle1;
+    self.cell.detailLabel.text = @"Detail";
+    self.cell.detailLabel.font = self.typographyScheme.button;
+    self.cell.mdc_adjustsFontForContentSizeCategory = YES;
+
+    // Then
+    [self generateSnapshotWithContentSizeCategoryAndNotificationPost:UIContentSizeCategoryExtraLarge
+                                                    andVerifyForView:self.cell];
+  }
 }
 
 @end
