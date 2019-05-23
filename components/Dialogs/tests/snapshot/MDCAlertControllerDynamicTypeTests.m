e0b5943be28a6a3b89e1c8a52c8f871fafdac68a
Cody Weaver <codyleeweaver@gmail.com>
2019-05-22 23:04:36 -0400

    stuff

--- components/Dialogs/tests/snapshot/MDCAlertControllerDynamicTypeTests.m
+++ components/Dialogs/tests/snapshot/MDCAlertControllerDynamicTypeTests.m
@@ -45,7 +45,7 @@
 
   // Uncomment below to recreate all the goldens (or add the following line to the specific
   // test you wish to recreate the golden for).
-    self.recordMode = YES;
+  //  self.recordMode = YES;
 
   self.alertController = [[AlertControllerDynamicTypeSnapshotTestFake alloc] init];
   self.alertController.title = @"Material";
