28be5bbfd5954657adca9b3b92299c9ff0dcb376
featherless <featherless@google.com>
2019-05-24 00:14:21 -0400

    [Typography] Copy-edit of UIFont+MaterialScalable.h
    Closes https://github.com/material-components/material-components-ios/issues/7474

--- components/Typography/src/UIFont+MaterialScalable.h
+++ components/Typography/src/UIFont+MaterialScalable.h
@@ -16,43 +16,38 @@
 
 #import "MDCFontTextStyle.h"
 
+/**
+ A representation of a mapping of UIContentSizeCategory keys to font size values.
+
+ The values of this dictionary are CGFloat values represented as an NSNumber. Each value defines the
+ font size to be used for a given content size category.
+ */
+typedef NSDictionary<UIContentSizeCategory, NSNumber *> * MDCScalingCurve;
+
 @interface UIFont (MaterialScalable)
 
 /**
- An associated scaling curve that can be used to create resized versions of the font based on a
- UIContentSizeCategory.
+ A custom scaling curve to be used when scaling this font for Dynamic Type.
 
- An associated scaling curve that is a dictionary that maps UISizeCategory to fontsize.
- The dictionary keys MUST include a complete set of size categories, from
+ The keys of a scaling curve MUST include the complete set of UIContentSizeCategory values, from
  UIContentSizeCategoryExtraSmall to UIContentSizeCategoryExtraExtraExtraLarge AND all
- UIContentSizeCategoryAccessibility categories.
-
- The dictionary values are the desired pointSize stored as a CGFloat wrapped in an NSNumber.
-
- Generally, clients will use MDCFontScaler to attach particular scaling curves to a font.
+ UIContentSizeCategoryAccessibility categories. If any of these keys are missing then any scaling
+ behavior that reads from this property is undefined.
  */
-@property(nonatomic, copy, nullable, setter=mdc_setScalingCurve:)
-    NSDictionary<UIContentSizeCategory, NSNumber *> *mdc_scalingCurve;
+@property(nonatomic, copy, nullable, setter=mdc_setScalingCurve:) MDCScalingCurve mdc_scalingCurve;
 
 /**
- Return a font with the same family, weight and traits, with a size based on the given size
- category and an associated scaling curve.
+ Returns a font with the same family, weight and traits, but whose point size is based on the given
+ size category and the font's @c mdc_scalingCurve.
 
- @param sizeCategory used to query the associated scaling curve for font size
- @return Font sized for the current size category OR self if there is no associated curve
+ @param sizeCategory The size category for which the font should be scaled.
+ @return A font whose point size matches the @c mdc_scalingCurve's current size category value, or
+ self if @c mdc_scalingCurve is nil.
  */
 - (nonnull UIFont *)mdc_scaledFontForSizeCategory:(nonnull UIContentSizeCategory)sizeCategory;
 
 /**
- Return a font with the same family, weight and traits, with a size based on the device's
- text size setting and an associated scaling curve.
-
- @return Font sized for the current size category OR self if there is no associated curve
- */
-- (nonnull UIFont *)mdc_scaledFontForCurrentSizeCategory;
-
-/**
- Return a font with the same family, weight and traits, with a font size based on the default
+ Returns a font with the same family, weight and traits, with a font size based on the default
  size category of UIContentSizeCategoryLarge.
 
  This can be used to return a font for a text element that should *not* be scaled with Dynamic
@@ -61,5 +56,16 @@
  @return Font sized for UIContentSizeCategoryLarge OR self if there is no associated curve
  */
 - (nonnull UIFont *)mdc_scaledFontAtDefaultSize;
+
+/**
+ Returns a font with the same family, weight and traits, but whose point size is the device's
+ text size setting and the font's @c mdc_scalingCurve.
+
+ @note Prefer @c -mdc_scaledFontForSizeCategory: because it encourages use of trait collections
+ instead.
+
+ @return Font sized for the current size category OR self if there is no associated curve
+ */
+- (nonnull UIFont *)mdc_scaledFontForCurrentSizeCategory;
 
 @end
