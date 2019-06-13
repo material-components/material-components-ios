34e98a258b66949fb675e28248477bc38c8d42d2
Yarden Eitan <yarneo@gmail.com>
2019-06-13 18:06:20 -0400

    update

--- components/Snackbar/src/MDCSnackbarMessage.h
+++ components/Snackbar/src/MDCSnackbarMessage.h
@@ -147,7 +147,7 @@ extern NSString *__nonnull const MDCSnackbarMessageBoldAttributeName;
 @property(nonatomic, readonly, nullable) NSString *voiceNotificationText;
 
 /*
- This property determines if an @c MDCSnackbar buttons should use the @c MDCRippleView behavior or not.
+ Determines if an @c MDCSnackbar buttons should use the @c MDCRippleView behavior or not.
  By setting this property to @c YES, @c MDCRippleView is used to provide the user visual
  touch feedback, instead of the legacy @c MDCInkView.
  @note Defaults to @c NO.
