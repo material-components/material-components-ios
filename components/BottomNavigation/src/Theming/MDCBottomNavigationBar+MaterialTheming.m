bff9f86219f9c0e75b772e889a2b635708739d3e
Cody Weaver <codyleeweaver@gmail.com>
2019-06-18 14:34:34 -0400

    Imp

--- components/BottomNavigation/src/Theming/MDCBottomNavigationBar+MaterialTheming.m
+++ components/BottomNavigation/src/Theming/MDCBottomNavigationBar+MaterialTheming.m
@@ -13,3 +13,46 @@
 // limitations under the License.
 
 #import "MDCBottomNavigationBar+MaterialTheming.h"
+
+#import "MaterialColorScheme.h"
+#import "MaterialShadowElevations.h"
+#import "MaterialTypographyScheme.h"
+
+static const CGFloat kUnselectedPrimaryAlpha = 0.74f;
+static const CGFloat kUnselectedSurfaceAlpha = 0.6f;
+
+@implementation MDCBottomNavigationBar (MaterialTheming)
+
+- (void)applyPrimaryThemeWithScheme:(nonnull id<MDCContainerScheming>)scheme {
+  [self applyPrimaryThemeWithColorScheme:scheme.colorScheme];
+  [self applyThemeWithTypographyScheme:scheme.typographyScheme];
+  self.elevation = MDCShadowElevationBottomNavigationBar;
+  self.itemsContentVerticalMargin = 0;
+}
+
+- (void)applyPrimaryThemeWithColorScheme:(nonnull id<MDCColorScheming>)colorScheme {
+  self.barTintColor = colorScheme.primaryColor;
+  self.selectedItemTintColor = colorScheme.onPrimaryColor;
+  self.selectedItemTitleColor = colorScheme.onPrimaryColor;
+  self.unselectedItemTintColor = [colorScheme.onPrimaryColor colorWithAlphaComponent:kUnselectedPrimaryAlpha];
+}
+
+- (void)applySurfaceThemeWithScheme:(nonnull id<MDCContainerScheming>)scheme {
+  [self applySurfaceThemeWithColorScheme:scheme.colorScheme];
+  [self applyThemeWithTypographyScheme:scheme.typographyScheme];
+  self.elevation = MDCShadowElevationBottomNavigationBar;
+  self.itemsContentVerticalMargin = 0;
+}
+
+- (void)applySurfaceThemeWithColorScheme:(nonnull id<MDCColorScheming>)colorScheme {
+  self.barTintColor = colorScheme.surfaceColor;
+  self.selectedItemTintColor = colorScheme.primaryColor;
+  self.selectedItemTitleColor = colorScheme.primaryColor;
+  self.unselectedItemTintColor = [colorScheme.onSurfaceColor colorWithAlphaComponent:kUnselectedSurfaceAlpha];
+}
+
+- (void)applyThemeWithTypographyScheme:(nonnull id<MDCTypographyScheming>)typographyScheme {
+  self.itemTitleFont = typographyScheme.caption;
+}
+
+@end
