47ec434870385ef2660fa5bc7f9c4549b86a2b8f
Yingwei Fan <yingweifan@google.com>
2019-06-21 23:07:22 -0700

    Resolve issues

--- components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
+++ components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
@@ -124,9 +124,9 @@ static const UIEdgeInsets kEdgeInsets = {.top = 12, .right = 16, .bottom = 12, .
   // Calculate the sizes of icon and label. Use them to calculate the total item view size.
   CGSize iconSize = [self.iconImageView sizeThatFits:maxSize];
   CGSize labelSize = [self.titleLabel sizeThatFits:maxSize];
-  CGFloat width = ceil(MAX(iconSize.width, labelSize.width) + horizontalPadding);
+  CGFloat width = (CGFloat)ceil(MAX(iconSize.width, labelSize.width) + horizontalPadding);
   width = MIN(kMaxWidth, MAX(kMinWidth, width));
-  CGFloat height = ceil(iconSize.height + labelSize.height + verticalPadding);
+  CGFloat height = (CGFloat)ceil(iconSize.height + labelSize.height + verticalPadding);
   height = MAX(kMinHeight, height);
   return CGSizeMake(width, height);
 }
