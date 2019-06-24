eb800c748d586e467b3553021f8e3f03ae81cfdb
Leon Li <alangli@alangli-macbookpro.roam.corp.google.com>
2019-06-24 12:39:45 -0700

    [Tab2] WIP

--- components/Tabs/tests/snapshot/TabBarView/MDCTabBarViewSnapshotTests.m
+++ components/Tabs/tests/snapshot/TabBarView/MDCTabBarViewSnapshotTests.m
@@ -16,8 +16,14 @@
 
 #import "MDCTabBarView.h"
 
-@interface MDCTabBarViewSnapshotTests : MDCSnapshotTestCase
+static const CGFloat kWidthWide = 1600;
+static const CGFloat kWidthNarrow = 240;
+static const CGFloat kHeightTall = 120;
+static const CGFloat kHeightShort = 48;
 
+
+@interface MDCTabBarViewSnapshotTests : MDCSnapshotTestCase
+@property(nonatomic, strong) MDCTabBarView *tabBar;
 @end
 
 @implementation MDCTabBarViewSnapshotTests
@@ -27,8 +33,15 @@
 
   // Uncomment below to recreate all the goldens (or add the following line to the specific
   // test you wish to recreate the golden for).
-  //  self.recordMode = YES;
+  self.recordMode = YES;
+
+  self.tabBar = [[MDCTabBarView alloc] init];
+
+  CGSize imageSize = CGSizeMake(24, 24);
+  self.tabBar.items =
+  @[ self.tabItem1, self.tabItem2, self.tabItem3, self.tabItem4, self.tabItem5 ];
 }
+
 
 #pragma mark - Helpers
 
@@ -39,14 +52,146 @@
 
 #pragma mark - Tests
 
+
 - (void)testTabBarFixedSize {
   // Given
   MDCTabBarView *tabBarView = [[MDCTabBarView alloc] init];
   tabBarView.bounds = CGRectMake(0, 0, 120, 56);
+  UITabBarItem *item1 = [[UITabBarItem alloc] initWithTitle:@"One" image:self.typicalIcon1 tag:0];
+  UITabBarItem *item2 = [[UITabBarItem alloc] initWithTitle:@"Two" image:self.typicalIcon1 tag:2];
+  UITabBarItem *item3 = [[UITabBarItem alloc] initWithTitle:@"Three" image:self.typicalIcon1 tag:5];
   tabBarView.backgroundColor = UIColor.blueColor;
 
   // Then
   [self generateSnapshotAndVerifyForView:tabBarView];
 }
+
+#pragma mark - Extreme sizes
+
+- (void)testJustifiedUnspecifiedAlwaysFiveItemsNarrowWidthShortHeightLTR {
+  // When
+  self.tabBar.selectedItem = self.tabItem2;
+  self.tabBar.frame = CGRectMake(0, 0, kWidthNarrow, kHeightShort);
+
+  // Then
+  [self generateSnapshotAndVerifyForView:self.tabBar];
+}
+
+- (void)testJustifiedUnspecifiedAlwaysFiveItemsNarrowWidthShortHeightRTL {
+  // When
+  self.tabBar.selectedItem = self.tabItem2;
+  self.tabBar.frame = CGRectMake(0, 0, kWidthNarrow, kHeightShort);
+
+  // Then
+  [self generateSnapshotAndVerifyForView:self.tabBar];
+}
+
+- (void)testJustifiedUnspecifiedAlwaysFiveItemsWideWidthTallHeightLTR {
+  // When
+  self.tabBar.selectedItem = self.tabItem2;
+  self.tabBar.frame = CGRectMake(0, 0, kWidthWide, kHeightTall);
+
+  // Then
+  [self generateSnapshotAndVerifyForView:self.tabBar];
+}
+
+- (void)testJustifiedUnspecifiedAlwaysFiveItemsWideWidthTallHeightRTL {
+  // When
+  self.tabBar.selectedItem = self.tabItem2;
+  self.tabBar.frame = CGRectMake(0, 0, kWidthWide, kHeightTall);
+
+  // Then
+  [self generateSnapshotAndVerifyForView:self.tabBar];
+}
+
+#pragma mark - Layout Adjustments
+
+//- (void)testTitlePositionAdjustmentJustifiedAdjacentCompactLTR {
+//  // Given
+//  MDCBottomNavigationSnapshotTestMutableTraitCollection *traitCollection =
+//  [[MDCBottomNavigationSnapshotTestMutableTraitCollection alloc] init];
+//  traitCollection.horizontalSizeClassOverride = UIUserInterfaceSizeClassCompact;
+//  self.tabBar.titleVisibility = MDCBottomtabBarTitleVisibilityAlways;
+//  self.tabBar.alignment = MDCBottomtabBarAlignmentJustifiedAdjacentTitles;
+//  self.tabBar.selectedItem = self.tabItem2;
+//  self.tabBar.traitCollectionOverride = traitCollection;
+//  CGSize fitSize = [self.tabBar sizeThatFits:CGSizeMake(kWidthWide, kHeightTall)];
+//  self.tabBar.frame = CGRectMake(0, 0, fitSize.width, fitSize.height);
+//  [self performInkTouchOnBar:self.tabBar item:self.tabItem1];
+//
+//  // When
+//  self.tabItem1.titlePositionAdjustment = UIOffsetMake(20, -20);
+//  self.tabItem3.titlePositionAdjustment = UIOffsetMake(-20, 20);
+//
+//  // Then
+//  [self generateSnapshotAndVerifyForView:self.tabBar];
+//}
+
+//- (void)testTitlePositionAdjustmentJustifiedAdjacentCompactRTL {
+//  // Given
+//  MDCBottomNavigationSnapshotTestMutableTraitCollection *traitCollection =
+//  [[MDCBottomNavigationSnapshotTestMutableTraitCollection alloc] init];
+//  traitCollection.horizontalSizeClassOverride = UIUserInterfaceSizeClassCompact;
+//  self.tabBar.titleVisibility = MDCBottomtabBarTitleVisibilityAlways;
+//  self.tabBar.alignment = MDCBottomtabBarAlignmentJustifiedAdjacentTitles;
+//  self.tabBar.selectedItem = self.tabItem2;
+//  self.tabBar.traitCollectionOverride = traitCollection;
+//  CGSize fitSize = [self.tabBar sizeThatFits:CGSizeMake(kWidthWide, kHeightTall)];
+//  self.tabBar.frame = CGRectMake(0, 0, fitSize.width, fitSize.height);
+//  [self performInkTouchOnBar:self.tabBar item:self.tabItem1];
+//  [self changeToRTLAndArabicWithTitle:MDCBottomNavigationTestShortTitleArabic];
+//
+//  // When
+//  self.tabItem1.titlePositionAdjustment = UIOffsetMake(20, -20);
+//  self.tabItem3.titlePositionAdjustment = UIOffsetMake(-20, 20);
+//
+//  // Then
+//  [self generateSnapshotAndVerifyForView:self.tabBar];
+//}
+
+//- (void)testTitlePositionAdjustmentJustifiedAdjacentRegularLTR {
+//  // Given
+//  MDCBottomNavigationSnapshotTestMutableTraitCollection *traitCollection =
+//  [[MDCBottomNavigationSnapshotTestMutableTraitCollection alloc] init];
+//  traitCollection.horizontalSizeClassOverride = UIUserInterfaceSizeClassRegular;
+//  self.tabBar.titleVisibility = MDCBottomtabBarTitleVisibilityAlways;
+//  self.tabBar.alignment = MDCBottomtabBarAlignmentJustifiedAdjacentTitles;
+//  self.tabBar.selectedItem = self.tabItem2;
+//  self.tabBar.traitCollectionOverride = traitCollection;
+//  CGSize fitSize = [self.tabBar sizeThatFits:CGSizeMake(kWidthWide, kHeightTall)];
+//  self.tabBar.frame = CGRectMake(0, 0, fitSize.width, fitSize.height);
+//  [self performInkTouchOnBar:self.tabBar item:self.tabItem1];
+//
+//  // When
+//  self.tabItem1.titlePositionAdjustment = UIOffsetMake(20, -20);
+//  self.tabItem3.titlePositionAdjustment = UIOffsetMake(-20, 20);
+//
+//  // Then
+//  [self generateSnapshotAndVerifyForView:self.tabBar];
+//}
+
+//- (void)testTitlePositionAdjustmentJustifiedAdjacentRegularRTL {
+//  // Given
+//  MDCBottomNavigationSnapshotTestMutableTraitCollection *traitCollection =
+//  [[MDCBottomNavigationSnapshotTestMutableTraitCollection alloc] init];
+//  traitCollection.horizontalSizeClassOverride = UIUserInterfaceSizeClassRegular;
+//  self.tabBar.titleVisibility = MDCBottomtabBarTitleVisibilityAlways;
+//  self.tabBar.alignment = MDCBottomtabBarAlignmentJustifiedAdjacentTitles;
+//  self.tabBar.selectedItem = self.tabItem2;
+//  self.tabBar.traitCollectionOverride = traitCollection;
+//  CGSize fitSize = [self.tabBar sizeThatFits:CGSizeMake(kWidthWide, kHeightTall)];
+//  self.tabBar.frame = CGRectMake(0, 0, fitSize.width, fitSize.height);
+//  [self performInkTouchOnBar:self.tabBar item:self.tabItem1];
+//  [self changeToRTLAndArabicWithTitle:MDCBottomNavigationTestShortTitleArabic];
+//
+//  // When
+//  self.tabItem1.titlePositionAdjustment = UIOffsetMake(20, -20);
+//  self.tabItem3.titlePositionAdjustment = UIOffsetMake(-20, 20);
+//
+//  // Then
+//  [self generateSnapshotAndVerifyForView:self.tabBar];
+//}
+
+
 
 @end
