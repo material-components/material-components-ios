bea01cb1a405465d3499e9460d01083b56de1e1c
Robert Moore <rsmoore@google.com>
2019-06-24 14:50:59 -0400

    [Tabs] KVO title, image, selectedImage.
    Adds Key-Value Observing (KVO) of the `title`, `image`, and `selectedImage` properties of UITabBarItem.
    
    Part of #7645

--- components/Tabs/src/TabBarView/MDCTabBarView.m
+++ components/Tabs/src/TabBarView/MDCTabBarView.m
@@ -18,6 +18,10 @@
 /** Minimum (typical) height of a Material Tab bar. */
 static const CGFloat kMinHeight = 48;
 
+static NSString *const kImageString = @"image";
+static NSString *const kSelectedImageString = @"selectedImage";
+static NSString *const kTitleString = @"title";
+
 @interface MDCTabBarView ()
 
 /** The views representing the items of this tab bar. */
@@ -47,6 +51,8 @@ static const CGFloat kMinHeight = 48;
     return;
   }
 
+  [self removeObserversFromTabBarItems];
+
   for (UIView *itemView in self.itemViews) {
     [itemView removeFromSuperview];
   }
@@ -73,6 +79,8 @@ static const CGFloat kMinHeight = 48;
   }
 
   self.selectedItem = newSelectedItem;
+
+  [self addObserversToTabBarItems];
   [self setNeedsLayout];
 }
 
@@ -91,6 +99,74 @@ static const CGFloat kMinHeight = 48;
   }
 
   _selectedItem = selectedItem;
+  UIView *itemView = self.itemViews[itemIndex];
+  if ([itemView isKindOfClass:[MDCTabBarViewItemView class]]) {
+    MDCTabBarViewItemView *selectedItemView = (MDCTabBarViewItemView *)itemView;
+    selectedItemView.image = selectedItem.selectedImage ?: selectedItem.image;
+  }
+}
+
+#pragma mark - Key-Value Observing (KVO)
+
+- (void)addObserversToTabBarItems {
+  for (UITabBarItem *item in self.items) {
+    [item addObserver:self
+           forKeyPath:kImageString
+              options:NSKeyValueObservingOptionNew
+              context:nil];
+    [item addObserver:self
+           forKeyPath:kSelectedImageString
+              options:NSKeyValueObservingOptionNew
+              context:nil];
+    [item addObserver:self
+           forKeyPath:kTitleString
+              options:NSKeyValueObservingOptionNew
+              context:nil];
+  }
+}
+
+- (void)removeObserversFromTabBarItems {
+  for (UITabBarItem *item in self.items) {
+    @try {
+      [item removeObserver:self forKeyPath:kImageString];
+      [item removeObserver:self forKeyPath:kSelectedImageString];
+      [item removeObserver:self forKeyPath:kTitleString];
+    } @catch (NSException *exception) {
+      if (exception) {
+        // No need to do anything if there are no observers.
+      }
+    }
+  }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey, id> *)change
+                       context:(void *)context {
+  if (!context) {
+    NSInteger selectedItemNum = 0;
+    for (NSUInteger i = 0; i < self.items.count; i++) {
+      UITabBarItem *item = self.items[i];
+      if (object == item) {
+        selectedItemNum = i;
+        break;
+      }
+    }
+    // Don't try to update custom views
+    UIView *itemView = self.itemViews[selectedItemNum];
+    if (![itemView isKindOfClass:[MDCTabBarViewItemView class]]) {
+      return;
+    }
+    MDCTabBarViewItemView *tabBarItemView = (MDCTabBarViewItemView *)itemView;
+
+    if ([keyPath isEqualToString:kImageString]) {
+      tabBarItemView.image = change[NSKeyValueChangeNewKey];
+    } else if ([keyPath isEqualToString:kSelectedImageString] && self.selectedItem == object) {
+      tabBarItemView.image = change[NSKeyValueChangeNewKey];
+    } else if ([keyPath isEqualToString:kTitleString]) {
+      tabBarItemView.title = change[NSKeyValueChangeNewKey];
+    }
+  }
 }
 
 #pragma mark - UIView
