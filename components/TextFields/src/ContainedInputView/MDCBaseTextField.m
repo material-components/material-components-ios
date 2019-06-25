d010e045f86f1424a779b12748f4ef79a95265b1
Andrew Overton <andrewoverton@google.com>
2019-06-25 10:03:28 -0400

    Add some RTL functionality to MDCBaseTextField

--- components/TextFields/src/ContainedInputView/MDCBaseTextField.m
+++ components/TextFields/src/ContainedInputView/MDCBaseTextField.m
@@ -14,5 +14,177 @@
 
 #import "MDCBaseTextField.h"
 
+#import <Foundation/Foundation.h>
+
+#import <MDFInternationalization/MDFInternationalization.h>
+
+@interface MDCBaseTextField ()
+
+@property(nonatomic, assign) UIUserInterfaceLayoutDirection layoutDirection;
+
+@end
+
 @implementation MDCBaseTextField
+
+#pragma mark Object Lifecycle
+
+- (instancetype)initWithFrame:(CGRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    [self commonMDCInputTextFieldInit];
+  }
+  return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    [self commonMDCInputTextFieldInit];
+  }
+  return self;
+}
+
+- (void)commonMDCInputTextFieldInit {
+  [self initializeProperties];
+}
+
+#pragma mark View Setup
+
+- (void)initializeProperties {
+  [self setUpLayoutDirection];
+}
+
+- (void)setUpLayoutDirection {
+  self.layoutDirection = self.mdf_effectiveUserInterfaceLayoutDirection;
+}
+
+#pragma mark UIView Overrides
+
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  [self setUpLayoutDirection];
+}
+
+#pragma mark UITextField Accessor Overrides
+
+- (void)setLeftViewMode:(UITextFieldViewMode)leftViewMode {
+  NSLog(@"Setting leftViewMode is not recommended. Consider setting leadingViewMode and "
+        @"trailingViewMode instead.");
+  [self mdc_setLeftViewMode:leftViewMode];
+}
+
+- (void)setRightViewMode:(UITextFieldViewMode)rightViewMode {
+  NSLog(@"Setting rightViewMode is not recommended. Consider setting leadingViewMode and "
+        @"trailingViewMode instead.");
+  [self mdc_setRightViewMode:rightViewMode];
+}
+
+- (void)setLeftView:(UIView *)leftView {
+  NSLog(@"Setting rightView and leftView are not recommended. Consider setting leadingView and "
+        @"trailingView instead.");
+  [self mdc_setLeftView:leftView];
+}
+
+- (void)setRightView:(UIView *)rightView {
+  NSLog(@"Setting rightView and leftView are not recommended. Consider setting leadingView and "
+        @"trailingView instead.");
+  [self mdc_setRightView:rightView];
+}
+
+#pragma mark Custom Accessors
+
+- (void)setTrailingView:(UIView *)trailingView {
+  if ([self isRTL]) {
+    [self mdc_setLeftView:trailingView];
+  } else {
+    [self mdc_setRightView:trailingView];
+  }
+}
+
+- (UIView *)trailingView {
+  if ([self isRTL]) {
+    return self.leftView;
+  } else {
+    return self.rightView;
+  }
+}
+
+- (void)setLeadingView:(UIView *)leadingView {
+  if ([self isRTL]) {
+    [self mdc_setRightView:leadingView];
+  } else {
+    [self mdc_setLeftView:leadingView];
+  }
+}
+
+- (UIView *)leadingView {
+  if ([self isRTL]) {
+    return self.rightView;
+  } else {
+    return self.leftView;
+  }
+}
+
+- (void)mdc_setLeftView:(UIView *)leftView {
+  [super setLeftView:leftView];
+}
+
+- (void)mdc_setRightView:(UIView *)rightView {
+  [super setRightView:rightView];
+}
+
+- (void)setTrailingViewMode:(UITextFieldViewMode)trailingViewMode {
+  if ([self isRTL]) {
+    [self mdc_setLeftViewMode:trailingViewMode];
+  } else {
+    [self mdc_setRightViewMode:trailingViewMode];
+  }
+}
+
+- (UITextFieldViewMode)trailingViewMode {
+  if ([self isRTL]) {
+    return self.leftViewMode;
+  } else {
+    return self.rightViewMode;
+  }
+}
+
+- (void)setLeadingViewMode:(UITextFieldViewMode)leadingViewMode {
+  if ([self isRTL]) {
+    [self mdc_setRightViewMode:leadingViewMode];
+  } else {
+    [self mdc_setLeftViewMode:leadingViewMode];
+  }
+}
+
+- (UITextFieldViewMode)leadingViewMode {
+  if ([self isRTL]) {
+    return self.rightViewMode;
+  } else {
+    return self.leftViewMode;
+  }
+}
+
+- (void)mdc_setLeftViewMode:(UITextFieldViewMode)leftViewMode {
+  [super setLeftViewMode:leftViewMode];
+}
+
+- (void)mdc_setRightViewMode:(UITextFieldViewMode)rightViewMode {
+  [super setRightViewMode:rightViewMode];
+}
+
+- (void)setLayoutDirection:(UIUserInterfaceLayoutDirection)layoutDirection {
+  if (_layoutDirection == layoutDirection) {
+    return;
+  }
+  _layoutDirection = layoutDirection;
+  [self setNeedsLayout];
+}
+
+#pragma mark Internationalization
+
+- (BOOL)isRTL {
+  return self.layoutDirection == UIUserInterfaceLayoutDirectionRightToLeft;
+}
+
 @end
