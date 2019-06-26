fc96bbbe4915ad54bfea584639b446d41b0603ed
Robert Moore <rsmoore@google.com>
2019-06-26 00:08:54 -0400

    [Tabs] KVO UITabBarItem's `title` and `image` properties (#7700)
    Adds Key-Value Observing (KVO) of the `title` and `image` properties of UITabBarItem. `selectedImage` is excluded intentionally because Material Tabs don't necessitate the support and it isn't required for the MVP.
    
    Part of #7645

--- components/Tabs/src/TabBarView/MDCTabBarView.m
+++ components/Tabs/src/TabBarView/MDCTabBarView.m
@@ -15,8 +15,14 @@
 #import "MDCTabBarView.h"
 #import "private/MDCTabBarViewItemView.h"
 
+// KVO contexts
+static char *const kKVOContextMDCTabBarView = "kKVOContextMDCTabBarView";
+
 /** Minimum (typical) height of a Material Tab bar. */
 static const CGFloat kMinHeight = 48;
+
+static NSString *const kImageKeyPath = @"image";
+static NSString *const kTitleKeyPath = @"title";
 
 @interface MDCTabBarView ()
 
@@ -42,6 +48,10 @@ static const CGFloat kMinHeight = 48;
   return self;
 }
 
+- (void)dealloc {
+  [self removeObserversFromTabBarItems];
+}
+
 #pragma mark - Properties
 
 - (void)setBarTintColor:(UIColor *)barTintColor {
@@ -58,6 +68,8 @@ static const CGFloat kMinHeight = 48;
   if (self.items == items || [self.items isEqual:items]) {
     return;
   }
+
+  [self removeObserversFromTabBarItems];
 
   for (UIView *itemView in self.itemViews) {
     [itemView removeFromSuperview];
@@ -86,6 +98,8 @@ static const CGFloat kMinHeight = 48;
   }
 
   self.selectedItem = newSelectedItem;
+
+  [self addObserversToTabBarItems];
   [self setNeedsLayout];
 }
 
@@ -102,6 +116,68 @@ static const CGFloat kMinHeight = 48;
   }
 
   _selectedItem = selectedItem;
+  if (itemIndex == NSNotFound) {
+    return;
+  }
+
+  UIView *itemView = self.itemViews[itemIndex];
+  if ([itemView isKindOfClass:[MDCTabBarViewItemView class]]) {
+    MDCTabBarViewItemView *selectedItemView = (MDCTabBarViewItemView *)itemView;
+    selectedItemView.iconImageView.image = selectedItem.image;
+  }
+
+  [self updateTitleColorForAllViews];
+}
+
+#pragma mark - Key-Value Observing (KVO)
+
+- (void)addObserversToTabBarItems {
+  for (UITabBarItem *item in self.items) {
+    [item addObserver:self
+           forKeyPath:kImageKeyPath
+              options:NSKeyValueObservingOptionNew
+              context:kKVOContextMDCTabBarView];
+    [item addObserver:self
+           forKeyPath:kTitleKeyPath
+              options:NSKeyValueObservingOptionNew
+              context:kKVOContextMDCTabBarView];
+  }
+}
+
+- (void)removeObserversFromTabBarItems {
+  for (UITabBarItem *item in self.items) {
+    [item removeObserver:self forKeyPath:kImageKeyPath context:kKVOContextMDCTabBarView];
+    [item removeObserver:self forKeyPath:kTitleKeyPath context:kKVOContextMDCTabBarView];
+  }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey, id> *)change
+                       context:(void *)context {
+  if (context == kKVOContextMDCTabBarView) {
+    if (!object) {
+      return;
+    }
+    NSUInteger indexOfObject = [self.items indexOfObject:object];
+    if (indexOfObject == NSNotFound) {
+      return;
+    }
+    // Don't try to update custom views
+    UIView *updatedItemView = self.itemViews[indexOfObject];
+    if (![updatedItemView isKindOfClass:[MDCTabBarViewItemView class]]) {
+      return;
+    }
+    MDCTabBarViewItemView *tabBarItemView = (MDCTabBarViewItemView *)updatedItemView;
+
+    if ([keyPath isEqualToString:kImageKeyPath]) {
+      tabBarItemView.iconImageView.image = change[NSKeyValueChangeNewKey];
+    } else if ([keyPath isEqualToString:kTitleKeyPath]) {
+      tabBarItemView.titleLabel.text = change[NSKeyValueChangeNewKey];
+    }
+  } else {
+    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+  }
   [self updateTitleColorForAllViews];
 }
 
