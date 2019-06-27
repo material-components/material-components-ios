1a38f2af4630af5477085cde76efa5ecf3d9d456
mikefan1991 <yingweifan@google.com>
2019-06-27 12:16:38 -0700

    [Tabs] Fix bugs (#7735)
    Fix small bugs in the MDCTabBarView and the MDCTabBarViewItemView.

--- components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
+++ components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
@@ -114,16 +114,11 @@ static const UIEdgeInsets kEdgeInsets = {.top = 12, .right = 16, .bottom = 12, .
   NSString *title = self.titleLabel.text;
   UIImage *icon = self.iconImageView.image;
   const CGFloat minHeight = (title && icon) ? kMinHeightTitleAndImage : kMinHeightTitleOrImageOnly;
+  const CGFloat maxHeight = MAX(minHeight, size.height);
 
   const CGFloat horizontalPadding = kEdgeInsets.left + kEdgeInsets.right;
   const CGFloat verticalPadding = kEdgeInsets.top + kEdgeInsets.bottom;
-
-  const CGFloat maxHeight = MAX(minHeight, size.height);
   CGSize maxSize = CGSizeMake(kMaxWidth - horizontalPadding, maxHeight - verticalPadding);
-  CGSize labelFitSize = self.titleLabel.intrinsicContentSize;
-  if (labelFitSize.width > maxSize.width) {
-    labelFitSize = [self.titleLabel sizeThatFits:CGSizeMake(maxSize.width, CGFLOAT_MAX)];
-  }
 
   // Calculate the sizes of icon and label. Use them to calculate the total item view size.
   CGSize iconSize = [self.iconImageView sizeThatFits:maxSize];
