81f49553ea78a9f83f8e1e7ff93ce3e2b8c895e7
Cody Weaver <codyleeweaver@gmail.com>
2019-06-27 10:29:50 -0400

    feedback

--- components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
+++ components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
@@ -132,14 +132,4 @@ static const UIEdgeInsets kEdgeInsets = {.top = 12, .right = 16, .bottom = 12, .
   return CGSizeMake(width, height);
 }
 
-#pragma mark - UIAccessibility
-
-- (NSString *)accessibilityHint {
-  return [super accessibilityLabel];
-}
-
-- (NSString *)accessibilityIdentifier {
-  return [super accessibilityIdentifier];
-}
-
 @end
