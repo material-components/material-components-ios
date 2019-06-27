6ca1daf8c912bb0bcc517f8777a25d064d17f375
Leon Li <alangli@google.com>
2019-06-26 17:46:48 -0700

    Add ripple effect

--- components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
+++ components/Tabs/src/TabBarView/private/MDCTabBarViewItemView.m
@@ -79,6 +79,11 @@ static const UIEdgeInsets kEdgeInsets = {.top = 12, .right = 16, .bottom = 12, .
     _titleLabel.isAccessibilityElement = NO;
     [_contentView addSubview:_titleLabel];
   }
+
+  if (!_rippleTouchController) {
+    _rippleTouchController = [[MDCRippleTouchController alloc] initWithView:self];
+    _rippleTouchController.rippleView.rippleStyle = MDCRippleStyleBounded;
+  }
 }
 
 #pragma mark - UIView
