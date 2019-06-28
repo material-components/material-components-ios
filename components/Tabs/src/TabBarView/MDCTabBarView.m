701904547a008c9d1c0997bb3c763776008ba456
Yingwei Fan <yingweifan@google.com>
2019-06-27 17:13:49 -0700

    Fix c-lang

--- components/Tabs/src/TabBarView/MDCTabBarView.m
+++ components/Tabs/src/TabBarView/MDCTabBarView.m
@@ -104,7 +104,8 @@ static NSString *const kAccessibilityTraitsKeyPath = @"accessibilityTraits";
     itemView.accessibilityLabel = item.accessibilityLabel;
     itemView.accessibilityHint = item.accessibilityHint;
     itemView.accessibilityIdentifier = item.accessibilityIdentifier;
-    itemView.accessibilityTraits = item.accessibilityTraits == UIAccessibilityTraitNone
+    itemView.accessibilityTraits =
+    item.accessibilityTraits == UIAccessibilityTraitNone
                                        ? UIAccessibilityTraitButton
                                        : item.accessibilityTraits;
     itemView.titleLabel.textColor = [self titleColorForState:UIControlStateNormal];
