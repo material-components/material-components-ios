262c563b59fa730b5bb4917e095ad4961e2d23d4
Robert Moore <rsmoore@google.com>
2019-07-02 13:06:15 -0400

    [Tabs] Customize selection indicator color.
    Adds the ability to set a custom selection indicator color to the tab bar.
    
    Closes #7657

--- components/Tabs/tests/snapshot/TabBarView/MDCTabBarViewSnapshotTests.m
+++ components/Tabs/tests/snapshot/TabBarView/MDCTabBarViewSnapshotTests.m
@@ -934,6 +934,39 @@ static NSString *const kItemTitleLong3Arabic = @"تحت أي قدما وإقام
   [self generateSnapshotAndVerifyForView:self.tabBarView];
 }
 
+- (void)testCustomSelectionIndicatorStrokeColor {
+  // Given
+  UITabBarItem *item1 = [[UITabBarItem alloc] initWithTitle:@"One" image:self.typicalIcon1 tag:0];
+  UITabBarItem *item2 = [[UITabBarItem alloc] initWithTitle:@"Two" image:self.typicalIcon2 tag:2];
+  UITabBarItem *item3 = [[UITabBarItem alloc] initWithTitle:@"Three" image:self.typicalIcon3 tag:3];
+  self.tabBarView.items = @[ item1, item2, item3 ];
+  [self.tabBarView setSelectedItem:item2 animated:NO];
+  [self.tabBarView sizeToFit];
+
+  // When
+  self.tabBarView.selectionIndicatorStrokeColor = UIColor.purpleColor;
+
+  // Then
+  [self generateSnapshotAndVerifyForView:self.tabBarView];
+}
+
+- (void)testSelectionIndicatorStrokeColorResetsToDefault {
+  // Given
+  UITabBarItem *item1 = [[UITabBarItem alloc] initWithTitle:@"One" image:self.typicalIcon1 tag:0];
+  UITabBarItem *item2 = [[UITabBarItem alloc] initWithTitle:@"Two" image:self.typicalIcon2 tag:2];
+  UITabBarItem *item3 = [[UITabBarItem alloc] initWithTitle:@"Three" image:self.typicalIcon3 tag:3];
+  self.tabBarView.items = @[ item1, item2, item3 ];
+  [self.tabBarView setSelectedItem:item2 animated:NO];
+  [self.tabBarView sizeToFit];
+  self.tabBarView.selectionIndicatorStrokeColor = UIColor.purpleColor;
+
+  // When
+  self.tabBarView.selectionIndicatorStrokeColor = nil;
+
+  // Then
+  [self generateSnapshotAndVerifyForView:self.tabBarView];
+}
+
 #pragma mark - Custom View support
 
 - (void)testCustomView {
