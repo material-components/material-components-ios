a401556670c9ec918c6f1191f797c42030c48c23
Bryan Oltman <bryanoltman@gmail.com>
2019-07-22 10:40:27 -0400

    [Chips] Add traitCollectionDidChangeBlock

--- components/Chips/src/MDCChipView.m
+++ components/Chips/src/MDCChipView.m
@@ -236,6 +236,13 @@ static inline CGSize CGSizeShrinkWithInsets(CGSize size, UIEdgeInsets edgeInsets
                                                 object:nil];
 }
 
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  if (self.traitCollectionDidChangeBlock) {
+    self.traitCollectionDidChangeBlock(self, previousTraitCollection);
+  }
+}
+
 - (void)setShapeGenerator:(id<MDCShapeGenerating>)shapeGenerator {
   if (shapeGenerator) {
     self.layer.cornerRadius = 0;
