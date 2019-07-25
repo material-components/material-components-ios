66435f1b685c363c2ee534d8f2a065cef369b2b0
Robert Moore <rsmoore@google.com>
2019-07-25 15:41:26 -0400

    [Color] Create umbrella header. (#8091)
    All components should have a top-level umbrella header for their includes.
    This allows easier refactoring of classes and files within the component.
    Creating an umbrella for Color and using it outside the component.
    
    Part of #8086

--- components/Ripple/tests/snapshot/MDCRippleViewSnapshotTests.m
+++ components/Ripple/tests/snapshot/MDCRippleViewSnapshotTests.m
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import "MaterialColor.h"
 #import "MaterialRipple.h"
 #import "MaterialSnapshot.h"
-#import "UIColor+MaterialDynamic.h"
 
 /**
  Creates a fake MDCRippleView that has its traitCollection overridden.
