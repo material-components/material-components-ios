d389bcc3461712b5dbfaf4b51d6e60fb73f4f800
Eric Lee <ericdavidlee@google.com>
2019-08-09 10:14:21 -0700

    [Beta] Fixes beta components CocoaPods build. (#8134)
    I was seeing build errors when compiling the beta components after installing using CocoaPods.  This appeared to fix those issues.

--- components/ActionSheet/src/ColorThemer/MDCActionSheetColorThemer.h
+++ components/ActionSheet/src/ColorThemer/MDCActionSheetColorThemer.h
@@ -12,10 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MaterialActionSheet.h"
-
 #import <Foundation/Foundation.h>
-
+#import <MaterialComponents/MaterialActionSheet.h>
 #import <MaterialComponents/MaterialColorScheme.h>
 
 /**
