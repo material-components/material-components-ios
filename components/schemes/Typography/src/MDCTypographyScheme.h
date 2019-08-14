db318be4d8ddafcc7fbe6abaa3f14dc83eb6f7d4
Wenyu Zhang <wenyuz@google.com>
2019-08-14 14:36:11 -0400

    CRC.

--- components/schemes/Typography/src/MDCTypographyScheme.h
+++ components/schemes/Typography/src/MDCTypographyScheme.h
@@ -94,7 +94,7 @@
  @warning This API will eventually be deprecated. Please use
  @c useCurrentContentSizeCategoryWhenApplied instead.
 */
-@property(nonatomic, readonly) BOOL mdc_adjustsFontForContentSizeCategory __deprecated;
+@property(nonatomic, readonly) BOOL mdc_adjustsFontForContentSizeCategory __deprecated_msg("Use useCurrentContentSizeCategoryWhenApplied instead.");
 
 @optional
 
