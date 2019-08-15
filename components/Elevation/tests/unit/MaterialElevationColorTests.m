a667b241f18a23f789bfb2b4e2f8235d36320d0a
Wenyu Zhang <8836258+wenyuzhang666@users.noreply.github.com>
2019-08-15 10:59:48 -0400

    [Color+Elevation] Add a convenience method to resolve dynamic color only when traitCollection's color appearance has changed. (#8315)
    Add this convenience method would help supporting theming extension in an easier way that we don't need to check color difference on traitCollection every time like https://github.com/material-components/material-components-ios/pull/8285/files#diff-48ca693ee2870d02b06a42900a1dc69dR73.

--- components/Elevation/tests/unit/MaterialElevationColorTests.m
+++ components/Elevation/tests/unit/MaterialElevationColorTests.m
@@ -296,4 +296,56 @@ static UIImage *fakeImageWithColorAndSize(UIColor *color, CGRect bounds) {
                                           secondColor:resolvedSecondRGBColor];
 }
 
+- (void)testResolvingColorWithSameCurrentTraitCollectionAndPreviousTraitCollection {
+  if (@available(iOS 13.0, *)) {
+    // Given
+    CGFloat elevation = (CGFloat)10;
+    UIColor *darkColor = UIColor.blackColor;
+    UIColor *lightColor = UIColor.whiteColor;
+    UIColor *dynamicColor = [UIColor colorWithUserInterfaceStyleDarkColor:darkColor
+                                                             defaultColor:lightColor];
+
+    // When
+    UITraitCollection *previousTraitCollection =
+        [UITraitCollection traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleDark];
+    UITraitCollection *currentTraitCollection =
+        [UITraitCollection traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleDark];
+    UIColor *resolvedColor =
+        [dynamicColor mdc_resolvedColorWithTraitCollection:currentTraitCollection
+                                   previousTraitCollection:previousTraitCollection
+                                                 elevation:elevation];
+    // Then
+    XCTAssertEqualObjects(resolvedColor, dynamicColor);
+  }
+}
+
+- (void)testResolvingColorWithDifferenceCurrentTraitCollectionAndPreviousTraitCollection {
+#if defined(__IPHONE_13_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0)
+  if (@available(iOS 13.0, *)) {
+    // Given
+    CGFloat elevation = (CGFloat)10;
+    UIColor *darkColor = UIColor.blackColor;
+    UIColor *lightColor = UIColor.whiteColor;
+    UIColor *dynamicColor = [UIColor colorWithUserInterfaceStyleDarkColor:darkColor
+                                                             defaultColor:lightColor];
+
+    // When
+    UITraitCollection *previousTraitCollection =
+        [UITraitCollection traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleDark];
+    UITraitCollection *currentTraitCollection =
+        [UITraitCollection traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleLight];
+    UIColor *resolvedColor =
+        [dynamicColor mdc_resolvedColorWithTraitCollection:currentTraitCollection
+                                   previousTraitCollection:previousTraitCollection
+                                                 elevation:elevation];
+    // Then
+    XCTAssertNotEqual(resolvedColor, dynamicColor);
+    UIColor *expectedColor =
+        [dynamicColor mdc_resolvedColorWithTraitCollection:currentTraitCollection
+                                                 elevation:elevation];
+    XCTAssertEqual(resolvedColor, expectedColor);
+  }
+#endif
+}
+
 @end
