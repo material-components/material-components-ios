0000ef4f9f5288973d80f336900dc749358b6eb6
featherless <featherless@google.com>
2019-09-10 09:31:39 -0400

    Fix.

--- components/schemes/Typography/src/BasicFontScheme/MDCBasicFontScheme.m
+++ components/schemes/Typography/src/BasicFontScheme/MDCBasicFontScheme.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCLegacyFontScheme.h"
+#import "MDCBasicFontScheme.h"
 
 @implementation MDCBasicFontScheme
 @end
