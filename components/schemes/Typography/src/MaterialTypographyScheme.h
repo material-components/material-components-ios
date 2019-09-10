e36bf9ee31fb2cda8f4b21efce74b12e0465ddfc
featherless <featherless@google.com>
2019-09-10 09:15:36 -0400

    Import.

--- components/schemes/Typography/src/MaterialTypographyScheme.h
+++ components/schemes/Typography/src/MaterialTypographyScheme.h
@@ -12,6 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCLegacyFontScheme.h"
-
+#import <MaterialComponents/MaterialTypography+BasicFontScheme.h>
 #import "MDCTypographyScheme.h"
