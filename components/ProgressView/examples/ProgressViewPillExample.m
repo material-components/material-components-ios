9dd8ba1b3682029754a84a5c4d7f4fab10777499
featherless <featherless@google.com>
2019-09-12 14:40:14 -0400

    Update ProgressViewPillExample.m

--- components/ProgressView/examples/ProgressViewPillExample.m
+++ components/ProgressView/examples/ProgressViewPillExample.m
@@ -95,11 +95,13 @@ static const CGFloat kProgressViewHeight = 4;
     @"progressViewHeight" : @(kProgressViewHeight),
   };
 
-  [NSLayoutConstraint activateConstraints:[NSLayoutConstraint
-        constraintsWithVisualFormat:@"V:|-(topMargin)-[progressView(==progressViewHeight)]"
-                            options:0
-                            metrics:metrics
-                              views:views]];
+  [NSLayoutConstraint
+      activateConstraints:
+          [NSLayoutConstraint
+              constraintsWithVisualFormat:@"V:|-(topMargin)-[progressView(==progressViewHeight)]"
+                                  options:0
+                                  metrics:metrics
+                                    views:views]];
 
   [NSLayoutConstraint activateConstraints:[NSLayoutConstraint
         constraintsWithVisualFormat:@"H:|-(horizontalMargin)-[progressView]-(horizontalMargin)-|"
