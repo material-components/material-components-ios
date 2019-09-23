fd3395b5297c9dd4e4aeb25c02dee6026720c962
Robert Moore <rsmoore@google.com>
2019-09-23 11:58:44 -0400

    [TypographyScheme] Replace `mdc_adjustsFontForContentSizeCategory` with (#8476)
    Replaces the deprecated API `mdc_adjustsFontForContentSizeCategory` with its
    stated replacement, `useCurrentContentSizeCategoryWhenApplied`. Also makes the
    replacement API required to ensure there is an available API to communicate
    this information to theming code.
    
    This change has already been tested internally and the necessary changes are captured in cl/270386732.
    
    Closes #7618

--- components/schemes/Typography/src/MDCTypographyScheming.h
+++ components/schemes/Typography/src/MDCTypographyScheming.h
@@ -86,33 +86,11 @@
 @property(nonatomic, nonnull, readonly, copy) UIFont *overline;
 
 /**
- Whether user interface elements should automatically resize based on the device's setting.
-
- This can be used by client to communicate whether they support dynamic type to both our theming
- functionality and embedded frameworks that also render UI.
-
- @warning This API will eventually be deprecated. Please use
- @c useCurrentContentSizeCategoryWhenApplied instead.
-
- @note  The value of @c useCurrentContentSizeCategoryWhenApplied (if implemented) and @c
- mdc_adjustsFontForContentSizeCategory must always be the same.
-
-*/
-@property(nonatomic, readonly) BOOL mdc_adjustsFontForContentSizeCategory __deprecated_msg(
-    "Use useCurrentContentSizeCategoryWhenApplied instead.");
-
-@optional
-
-/**
  A hint for how fonts in this scheme should be applied to components in relation to Dynamic Type.
 
  If this flag is enabled and the typography scheme's font is scalable with Dynamic Type, then fonts
  should be adjusted for the current Dynamic Type content size category prior to being assigned to
  the component.
-
- @note This flag will become required in the future as a replacement for
- @c mdc_adjustsFontForContentSizeCategory. The value of this flag needs to be the same as @c
- mdc_adjustsFontForContentSizeCategory.
  */
 @property(nonatomic, assign, readonly) BOOL useCurrentContentSizeCategoryWhenApplied;
 
