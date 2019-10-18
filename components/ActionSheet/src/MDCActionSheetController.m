14f5cd977a7f9ae7e0927553ac82471162b8ff76
Robert Moore <rsmoore@google.com>
2019-10-18 15:35:06 -0400

    Fix disabling the flag.

--- components/ActionSheet/src/MDCActionSheetController.m
+++ components/ActionSheet/src/MDCActionSheetController.m
@@ -478,9 +478,13 @@ static const CGFloat kDividerDefaultAlpha = (CGFloat)0.12;
 
 - (void)setAlwaysAlignTitleLeadingEdges:(BOOL)alignTitles {
   _alwaysAlignTitleLeadingEdges = alignTitles;
-  // Check to make sure at least one action has an image. If not then all actions will align already
-  // and we don't need to add padding.
-  self.addLeadingPaddingToCell = [self anyActionHasAnImage];
+  if (alignTitles) {
+    // Check to make sure at least one action has an image. If not then all actions will align
+    // already and we don't need to add padding.
+    self.addLeadingPaddingToCell = [self anyActionHasAnImage];
+  } else {
+    self.addLeadingPaddingToCell = NO;
+  }
   [self.tableView reloadData];
 }
 
