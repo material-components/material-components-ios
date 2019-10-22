ae2c25a5b1214406c928e0b7b1b1ad823bfaa2ae
Yarden Eitan <yarneo@gmail.com>
2019-10-22 16:29:13 -0400

    adding snapshot tests to chips

--- components/Chips/tests/snapshot/MDCChipViewCustomTraitCollectionSnapshotTests.m
+++ components/Chips/tests/snapshot/MDCChipViewCustomTraitCollectionSnapshotTests.m
@@ -47,7 +47,7 @@
 
   // Uncomment below to recreate all the goldens (or add the following line to the specific
   // test you wish to recreate the golden for).
-  //            self.recordMode = YES;
+  //          self.recordMode = YES;
 
   self.chip = [[MDCChipViewWithCustomTraitCollection alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
   self.containerScheme = [[MDCContainerScheme alloc] init];
