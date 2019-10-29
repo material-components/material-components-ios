19e987a5d871d51159ee1a30ee297dfbdcd60f79
Yarden Eitan <yarneo@gmail.com>
2019-10-29 13:14:52 -0400

    clang

--- components/TextFields/src/MDCTextFieldPositioningDelegate.h
+++ components/TextFields/src/MDCTextFieldPositioningDelegate.h
@@ -29,8 +29,9 @@
  The actual input view and the rendered inputted text's position is determined by applying these
  insets to the bounds.
 
- Note: This is a convenience method that calls `textInsets:withSizeThatFitsWidthHint:` with a width hint of 0.
- 
+ Note: This is a convenience method that calls `textInsets:withSizeThatFitsWidthHint:` with a width
+ hint of 0.
+
  @param defaultInsets The value of text container insets that the MDCTextInput has calculated by
  default.
  */
