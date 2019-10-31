d88229c76db2dcdc53911ac26942d2cd2c4ec5f5
Andrew Overton <andrewoverton@google.com>
2019-10-31 13:51:59 -0400

    Update unit tests

--- components/TextFields/tests/unit/ContainedInputView/MDCBaseTextFieldTests.m
+++ components/TextFields/tests/unit/ContainedInputView/MDCBaseTextFieldTests.m
@@ -184,7 +184,7 @@
   [textField layoutIfNeeded];
 
   // Then
-  CGRect expectedClearButtonFrame = CGRectMake(99, 22, 19, 19);
+  CGRect expectedClearButtonFrame = CGRectMake(99, 20, 19, 19);
   CGRect actualClearButtonFrame = [textField clearButtonRectForBounds:textFieldFrame];
   XCTAssertTrue(CGRectEqualToRect(actualClearButtonFrame, expectedClearButtonFrame));
 }
@@ -305,7 +305,7 @@
 
   // Then
   CGSize newSize = textField.frame.size;
-  CGSize correctSize = CGSizeMake(130, 63);
+  CGSize correctSize = CGSizeMake(130, 59);
   XCTAssertTrue(CGSizeEqualToSize(newSize, correctSize));
 }
 
