f791b575978360c4e2630c5bcc17323cf0a304b7
Wenyu Zhang <8836258+wenyuzhang666@users.noreply.github.com>
2019-11-12 12:09:04 -0500

    [AppBar] Add API to auto adjust height based on HeaderStackView for app bar. (#8764)
    Part of https://github.com/material-components/material-components-ios/issues/8677.
    
    This API helps to reduce the complexity of calculating minimumHeight and maximumHeight on FlexibleHeader when user wants to update the app bar height based on header stack view.
    
    This flag is required to be set during configuration stage. The reset behavior (turning back to NO) was not implemented because it would require adding API to minMaxHeight class and all configuration needs be to be set before flexible header is used. This feature can be added if needed.

--- components/AppBar/src/MDCAppBarViewController.m
+++ components/AppBar/src/MDCAppBarViewController.m
@@ -149,6 +149,14 @@ static NSString *const kMaterialAppBarBundle = @"MaterialAppBar.bundle";
   return backBarButtonItem;
 }
 
+- (void)setShouldAdjustHeightBasedOnHeaderStackView:(BOOL)shouldAdjustHeightBasedOnHeaderStackView {
+  _shouldAdjustHeightBasedOnHeaderStackView = shouldAdjustHeightBasedOnHeaderStackView;
+  if (shouldAdjustHeightBasedOnHeaderStackView) {
+    self.headerView.minMaxHeightIncludesSafeArea = NO;
+    [self adjustHeightBasedOnHeaderStackView];
+  }
+}
+
 - (void)setInferTopSafeAreaInsetFromViewController:(BOOL)inferTopSafeAreaInsetFromViewController {
   [super setInferTopSafeAreaInsetFromViewController:inferTopSafeAreaInsetFromViewController];
 
@@ -242,6 +250,10 @@ static NSString *const kMaterialAppBarBundle = @"MaterialAppBar.bundle";
     // height to make it smaller when the status bar is hidden.
     _verticalConstraint.constant = MDCDeviceTopSafeAreaInset();
   }
+
+  if (self.shouldAdjustHeightBasedOnHeaderStackView) {
+    [self adjustHeightBasedOnHeaderStackView];
+  }
 }
 
 - (void)didMoveToParentViewController:(UIViewController *)parent {
@@ -274,6 +286,16 @@ static NSString *const kMaterialAppBarBundle = @"MaterialAppBar.bundle";
   } else {
     [pvc dismissViewControllerAnimated:YES completion:nil];
   }
+}
+
+#pragma mark - Private
+
+- (void)adjustHeightBasedOnHeaderStackView {
+  CGFloat heightSum = 0;
+  heightSum += [self.headerStackView.topBar sizeThatFits:self.view.bounds.size].height;
+  heightSum += [self.headerStackView.bottomBar sizeThatFits:self.view.bounds.size].height;
+  self.headerView.minimumHeight = heightSum;
+  self.headerView.maximumHeight = heightSum;
 }
 
 @end
