8c82e4ed65f3efa97fd34aea2f0189100d4d4e42
Andrew Overton <andrewoverton@google.com>
2019-11-13 12:39:48 -0500

    Run clang

--- components/TextFields/src/private/MDCTextInputCommonFundament.h
+++ components/TextFields/src/private/MDCTextInputCommonFundament.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MDCTextInput.h"
 #import "MDCButton.h"
+#import "MDCTextInput.h"
 
 extern const CGFloat MDCTextInputBorderRadius;
 extern const CGFloat MDCTextInputFullPadding;
