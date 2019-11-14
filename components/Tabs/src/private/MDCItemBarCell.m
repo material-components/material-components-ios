fe35550a8d98cb9dc5630b929c88736ff7a65f5d
Robert Moore <rsmoore@google.com>
2019-11-13 23:43:37 -0500

    [Tabs] Use built-in "tabs" behavior on iOS 10+
    Although not available on iOS 9, on iOS 10 the MDCTabBar can use the
    UIAccessibilityTraitTabBar so that its accessibility elements will be
    decorated using the "tab, 1 of 3" information automatically. This change also
    improves interaction with Voice Control because that relies on matching the
    `accessibilityLabel`.
    
    Improves #6757 for iOS 10+

--- components/Tabs/src/private/MDCItemBarCell.m
+++ components/Tabs/src/private/MDCItemBarCell.m
@@ -375,21 +375,29 @@ static const NSTimeInterval kSelectionAnimationDuration = 0.3;
     [labelComponents addObject:_badgeValue];
   }
 
-  // Describe as "tab, X of Y"
-  NSString *tabLabel =
-      [[self class] localizedStringWithKey:kMDCItemBarStringKeyAccessibilityTabElementLabel];
-  if (tabLabel) {
-    [labelComponents addObject:tabLabel];
-  }
+  NSOperatingSystemVersion iOS10Version = {10, 0, 0};
+  NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+  BOOL isBelowiOS10 = ![processInfo isOperatingSystemAtLeastVersion:iOS10Version];
 
-  NSString *positionFormat =
-      [[self class] localizedStringWithKey:kMDCItemBarStringKeyAccessibilityTabPositionFormat];
-  if (positionFormat) {
-    if (_itemIndex != NSNotFound && _itemCount > 0) {
-      int position = (int)(_itemIndex + 1);
-      NSString *localizedPosition =
-          [NSString localizedStringWithFormat:positionFormat, position, (int)_itemCount];
-      [labelComponents addObject:localizedPosition];
+  // On iOS 10+, MDCTabBar will receive the UIAccessibilityTraitTabBar, so this logic is
+  // unnecessary.
+  if (isBelowiOS10) {
+    // Describe as "tab, X of Y"
+    NSString *tabLabel =
+        [[self class] localizedStringWithKey:kMDCItemBarStringKeyAccessibilityTabElementLabel];
+    if (tabLabel) {
+      [labelComponents addObject:tabLabel];
+    }
+
+    NSString *positionFormat =
+        [[self class] localizedStringWithKey:kMDCItemBarStringKeyAccessibilityTabPositionFormat];
+    if (positionFormat) {
+      if (_itemIndex != NSNotFound && _itemCount > 0) {
+        int position = (int)(_itemIndex + 1);
+        NSString *localizedPosition =
+            [NSString localizedStringWithFormat:positionFormat, position, (int)_itemCount];
+        [labelComponents addObject:localizedPosition];
+      }
     }
   }
 
--- components/Tabs/tests/unit/MDCItemBarTests.m
+++ components/Tabs/tests/unit/MDCItemBarTests.m
@@ -16,15 +16,42 @@
 #import <XCTest/XCTest.h>
 
 #import "../../src/private/MDCItemBar.h"
+#import "../../src/private/MDCItemBarStyle.h"
 
 @interface MDCItemBar (Testing)
 - (UITabBarItem *)itemAtIndexPath:(NSIndexPath *)indexPath;
 @end
 
 @interface MDCItemBarTests : XCTestCase
+
+/** The item bar being tested. */
+@property(nonatomic, strong) MDCItemBar *itemBar;
+
 @end
 
 @implementation MDCItemBarTests
+
+- (void)setUp {
+  [super setUp];
+
+  MDCItemBarStyle *style = [[MDCItemBarStyle alloc] init];
+  style.unselectedTitleFont = [UIFont systemFontOfSize:12];
+  style.selectedTitleFont = [UIFont systemFontOfSize:12];
+  MDCItemBar *itemBar = [[MDCItemBar alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+  [itemBar applyStyle:style];
+  UITabBarItem *item1 = [[UITabBarItem alloc] initWithTitle:@"Item 1" image:nil tag:0];
+  UITabBarItem *item2 = [[UITabBarItem alloc] initWithTitle:@"Item 2" image:nil tag:1];
+  itemBar.items = @[ item1, item2 ];
+  itemBar.selectedItem = item1;
+  [itemBar layoutIfNeeded];
+  self.itemBar = itemBar;
+}
+
+- (void)tearDown {
+  self.itemBar = nil;
+
+  [super tearDown];
+}
 
 - (void)testItemAtIndexPath {
   // Given
@@ -46,6 +73,52 @@
   XCTAssertNil([itemBar itemAtIndexPath:indexPathWithNonZeroSection]);
   NSIndexPath *indexPathWithItemEqualToNegativeOne = [NSIndexPath indexPathForItem:-1 inSection:0];
   XCTAssertNil([itemBar itemAtIndexPath:indexPathWithItemEqualToNegativeOne]);
+}
+
+- (void)testItemBarIsNotAccessibilityElement {
+  // Given
+  MDCItemBar *itemBar = [[MDCItemBar alloc] init];
+
+  // Then
+  XCTAssertFalse(itemBar.isAccessibilityElement);
+}
+
+- (void)testItemBarIsAccessibilityTabBarOniOS10Plus {
+  if (@available(iOS 10.0, *)) {
+    // When
+    self.itemBar.accessibilityTraits = UIAccessibilityTraitLink;
+
+    // Then
+    XCTAssertEqual(self.itemBar.accessibilityTraits,
+                   UIAccessibilityTraitTabBar | UIAccessibilityTraitLink);
+  }
+}
+
+- (void)testItemBarViewIncludesTabInAccessibilityLabelOniOS9 {
+  NSOperatingSystemVersion iOS10Version = {10, 0, 0};
+  if (![NSProcessInfo.processInfo isOperatingSystemAtLeastVersion:iOS10Version]) {
+    // When
+    UIView *view = [self.itemBar accessibilityElementForItem:self.itemBar.items.firstObject];
+    XCTAssertTrue([view isKindOfClass:[UIView class]]);
+    if ([view isKindOfClass:[UIView class]]) {
+      NSString *accessibilityLabel = ((UIView *)view).accessibilityLabel;
+      XCTAssertTrue([accessibilityLabel localizedCaseInsensitiveContainsString:@"tab"],
+                    @"Item bar items should include the faked 'tab' information on iOS 9.");
+    }
+  }
+}
+
+- (void)testItemBarViewDoesNotIncludesTabInAccessibilityLabelOniOS10Plus {
+  if (@available(iOS 10.0, *)) {
+    // When
+    UIView *view = [self.itemBar accessibilityElementForItem:self.itemBar.items.firstObject];
+    XCTAssertTrue([view isKindOfClass:[UIView class]]);
+    if ([view isKindOfClass:[UIView class]]) {
+      NSString *accessibilityLabel = ((UIView *)view).accessibilityLabel;
+      XCTAssertFalse([accessibilityLabel localizedCaseInsensitiveContainsString:@"tab"],
+                     @"Item bar items should not include the faked 'tab' information on iOS 9.");
+    }
+  }
 }
 
 @end
