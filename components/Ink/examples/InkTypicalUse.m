cdaf0977567486546a633704809c759211f80b94
featherless <featherless@google.com>
2019-11-15 10:15:15 -0500

    [Ink] Consolidate example code into a single file. (#8890)
    Cleanup as part of https://github.com/material-components/material-components-ios/issues/8883

--- components/Ink/examples/InkTypicalUse.m
+++ components/Ink/examples/InkTypicalUse.m
@@ -16,12 +16,17 @@
 
 #import "MaterialInk.h"
 #import "MaterialPalettes.h"
+#import "MaterialTypography.h"
 
-#import "supplemental/InkTypicalUseSupplemental.h"
+@interface ExampleShapes : UIView
+@end
 
-@interface InkTypicalUseViewController () <MDCInkTouchControllerDelegate>
+@interface InkTypicalUseViewController : UIViewController <MDCInkTouchControllerDelegate>
 
 @property(nonatomic, strong) NSMutableArray *inkTouchControllers;  // MDCInkTouchControllers.
+@property(nonatomic, strong) ExampleShapes *shapes;
+@property(nonatomic, strong) UIView *legacyShape;
+@property(nonatomic, weak) UIView *containerView;
 
 @end
 
@@ -74,6 +79,130 @@
            atTouchLocation:(CGPoint)location {
   NSLog(@"InkTouchController %p did process ink view: %p at touch location: %@", inkTouchController,
         inkView, NSStringFromCGPoint(location));
+}
+
+#pragma mark - Supplemental
+
+- (void)setupExampleViews {
+  self.view.backgroundColor = UIColor.whiteColor;
+
+  CGRect boundedTitleLabelFrame =
+      CGRectMake(0, CGRectGetHeight(self.shapes.frame), CGRectGetWidth(self.shapes.frame), 24);
+  UILabel *boundedTitleLabel = [[UILabel alloc] initWithFrame:boundedTitleLabelFrame];
+  boundedTitleLabel.text = @"Ink";
+  boundedTitleLabel.textAlignment = NSTextAlignmentCenter;
+  boundedTitleLabel.font = [MDCTypography captionFont];
+  boundedTitleLabel.alpha = [MDCTypography captionFontOpacity];
+  [self.shapes addSubview:boundedTitleLabel];
+
+  self.legacyShape.autoresizingMask =
+      UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
+      UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin;
+
+  self.legacyShape.backgroundColor = MDCPalette.greyPalette.tint800;
+
+  CGRect legacyTitleLabelFrame = CGRectMake(0, CGRectGetHeight(self.legacyShape.frame),
+                                            CGRectGetWidth(self.legacyShape.frame), 36);
+  UILabel *legacyTitleLabel = [[UILabel alloc] initWithFrame:legacyTitleLabelFrame];
+  legacyTitleLabel.text = @"Legacy Ink";
+  legacyTitleLabel.textAlignment = NSTextAlignmentCenter;
+  legacyTitleLabel.font = [MDCTypography captionFont];
+  legacyTitleLabel.alpha = [MDCTypography captionFontOpacity];
+  [self.legacyShape addSubview:legacyTitleLabel];
+}
+
+- (void)viewWillLayoutSubviews {
+  if (@available(iOS 11.0, *)) {
+    UIEdgeInsets safeAreaInsets = self.view.safeAreaInsets;
+    self.containerView.frame =
+        CGRectMake(safeAreaInsets.left, safeAreaInsets.top,
+                   CGRectGetWidth(self.view.frame) - safeAreaInsets.left - safeAreaInsets.right,
+                   CGRectGetHeight(self.view.frame) - safeAreaInsets.top - safeAreaInsets.bottom);
+  } else {
+    self.containerView.frame =
+        CGRectMake(0, self.topLayoutGuide.length, CGRectGetWidth(self.view.frame),
+                   CGRectGetHeight(self.view.frame) - self.topLayoutGuide.length);
+  }
+
+  CGFloat offset = 8;
+  CGFloat shapeDimension = 200;
+  CGFloat spacing = 16;
+  if (CGRectGetHeight(self.containerView.frame) > CGRectGetWidth(self.containerView.frame)) {
+    self.shapes.center = CGPointMake(self.containerView.center.x,
+                                     self.containerView.center.y - shapeDimension - offset);
+    self.legacyShape.center = CGPointMake(self.containerView.center.x,
+                                          self.containerView.center.y + spacing * 2 + offset);
+  } else {
+    self.shapes.center = CGPointMake(self.containerView.center.x - shapeDimension / 2 - spacing * 2,
+                                     self.containerView.center.y / 2 + spacing * 2);
+    self.legacyShape.center =
+        CGPointMake(self.containerView.center.x + shapeDimension / 2 + spacing * 2,
+                    self.containerView.center.y / 2 + spacing * 2);
+  }
+}
+
+@end
+
+@interface ExampleShapes ()
+@end
+
+@implementation ExampleShapes
+
+- (id)initWithFrame:(CGRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    CGFloat padding = 8;
+    CGFloat bigViewFrameHeight = 130;
+    CGRect bigViewFrame =
+        CGRectMake(padding, padding, CGRectGetWidth(frame) - 2 * padding, bigViewFrameHeight);
+    UIView *bigView = [[UIView alloc] initWithFrame:bigViewFrame];
+    bigView.backgroundColor = MDCPalette.greyPalette.tint800;
+    [self addSubview:bigView];
+
+    CGFloat buttonViewDim = 50;
+    CGFloat pseudoButtonViewHeight = 40;
+    CGFloat fabPadding = 6;
+    CGRect pseudoButtonViewFrame = CGRectMake(
+        padding, padding + bigViewFrameHeight + fabPadding + padding,
+        frame.size.width - 2 * padding - buttonViewDim - fabPadding * 3, pseudoButtonViewHeight);
+    UIView *pseudoButtonView = [[UIView alloc] initWithFrame:pseudoButtonViewFrame];
+    pseudoButtonView.backgroundColor = MDCPalette.greyPalette.tint800;
+    pseudoButtonView.layer.cornerRadius = 5;
+    pseudoButtonView.clipsToBounds = YES;
+    [self addSubview:pseudoButtonView];
+
+    CGFloat pseudoFABViewFrameLeft =
+        padding + CGRectGetWidth(frame) - 2 * padding - buttonViewDim + padding - fabPadding * 2;
+    CGRect pseudoFABViewFrame =
+        CGRectMake(pseudoFABViewFrameLeft, padding + bigViewFrameHeight + padding,
+                   buttonViewDim + fabPadding, buttonViewDim + fabPadding);
+    UIView *pseudoFABView = [[UIView alloc] initWithFrame:pseudoFABViewFrame];
+    pseudoFABView.backgroundColor = MDCPalette.greyPalette.tint800;
+    pseudoFABView.layer.cornerRadius = 28;
+    pseudoFABView.clipsToBounds = YES;
+    [self addSubview:pseudoFABView];
+
+    self.autoresizingMask =
+        UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
+        UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin;
+  }
+  return self;
+}
+
+@end
+
+#pragma mark - InkTypicalUseViewController
+
+@implementation InkTypicalUseViewController (CatalogByConvention)
+
++ (NSDictionary *)catalogMetadata {
+  return @{
+    @"breadcrumbs" : @[ @"Ink", @"Ink" ],
+    @"description" : @"The Ink component provides a radial action in the form of a visual ripple "
+                     @"of ink expanding outward from the user's touch.",
+    @"primaryDemo" : @YES,
+    @"presentable" : @YES,
+  };
 }
 
 @end
