7cc3fc408853c13698686db2e898fc8a62cf36ad
Wenyu Zhang <wenyuz@google.com>
2019-11-27 10:02:48 -0500

    Bumped version number to 94.4.0.

--- components/LibraryInfo/src/MDCLibraryInfo.m
+++ components/LibraryInfo/src/MDCLibraryInfo.m
@@ -19,7 +19,7 @@
 // This string is updated automatically as a part of the release process and should not be edited
 // manually. Do not rename this constant or change the formatting without updating the release
 // scripts.
-static NSString const *MDCLibraryInfoVersionString = @"94.3.0";
+static NSString const *MDCLibraryInfoVersionString = @"94.4.0";
 
 @implementation MDCLibraryInfo
 
