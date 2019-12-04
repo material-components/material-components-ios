b80a7e6d9062839eebf6d3a75add513ed2f664f5
Andrew Overton <andrewoverton@google.com>
2019-12-04 10:18:40 -0500

    Add swift storyboard example

---
+++ components/TextControls/examples/MDCTextControlTextFieldsStoryboardExample.swift
@@ -0,0 +1,59 @@
+// Copyright 2019-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+
+final class MDCTextControlTextFieldsStoryboardExample: UIViewController {
+
+  @IBOutlet weak var filledTextField: MDCFilledTextField!
+  @IBOutlet weak var outlinedTextField: MDCOutlinedTextField!
+
+  @objc var containerScheme: MDCContainerScheming = MDCContainerScheme()
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setUpTextFields()
+  }
+
+  func setUpTextFields() {
+    filledTextField.label.text = "label text"
+    filledTextField.placeholder = "placeholder text"
+    filledTextField.applyTheme(withScheme: containerScheme)
+
+    outlinedTextField.label.text = "label text"
+    outlinedTextField.placeholder = "placeholder text"
+    outlinedTextField.applyTheme(withScheme: containerScheme)
+  }
+}
+
+// MARK: - CatalogByConvention
+
+extension MDCTextControlTextFieldsStoryboardExample {
+  @objc class func catalogMetadata() -> [String: Any] {
+    return [
+      "breadcrumbs": ["Text Controls", "MDCTextControl TextFields (Storyboard)"],
+      "primaryDemo": false,
+      "presentable": false,
+      "storyboardName": "MDCTextControlTextFieldsStoryboardExample"
+    ]
+  }
+}
