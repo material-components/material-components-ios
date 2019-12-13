f33e93fcaaed28a2dc9f8b1045bdb005586ccc27
Randall Li <randallli@google.com>
2019-12-13 12:13:36 -0500

    renamed file too

--- components/private/Application/src/UIApplication+MDCAppExtensions.m
+++ components/private/Application/src/UIApplication+MDCAppExtensions.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "UIApplication+AppExtensions.h"
+#import "UIApplication+MDCAppExtensions.h"
 
 @implementation UIApplication (MDCAppExtensions)
 
