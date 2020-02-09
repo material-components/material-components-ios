4e5d7a507aeb38808e9fe43f90021694fa62a9c4
Nobody <no-reply@google.com>
2020-02-08 17:09:57 -0800

    internal change
    PiperOrigin-RevId: 294029780

--- components/FlexibleHeader/src/MDCFlexibleHeaderView.m
+++ components/FlexibleHeader/src/MDCFlexibleHeaderView.m
@@ -325,6 +325,8 @@ static inline MDCFlexibleHeaderShiftBehavior ShiftBehaviorForCurrentAppContext(
   self.layer.shadowRadius = 4;
   self.layer.shadowOpacity = 0;
 
+  self.minimumHeaderViewHeightAfterScrolling = 0.0;
+
   NSString *voiceOverNotification;
   if (@available(iOS 11.0, *)) {
     voiceOverNotification = UIAccessibilityVoiceOverStatusDidChangeNotification;
@@ -1130,22 +1132,7 @@ static inline MDCFlexibleHeaderShiftBehavior ShiftBehaviorForCurrentAppContext(
       }
 
       // Calculate the upper bound of the accumulator based on what phase we're in.
-
-      CGFloat upperBound;
-
-      if (self.canAlwaysExpandToMaximumHeight && ![self fhv_canShiftOffscreen]) {
-        // Don't allow any shifting.
-        upperBound = 0;
-      } else if (headerHeight < 0) {
-        // Header is shifting while detached from content.
-        upperBound = [self fhv_accumulatorMax] + [self fhv_anchorLength];
-      } else if (headerHeight < self.minMaxHeight.minimumHeightWithTopSafeArea) {
-        // Header is shifting while attached to content.
-        upperBound = [self fhv_accumulatorMax];
-      } else {
-        // Header is not shifting.
-        upperBound = 0;
-      }
+      CGFloat upperBound = [self upperBoundWithHeaderHeight:headerHeight];
 
       // Ensure that we don't lose any deltaY by first capping the accumulator within its valid
       // range.
@@ -1176,6 +1163,33 @@ static inline MDCFlexibleHeaderShiftBehavior ShiftBehaviorForCurrentAppContext(
 
   _shiftAccumulatorLastContentOffset = [self fhv_boundedContentOffset];
   _shiftAccumulatorLastContentOffsetIsValid = YES;
+}
+
+- (CGFloat)upperBoundWithHeaderHeight:(CGFloat)headerHeight {
+  CGFloat upperBound;
+  if (self.canAlwaysExpandToMaximumHeight && ![self fhv_canShiftOffscreen]) {
+    // Don't allow any shifting.
+    upperBound = 0;
+  } else if (headerHeight < 0) {
+    if (self.minimumHeaderViewHeightAfterScrolling != 0.0) {
+      upperBound = self.minMaxHeight.maximumHeightWithoutTopSafeArea -
+                   self.minimumHeaderViewHeightAfterScrolling;
+    } else {
+      upperBound = [self fhv_accumulatorMax] + [self fhv_anchorLength];
+    }
+  } else if (headerHeight < self.minMaxHeight.minimumHeightWithTopSafeArea) {
+    if (self.minimumHeaderViewHeightAfterScrolling != 0.0) {
+      upperBound = self.minMaxHeight.maximumHeightWithoutTopSafeArea -
+                   self.minimumHeaderViewHeightAfterScrolling;
+    } else {
+      upperBound = [self fhv_accumulatorMax];
+    }
+
+  } else {
+    // Header is not shifting.
+    upperBound = 0;
+  }
+  return upperBound;
 }
 
 - (CGFloat)fhv_anchorLength {
