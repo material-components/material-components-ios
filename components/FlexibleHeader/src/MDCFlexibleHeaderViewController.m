8cd6794739ad33347bfbe7569b80041bbcbdeb9f
Bryan Oltman <bryanoltman@google.com>
2020-02-14 13:20:12 -0500

    [Material][Availability] Migrates Flexible Header to MDCAvailability. (#9744)
    [Material][Availability] Migrates Flexible Header to MDCAvailability.

--- components/FlexibleHeader/src/MDCFlexibleHeaderViewController.m
+++ components/FlexibleHeader/src/MDCFlexibleHeaderViewController.m
@@ -14,14 +14,15 @@
 
 #import "MDCFlexibleHeaderViewController.h"
 
-#import "private/MDCFlexibleHeaderHairline.h"
-#import "private/MDCFlexibleHeaderView+Private.h"
+#import <MDFTextAccessibility/MDFTextAccessibility.h>
 #import "MDCFlexibleHeaderContainerViewController.h"
 #import "MDCFlexibleHeaderView+ShiftBehavior.h"
 #import "MDCFlexibleHeaderView.h"
 #import "MaterialApplication.h"
+#import "MaterialAvailability.h"
 #import "MaterialUIMetrics.h"
-#import <MDFTextAccessibility/MDFTextAccessibility.h>
+#import "private/MDCFlexibleHeaderHairline.h"
+#import "private/MDCFlexibleHeaderView+Private.h"
 
 @interface UIView ()
 - (UIEdgeInsets)safeAreaInsets;  // For pre-iOS 11 SDK targets.
@@ -40,11 +41,11 @@ static inline UIStatusBarStyle StatusBarStyleOnBackgroundColor(UIColor *color) {
     return UIStatusBarStyleLightContent;
   }
 
-#if defined(__IPHONE_13_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0)
+#if MDC_AVAILABLE_SDK_IOS(13_0)
   if (@available(iOS 13.0, *)) {
     return UIStatusBarStyleDarkContent;
   }
-#endif
+#endif  // MDC_AVAILABLE_SDK_IOS(13_0)
 
   return UIStatusBarStyleDefault;
 }
