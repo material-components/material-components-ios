a204ee6f8e9665cc3318822e58bb507a04232b1f
Randall Li <randallli@google.com>
2020-02-18 13:41:47 -0500

    [Material][Availability] Migrates BottomAppBar to MDCAvailability. (#9729)
    [Material][Availability] Migrates BottomAppBar to MDCAvailability.

--- components/BottomAppBar/tests/snapshot/MDCBottomAppBarSnapshotTests.m
+++ components/BottomAppBar/tests/snapshot/MDCBottomAppBarSnapshotTests.m
@@ -14,6 +14,7 @@
 
 #import "MaterialSnapshot.h"
 
+#import "MaterialAvailability.h"
 #import "MaterialBottomAppBar.h"
 
 @interface MDCBottomAppBarSnapshotTests : MDCSnapshotTestCase
@@ -155,7 +156,7 @@
 }
 
 - (void)testDynamicColorSupportOniOS13AndAbove {
-#if defined(__IPHONE_13_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0)
+#if MDC_AVAILABLE_SDK_IOS(13_0)
   if (@available(iOS 13.0, *)) {
     // Given
     UIColor *barTintDynamicColor =
@@ -188,7 +189,7 @@
         [self.appBar mdc_addToBackgroundViewWithInsets:UIEdgeInsetsMake(50, 50, 50, 50)];
     [self snapshotVerifyViewForIOS13:snapshotView];
   }
-#endif
+#endif  // MDC_AVAILABLE_SDK_IOS(13_0)
 }
 
 - (void)testIntrinsicHeight {
