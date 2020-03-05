256f41bf8c41a2cf06cd39ac0af174f5ae6c2dde
Bryan Oltman <bryanoltman@gmail.com>
2020-03-05 11:04:11 -0500

    [Chips] In MDCChipField, account for horizontal padding when calculating available width for text field

--- components/Chips/src/MDCChipField.m
+++ components/Chips/src/MDCChipField.m
@@ -734,7 +734,7 @@ static inline UIBezierPath *MDCPathForClearButtonImageFrame(CGRect frame) {
   }
 
   CGRect lastChipFrame = [chipFrames.lastObject CGRectValue];
-  return boundsWidth - CGRectGetMaxX(lastChipFrame);
+  return boundsWidth - CGRectGetMaxX(lastChipFrame) - MDCChipFieldHorizontalInset;
 }
 
 // The width of the text input + the clear button.
