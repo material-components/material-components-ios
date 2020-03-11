1116209825249a36e64c9f150b4dfca89c26b60f
featherless <featherless@google.com>
2020-03-11 11:24:48 -0400

    [FlexibleHeader] Add unit test to reproduce https://github.com/material-components/material-components-ios/issues/9863. (#9880)
    The flexible header view controller's hairline object should not be nil after initialization of the controller, but it is.
    
    Pre-work for resolving https://github.com/material-components/material-components-ios/issues/9863.

--- components/FlexibleHeader/tests/unit/FlexibleHeaderHairlineTests.m
+++ components/FlexibleHeader/tests/unit/FlexibleHeaderHairlineTests.m
@@ -15,6 +15,11 @@
 #import <XCTest/XCTest.h>
 
 #import "MDCFlexibleHeaderHairline.h"
+#import "MaterialFlexibleHeader.h"
+
+@interface MDCFlexibleHeaderViewController (UnitTesting)
+@property(nonatomic, strong) MDCFlexibleHeaderHairline *hairline;
+@end
 
 @interface FlexibleHeaderHairlineTests : XCTestCase
 @property(nonatomic, strong) MDCFlexibleHeaderHairline *hairline;
@@ -42,6 +47,54 @@
   XCTAssertEqualObjects(self.hairline.color, [UIColor blackColor]);
   XCTAssertEqual(self.hairline.containerView, self.containerView);
   XCTAssertEqual(self.hairline.containerView.subviews.count, 0);
+}
+
+- (void)testFlexibleHeaderViewControllerDefaults {
+  // Given
+  MDCFlexibleHeaderViewController *fhvc = [[MDCFlexibleHeaderViewController alloc] init];
+
+  // Then
+  // TODO(https://github.com/material-components/material-components-ios/issues/9863): This should
+  // be non-nil.
+  XCTAssertNil(fhvc.hairline);
+}
+
+- (void)testFlexibleHeaderViewControllerShowsHairlineTruePersists {
+  // Given
+  MDCFlexibleHeaderViewController *fhvc = [[MDCFlexibleHeaderViewController alloc] init];
+
+  // When
+  fhvc.showsHairline = YES;
+
+  // Then
+  XCTAssertTrue(fhvc.showsHairline);
+}
+
+- (void)testFlexibleHeaderViewControllerShowsHairlineFalsePersists {
+  // Given
+  MDCFlexibleHeaderViewController *fhvc = [[MDCFlexibleHeaderViewController alloc] init];
+
+  // When
+  fhvc.showsHairline = NO;
+
+  // Then
+  // TODO(https://github.com/material-components/material-components-ios/issues/9863): This should
+  // be NO.
+  XCTAssertTrue(fhvc.showsHairline);
+}
+
+- (void)testFlexibleHeaderViewControllerColorPersists {
+  // Given
+  MDCFlexibleHeaderViewController *fhvc = [[MDCFlexibleHeaderViewController alloc] init];
+  UIColor *color = [UIColor redColor];
+
+  // When
+  fhvc.hairlineColor = color;
+
+  // Then
+  // TODO(https://github.com/material-components/material-components-ios/issues/9863): This should
+  // be equal to color.
+  XCTAssertNil(fhvc.hairlineColor);
 }
 
 #pragma mark - Visibility
