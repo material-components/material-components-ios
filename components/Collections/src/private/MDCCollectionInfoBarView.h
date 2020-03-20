97f26d5767864f49bb0f22d2ef6ce9f18c524736
Bryan Oltman <bryanoltman@google.com>
2020-03-20 10:32:34 -0700

    [Collections] Move protocol definitions into separate header files
    PiperOrigin-RevId: 302054424

--- components/Collections/src/private/MDCCollectionInfoBarView.h
+++ components/Collections/src/private/MDCCollectionInfoBarView.h
@@ -14,7 +14,13 @@
 
 #import <UIKit/UIKit.h>
 
+// TODO(b/151929968): Delete import of delegate headers when client code has been migrated to no
+// longer import delegates as transitive dependencies.
+#import "MDCCollectionInfoBarViewDelegate.h"
+
 @class MDCCollectionInfoBarView;
+
+@protocol MDCCollectionInfoBarViewDelegate;
 
 /** The info bar supplementary view kind when shown in header. */
 extern NSString *_Nonnull const MDCCollectionInfoBarKindHeader;
@@ -38,71 +44,6 @@ typedef NS_ENUM(NSUInteger, MDCCollectionInfoBarViewStyle) {
 };
 
 /** Delegate protocol for the MDCCollectionInfoBarView. */
-@protocol MDCCollectionInfoBarViewDelegate <NSObject>
-@required
-/**
- Allows the receiver to update the info bar after it has been created.
-
- @param infoBar The MDCCollectionInfoBarView info bar.
- */
-- (void)updateControllerWithInfoBar:(nonnull MDCCollectionInfoBarView *)infoBar;
-
-@optional
-/**
- Allows the receiver to get notifed when a tap gesture has been performed on the info bar.
-
- @param infoBar The MDCCollectionInfoBarView info bar.
- */
-- (void)didTapInfoBar:(nonnull MDCCollectionInfoBarView *)infoBar;
-
-/**
- Allows the receiver to get notifed when an info bar will show.
-
- @param infoBar The MDCCollectionInfoBarView info bar.
- @param animated YES the transition will be animated; otherwise, NO.
- @param willAutoDismiss YES the info bar will be auto-dismissed after the time interval
-        set in @c autoDismissAfterDuration.
- */
-- (void)infoBar:(nonnull MDCCollectionInfoBarView *)infoBar
-    willShowAnimated:(BOOL)animated
-     willAutoDismiss:(BOOL)willAutoDismiss;
-
-/**
- Allows the receiver to get notifed after an info bar did show.
-
- @param infoBar The MDCCollectionInfoBarView info bar.
- @param animated YES the transition was animated; otherwise, NO.
- @param willAutoDismiss YES the info bar will be auto-dismissed after the time interval
-        set in @c autoDismissAfterDuration.
- */
-- (void)infoBar:(nonnull MDCCollectionInfoBarView *)infoBar
-    didShowAnimated:(BOOL)animated
-    willAutoDismiss:(BOOL)willAutoDismiss;
-
-/**
- Allows the receiver to get notifed when an info bar will dismiss.
-
- @param infoBar The MDCCollectionInfoBarView info bar.
- @param animated YES the transition will be animated; otherwise, NO.
- @param willAutoDismiss YES the info bar will be auto-dismissed after the time interval
-        set in @c autoDismissAfterDuration.
- */
-- (void)infoBar:(nonnull MDCCollectionInfoBarView *)infoBar
-    willDismissAnimated:(BOOL)animated
-        willAutoDismiss:(BOOL)willAutoDismiss;
-
-/**
- Allows the receiver to get notifed after an info bar has been dismissed.
-
- @param infoBar The MDCCollectionInfoBarView info bar.
- @param animated YES the transition was animated; otherwise, NO.
- @param didAutoDismiss YES the info bar was auto-dismissed.
- */
-- (void)infoBar:(nonnull MDCCollectionInfoBarView *)infoBar
-    didDismissAnimated:(BOOL)animated
-        didAutoDismiss:(BOOL)didAutoDismiss;
-
-@end
 
 /**
  The MDCCollectionInfoBarView class provides an implementation of
