a8a560da42e41a8268f4081df0810f9e19d934f7
Yarden Eitan <yar@google.com>
2020-03-24 02:34:40 -0700

    [ProgressView] Adding default accessibility label
    PiperOrigin-RevId: 302624787

--- MaterialComponents.podspec
+++ MaterialComponents.podspec
@@ -1215,6 +1215,10 @@ Pod::Spec.new do |mdc|
     component.public_header_files = "components/#{component.base_name}/src/*.h"
     component.source_files = [
       "components/#{component.base_name}/src/*.{h,m}",
+      "components/#{component.base_name}/src/private/*.{h,m}"
+    ]
+    component.resources = [
+      "components/#{component.base_name}/src/Material#{component.base_name}.bundle"
     ]
 
     component.dependency "MDFInternationalization"
--- components/ProgressView/src/MDCProgressView.m
+++ components/ProgressView/src/MDCProgressView.m
@@ -16,9 +16,11 @@
 
 #include <tgmath.h>
 
-#import <MDFInternationalization/MDFInternationalization.h>
-#import "MaterialMath.h"
 #import "MaterialPalettes.h"
+#import "MaterialProgressViewStrings.h"
+#import "MaterialProgressViewStrings_table.h"
+#import "MaterialMath.h"
+#import <MDFInternationalization/MDFInternationalization.h>
 
 static inline UIColor *MDCProgressViewDefaultTintColor(void) {
   return MDCPalette.bluePalette.tint500;
@@ -28,6 +30,9 @@ static inline UIColor *MDCProgressViewDefaultTintColor(void) {
 static const CGFloat MDCProgressViewTrackColorDesaturation = (CGFloat)0.3;
 
 static const NSTimeInterval MDCProgressViewAnimationDuration = 0.25;
+
+// The Bundle for string resources.
+static NSString *const kBundle = @"MaterialProgressView.bundle";
 
 @interface MDCProgressView ()
 @property(nonatomic, strong) UIView *progressView;
@@ -266,7 +271,35 @@ static const NSTimeInterval MDCProgressViewAnimationDuration = 0.25;
 }
 
 - (NSString *)accessibilityLabel {
-  return self.accessibilityProgressView.accessibilityLabel;
+  return self.accessibilityProgressView.accessibilityLabel ?: [self defaultAccessibilityLabel];
+}
+
+- (NSString *)defaultAccessibilityLabel {
+  MaterialProgressViewStringId keyIndex = kStr_MaterialProgressViewAccessibilityLabel;
+  NSString *key = kMaterialProgressViewStringTable[keyIndex];
+  return NSLocalizedStringFromTableInBundle(key, kMaterialProgressViewStringsTableName,
+                                            [[self class] bundle], @"Progress View");
+}
+
+#pragma mark - Resource Bundle
+
++ (NSBundle *)bundle {
+  static NSBundle *bundle = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    bundle = [NSBundle bundleWithPath:[self bundlePathWithName:kBundle]];
+  });
+
+  return bundle;
+}
+
++ (NSString *)bundlePathWithName:(NSString *)bundleName {
+  // In iOS 8+, we could be included by way of a dynamic framework, and our resource bundles may
+  // not be in the main .app bundle, but rather in a nested framework, so figure out where we live
+  // and use that as the search location.
+  NSBundle *bundle = [NSBundle bundleForClass:[MDCProgressView class]];
+  NSString *resourcePath = [(nil == bundle ? [NSBundle mainBundle] : bundle) resourcePath];
+  return [resourcePath stringByAppendingPathComponent:bundleName];
 }
 
 #pragma mark Private
