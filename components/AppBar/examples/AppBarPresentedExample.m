783c9de40ed114b6814fcf724e5d830e71b709ef
Jeff Verkoeyen <featherless@google.com>
2020-05-11 02:06:55 -0700

    [AppBar] Extract a sample app bar view controller to supplemental.
    This controller will be reused in other examples.
    
    PiperOrigin-RevId: 310869906

--- components/AppBar/examples/AppBarPresentedExample.m
+++ components/AppBar/examples/AppBarPresentedExample.m
@@ -14,119 +14,16 @@
 
 #import <UIKit/UIKit.h>
 
-#import "MaterialAppBar+Theming.h"
+#import "AppBarSampleViewController.h"
 #import "MaterialAppBar.h"
-#import "MaterialButtons+Theming.h"
+#import "MaterialAppBar+Theming.h"
 #import "MaterialButtons.h"
+#import "MaterialButtons+Theming.h"
 #import "MaterialContainerScheme.h"
-
-@interface PresentedDemoViewController : UICollectionViewController
-@property(nonatomic, strong) MDCAppBarViewController *appBarViewController;
-@property(nonatomic, strong) id<MDCContainerScheming> containerScheme;
-@end
-
-@implementation PresentedDemoViewController
-
-- (void)dealloc {
-  // Required for pre-iOS 11 devices because we've enabled observesTrackingScrollViewScrollEvents.
-  self.appBarViewController.headerView.trackingScrollView = nil;
-}
-
-- (id)init {
-  UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-  layout.minimumInteritemSpacing = 10;
-
-  self = [super initWithCollectionViewLayout:layout];
-
-  if (self) {
-    self.title = @"Presented App Bar";
-
-    _appBarViewController = [[MDCAppBarViewController alloc] init];
-
-    // Behavioral flags.
-    _appBarViewController.inferTopSafeAreaInsetFromViewController = YES;
-    _appBarViewController.headerView.minMaxHeightIncludesSafeArea = NO;
-
-    [self addChildViewController:_appBarViewController];
-  }
-  return self;
-}
-
-- (void)viewDidLoad {
-  [super viewDidLoad];
-
-  // Allows us to avoid forwarding events, but means we can't enable shift behaviors.
-  self.appBarViewController.headerView.observesTrackingScrollViewScrollEvents = YES;
-  [_appBarViewController applyPrimaryThemeWithScheme:self.containerScheme];
-
-  // Need to update the status bar style after applying the theme.
-  [self setNeedsStatusBarAppearanceUpdate];
-
-  _appBarViewController.headerView.trackingScrollView = self.collectionView;
-
-  [self.view addSubview:_appBarViewController.view];
-  [_appBarViewController didMoveToParentViewController:self];
-
-  [self.collectionView registerClass:[UICollectionViewCell class]
-          forCellWithReuseIdentifier:@"Cell"];
-
-  self.navigationItem.leftBarButtonItem =
-      [[UIBarButtonItem alloc] initWithTitle:@"Dismiss"
-                                       style:UIBarButtonItemStyleDone
-                                      target:self
-                                      action:@selector(dismiss)];
-}
-
-- (UIStatusBarStyle)preferredStatusBarStyle {
-  return UIStatusBarStyleLightContent;
-}
-
-- (void)dismiss {
-  [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
-}
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView
-     numberOfItemsInSection:(NSInteger)section {
-  return 100;
-}
-
-- (CGSize)preferredContentSize {
-  return CGSizeMake(300, 300);
-}
-
-- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
-                           cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-  UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell"
-                                                                         forIndexPath:indexPath];
-  switch (indexPath.row % 3) {
-    case 0:
-      cell.backgroundColor = [UIColor colorWithWhite:(CGFloat)0.2 alpha:1];
-      break;
-    case 1:
-      cell.backgroundColor = [UIColor colorWithWhite:(CGFloat)0.5 alpha:1];
-      break;
-    case 2:
-      cell.backgroundColor = [UIColor colorWithWhite:(CGFloat)0.7 alpha:1];
-      break;
-    default:
-      break;
-  }
-
-  return cell;
-}
-
-- (CGSize)collectionView:(UICollectionView *)collectionView
-                    layout:(UICollectionViewLayout *)collectionViewLayout
-    sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-  CGRect collectionViewFrame = collectionView.frame;
-  return CGSizeMake(collectionViewFrame.size.width / 2 - 14, 40);
-}
-
-@end
 
 @interface AppBarPresentedExample : UIViewController
 
-@property(nonatomic, strong) PresentedDemoViewController *demoViewController;
+@property(nonatomic, strong) AppBarSampleViewController *demoViewController;
 @property(nonatomic, strong) id<MDCContainerScheming> containerScheme;
 
 @end
@@ -142,7 +39,7 @@
 
   self.view.backgroundColor = [UIColor colorWithWhite:(CGFloat)0.95 alpha:1];
 
-  self.demoViewController = [[PresentedDemoViewController alloc] init];
+  self.demoViewController = [[AppBarSampleViewController alloc] init];
   self.demoViewController.containerScheme = self.containerScheme;
 
   // Need to update the status bar style after applying the theme.
