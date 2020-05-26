45b9b222e30dbd77e508ff742892ea1fe45b650b
Wenyu Zhang <wenyuz@google.com>
2020-05-26 11:10:22 -0700

    [Color] Add colorWithAccessibilityContrastHigh:normal: API to support Increase Contrast color.
    PiperOrigin-RevId: 313229477

--- components/private/Color/src/UIColor+MaterialDynamic.h
+++ components/private/Color/src/UIColor+MaterialDynamic.h
@@ -27,6 +27,22 @@
                                              defaultColor:(nonnull UIColor *)defaultColor;
 
 /**
+ Returns a dynamic color object that resolves to high contrast color when current
+ @c accessibilityContrast is @c UIAccessibilityContrastHigh, and to normal contrast color
+ when @c accessibilityContrast is @c UIAccessibilityContrastNormal.
+
+ The high contrast color and normal contrast color can be dynamic color objects. This method can
+ be used together with @c colorWithUserInterfaceStyleDarkColor:defaultColor:.
+
+ For pre iOS 13, this method always returns the normal contrast color.
+
+ @param highContrastColor A color object for high contrast color.
+ @param normalContrastColor A default color object for normal contrast color.
+ */
++ (nonnull UIColor *)colorWithAccessibilityContrastHigh:(nonnull UIColor *)highContrastColor
+                                                 normal:(nonnull UIColor *)normalContrastColor;
+
+/**
  Returns the version of the current color that takes the specified traits into account.
 
  @note On pre-iOS 13 the orginal color is returned.
