f2f99a45206b546991b4ef19fef501997940ba58
Wenyu Zhang <wenyuz@google.com>
2020-06-01 07:21:58 -0700

    [Banner] Add example snapshot tests for verifying MDCColorSchemeDefaultsMaterial201907 color scheme.
    PiperOrigin-RevId: 314125715

--- components/Banner/examples/BannerTypicalUseExampleViewController.m
+++ components/Banner/examples/BannerTypicalUseExampleViewController.m
@@ -157,20 +157,6 @@ static NSString *const exampleSuperLongText =
   };
 }
 
-- (NSDictionary<NSString *, void (^)(void)> *)testRunners {
-  NSMutableDictionary *runners = [NSMutableDictionary dictionary];
-  NSArray<BannerExampleUseInfo *> *examples = [self getBannerExampleList];
-  for (BannerExampleUseInfo *example in examples) {
-    runners[example.displayName] = ^{
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-      [example.exampleUseTarget performSelector:example.exampleUseSelector];
-#pragma clang diagnostic pop
-    };
-  }
-  return runners;
-}
-
 #pragma mark - UIViewController
 
 - (void)viewWillLayoutSubviews {
@@ -534,6 +520,40 @@ static NSString *const exampleSuperLongText =
     [target performSelector:bannerExampleUseInfo.exampleUseSelector];
   }
 #pragma clang diagnostic pop
+}
+
+@end
+
+@implementation BannerTypicalUseExampleViewController (SnapshotTestingByConvention)
+
+- (NSDictionary<NSString *, void (^)(void)> *)testRunners {
+  NSMutableDictionary *runners = [NSMutableDictionary dictionary];
+  NSArray<BannerExampleUseInfo *> *examples = [self getBannerExampleList];
+  for (BannerExampleUseInfo *example in examples) {
+    __weak BannerTypicalUseExampleViewController *weakSelf = self;
+    NSString *defaultTestName = example.displayName;
+    runners[defaultTestName] = ^{
+      MDCContainerScheme *containerScheme = [[MDCContainerScheme alloc] init];
+      weakSelf.containerScheme = containerScheme;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+      [example.exampleUseTarget performSelector:example.exampleUseSelector];
+#pragma clang diagnostic pop
+    };
+    NSString *dynamic201907ColorSchemeTestName =
+        [NSString stringWithFormat:@"example.displayName_%@", @"dynamic201907ColorScheme"];
+    runners[dynamic201907ColorSchemeTestName] = ^{
+      MDCContainerScheme *containerScheme = [[MDCContainerScheme alloc] init];
+      containerScheme.colorScheme =
+          [[MDCSemanticColorScheme alloc] initWithDefaults:MDCColorSchemeDefaultsMaterial201907];
+      weakSelf.containerScheme = containerScheme;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+      [example.exampleUseTarget performSelector:example.exampleUseSelector];
+#pragma clang diagnostic pop
+    };
+  }
+  return runners;
 }
 
 @end
