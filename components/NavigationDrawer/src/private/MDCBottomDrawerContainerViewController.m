29a39ea375bcdaddcac5b5da1daed803be18cb69
Nobody <no-reply@google.com>
2020-07-01 10:42:04 -0700

    Fix bug where MDCBottomDrawerContainerViewController has the size of the screen instead of the window when in iPad Slide Over.
    BEGIN_PUBLIC
    Fix bug where MDCBottomDrawerContainerViewController has the size of the screen instead of the window when in iPad Slide Over.
    END_PUBLIC
    
    MDCBottomDrawerContainerViewController has the wrong size in iPad Slide Over (when the window height is shorter than the screen height), causing GTETaskCreationViewController to have the wrong y origin b/c it thinks it's within a taller window.
    
    Note: this is 1 of 2 parts to the fix for b/only fixes. The after screenshots below are only partially correct and will be fixed in the next CL.
    
    Before and after screenshots in b/159595242.
    
    PiperOrigin-RevId: 319253730

--- components/NavigationDrawer/src/private/MDCBottomDrawerContainerViewController.m
+++ components/NavigationDrawer/src/private/MDCBottomDrawerContainerViewController.m
@@ -631,6 +631,10 @@ NSString *const kMDCBottomDrawerScrollViewAccessibilityIdentifier =
 - (void)viewWillLayoutSubviews {
   [super viewWillLayoutSubviews];
 
+  // Have _contentHeaderTopInset recalculated the next time self.contentHeaderTopInset is called so
+  // it has the correct value when the app is in iPad Slide Over.
+  _contentHeaderTopInset = NSNotFound;
+
   [self setupLayout];
   UIView *topView;
   if (self.headerViewController) {
