feecc3679e9abae53866f9714a9d725ce408ae68
Yarden Eitan <yar@google.com>
2020-08-12 09:32:34 -0400

    Bumped version number to 113.0.0.

--- components/LibraryInfo/src/MDCLibraryInfo.m
+++ components/LibraryInfo/src/MDCLibraryInfo.m
@@ -19,7 +19,7 @@
 // This string is updated automatically as a part of the release process and should not be edited
 // manually. Do not rename this constant or change the formatting without updating the release
 // scripts.
-static NSString* const kMDCLibraryInfoVersionString = @"112.1.0";
+static NSString* const kMDCLibraryInfoVersionString = @"113.0.0";
 
 @implementation MDCLibraryInfo
 
