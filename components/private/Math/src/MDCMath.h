material-components/material-components-ios

commit e1afe22cfb01722fd7e52a3ac61503e90e3a89c9

[Math] Deprecate all APIs that have a direct standard library equivalent.
Prefer using tgmath.h to get double/float-based APIs instead.

PiperOrigin-RevId: 333511426

components/private/Math/src/MDCMath.h
@@ -17,7 +17,7 @@
 #import <UIKit/UIKit.h>
 #import <math.h>
 
-static inline CGFloat MDCSin(CGFloat value) {
+__deprecated_msg("Use sin instead.") static inline CGFloat MDCSin(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return sin(value);
 #else
@@ -25,7 +25,7 @@ static inline CGFloat MDCSin(CGFloat value) {
 #endif
 }
 
-static inline CGFloat MDCCos(CGFloat value) {
+__deprecated_msg("Use cos instead.") static inline CGFloat MDCCos(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return cos(value);
 #else
@@ -33,7 +33,7 @@ static inline CGFloat MDCCos(CGFloat value) {
 #endif
 }
 
-static inline CGFloat MDCAtan2(CGFloat y, CGFloat x) {
+__deprecated_msg("Use atan2 instead.") static inline CGFloat MDCAtan2(CGFloat y, CGFloat x) {
 #if CGFLOAT_IS_DOUBLE
   return atan2(y, x);
 #else
@@ -41,7 +41,7 @@ static inline CGFloat MDCAtan2(CGFloat y, CGFloat x) {
 #endif
 }
 
-static inline CGFloat MDCCeil(CGFloat value) {
+__deprecated_msg("Use ceil instead.") static inline CGFloat MDCCeil(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return ceil(value);
 #else
@@ -49,7 +49,7 @@ static inline CGFloat MDCCeil(CGFloat value) {
 #endif
 }
 
-static inline CGFloat MDCFabs(CGFloat value) {
+__deprecated_msg("Use fabs instead.") static inline CGFloat MDCFabs(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return fabs(value);
 #else
@@ -77,7 +77,7 @@ static inline BOOL MDCCGFloatEqual(CGFloat a, CGFloat b) {
   return (MDCFabs(a - b) < constantK * epsilon * MDCFabs(a + b) || MDCFabs(a - b) < min);
 }
 
-static inline CGFloat MDCFloor(CGFloat value) {
+__deprecated_msg("Use floor instead.") static inline CGFloat MDCFloor(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return floor(value);
 #else
@@ -85,7 +85,7 @@ static inline CGFloat MDCFloor(CGFloat value) {
 #endif
 }
 
-static inline CGFloat MDCHypot(CGFloat x, CGFloat y) {
+__deprecated_msg("Use hypot instead.") static inline CGFloat MDCHypot(CGFloat x, CGFloat y) {
 #if CGFLOAT_IS_DOUBLE
   return hypot(x, y);
 #else
@@ -98,7 +98,7 @@ static inline BOOL MDCCGFloatIsExactlyZero(CGFloat value) {
   return (value == 0);
 }
 
-static inline CGFloat MDCPow(CGFloat value, CGFloat power) {
+__deprecated_msg("Use pow instead.") static inline CGFloat MDCPow(CGFloat value, CGFloat power) {
 #if CGFLOAT_IS_DOUBLE
   return pow(value, power);
 #else
@@ -106,7 +106,7 @@ static inline CGFloat MDCPow(CGFloat value, CGFloat power) {
 #endif
 }
 
-static inline CGFloat MDCRint(CGFloat value) {
+__deprecated_msg("Use rint instead.") static inline CGFloat MDCRint(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return rint(value);
 #else
@@ -114,7 +114,7 @@ static inline CGFloat MDCRint(CGFloat value) {
 #endif
 }
 
-static inline CGFloat MDCRound(CGFloat value) {
+__deprecated_msg("Use round instead.") static inline CGFloat MDCRound(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return round(value);
 #else
@@ -122,7 +122,7 @@ static inline CGFloat MDCRound(CGFloat value) {
 #endif
 }
 
-static inline CGFloat MDCSqrt(CGFloat value) {
+__deprecated_msg("Use sqrt instead.") static inline CGFloat MDCSqrt(CGFloat value) {
 #if CGFLOAT_IS_DOUBLE
   return sqrt(value);
 #else

commit 139d7f628ec0d02ec545920899a43427d33e183a

[Math] Remove use of deprecated methods.
This fixes a bug introduced by [e1afe22](https://github.com/material-components/material-components-ios/commit/e1afe22cfb01722fd7e52a3ac61503e90e3a89c9) which caused failing builds on Xcode 12 due to use of deprecated methods.

Specifically: the related commit above deprecated the custom MDCMath-specific methods, but MDCMath.h itself continues to use those same deprecated methods. This causes builds to fail (v117, Xcode 12, included as a Pod); previous version with v116 or a custom version with my commit resolves the issue.

### Thanks for starting a pull request on Material Components!

#### Don't forget:
- [X] Identify the component the PR relates to in brackets in the title. ```[Buttons] Updated documentation```
- [ ] Link to GitHub issues it solves. ```closes #1234```
- [ ] Please add unit tests, snapshot tests, or manual testing steps depending on the change.
- [ ] Sign the CLA bot. You can do this once the pull request is opened.

[Contributing](https://github.com/material-components/material-components-ios/tree/develop/contributing) has more information and tips for a great
pull request.

Closes https://github.com/material-components/material-components-ios/pull/10091

COPYBARA_INTEGRATE_REVIEW=https://github.com/material-components/material-components-ios/pull/10091 from aronbudinszky:develop d8caae97a1777857dea959e34130b2af87ba4a61
PiperOrigin-RevId: 336098267

components/private/Math/src/MDCMath.h
@@ -74,7 +74,7 @@ static inline BOOL MDCCGFloatEqual(CGFloat a, CGFloat b) {
   const CGFloat epsilon = FLT_EPSILON;
   const CGFloat min = FLT_MIN;
 #endif
-  return (MDCFabs(a - b) < constantK * epsilon * MDCFabs(a + b) || MDCFabs(a - b) < min);
+  return (fabs(a - b) < constantK * epsilon * fabs(a + b) || fabs(a - b) < min);
 }
 
 __deprecated_msg("Use floor instead.") static inline CGFloat MDCFloor(CGFloat value) {
@@ -143,7 +143,7 @@ static inline CGFloat MDCCeilScaled(CGFloat value, CGFloat scale) {
     return 0;
   }
 
-  return MDCCeil(value * scale) / scale;
+  return ceil(value * scale) / scale;
 }
 
 /**
@@ -159,7 +159,7 @@ static inline CGFloat MDCFloorScaled(CGFloat value, CGFloat scale) {
     return 0;
   }
 
-  return MDCFloor(value * scale) / scale;
+  return floor(value * scale) / scale;
 }
 
 /**
@@ -186,13 +186,13 @@ static inline CGRect MDCRectAlignToScale(CGRect rect, CGFloat scale) {
   }
 
   CGPoint originalMinimumPoint = CGPointMake(CGRectGetMinX(rect), CGRectGetMinY(rect));
-  CGPoint newOrigin = CGPointMake(MDCFloor(originalMinimumPoint.x * scale) / scale,
-                                  MDCFloor(originalMinimumPoint.y * scale) / scale);
+  CGPoint newOrigin = CGPointMake(floor(originalMinimumPoint.x * scale) / scale,
+                                  floor(originalMinimumPoint.y * scale) / scale);
   CGSize adjustWidthHeight =
       CGSizeMake(originalMinimumPoint.x - newOrigin.x, originalMinimumPoint.y - newOrigin.y);
   return CGRectMake(newOrigin.x, newOrigin.y,
-                    MDCCeil((CGRectGetWidth(rect) + adjustWidthHeight.width) * scale) / scale,
-                    MDCCeil((CGRectGetHeight(rect) + adjustWidthHeight.height) * scale) / scale);
+                    ceil((CGRectGetWidth(rect) + adjustWidthHeight.width) * scale) / scale,
+                    ceil((CGRectGetHeight(rect) + adjustWidthHeight.height) * scale) / scale);
 }
 
 static inline CGPoint MDCPointRoundWithScale(CGPoint point, CGFloat scale) {
@@ -200,7 +200,7 @@ static inline CGPoint MDCPointRoundWithScale(CGPoint point, CGFloat scale) {
     return CGPointZero;
   }
 
-  return CGPointMake(MDCRound(point.x * scale) / scale, MDCRound(point.y * scale) / scale);
+  return CGPointMake(round(point.x * scale) / scale, round(point.y * scale) / scale);
 }
 
 /**
@@ -217,7 +217,7 @@ static inline CGSize MDCSizeCeilWithScale(CGSize size, CGFloat scale) {
     return CGSizeZero;
   }
 
-  return CGSizeMake(MDCCeil(size.width * scale) / scale, MDCCeil(size.height * scale) / scale);
+  return CGSizeMake(ceil(size.width * scale) / scale, ceil(size.height * scale) / scale);
 }
 
 /**