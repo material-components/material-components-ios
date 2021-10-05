51cd81651e924ad92511912c33d54a6ad04d2370
Jeff Verkoeyen <featherless@google.com>
2021-10-05 09:57:38 -0700

    Remove all pre-iOS 12 logic.
    PiperOrigin-RevId: 401011227

--- components/private/UIMetrics/src/MDCLayoutMetrics.m
+++ components/private/UIMetrics/src/MDCLayoutMetrics.m
@@ -14,41 +14,37 @@
 
 #import "MDCLayoutMetrics.h"
 
-#import "MaterialApplication.h"
+#import "UIApplication+MDCAppExtensions.h"
 
 const CGFloat MDCFixedStatusBarHeightOnPreiPhoneXDevices = 20;
 
 static BOOL HasHardwareSafeAreas(void) {
   static BOOL hasHardwareSafeAreas = NO;
-  if (@available(iOS 11.0, *)) {
-    static BOOL hasCheckedForHardwareSafeAreas = NO;
-    static dispatch_once_t onceToken;
-    if (!hasCheckedForHardwareSafeAreas && [UIApplication mdc_safeSharedApplication].keyWindow) {
-      dispatch_once(&onceToken, ^{
-        UIEdgeInsets insets = [UIApplication mdc_safeSharedApplication].keyWindow.safeAreaInsets;
-        hasHardwareSafeAreas = (insets.top > MDCFixedStatusBarHeightOnPreiPhoneXDevices ||
-                                insets.left > 0 || insets.bottom > 0 || insets.right > 0);
+  static BOOL hasCheckedForHardwareSafeAreas = NO;
+  static dispatch_once_t onceToken;
+  if (!hasCheckedForHardwareSafeAreas && [UIApplication mdc_safeSharedApplication].keyWindow) {
+    dispatch_once(&onceToken, ^{
+      UIEdgeInsets insets = [UIApplication mdc_safeSharedApplication].keyWindow.safeAreaInsets;
+      hasHardwareSafeAreas = (insets.top > MDCFixedStatusBarHeightOnPreiPhoneXDevices ||
+                              insets.left > 0 || insets.bottom > 0 || insets.right > 0);
 
-        hasCheckedForHardwareSafeAreas = YES;
-      });
-    }
+      hasCheckedForHardwareSafeAreas = YES;
+    });
   }
   return hasHardwareSafeAreas;
 }
 
 CGFloat MDCDeviceTopSafeAreaInset(void) {
   CGFloat topInset = MDCFixedStatusBarHeightOnPreiPhoneXDevices;
-  if (@available(iOS 11.0, *)) {
-    // Devices with hardware safe area insets have fixed insets that depend on the device
-    // orientation. On such devices, we aren't interested in the status bar's height because the
-    // hardware safe area insets are what ultimately define the margins for the app. If we are
-    // running on such a device, we read from the safe area insets instead of the fixed status bar
-    // height so that we react to changes in safe area insets (usually due to orientation changes)
-    // and update the fixed margin accordingly.
-    if (HasHardwareSafeAreas()) {
-      UIEdgeInsets insets = [UIApplication mdc_safeSharedApplication].keyWindow.safeAreaInsets;
-      topInset = insets.top;
-    }
+  // Devices with hardware safe area insets have fixed insets that depend on the device
+  // orientation. On such devices, we aren't interested in the status bar's height because the
+  // hardware safe area insets are what ultimately define the margins for the app. If we are
+  // running on such a device, we read from the safe area insets instead of the fixed status bar
+  // height so that we react to changes in safe area insets (usually due to orientation changes)
+  // and update the fixed margin accordingly.
+  if (HasHardwareSafeAreas()) {
+    UIEdgeInsets insets = [UIApplication mdc_safeSharedApplication].keyWindow.safeAreaInsets;
+    topInset = insets.top;
   }
   return topInset;
 }
