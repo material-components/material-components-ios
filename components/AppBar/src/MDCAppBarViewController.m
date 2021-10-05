b5443f4df0bc386e5371f355d51e39b3f4d69da0
Jeff Verkoeyen <featherless@google.com>
2021-10-05 10:00:12 -0700

    Remove all pre-iOS 12 logic.
    PiperOrigin-RevId: 401011862

--- components/AppBar/src/MDCAppBarViewController.m
+++ components/AppBar/src/MDCAppBarViewController.m
@@ -19,13 +19,15 @@
 #import "private/MaterialAppBarStrings.h"
 #import "private/MaterialAppBarStrings_table.h"
 #import "MDCAppBarViewControllerAccessibilityPerformEscapeDelegate.h"
-#import "MaterialFlexibleHeader.h"
-#import "MaterialHeaderStackView.h"
-#import "MaterialNavigationBar.h"
-#import "MaterialShadowElevations.h"
-#import "MaterialShadowLayer.h"
+#import "MDCFlexibleHeaderView.h"
+#import "MDCFlexibleHeaderViewController.h"
+#import "MDCHeaderStackView.h"
+#import "MDCNavigationBar.h"
+#import "MDCShadowElevations.h"
+#import "MDCShadowLayer.h"
 #import "MaterialIcons+ic_arrow_back.h"
-#import "MaterialUIMetrics.h"
+#import "MDCIcons.h"
+#import "MDCLayoutMetrics.h"
 
 static NSString *const kBarStackKey = @"barStack";
 
@@ -251,11 +253,9 @@ static NSString *const kMaterialAppBarBundle = @"MaterialAppBar.bundle";
 - (void)viewWillLayoutSubviews {
   [super viewWillLayoutSubviews];
 
-  if (@available(iOS 11.0, *)) {
-    // We only update the top inset on iOS 11 because previously we were not adjusting the header
-    // height to make it smaller when the status bar is hidden.
-    _verticalConstraint.constant = [self verticalContraintLength];
-  }
+  // We only update the top inset on iOS 11 because previously we were not adjusting the header
+  // height to make it smaller when the status bar is hidden.
+  _verticalConstraint.constant = [self verticalContraintLength];
 
   if (self.shouldAdjustHeightBasedOnHeaderStackView) {
     [self adjustHeightBasedOnHeaderStackView];
