21d89d88ec17b99845341acc1b2443488feb2eb1
Jeff Verkoeyen <featherless@google.com>
2021-10-05 09:51:31 -0700

    Remove all pre-iOS 12 logic.
    PiperOrigin-RevId: 401009899

--- components/Ripple/examples/UICollectionViewWithRippleExample.swift
+++ components/Ripple/examples/UICollectionViewWithRippleExample.swift
@@ -73,18 +73,14 @@ class UICollectionViewWithRippleExample: UIViewController,
     rippleTouchController = MDCRippleTouchController(view: collectionView)
     rippleTouchController?.delegate = self
 
-    if #available(iOS 11, *) {
-      let guide = view.safeAreaLayoutGuide
-      NSLayoutConstraint.activate([
-        collectionView.leftAnchor.constraint(equalTo: guide.leftAnchor),
-        collectionView.rightAnchor.constraint(equalTo: guide.rightAnchor),
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-        collectionView.bottomAnchor.constraint(equalTo: guide.bottomAnchor),
-      ])
-      collectionView.contentInsetAdjustmentBehavior = .always
-    } else {
-      preiOS11Constraints()
-    }
+    let guide = view.safeAreaLayoutGuide
+    NSLayoutConstraint.activate([
+      collectionView.leftAnchor.constraint(equalTo: guide.leftAnchor),
+      collectionView.rightAnchor.constraint(equalTo: guide.rightAnchor),
+      collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: guide.bottomAnchor),
+    ])
+    collectionView.contentInsetAdjustmentBehavior = .always
   }
 
   func preiOS11Constraints() {
