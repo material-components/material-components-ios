bf756d71b63013890dcdee5d7c0cb12e87defd13
Jeff Verkoeyen <featherless@google.com>
2021-10-29 05:59:06 -0700

    [ActionSheet] Enforce the supported minimum OS policy at build time.
    PiperOrigin-RevId: 406341576

--- components/ActionSheet/src/private/MDCActionSheetItemTableViewCell.h
+++ components/ActionSheet/src/private/MDCActionSheetItemTableViewCell.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import "MDCMinimumOS.h"  // IWYU pragma: keep
+
 #import <MaterialComponents/MaterialInk.h>
 #import <UIKit/UIKit.h>
 #import "MDCActionSheetController.h"
