8491655e82166cb8e421cc98b68b1fd02c908d4a
Janette James <mayzie@google.com>
2022-06-30 23:25:30 -0700

    Mark as to be deprecated
    PiperOrigin-RevId: 458393868

--- components/private/KeyboardWatcher/src/MDCKeyboardWatcher.h
+++ components/private/KeyboardWatcher/src/MDCKeyboardWatcher.h
@@ -15,6 +15,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+API_DEPRECATED_BEGIN("Use standard Foundation APIs instead.", ios(12, API_TO_BE_DEPRECATED))
+
 // These notifications mirror their UIKeyboard* counterparts. They are posted after the keyboard
 // watcher has updated its own internal state, so listeners are safe to query the keyboard watcher
 // for its values.
@@ -60,3 +62,5 @@ OBJC_EXTERN NSString *const MDCKeyboardWatcherKeyboardWillChangeFrameNotificatio
     CGFloat keyboardOffset __deprecated_msg("Use visibleKeyboardHeight instead of keyboardOffset");
 
 @end
+
+API_DEPRECATED_END
