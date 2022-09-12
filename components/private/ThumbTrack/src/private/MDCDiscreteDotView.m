da096e800a32dd9e90d4b2aa034d826a3291a199
Nobody <no-reply@google.com>
2022-09-12 14:15:35 -0700

    Replace (trackHeight - dotHeight) / 2 with circleOriginY to fix -Wunused-but-set-variable warning.
    PiperOrigin-RevId: 473842186

--- components/private/ThumbTrack/src/private/MDCDiscreteDotView.m
+++ components/private/ThumbTrack/src/private/MDCDiscreteDotView.m
@@ -64,12 +64,11 @@
     CGFloat trackHeight = CGRectGetHeight(self.bounds);
     CGFloat dotHeight = MIN(2, trackHeight);
     CGFloat dotWidth = MIN(2, trackHeight);
-    CGFloat circleOriginY = (trackHeight - dotHeight) / 2;
     if (trackHeight > 4) {
       dotHeight = trackHeight / 2;
-      circleOriginY = (trackHeight - dotHeight) / 2;
     }
-    CGRect dotRect = CGRectMake(0, (trackHeight - dotHeight) / 2, dotWidth, dotHeight);
+    CGFloat circleOriginY = (trackHeight - dotHeight) / 2;
+    CGRect dotRect = CGRectMake(0, circleOriginY, dotWidth, dotHeight);
     // Increment within the bounds
     CGFloat absoluteIncrement = (CGRectGetWidth(self.bounds) - dotWidth) / (_numDiscreteDots - 1);
     // Increment within 0..1
