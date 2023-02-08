15aa21bd6f3c48821f545e9c140700eef51c14cd
Nobody <no-reply@google.com>
2023-02-08 14:02:05 -0800

    Add missing dependency on QuartzCore
    PiperOrigin-RevId: 508176668

--- components/Shadow/tests/snapshot/MDCShadowSnapshotTests.m
+++ components/Shadow/tests/snapshot/MDCShadowSnapshotTests.m
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import <QuartzCore/QuartzCore.h>
+
 #import "MDCAvailability.h"
 #import "MDCShadow.h"
 #import "MDCShadowsCollection.h"
