16e9402a8f7a8ca1c8dfd7e032bb544a18eba584
Janette James <mayzie@google.com>
2024-09-18 11:39:23 -0700

    Delete MDCBasicFontScheme
    PiperOrigin-RevId: 676070248

--- components/schemes/Typography/src/MaterialTypographyScheme.h
+++ components/schemes/Typography/src/MaterialTypographyScheme.h
@@ -12,5 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MaterialTypographyScheme+BasicFontScheme.h"  // IWYU pragma: keep // SchemesSubtargetImport  // IWYU pragma: keep
 #import "MDCTypographyScheme.h"  // IWYU pragma: keep
